4e6784bca2935c7f8ad0c3cd5e8c3842f5003383
Vendicated <vendicated@riseup.net>
2022-08-28 14:13:34 +0200

    Make prettier work for anon funcs

--- utils/prettier.js
+++ utils/prettier.js
@@ -3,7 +3,7 @@ const electron = require("electron");
 
 // cry
 Function.prototype.prettier = function () {
-    return prettier.format(this.toString(), { filepath: "index.js" });
+    return prettier.format("var _$_ =\n// This var above is there to make anonymous functions work. Actual code below\n//\n" + this.toString(), { filepath: "index.js" });
 };
 
 electron.webFrame.top.context.Function.prototype.prettier = Function.prototype.prettier;
