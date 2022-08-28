aaf375eaddb3e24efe2203c78b0ee49b1b56c0b5
Vendicated <vendicated@riseup.net>
2022-08-28 16:48:32 +0200

    fix: Run patchWebpack in webFrame instead of preload

--- VencordApi.js
+++ VencordApi.js
@@ -1,6 +1,8 @@
 module.exports = class VencordApi {
     Webpack = require("./utils/webpack");
     Api = require("./api");
+    Constants = require("./constants");
+    patches = require("./patches").patches;
 
     get d() {
         console.log("8==>");
--- utils/patchWebpack.js
+++ utils/patchWebpack.js
@@ -1,9 +1,7 @@
 const { WEBPACK_CHUNK } = require("../constants");
 const electron = require("electron");
-const { patches } = require("../patches");
-const { init, subscriptions } = require("./webpack");
+const { init } = require("./webpack");
 
-let originalPush;
 let webpackChunk;
 
 Object.defineProperty(electron.webFrame.top.context, WEBPACK_CHUNK, {
@@ -15,7 +13,7 @@ Object.defineProperty(electron.webFrame.top.context, WEBPACK_CHUNK, {
         // We don't want to override the native one, so check for "push"
         if (v && !v.push.toString().includes("push")) {
             init(v);
-            patchPush(v);
+            electron.webFrame.executeJavaScript(`(() => (${patchPush.toString()})())();`);
             delete electron.webFrame.top.context[WEBPACK_CHUNK];
             electron.webFrame.top.context[WEBPACK_CHUNK] = v;
         }
@@ -24,74 +22,78 @@ Object.defineProperty(electron.webFrame.top.context, WEBPACK_CHUNK, {
     configurable: true
 });
 
-function patchPush(instance) {
-    originalPush = instance.push;
-    Object.defineProperty(instance, "push", {
+function patchPush() {
+    function handlePush(chunk, noIdeaWhatThisIs) {
+        const modules = chunk[1];
+        const { subscriptions } = Vencord.Webpack;
+        const { patches } = Vencord;
+
+        try {
+            for (const id in modules) {
+                let mod = modules[id];
+                let code = mod.toString();
+                const originalMod = mod;
+                const patchedBy = new Set();
+
+                modules[id] = function (module, exports, require) {
+                    try {
+                        mod(module, exports, require);
+                    } catch (err) {
+                        // Just rethrow discord errors
+                        if (mod === originalMod) throw err;
+
+                        console.error("[Webpack] Error in patched chunk", err);
+                        return originalMod(module, exports, require);
+                    }
+
+                    for (const [filter, callback] of subscriptions) {
+                        try {
+                            if (filter(exports)) {
+                                subscriptions.delete(filter);
+                                callback(exports);
+                            } else if (exports.default && filter(exports.default)) {
+                                subscriptions.delete(filter);
+                                callback(exports.default);
+                            }
+                        } catch (err) {
+                            console.error("[Webpack] Error while firing callback for webpack chunk", err);
+                        }
+                    }
+                };
+
+                for (let i = 0; i < patches.length; i++) {
+                    const patch = patches[i];
+                    if (code.includes(patch.find)) {
+                        patchedBy.add(patch.plugin);
+                        const lastMod = mod;
+                        const lastCode = code;
+                        try {
+                            const newCode = code.replace(patch.replacement.match, patch.replacement.replace);
+                            const newMod = eval(`// Webpack Module ${id} - Patched by ${[...patchedBy].join(", ")}\n${newCode}\n//# sourceURL=WebpackModule${id}`);
+                            code = newCode;
+                            mod = newMod;
+                            patches.splice(i--, 1);
+                        } catch (err) {
+                            console.error("[Webpack] Failed to apply patch of", patch.plugin, err);
+                            code = lastCode;
+                            mod = lastMod;
+                            patchedBy.delete(patch.plugin);
+                        }
+                    }
+                }
+            }
+        } catch (err) {
+            console.error("oopsie", err);
+        }
+
+        return handlePush.original.call(window[Vencord.Constants.WEBPACK_CHUNK], chunk, noIdeaWhatThisIs);
+    }
+
+    handlePush.original = window[Vencord.Constants.WEBPACK_CHUNK].push;
+    Object.defineProperty(window[Vencord.Constants.WEBPACK_CHUNK], "push", {
         get: () => handlePush,
-        set: (v) => (originalPush = v),
+        set: (v) => (handlePush.original = v),
         configurable: true
     });
 }
 
-function handlePush(chunk, noIdeaWhatThisIs) {
-    const modules = chunk[1];
-    try {
-        for (const id in modules) {
-            let mod = modules[id];
-            let code = mod.toString();
-            const originalMod = mod;
-            const patchedBy = new Set();
-
-            modules[id] = function (module, exports, require) {
-                try {
-                    mod(module, exports, require);
-                } catch (err) {
-                    // Just rethrow discord errors
-                    if (mod === originalMod) throw err;
-
-                    console.error("[Webpack] Error in patched chunk", err);
-                    return originalMod(module, exports, require);
-                }
-
-                for (const [filter, callback] of subscriptions) {
-                    try {
-                        if (filter(exports)) {
-                            subscriptions.delete(filter);
-                            callback(exports);
-                        } else if (exports.default && filter(exports.default)) {
-                            subscriptions.delete(filter);
-                            callback(exports.default);
-                        }
-                    } catch (err) {
-                        console.error("[Webpack] Error while firing callback for webpack chunk", err);
-                    }
-                }
-            };
-
-            for (let i = 0; i < patches.length; i++) {
-                const patch = patches[i];
-                if (code.includes(patch.find)) {
-                    patchedBy.add(patch.plugin);
-                    const lastMod = mod;
-                    const lastCode = code;
-                    try {
-                        const newCode = code.replace(patch.replacement.match, patch.replacement.replace);
-                        const newMod = eval(`// Webpack Module ${id} - Patched by ${[...patchedBy].join(", ")}\n(window)=>{return ${newCode}}\n//# sourceURL=WebpackModule${id}`)(electron.webFrame.top.context);
-                        code = newCode;
-                        mod = newMod;
-                        patches.splice(i--, 1);
-                    } catch (err) {
-                        console.error("[Webpack] Failed to apply patch of", patch.plugin, err);
-                        code = lastCode;
-                        mod = lastMod;
-                        patchedBy.delete(patch.plugin);
-                    }
-                }
-            }
-        }
-    } catch (err) {
-        console.error("oopsie", err);
-    }
-
-    return originalPush.call(window[WEBPACK_CHUNK], chunk, noIdeaWhatThisIs);
-}
