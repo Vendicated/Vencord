1709ab61ef2138d350b3d45a3eba0d94d5adf0a1
Vendicated <vendicated@riseup.net>
2022-08-29 18:45:58 +0200

    Fix NPE when opening DevTools

--- src/VencordNative.ts
+++ src/VencordNative.ts
@@ -7,5 +7,5 @@ export default {
             cb(css);
         });
     },
-    getQuickCss: () => ipcRenderer.invoke(IPC_GET_QUICK_CSS)
+    getQuickCss: () => ipcRenderer.invoke(IPC_GET_QUICK_CSS) as Promise<string>
 };
