b2f762fda8ec93feab123891e7c01efda00388c8
Vendicated <vendicated@riseup.net>
2022-08-31 22:09:36 +0200

    Revert patcher to console.log

--- src/patcher.ts
+++ src/patcher.ts
@@ -3,10 +3,8 @@ import electron, { app, BrowserWindowConstructorOptions } from "electron";
 import installExt, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 import { join } from "path";
 import { initIpc } from './ipcMain';
-import Logger from "./utils/logger";
 
-const logger = new Logger("Patcher", "#700b90")
-logger.log("[Vencord] Starting up...");
+console.log("[Vencord] Starting up...");
 
 class BrowserWindow extends electron.BrowserWindow {
     constructor(options: BrowserWindowConstructorOptions) {
@@ -50,8 +48,8 @@ process.env.DATA_DIR = join(app.getPath("userData"), "..", "Vencord");
 
 electron.app.whenReady().then(() => {
     installExt(REACT_DEVELOPER_TOOLS)
-        .then(() => logger.log("Installed React DevTools"))
-        .catch((err) => logger.error("Failed to install React DevTools", err));
+        .then(() => console.info("Installed React DevTools"))
+        .catch((err) => console.error("Failed to install React DevTools", err));
 
     // Remove CSP
     electron.session.defaultSession.webRequest.onHeadersReceived(({ responseHeaders, url }, cb) => {
