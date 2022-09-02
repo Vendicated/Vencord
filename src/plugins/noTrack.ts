fdf8433bd3df282c3c93e5af427d0fc811d767a8
Cynthia Foxwell <gamers@riseup.net>
2022-09-01 21:17:52 -0600

    noTrack: switch to patches

--- src/plugins/noTrack.ts
+++ src/plugins/noTrack.ts
@@ -8,25 +8,20 @@ export default definePlugin({
     description: "Disable Discord's tracking and crash reporting",
     author: "Vendicated",
     required: true,
-    start() {
-        findByProps("getSuperPropertiesBase64", "track").track = DO_NOTHING;
-        findByProps("submitLiveCrashReport").submitLiveCrashReport = DO_NOTHING;
-        findByProps("AnalyticsActionHandlers").AnalyticsActionHandlers.handleTrack = DO_NOTHING;
-
-        const sentry = window.__SENTRY__;
-        sentry.logger.disable();
-
-        sentry.hub.addBreadcrumb = DO_NOTHING;
-        sentry.hub.getClient().close(0);
-        sentry.hub.getScope().clear();
-
-        const c = console;
-        for (const method in c) {
-            if (c[method].__sentry_original__)
-                c[method] = c[method].__sentry_original__;
-            if (c[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__?.__sentry_original__)
-                c[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__ = c[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__.__sentry_original__;
+    patches: [
+        {
+            find: "TRACKING_URL:",
+            replacement: {
+                match: /=\(0,.\.analyticsTrackingStoreMaker\)/,
+                replace: "=(function(){})",
+            },
+        },
+        {
+            find: "window.DiscordSentry=",
+            replacement: {
+                match: /window\.DiscordSentry=\(0,.\.initSentry\)\(\)/,
+                replace: "",
+            }
         }
-    }
-
+    ]
 });
