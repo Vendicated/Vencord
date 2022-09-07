96fed019b876a441d5bca958d8682057dd9ca36f
Vendicated <vendicated@riseup.net>
2022-09-07 15:27:52 +0200

    feat: 20% more swag

--- src/plugins/cumcord.ts
+++ src/plugins/cumcord.ts
@@ -1,14 +1,14 @@
 import definePlugin from "../utils/types";
 
 export default definePlugin({
-  name: "cumcord",
-  description: "Loads cumcord. That's it",
-  author: "Vendicated",
-  async start() {
-    const cum = await fetch("https://raw.githubusercontent.com/Cumcord/Cumcord/stable/dist/build.js");
-    (0, eval)(await cum.text());
-  },
-  stop() {
-    window.cumcord?.uninject();
-  },
+    name: "cumcord",
+    description: "Loads cumcord. That's it",
+    author: "Vendicated",
+    async start() {
+        const cum = await fetch("https://raw.githubusercontent.com/Cumcord/Cumcord/stable/dist/build.js");
+        (0, eval)(await cum.text());
+    },
+    stop() {
+        window.cumcord?.uninject();
+    },
 });
