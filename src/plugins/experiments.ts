815131ddb7f876dafe5b3fa368ab02da1265a65f
Vendicated <vendicated@riseup.net>
2022-09-27 18:08:11 +0200

    Fix Experiments

--- src/plugins/experiments.ts
+++ src/plugins/experiments.ts
@@ -11,7 +11,7 @@ export default definePlugin({
             replace: "true"
         }
     }, {
-        find: "canUseOverrides",
+        find: 'type:"user",revision',
         replacement: {
             match: /(\w)\|\|"CONNECTION_OPEN".+?;/g,
             replace: "$1=!0;"
