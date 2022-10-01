c1d1619471c5af43637dda0a38ea75aeb4be876c
botato <contact@swezy.dev>
2022-09-30 22:23:41 -0400

    feat(plugin): iLoveSpam (#19)

--- src/plugins/plainFolderIcon.ts
+++ src/plugins/plainFolderIcon.ts
@@ -1,12 +1,10 @@
+import { Devs } from "../utils/constants";
 import definePlugin from "../utils/types";
 
 export default definePlugin({
-    name: "Folder SVG Icon",
+    name: "PlainFolderIcon",
     description: "Doesn't show the small guild icons in folders",
-    authors: [{
-        name: "botato",
-        id: 440990343899643943n
-    }],
+    authors: [Devs.botato],
     patches: [{
         find: "().expandedFolderIconWrapper",
         replacement: [{
