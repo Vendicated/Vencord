cac77dce406be92755519be595cf460e8f758b16
Vendicated <vendicated@riseup.net>
2022-10-01 17:05:18 +0200

    Fix DiscordNative.desktopCapture.getDesktopCaptureSources() in electron17+

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,24 @@
-import { contextBridge, webFrame } from "electron";
+import electron, { contextBridge, webFrame } from "electron";
 import { readFileSync } from "fs";
 import { join } from "path";
 import VencordNative from "./VencordNative";
+import { ipcRenderer } from 'electron';
+import IpcEvents from "./utils/IpcEvents";
+
+if (electron.desktopCapturer === void 0) {
+    // Fix for desktopCapturer being main only in Electron 17+
+    // Discord accesses this in discord_desktop_core (DiscordNative.desktopCapture.getDesktopCaptureSources)
+    // and errors with cannot "read property getSources() of undefined"
+    // see discord_desktop_core/app/discord_native/renderer/desktopCapture.js
+    const electronPath = require.resolve("electron");
+    delete require.cache[electronPath]!.exports;
+    require.cache[electronPath]!.exports = {
+        ...electron,
+        desktopCapturer: {
+            getSources: (opts) => ipcRenderer.invoke(IpcEvents.GET_DESKTOP_CAPTURE_SOURCES, opts)
+        }
+    };
+}
 
 contextBridge.exposeInMainWorld("VencordNative", VencordNative);
 
