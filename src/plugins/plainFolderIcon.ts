97491a88137f57ee18cc8adfc711fc4b1aa0f8f8
Ven <vendicated@riseup.net>
2022-10-01 04:18:51 +0200

    Update and rename folderSvgIcon.ts to plainFolderIcon.ts

--- src/plugins/plainFolderIcon.ts
+++ src/plugins/plainFolderIcon.ts
@@ -2,7 +2,7 @@ import { Devs } from "../utils/constants";
 import definePlugin from "../utils/types";
 
 export default definePlugin({
-    name: "Folder SVG Icon",
+    name: "PlainFolderIcon",
     description: "Doesn't show the small guild icons in folders",
     authors: [Devs.botato],
     patches: [{
