b6ab5169b3b319e7553c175bc40173baf73bfc80
Ruthenic <mdrakea3@tutanota.com>
2022-10-02 16:59:01 -0400

    Fix ify not preventing auto-pausing without premium

--- src/plugins/ify.ts
+++ src/plugins/ify.ts
@@ -11,11 +11,18 @@ export default definePlugin({
             replacement: [{
                 match: /\.isPremium=.;/,
                 replace: ".isPremium=true;",
-            }, ...["SPEAKING", "VOICE_STATE_UPDATES", "MEDIA_ENGINE_SET_DESKTOP_SOURCE"].map(event => ({
+            }, /* ...["SPEAKING", "VOICE_STATE_UPDATES", "MEDIA_ENGINE_SET_DESKTOP_SOURCE"].map(event => ({
                 match: new RegExp(`${event}:function\\(.\\){.+?}(,|}\\))`),
                 replace: (_, ending) => `${event}:function(){}${ending}`,
-            })),
+            })), */
             ],
         },
+		{
+			find: "Playback auto paused",
+			replacement: {
+				match: /.{1,2}\.default\.track\(.{1,2}\..{1,3}\.SPOTIFY_AUTO_PAUSED\)/,
+                replace: ""
+			}
+		}
     ]
 });
