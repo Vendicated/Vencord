3b468c1c08f78f174ccd2ad970adc00e2e8e9045
Ruthenic <mdrakea3@tutanota.com>
2022-10-03 16:54:50 -0400

    Actually fix ify's auto-pausing prevention

--- src/plugins/ify.ts
+++ src/plugins/ify.ts
@@ -8,21 +8,17 @@ export default definePlugin({
     patches: [
         {
             find: '.displayName="SpotifyStore"',
-            replacement: [{
+            replacement: {
                 match: /\.isPremium=.;/,
                 replace: ".isPremium=true;",
-            }, /* ...["SPEAKING", "VOICE_STATE_UPDATES", "MEDIA_ENGINE_SET_DESKTOP_SOURCE"].map(event => ({
-                match: new RegExp(`${event}:function\\(.\\){.+?}(,|}\\))`),
-                replace: (_, ending) => `${event}:function(){}${ending}`,
-            })), */
-            ],
+            }
         },
 		{
 			find: "Playback auto paused",
 			replacement: {
-				match: /.{1,2}\.default\.track\(.{1,2}\..{1,3}\.SPOTIFY_AUTO_PAUSED\)/,
-                replace: ""
-			}
+                match: /function (.{1,2})\(\).{0,200}SPOTIFY_AUTO_PAUSED\);.{0,}}}}/,
+                replace: "function $1(){}"
+            }
 		}
     ]
 });
