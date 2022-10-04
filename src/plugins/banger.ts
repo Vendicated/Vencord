7a051b4d907b05a077b9ac91e0d16142cff65f8d
GlitchyChan <contact@swezy.dev>
2022-10-04 19:38:38 +0000

    fix(plugins): Fixed BANger (#41)
    * fix(plugins): Fixed BANger
    
    That was easy.
    
    * add(Devs): Added Myself to Devs
    
    * Update banger.ts
    
    Co-authored-by: Ven <vendicated@riseup.net>

--- src/plugins/banger.ts
+++ src/plugins/banger.ts
@@ -1,15 +1,19 @@
 import definePlugin from "../utils/types";
+import { Devs } from "../utils/constants";
 
 export default definePlugin({
     name: "BANger",
     description: "Replaces the GIF in the ban dialogue with a custom one.",
-    authors: [{
-        name: "Xinto",
-        id: 423915768191647755n
-    }],
+    authors: [
+        {
+            name: "Xinto",
+            id: 423915768191647755n
+        },
+        Devs.Glitch
+    ],
     patches: [
         {
-            find: "BanConfirm",
+            find: "BAN_CONFIRM_TITLE.",
             replacement: {
                 match: /src:\w\(\d+\)/g,
                 replace: 'src: "https://i.imgur.com/wp5q52C.mp4"'
