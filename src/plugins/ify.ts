c442f6117d5a44a19a9b2c8c6ea8f26bc33bcc64
Drake <53356436+Ruthenic@users.noreply.github.com>
2022-10-04 15:54:46 -0400

    Combine two patches in ify

--- src/plugins/ify.ts
+++ src/plugins/ify.ts
@@ -5,20 +5,14 @@ export default definePlugin({
     name: "Ify",
     description: "Disabes Spotify auto-pausing and premium checks",
     authors: [Devs.Cyn],
-    patches: [
-        {
-            find: '.displayName="SpotifyStore"',
-            replacement: {
-                match: /\.isPremium=.;/,
-                replace: ".isPremium=true;",
-            }
-        },
-		{
-			find: "Playback auto paused",
-			replacement: {
-                match: /function (.{1,2})\(\).{0,200}SPOTIFY_AUTO_PAUSED\);.{0,}}}}/,
-                replace: "function $1(){}"
-            }
-		}
-    ]
+    patches: {
+        find: '.displayName="SpotifyStore"',
+        replacement: [{
+            match: /\.isPremium=.;/,
+            replace: ".isPremium=true;",
+        }, {
+            match: /function (.{1,2})\(\).{0,200}SPOTIFY_AUTO_PAUSED\);.{0,}}}}/,
+            replace: "function $1(){}"
+        }]
+    }
 });
