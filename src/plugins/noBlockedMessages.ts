d2da02989ec81bf4bebb3488b37c49e8818d9165
Diamond <contact@swezy.dev>
2022-10-04 07:46:21 -0700

    feat: no blocked messages plugin (#34)

---
+++ src/plugins/noBlockedMessages.ts
@@ -0,0 +1,19 @@
+import { Devs } from "../utils/constants";
+import definePlugin from "../utils/types";
+
+export default definePlugin({
+    name: "NoBlockedMessages",
+    description: "Hides all blocked messages from chat completely.",
+    authors: [Devs.rushii],
+    patches: [
+        {
+            find: 'safety_prompt:"DMSpamExperiment",response:"show_redacted_messages"',
+            replacement: [
+                {
+                    match: /collapsedReason;return (?=\w{1,2}.createElement)/,
+                    replace: "collapsedReason; return null;"
+                }
+            ]
+        }
+    ]
+});
