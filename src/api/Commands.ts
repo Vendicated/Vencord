cfa196f582ec5a9464dc78ca1ac279600b6005f7
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2022-10-05 01:02:11 +0300

    no more (x as any)

--- src/api/Commands.ts
+++ src/api/Commands.ts
@@ -1,47 +1,49 @@
 import type { Command, Option } from "./Commands.d";
 
-const getBuiltInCommands = () =>
-    (Vencord.Api.Commands as any)._BuiltIn as Command[];
+// @ts-expect-error
+const getBuiltInCommands = () => Vencord.Api.Commands._BuiltIn as Command[];
 
 const getUniqueCommandId = () => {
     const existingIds = getBuiltInCommands().map((i) =>
-        parseInt((i as any).id as string)
+        // @ts-expect-error
+        parseInt(i.id as string)
     );
     return (
-        (Math.abs(existingIds.sort((a, b) => a - b)[0]) + 1) *
-        -1
+        (Math.abs(
+            existingIds.sort((a, b) => a - b)[0]) + 1
+        ) * -1
     ).toString();
 };
 
 function modifyOpt(opt: Option) {
-    if (opt.displayName == undefined) opt.displayName = opt.name;
-    if (opt.displayDescription == undefined)
-        opt.displayDescription = opt.description;
+    opt.displayName ||= opt.name;
+    opt.displayDescription ||= opt.description;
     opt.options?.forEach(modifyOpt);
 }
 
 export const commands = new Map<string, Command>();
 export function registerCommand(command: Command) {
-    (command as any).id = getUniqueCommandId();
-    (command as any).applicationId = "-1"; // -1 is for the built-in category
+    // @ts-expect-error
+    command.id = getUniqueCommandId();
+    // @ts-expect-error
+    command.applicationId = "-1"; // -1 is for the built-in category
 
-    if (command.displayName == undefined) command.displayName = command.name;
-    if (command.displayDescription == undefined)
-        command.displayDescription = command.description;
+    command.displayName ||= command.name;
+    command.displayDescription ||= command.description;
 
     command.options?.forEach(modifyOpt);
-    commands[(command as any).id + "_" + command.name] = command;
+    // @ts-expect-error
+    commands[command.id + "_" + command.name] = command;
     getBuiltInCommands().push(command);
 }
 
 export function unregisterCommand(command: Command) {
-    delete commands[(command as any).id + "_" + command];
+    // @ts-expect-error
+    delete commands[command.id + "_" + command];
     const cmds = getBuiltInCommands();
     for (const cmd of cmds) {
-        if (
-            (cmd as any).id + "_" + cmd.name ===
-            (command as any).id + "_" + command.name
-        ) {
+        // @ts-expect-error
+        if (cmd.id + "_" + cmd.name === command.id + "_" + command.name) {
             cmds.splice(cmds.indexOf(cmd), 1);
         }
     }
