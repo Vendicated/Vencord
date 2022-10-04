f1a47fd56170874535309ed2b64639654de087ab
Ruthenic <mdrakea3@tutanota.com>
2022-10-04 16:09:54 -0400

    Fix patches array in ify
    i dun goofed

--- src/plugins/ify.ts
+++ src/plugins/ify.ts
@@ -5,14 +5,14 @@ export default definePlugin({
     name: "Ify",
     description: "Disabes Spotify auto-pausing and premium checks",
     authors: [Devs.Cyn],
-    patches: {
+    patches: [{
         find: '.displayName="SpotifyStore"',
         replacement: [{
             match: /\.isPremium=.;/,
-            replace: ".isPremium=true;",
+            replace: ".isPremium=true;"
         }, {
             match: /function (.{1,2})\(\).{0,200}SPOTIFY_AUTO_PAUSED\);.{0,}}}}/,
             replace: "function $1(){}"
         }]
-    }
+    }]
 });
