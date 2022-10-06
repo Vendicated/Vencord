72243e6be2fa037327c8fe798992a332849032ff
X1nto <burtkni@yandex.ru>
2022-10-06 13:17:05 +0400

    Add the DevilBro simulator plugin

---
+++ src/plugins/itsdevilbrotime.ts
@@ -0,0 +1,30 @@
+import { registerCommand, unregisterCommand } from "../api/Commands";
+import { Devs } from "../utils/constants";
+import definePlugin from "../utils/types";
+
+export default definePlugin({
+    name: "It's DevilBro Time!",
+    description: "Simulates DevilBro's plugins' memory leaks",
+    authors: [
+        Devs.Xinto
+    ],
+    start() {
+        registerCommand({
+            name: "memoryleak",
+            description: "Generate a memory leak",
+            execute(args, ctx) {
+                const channelId = parseInt(ctx.channel.id)
+                let arr: string[] = []
+                for (let i = 0; i < channelId; i++) {
+                    arr[i] = (i * channelId).toString()
+                }
+                return {
+                    content: "You did indeed generate a memory leak"
+                }
+            },
+        }, "itsdevilbrotime")
+    },
+    stop() {
+        unregisterCommand("memoryleak")
+    },
+})
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -34,5 +34,9 @@ export const Devs = Object.freeze({
     Glitch: {
         name: "Glitchy",
         id: 269567451199569920n
+    },
+    Xinto: {
+        name: "Xinto",
+        id: 423915768191647755n
     }
 });
