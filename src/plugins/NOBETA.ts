430111d6c215907e153d2b5380ba7dcdff6fdca2
emir4169 <89991965+emir4169@users.noreply.github.com>
2022-10-09 19:30:49 +0300

    Merging PRs that vencord didnt
    haha

---
+++ src/plugins/NOBETA.ts
@@ -0,0 +1,20 @@
+import definePlugin from "../utils/types";
+import { addPreSendListener, MessageObject, removePreSendListener } from "../api/MessageEvents";
+
+export default definePlugin({
+    name: "NoBetaURLs",
+    description: "Removes the canary and ptb subdomains from urls",
+    authors: [{ name: "ICodeInAssembly", id: 702973430449832038n }],
+    dependencies: ["MessageEventsAPI"],
+    removeBetas(msg: MessageObject) {
+        msg.content = msg.content.replace(/(?<=https:\/\/)(canary.|ptb.)(?=discord(?:app)?.com\/channels\/(?:\d{17,20}|@me)\/\d{17,20}\/\d{17,20})/g, ""); // Ven W
+    },
+
+    start() {
+        this.preSend = addPreSendListener((_, msg) => this.removeBetas(msg));
+    },
+
+    stop() {
+        removePreSendListener(this.preSend);
+    }
+});
