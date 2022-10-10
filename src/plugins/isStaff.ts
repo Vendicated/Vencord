f2e85ffe444f4d52005be6509481b0da018b3566
BanTheNons <banthenons@gmail.com>
2022-10-10 22:16:15 +0300

    fix(plugin): fix isStaff returning true for all users

--- src/plugins/isStaff.ts
+++ src/plugins/isStaff.ts
@@ -5,18 +5,24 @@ export default definePlugin({
     name: "isStaff",
     description:
         "Gives access to client devtools & other things locked behind isStaff",
-    authors: [Devs.Megu],
+    authors: [
+        Devs.Megu,
+        {
+            name: "Nickyux",
+            id: 427146305651998721n
+        },
+        {
+            name: "BanTheNons",
+            id: 460478012794863637n
+        }
+    ],
     patches: [
         {
             find: ".isStaff=function(){",
             replacement: [
                 {
-                    match: /(\w+)\.isStaff=function\(\){return\s*!1};/,
-                    replace: "$1.isStaff=function(){return true};",
-                },
-                {
-                    match: /return\s*\w+\.hasFlag\(.+?STAFF\)}/,
-                    replace: "return true}",
+                    match: /return\s*(\w)+\.hasFlag\((.+?)\.STAFF\)}/,
+                    replace: "return Vencord.Webpack.Common.UserStore.getCurrentUser().id===$1.id||$1.hasFlag($2.STAFF)}"
                 },
                 {
                     match: /hasFreePremium=function\(\){return this.isStaff\(\)\s*\|\|/,
