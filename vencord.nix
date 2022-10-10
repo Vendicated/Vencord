bfdc6b63fa924570a4cf8fd26d9d2de4bc49bf7e
pupbrained <mars@pupbrained.xyz>
2022-10-10 15:44:36 -0400

    formatting stuff bc this looked awful

--- vencord.nix
+++ vencord.nix
@@ -23,22 +23,21 @@ in
 
       cp -a --remove-destination $(readlink "$out/opt/DiscordCanary/.DiscordCanary-wrapped") "$out/opt/DiscordCanary/.DiscordCanary-wrapped"
       cp -a --remove-destination $(readlink "$out/opt/DiscordCanary/DiscordCanary") "$out/opt/DiscordCanary/DiscordCanary"
+
       if grep '\0' $out/opt/DiscordCanary/DiscordCanary && wrapperCmd=$(${extractCmd} $out/opt/DiscordCanary/DiscordCanary) && [[ $wrapperCmd ]]; then
-        # Binary wrapper
         parseMakeCWrapperCall() {
-          shift # makeCWrapper
-          oldExe=$1; shift
-          oldWrapperArgs=("$@")
+            shift
+            oldExe=$1; shift
+            oldWrapperArgs=("$@")
         }
         eval "parseMakeCWrapperCall ''${wrapperCmd//"${discord-canary.out}"/"$out"}"
-        # Binary wrapper
         makeWrapper $oldExe $out/opt/DiscordCanary/DiscordCanary "''${oldWrapperArgs[@]}" --add-flags "${extraElectronArgs}"
       else
-        # Normal wrapper
         substituteInPlace $out/opt/DiscordCanary/DiscordCanary \
         --replace '${discord-canary.out}' "$out" \
         --replace '"$@"' '${extraElectronArgs} "$@"'
       fi
+
       substituteInPlace $out/opt/DiscordCanary/DiscordCanary --replace '${discord-canary.out}' "$out"
     '';
 
