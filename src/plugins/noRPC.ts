0057bb99f85d77820d9d18bedf19c4c8108cad10
Cynthia Foxwell <gamers@riseup.net>
2022-10-10 20:33:22 -0600

    feat(plugin): No RPC

---
+++ src/plugins/noRPC.ts
@@ -0,0 +1,17 @@
+import definePlugin from "../utils/types";
+import {Devs} from "../utils/constants";
+
+export default definePlugin({
+    name: "No RPC",
+    description: "Disables Discord's RPC server.",
+    authors: [Devs.Cyn],
+    patches: [
+        {
+            find: '.ensureModule("discord_rpc")',
+            replacement: {
+                match: /\.ensureModule\("discord_rpc"\)\.then\(\(.+?\)\)}/,
+                replace: '.ensureModule("discord_rpc")}',
+            },
+        },
+    ],
+});
