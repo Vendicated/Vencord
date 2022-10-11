4d6d0d58add3022ce3ba4ba4e5659b8581dcb495
Vendicated <contact@swezy.dev>
2022-10-11 17:48:14 +0200

    Switch to standalone-electron-types, -200MB npms

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="standalone-electron-types"/>
+
 declare module "plugins" {
     const plugins: Record<string, import("./utils/types").Plugin>;
     export default plugins;
