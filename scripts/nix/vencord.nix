8e3757696f438b8fd53a5e97a14663aabaab4b0a
pupbrained <mars@pupbrained.xyz>
2022-10-11 21:38:39 -0400

    the urge to explode

--- scripts/nix/vencord.nix
+++ scripts/nix/vencord.nix
@@ -23,7 +23,9 @@
       nativeBuildInputs = with pkgs; [git];
 
       buildPhase = ''
-        cp -r $PWD/node_modules/vencord/* $PWD
+        cp -r $PWD/node_modules/vencord/package.json $PWD
+        cp -r $PWD/node_modules/vencord/pnpm-lock.yaml $PWD
+        cp -r $PWD/node_modules/vencord/build.mjs $PWD
         ${pkgs.nodePackages.pnpm}/bin/pnpm build nix ${revision}
       '';
     };
