1bbb05287cfb8c39b2e456a730880e47194689a2
pupbrained <mars@pupbrained.xyz>
2022-10-11 20:32:58 -0400

    suffering

--- scripts/nix/vencord.nix
+++ scripts/nix/vencord.nix
@@ -13,9 +13,17 @@
       packageJSON = ../../package.json;
       pnpmLock = ../../pnpm-lock.yaml;
 
-      nativeBuildInputs = [pkgs.git];
+      overrides = {
+        electron = drv:
+          drv.overrideAttrs (old: {
+            ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+          });
+      };
+
+      nativeBuildInputs = with pkgs; [git];
 
       buildPhase = ''
+        cp -r $PWD/node_modules/vencord/* $PWD
         ${pkgs.nodePackages.pnpm}/bin/pnpm build nix ${revision}
       '';
     };
