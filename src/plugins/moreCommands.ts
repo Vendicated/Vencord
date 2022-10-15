8ce1a4db5a8c0bb932715428c42a1da60d1b9927
echo <91651232+exhq@users.noreply.github.com>
2022-10-15 22:46:42 +0330

    added mock command

--- src/plugins/moreCommands.ts
+++ src/plugins/moreCommands.ts
@@ -1,12 +1,25 @@
 import definePlugin from "../utils/types";
-import { ApplicationCommandInputType, sendBotMessage, findOption, OptionalMessageOption } from "../api/Commands";
+import { ApplicationCommandInputType, OptionalMessageOption, sendBotMessage, findOption, RequiredMessageOption } from "../api/Commands";
 import { Devs } from "../utils/constants";
 
+
+function mock(input): string{
+    let output = ""
+    for (const i of input.toLowerCase()){
+        if (i.match(/^[A-Z]+$/i) && Math.random() < 0.4){
+            output += i.toUpperCase()
+        } else {
+            output += i
+        }
+    }
+return output
+}
 export default definePlugin({
     name: "MoreCommands",
-    description: "echo, lenny",
+    description: "echo, lenny, mock",
     authors: [
         Devs.Arjix,
+        Devs.echo,
         {
             name: "ICodeInAssembly",
             id: 702973430449832038n
@@ -31,6 +44,14 @@ export default definePlugin({
             options: [OptionalMessageOption],
             execute: opts => ({
                 content: findOption(opts, "message", "") + " ( ͡° ͜ʖ ͡°)"
+            }),
+        },
+        {
+            name: "mock",
+            description: "mOcK peOpLE",
+            options: [RequiredMessageOption],
+            execute: opts => ({
+                content: mock(findOption(opts, "message", ""))
             }),
         },
     ]
