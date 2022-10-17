c1b4d14baa6d029823aa6a093230ebc67eb1ec73
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2022-10-17 17:48:11 -0400

    Fart: add settings

--- src/plugins/fart.ts
+++ src/plugins/fart.ts
@@ -1,7 +1,8 @@
 import { ApplicationCommandOptionType } from "../api/Commands";
 import { Devs } from "../utils/constants";
-import definePlugin from "../utils/types";
+import definePlugin, { OptionType } from "../utils/types";
 import { makeLazy } from "../utils/misc";
+import { Settings } from "../Vencord";
 
 export default definePlugin({
     name: "Fart2",
@@ -22,12 +23,20 @@ export default definePlugin({
 
         execute(args) {
             const fart = new Audio("https://raw.githubusercontent.com/ItzOnlyAnimal/AliuPlugins/main/fart.mp3");
-            fart.volume = 0.3;
+            fart.volume = Settings.plugins["Fart2"].volume;
             fart.play();
 
             return {
                 content: (args[0]) ? `<@${args[0].value}> fart` : "fart"
             };
         },
-    }]
+    }],
+    options: {
+        volume: {
+            type: OptionType.NUMBER,
+            description: "Loudness volume",
+            default: 0.5,
+            restartNeeded: false
+        }
+    }
 });
