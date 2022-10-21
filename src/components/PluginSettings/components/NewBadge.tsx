8f425b8ead3c9096e1d3ee742da770b1386f45af
Nickyux <nmsturcke@gmail.com>
2022-10-21 22:21:55 +0200

    change background color of badge

--- src/components/PluginSettings/components/NewBadge.tsx
+++ src/components/PluginSettings/components/NewBadge.tsx
@@ -1,5 +1,5 @@
 export function NewBadge(): JSX.Element {
     return (
-        <div style={{ backgroundColor: "#5865F2", justifySelf: "flex-end", marginLeft: "auto" }} className="textBadge-1fdDPJ base-3IDx3L baseShapeRound-3epLEv">New</div>
+        <div style={{ backgroundColor: "#ED4245", justifySelf: "flex-end", marginLeft: "auto" }} className="textBadge-1fdDPJ base-3IDx3L baseShapeRound-3epLEv">New</div>
     );
 }
