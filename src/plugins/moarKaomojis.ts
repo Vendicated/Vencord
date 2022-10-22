1061a1713372d99fdc556fc8d5d59a250f8536af
Jakup <jacobtm@jacobtm.tk>
2022-10-22 11:41:32 +0200

    moarKaomojis but better i guess

--- src/plugins/moarKaomojis.ts
+++ src/plugins/moarKaomojis.ts
@@ -1,13 +1,10 @@
 import definePlugin from "../utils/types";
 import { ApplicationCommandInputType, OptionalMessageOption, sendBotMessage, findOption, RequiredMessageOption } from "../api/Commands";
-import { Devs } from "../utils/constants";
 
 export default definePlugin({
     name: "moarKaomojis",
-    description: "Sometimes you want to express yourself with moar kaomojis",
+    description: "Adds more Kaomojis to discord. ヽ(´▽`)/",
     authors: [
-        Devs.Arjix,
-        Devs.echo,
         {
             name: "Jacob.Tm",
             id: 302872992097107991n
@@ -15,57 +12,18 @@ export default definePlugin({
     ],
     dependencies: ["CommandsAPI"],
     commands: [
-        {
-            name: "dissatisfaction",
-            description: "＞﹏＜",
-            options: [OptionalMessageOption],
-            execute: opts => ({
-                content: findOption(opts, "message", "") + " ＞﹏＜"
-            }),
-        },
-        {
-            name: "happy",
-            description: "ヽ(´▽`)/",
-            options: [OptionalMessageOption],
-            execute: opts => ({
-                content: findOption(opts, "message", "") + " ヽ(´▽`)/"
-            }),
-        },
-        {
-            name: "crying",
-            description: "ಥ_ಥ",
-            options: [OptionalMessageOption],
-            execute: opts => ({
-                content: findOption(opts, "message", "") + " ಥ_ಥ"
-            }),
-        },
-        {
-            name: "anger",
-            description: "ヽ(ｏ`皿′ｏ)ﾉ",
-            options: [OptionalMessageOption],
-            execute: opts => ({
-                content: findOption(opts, "message", "") + " ヽ(ｏ`皿′ｏ)ﾉ"
-
-            }),
-        },
-        {
-            name: "joy",
-            description: "<(￣︶￣)>",
-            options: [OptionalMessageOption],
-            execute: opts => ({
-                content: findOption(opts, "message", "") + " <(￣︶￣)>"
-            }),
-        },
-        {
-            name: "???",
-            description: "easter egg",
-            execute() {
-                const audio = new Audio("https://github.com/Jacob1Tm/Vencord-plugins-files/blob/main/secret-2.mp3?raw=true");
-                audio.play();
-                return {
-                    content: "Just got rickrolled"
-                }
-            }
-        }
-    ]
+        { name: "dissatisfaction", description: "＞﹏＜"},
+        { name: "smug", description: " ಠ_ಠ"},
+        { name: "happy", description: " ヽ(´▽`)/"},
+        { name: "crying", description: " ಥ_ಥ"},
+        { name: "angry", description: " ヽ(｀Д´)ﾉ"},
+        { name: "anger", description: " ヽ(ｏ`皿′ｏ)ﾉ"},
+        { name: "joy", description: " <(￣︶￣)>"},
+    ].map(data => ({
+        ...data,
+        options: [OptionalMessageOption],
+        execute: opts => ({
+            content: findOption(opts, "message", "") + data.description
+        })
+    }))
 });
