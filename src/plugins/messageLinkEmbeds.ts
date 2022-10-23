2945df872101112f6f48ad107c1a4ca50ce1a307
ActuallyTheSun <78964224+ActuallyTheSun@users.noreply.github.com>
2022-10-23 12:37:53 +0300

    redo message fetching and other improvements

--- src/plugins/messageLinkEmbeds.ts
+++ src/plugins/messageLinkEmbeds.ts
@@ -1,7 +1,8 @@
-import { MessageStore, GuildMemberStore, ChannelStore, GuildStore } from "../webpack/common";
-import { waitFor } from "../webpack";
+import { MessageStore, GuildMemberStore, ChannelStore, GuildStore, FluxDispatcher } from "../webpack/common";
+import { find, waitFor } from "../webpack";
 import definePlugin from "../utils/types";
-import { Embed } from "discord-types/general";
+import { Embed as _Embed, Message } from "discord-types/general";
+import { lazy } from "../utils/misc";
 
 const replacement = `
 const msgLink = $2.message.content?.match(Vencord.Plugins.plugins.MessageLinkEmbeds.messageLinkRegex)?.[1];
@@ -19,28 +20,40 @@ var
   $4 = $2.renderSuppressEmbeds /* Don't add a semicolon here, there are more definitions after this. */
 `.trim().replace(/(?<=[^(?:const)|(?:var)])\s+/gm, "");
 
-let fetchMessages;
-waitFor(["fetchMessages", "editMessage"], _ => ({ fetchMessages } = _));
-let hasJustStarted = true;
-function fetchMessage(channelId: string, messageId: string) {
-    // function can be called immediately after load, meaning that without a timeout it would be fetching messages that should be cached
-    if (hasJustStarted) {
-        setTimeout(() => {
-            if (MessageStore.getMessage(channelId, messageId)) return;
-            fetchMessages({
-                channelId,
-                // "jump" means to fetch messages around it
-                jump: messageId,
-                limit: 3
-            });
-        }, 300);
-        hasJustStarted = false;
+const cache: { [id: string]: Message } = {};
+
+let get: (...args) => Promise<any>;
+waitFor(["get", "getAPIBaseURL"], _ => ({ get } = _));
+let toTimestamp = lazy(() => find(m => m.prototype?.toDate));
+
+function getMessage(channelID: string, messageID: string) {
+    get({
+        // TODO: don't hardcode endpoint
+        url: `/channels/${channelID}/messages`,
+        query: {
+            limit: 1,
+            around: messageID
+        },
+        retries: 2
+    }).then(res => {
+        const message = res.body?.[0];
+        if (!message) return;
+        message.timestamp = toTimestamp()(message.timestamp);
+        if (message.embeds[0]?.type === "rich") {
+            message.embeds[0].rawDescription = message.embeds[0].description || "";
+            message.embeds[0].rawTitle = message.embeds[0].title || "";
+            message.embeds[0].color &&= "#" + message.embeds[0].color.toString(16);
+        }
+        cache[message.id] = message;
+    }).catch(() => {});
+}
+
+interface Embed extends _Embed {
+    _isMessageEmbed?: boolean,
+    footer?: {
+        text: string,
+        iconURL?: string
     }
-    else fetchMessages({
-        channelId,
-        jump: messageId,
-        limit: 3
-    });
 }
 
 export default definePlugin({
@@ -60,7 +73,7 @@ export default definePlugin({
 
     messageLinkRegex: /https?:\/\/(?:\w+\.)?discord(?:app)?\.com\/channels\/((?:\d{17,19}|@me)\/\d{17,19}\/\d{17,19})/,
 
-    getImage(message: any) {
+    getImage(message: Message) {
         if (message.attachments[0] && !message.attachments[0].content_type!.startsWith("video/")) return {
             height: message.attachments[0].height,
             width: message.attachments[0].width,
@@ -69,7 +82,8 @@ export default definePlugin({
         };
         const firstEmbed = message.embeds[0];
         if (!firstEmbed) return null;
-        if (firstEmbed.type === "image" || (firstEmbed.type === "rich" && firstEmbed.image)) return { ...firstEmbed.image };
+        if (firstEmbed.type === "image" || (firstEmbed.type === "rich" && firstEmbed.image))
+            return firstEmbed.image ? { ...firstEmbed.image } : { ...firstEmbed.thumbnail };
         if (firstEmbed.type === "gifv" && !firstEmbed.url!.match(/https:\/\/(?:www.)?tenor\.com/)) return {
             height: firstEmbed.thumbnail!.height,
             width: firstEmbed.thumbnail!.width,
@@ -80,19 +94,18 @@ export default definePlugin({
 
     generateEmbed(messageURL: string, existingEmbeds: Embed[]) {
         const [guildID, channelID, messageID] = messageURL.split("/");
-        if (existingEmbeds.find(i => i.id === `messageLinkEmbeds-${messageID}`)) return [...existingEmbeds];
-        const message = MessageStore.getMessage(channelID, messageID);
+        if (existingEmbeds.find(i => i._isMessageEmbed)) return [...existingEmbeds];
+        const message = MessageStore.getMessage(channelID, messageID) || cache[messageID];
         if (existingEmbeds.find(i => i.id === "messageLinkEmbeds-1")) {
             if (!message) return [...existingEmbeds];
             else existingEmbeds = existingEmbeds.filter(i => i.id !== "messageLinkEmbeds-1");
         }
         if (!message) {
-            fetchMessage(channelID, messageID);
+            getMessage(channelID, messageID);
             return [...existingEmbeds, {
                 author: {
                     name: "Clyde#0000",
-                    // is it a bad idea to hardcode the clyde pfp like this?
-                    // couldn't find a variable for its link anywhere
+                    // TODO: don't hardcode icon url
                     iconURL: "https://discord.com/assets/18126c8a9aafeefa76bbb770759203a9.png",
                     iconProxyURL: "https://discord.com/assets/18126c8a9aafeefa76bbb770759203a9.png"
                 },
@@ -100,40 +113,41 @@ export default definePlugin({
                 id: "messageLinkEmbeds-1",
                 fields: [],
                 type: "rich"
+                // _isMessageEmbed missing is intentional
             }];
         }
 
-        const hasActualEmbed = !!(message.author.bot && message.embeds[0]?.type === "rich" && message.embeds[0].id.match(/embed_\d+/));
+        const firstEmbed = message.embeds[0] as Embed;
+        const hasActualEmbed = !!(message.author.bot && firstEmbed?.type === "rich" && (!firstEmbed.id || firstEmbed.id.match(/embed_\d+/)));
 
         const embeds = [...existingEmbeds, {
             author: {
                 iconProxyURL: `https://${window.GLOBAL_ENV.CDN_HOST}/avatars/${message.author.id}/${message.author.avatar}`,
                 iconURL: `https://${window.GLOBAL_ENV.CDN_HOST}/avatars/${message.author.id}/${message.author.avatar}`,
-                name: `${message.author.username}#${message.author.discriminator}${hasActualEmbed && message.embeds[0].author ?
-                    ` | ${message.embeds[0].author?.name}` : ""}`,
-                url: hasActualEmbed ? message.embeds[0].author?.url! : undefined,
+                name: `${message.author.username}#${message.author.discriminator}${hasActualEmbed && firstEmbed.author ?
+                    ` | ${firstEmbed.author?.name}` : ""}`,
+                url: hasActualEmbed ? firstEmbed.author?.url! : undefined,
             },
-            color: hasActualEmbed ? message.embeds[0].color : GuildMemberStore.getMember(guildID, message.author.id)?.colorString,
+            color: hasActualEmbed ? firstEmbed.color :
+                GuildMemberStore.getMember(guildID, message.author.id)?.colorString,
             image: this.getImage(message),
-            rawDescription: hasActualEmbed && message.embeds[0].rawDescription.length ? message.embeds[0].rawDescription :
+            rawDescription: hasActualEmbed && firstEmbed.rawDescription.length ? firstEmbed.rawDescription :
                 message.content.length ? message.content :
                     `[no content, ${message.attachments.length} attachment${message.attachments.length !== 1 ? "s" : ""}]`,
             footer: {
                 text: guildID === "@me" ? "Direct Message" :
                     "#" + ChannelStore.getChannel(channelID).name +
-                    ` (${GuildStore.getGuild(guildID).name})${hasActualEmbed ?
-                        // apparently embeds don't have footers?
-                        // @ts-expect-error
-                        ` | ${message.embeds[0].footer?.text || ""}` : ""}`,
-                // @ts-expect-error
-                iconURL: hasActualEmbed ? message.embeds[0].footer?.iconURL : undefined,
+                    ` (${GuildStore.getGuild(guildID).name})${hasActualEmbed && firstEmbed.footer ?
+                        ` | ${firstEmbed.footer.text}` : ""}`,
+                iconURL: hasActualEmbed ? firstEmbed.footer?.iconURL : undefined,
             },
-            rawTitle: hasActualEmbed ? message.embeds[0].rawTitle : undefined,
-            thumbnail: hasActualEmbed ? { ...message.embeds[0].thumbnail } : undefined,
+            rawTitle: hasActualEmbed ? firstEmbed.rawTitle : undefined,
+            thumbnail: hasActualEmbed ? { ...firstEmbed.thumbnail } : undefined,
             timestamp: message.timestamp,
             id: `messageLinkEmbeds-${messageID}`,
             fields: [],
-            type: "rich"
+            type: "rich",
+            _isMessageEmbed: true
         }];
 
         return embeds;
