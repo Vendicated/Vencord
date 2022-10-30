9e5530e515df2c89993cc4b3e88288a1026ffece
Berlin <39058370+somerandomcloud@users.noreply.github.com>
2022-10-30 17:56:02 +0100

    Update noBlockedMessages.ts

--- src/plugins/noBlockedMessages.ts
+++ src/plugins/noBlockedMessages.ts
@@ -16,13 +16,17 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { lazyWebpack } from "../utils";
 import { Devs } from "../utils/constants";
-import definePlugin from "../utils/types";
+import definePlugin, { OptionType } from "../utils/types";
+import { Settings } from "../Vencord";
+import { filters } from "../webpack";
+const RelationshipStore = lazyWebpack(filters.byProps(["getRelationships", "isBlocked"]));
 
 export default definePlugin({
     name: "NoBlockedMessages",
     description: "Hides all blocked messages from chat completely.",
-    authors: [Devs.rushii],
+    authors: [Devs.rushii, Devs.Samu],
     patches: [
         {
             find: 'safety_prompt:"DMSpamExperiment",response:"show_redacted_messages"',
@@ -32,6 +36,28 @@ export default definePlugin({
                     replace: "collapsedReason; return null;"
                 }
             ]
+        },
+        {
+            find: "displayName=\"MessageStore\"",
+            predicate: () => Settings.plugins.NoBlockedMessages.ignoreBlockedMessages === true,
+            replacement: [
+                {
+                    match: /(?<=MESSAGE_CREATE:function\((\w)\){var \w=\w\.channelId,\w=\w\.message,\w=\w\.isPushNotification,\w=\w\.\w\.getOrCreate\(\w\));/,
+                    replace: ";if(Vencord.Plugins.plugins.IgnoreBlockedUsers.isBlocked(n))return;"
+                }
+            ]
         }
-    ]
+    ],
+    options: {
+        ignoreBlockedMessages: {
+            description: "Completely ignores (recent) incoming messages from blocked users (locally).",
+            type: OptionType.BOOLEAN,
+            default: false,
+            restartNeeded: true,
+        },
+    },
+    isBlocked(message) {
+        if (RelationshipStore.isBlocked(message.author.id)) return true;
+        else return false;
+    }
 });
