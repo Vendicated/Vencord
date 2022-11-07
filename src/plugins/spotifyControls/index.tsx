281be8ada0e73c4255fafcfea9ed29a56beeb0bc
Vendicated <contact@swezy.dev>
2022-11-07 22:36:06 +0100

    me when i dont depend on MenuItemDeobfuscatorApi

--- src/plugins/spotifyControls/index.tsx
+++ src/plugins/spotifyControls/index.tsx
@@ -24,6 +24,7 @@ export default definePlugin({
     name: "SpotifyControls",
     description: "Spotify Controls",
     authors: [Devs.Ven],
+    dependencies: ["MenuItemDeobfuscatorApi"],
     patches: [
         {
             find: "showTaglessAccountPanel:",
