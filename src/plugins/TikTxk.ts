a5153403fb89099f23b0544fe1b2f156cced4548
Berlin <39058370+somerandomcloud@users.noreply.github.com>
2022-11-10 17:04:17 +0100

    Create TikTxk.ts

---
+++ src/plugins/TikTxk.ts
@@ -0,0 +1,42 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Samu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { addPreSendListener, MessageObject, removePreSendListener } from "../api/MessageEvents";
+import { Devs } from "../utils/constants";
+import definePlugin from "../utils/types";
+
+const re = /(?<=https:\/\/(vm\.|www\.))tiktok.com/g;
+
+export default definePlugin({
+    name: "TikTxk",
+    description: "Uses TikTxk to improve tiktok embeds on send",
+    authors: [Devs.Samu],
+    dependencies: ["MessageEventsAPI"],
+
+    addPrefix(msg: MessageObject) {
+        msg.content = msg.content.replace(re, "tiktxk.com");
+    },
+
+    start() {
+        this.preSend = addPreSendListener((_, msg) => this.addPrefix(msg));
+    },
+
+    stop() {
+        removePreSendListener(this.preSend);
+    }
+});
