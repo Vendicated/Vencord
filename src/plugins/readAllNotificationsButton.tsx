803c2ff0595a0821cea76ef8c8472a970bfe587c
Ven <vendicated@riseup.net>
2022-11-18 23:44:27 +0100

    unnamespace

--- src/plugins/readAllNotificationsButton.tsx
+++ src/plugins/readAllNotificationsButton.tsx
@@ -21,57 +21,45 @@ import { Devs } from "../utils/constants";
 import definePlugin from "../utils/types";
 import { Button, FluxDispatcher, GuildChannelStore, GuildStore, React, ReadStateStore } from "../webpack/common";
 
-namespace ReadAllButton {
+function onClick() {
+    const channels: Array<any> = [];
 
-    function onClick() {
-        const channels: Array<any> = [];
+    Object.values(GuildStore.getGuilds()).forEach(guild => {
+        GuildChannelStore.getChannels(guild.id).SELECTABLE.forEach((c: { channel: { id: string; }; }) => {
+            if (!ReadStateStore.hasUnread(c.channel.id)) return;
 
-        Object.values(GuildStore.getGuilds()).forEach(guild => {
-
-            GuildChannelStore.getChannels(guild.id).SELECTABLE.forEach((c: { channel: { id: string; }; }) => {
-                // PrivateChannelsStore.preload(guildId, channelId, false);
-
-                if (!ReadStateStore.hasUnread(c.channel.id)) return;
-
-                channels.push({
-                    channelId: c.channel.id,
-                    // messageId: c.channel?.lastMessageId,
-                    messageId: ReadStateStore.lastMessageId(c.channel.id),
-                    readStateType: 0
-                });
+            channels.push({
+                channelId: c.channel.id,
+                // messageId: c.channel?.lastMessageId,
+                messageId: ReadStateStore.lastMessageId(c.channel.id),
+                readStateType: 0
             });
         });
+    });
 
-        FluxDispatcher.dispatch({
-            type: "BULK_ACK",
-            context: "APP",
-            channels: channels
-        });
-    }
-
-    export const Element = () => {
-        return <Button
-            onClick={onClick}
-            size={Button.Sizes.MIN}
-            color={Button.Colors.BRAND}
-            style={{ marginTop: "2px", marginBottom: "8px", marginLeft: "9px" }}
-        > Read all </Button>;
-    };
-
+    FluxDispatcher.dispatch({
+        type: "BULK_ACK",
+        context: "APP",
+        channels: channels
+    });
 }
+
+const ReadAllButton = () => (
+    <Button
+        onClick={onClick}
+        size={Button.Sizes.MIN}
+        color={Button.Colors.BRAND}
+        style={{ marginTop: "2px", marginBottom: "8px", marginLeft: "9px" }}
+    >Read all</Button>;
+);
 
 export default definePlugin({
     name: "ReadAllNotificationsButton",
-
-    authors: [Devs.kemo],
-
     description: "Read all server notifications with a single button click!",
-
+    authors: [Devs.kemo],
     dependencies: ["ServerListAPI"],
 
-    renderReadAllButton: () => {
-        return <ReadAllButton.Element />;
-    },
+    renderReadAllButton: () => <ReadAllButton />,
 
     start() {
         addElement(RenderPosition.In, this.renderReadAllButton);
