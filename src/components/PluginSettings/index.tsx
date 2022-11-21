99d3a8f8772359eb746b4be3e99a4b094d4eec54
nmsturcke <30734036+nmsturcke@users.noreply.github.com>
2022-11-21 19:10:46 +0100

    Remove badges from plugins if all are new
    Don't adding the new badge to all plugins as it is *theoretically* impossible for all of them to be new at once.

--- src/components/PluginSettings/index.tsx
+++ src/components/PluginSettings/index.tsx
@@ -265,7 +265,7 @@ export default ErrorBoundary.wrap(function Settings() {
         );
     };
 
-    const [newPlugins, error, newPluginsLoading] = useAwaiter(() => DataStore.get("Vencord_existingPlugins").then((existingPlugins: Record<string, number>) => {
+    let [newPlugins, error, newPluginsLoading] = useAwaiter(() => DataStore.get("Vencord_existingPlugins").then((existingPlugins: Record<string, number>) => {
         const dateNow: number = Date.now() / 1000;
         const Vencord_existingPlugins: Record<string, number> = {};
         const newPlugins: Array<string> = [];
@@ -278,6 +278,10 @@ export default ErrorBoundary.wrap(function Settings() {
         DataStore.set("Vencord_existingPlugins", Vencord_existingPlugins);
         return newPlugins;
     }));
+
+    if (JSON.stringify(newPlugins) === JSON.stringify(sortedPluginNames)) {
+        newPlugins = [];
+    }
 
     return (
         <Forms.FormSection>
