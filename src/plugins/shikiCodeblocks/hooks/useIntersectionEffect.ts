d648e1e2e568e5a2856757a3b00263b650739ac2
Vap0r1ze <superdash993@gmail.com>
2022-11-28 08:08:19 -0500

    lint

--- src/plugins/shikiCodeblocks/hooks/useIntersectionEffect.ts
+++ src/plugins/shikiCodeblocks/hooks/useIntersectionEffect.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { React } from "../../../webpack/common";
+import { React } from "@webpack/common";
 
 export const isInsterecting = (el: HTMLElement) => {
     const elementBox = el.getBoundingClientRect();
