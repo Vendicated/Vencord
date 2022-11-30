ea650ad5ecd1e82ebb16515a8f289e734a67772f
nmsturcke <30734036+nmsturcke@users.noreply.github.com>
2022-11-30 20:01:56 +0100

    fix imports frfr

--- src/components/PluginSettings/components/BadgeComponent.tsx
+++ src/components/PluginSettings/components/BadgeComponent.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { BadgeStyle } from "../styles";
+import { BadgeStyle } from "@components/PluginSettings/styles";
 
 export function Badge({ text, color }): JSX.Element {
     return (
