892a71049377e2f246e1c18f7a212cea10fab657
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-12-08 17:26:03 -0600

    bruh

--- src/plugins/bypassNsfwGate.ts
+++ src/plugins/bypassNsfwGate.ts
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
 
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
