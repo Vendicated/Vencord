5ca1a4bafdc6bc1e62c0db91ee72bbc72adade80
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2022-12-08 17:24:35 -0600

    run prettier again

--- src/plugins/bypassNsfwGate.ts
+++ src/plugins/bypassNsfwGate.ts
@@ -20,17 +20,17 @@ import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
-  name: "Bypass NSFW Gate",
-  description:
-    "Allows you to access NSFW channels even if you are not 18 years old",
-  authors: [Devs.Commandtechno],
-  patches: [
-    {
-      find: ".nsfwAllowed=null",
-      replacement: {
-        match: /(\w+)\.nsfwAllowed=/,
-        replace: "$1.nsfwAllowed=true;",
-      },
-    },
-  ],
+    name: "Bypass NSFW Gate",
+    description:
+        "Allows you to access NSFW channels even if you are not 18 years old",
+    authors: [Devs.Commandtechno],
+    patches: [
+        {
+            find: ".nsfwAllowed=null",
+            replacement: {
+                match: /(\w+)\.nsfwAllowed=/,
+                replace: "$1.nsfwAllowed=true;",
+            },
+        },
+    ],
 });
