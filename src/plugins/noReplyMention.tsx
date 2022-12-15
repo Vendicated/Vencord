f6122a00ca9d5259ca8c97e88045ee9b9936ab70
Pedro <nekkan@protonmail.ch>
2022-12-15 11:05:44 -0300

    feat(NoReplyMention): exempt list support (#337)

--- src/plugins/noReplyMention.tsx
+++ src/plugins/noReplyMention.tsx
@@ -16,20 +16,43 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { Settings } from "@api/settings";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
+
+interface Reply {
+    message: {
+        author: {
+            id: string;
+        };
+    };
+}
 
 export default definePlugin({
     name: "NoReplyMention",
     description: "Disables reply pings by default",
-    authors: [Devs.DustyAngel47],
+    authors: [Devs.DustyAngel47, Devs.axyie],
+    options: {
+        exemptList: {
+            description:
+                "List of users to exempt from this plugin (separated by commas)",
+            type: OptionType.STRING,
+            default: "1234567890123445,1234567890123445",
+        },
+    },
+    shouldMention(reply: Reply) {
+        return Settings.plugins.NoReplyMention.exemptList.includes(
+            reply.message.author.id
+        );
+    },
     patches: [
         {
             find: "CREATE_PENDING_REPLY:function",
             replacement: {
                 match: /CREATE_PENDING_REPLY:function\((.{1,2})\){/,
-                replace: "CREATE_PENDING_REPLY:function($1){$1.shouldMention=false;"
-            }
-        }
-    ]
+                replace:
+                    "CREATE_PENDING_REPLY:function($1){$1.shouldMention=Vencord.Plugins.plugins.NoReplyMention.shouldMention($1);",
+            },
+        },
+    ],
 });
