4ab8e6303d3a6b0c1ce45b0bae88a7435df9d4e5
Sammy <sammy@sammcheese.net>
2022-12-19 16:31:43 +0100

    use JSX instead of React + remove unnecessary dependency

--- src/plugins/invisiblechat/index.tsx
+++ src/plugins/invisiblechat/index.tsx
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import definePlugin, { OptionType } from "@utils/types";
-import { FluxDispatcher, React } from "@webpack/common";
+import definePlugin from "@utils/types";
+import { FluxDispatcher } from "@webpack/common";
 
 import { buildDecModal } from "./components/DecryptionModal";
 import { buildEncModal } from "./components/EncryptionModal";
@@ -40,31 +40,25 @@ const PopoverIcon = () => {
 
 function Indicator() {
     return (
-        React.createElement("img", {
-            src: "https://cdn-icons-png.flaticon.com/32/3064/3064130.png",
-            width: "20",
-            style: { marginBottom: -4 }
-        })
+        <img src="https://cdn-icons-png.flaticon.com/32/3064/3064130.png" width={20} style={{ marginBottom: -4 }}>
+        </img>
     );
+
 }
 
 function ChatbarIcon() {
     return (
-        React.createElement(
-            "svg",
-            {
-                key: "Encrypt Message",
-                fill: "#EBEBEB",
-                width: "30",
-                height: "30",
-                viewBox: "0 0 64 64",
-                style: { marginTop: 7 },
-                onClick: () => buildEncModal(),
-            },
-            React.createElement("path", {
-                d: "M 32 9 C 24.832 9 19 14.832 19 22 L 19 27.347656 C 16.670659 28.171862 15 30.388126 15 33 L 15 49 C 15 52.314 17.686 55 21 55 L 43 55 C 46.314 55 49 52.314 49 49 L 49 33 C 49 30.388126 47.329341 28.171862 45 27.347656 L 45 22 C 45 14.832 39.168 9 32 9 z M 32 13 C 36.963 13 41 17.038 41 22 L 41 27 L 23 27 L 23 22 C 23 17.038 27.037 13 32 13 z",
-            }),
-        )
+        <svg
+            key="Encrypt Message"
+            fill="#EBEBEB"
+            width="30"
+            height="30"
+            viewBox={"0 0 64 64"}
+            style={{ marginTop: 7 }}
+            onClick={() => buildEncModal()}
+        >
+            <path d="M 32 9 C 24.832 9 19 14.832 19 22 L 19 27.347656 C 16.670659 28.171862 15 30.388126 15 33 L 15 49 C 15 52.314 17.686 55 21 55 L 43 55 C 46.314 55 49 52.314 49 49 L 49 33 C 49 30.388126 47.329341 28.171862 45 27.347656 L 45 22 C 45 14.832 39.168 9 32 9 z M 32 13 C 36.963 13 41 17.038 41 22 L 41 27 L 23 27 L 23 22 C 23 17.038 27.037 13 32 13 z" />
+        </svg>
     );
 }
 
@@ -104,7 +98,6 @@ export default definePlugin({
             }
         },
     ],
-    dependencies: ["MessageEventsAPI"],
     EMBED_URL: "https://embed.sammcheese.net",
     INV_DETECTION: new RegExp(/( \u200c|\u200d |[\u2060-\u2064])[^\u200b]/),
     URL_DETECTION: new RegExp(
