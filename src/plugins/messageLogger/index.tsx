8ef8fc6e366e2a9948b6e25230af96a00fd35401
Vendicated <vendicated@riseup.net>
2022-12-23 19:58:48 +0100

    should probably actually import ML css

--- src/plugins/messageLogger/index.tsx
+++ src/plugins/messageLogger/index.tsx
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import "./messageLogger.css";
+
 import { Settings } from "@api/settings";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
