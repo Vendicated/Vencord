2901263baa8a92d255affc4966a7e2166d4867f8
whqwert <94757998+whqwert@users.noreply.github.com>
2023-01-04 12:23:22 -0600

    Add RevealAllSpoilers plugin

---
+++ src/plugins/revealAllSpoilers.ts
@@ -0,0 +1,53 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+interface SpoilerEvent {
+    ctrlKey: boolean;
+    target: HTMLSpanElement;
+}
+
+export default definePlugin({
+    name: "RevealAllSpoilers",
+    description: "Reveal all spoilers in a message by ctrl-clicking a spoiler",
+    authors: [Devs.whqwert],
+
+    patches: [
+        {
+            find: ".revealSpoiler=",
+            replacement: {
+                match: /\.revealSpoiler=function\((.{1,3})\){/,
+                replace: ".revealSpoiler=function($1){Vencord.Plugins.plugins.RevealAllSpoilers.reveal($1);"
+            }
+        }
+    ],
+
+    reveal(event: SpoilerEvent) {
+        const { ctrlKey, target } = event;
+        if (!ctrlKey) { return; }
+
+        for (const spoiler of target.parentElement!.querySelectorAll(
+            "span[class*=spoilerText-][class*=hidden-]"
+        )) {
+            (spoiler as HTMLElement).click();
+        }
+    }
+
+});
