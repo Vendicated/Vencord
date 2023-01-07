43a9c54dd721adabdde442beb29556e6aa650374
Vendicated <vendicated@riseup.net>
2023-01-07 22:44:00 +0100

    Add tooltip to chat indicator

--- src/plugins/invisiblechat/index.tsx
+++ src/plugins/invisiblechat/index.tsx
@@ -31,8 +31,9 @@ const ButtonWrapperClasses = findByPropsLazy("buttonWrapper", "buttonContent");
 
 let steggo: any;
 
-const PopOverIcon = () => {
+function PopOverIcon() {
     return (
+
         <svg
             fill="var(--header-secondary)"
             width={24} height={24}
@@ -41,16 +42,25 @@ const PopOverIcon = () => {
             <path d="M 32 9 C 24.832 9 19 14.832 19 22 L 19 27.347656 C 16.670659 28.171862 15 30.388126 15 33 L 15 49 C 15 52.314 17.686 55 21 55 L 43 55 C 46.314 55 49 52.314 49 49 L 49 33 C 49 30.388126 47.329341 28.171862 45 27.347656 L 45 22 C 45 14.832 39.168 9 32 9 z M 32 13 C 36.963 13 41 17.038 41 22 L 41 27 L 23 27 L 23 22 C 23 17.038 27.037 13 32 13 z" />
         </svg>
     );
-};
+}
 
 
 function Indicator() {
     return (
-        <img
-            src="https://github.com/SammCheese/invisible-chat/raw/NewReplugged/src/assets/lock.png"
-            width={20}
-            style={{ marginBottom: -4 }}
-        />
+        <Tooltip text="This message has a hidden message! (InvisibleChat)">
+            {({ onMouseEnter, onMouseLeave }) => (
+                <img
+                    aria-label="Hidden Message Indicator (InvisibleChat)"
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    src="https://github.com/SammCheese/invisible-chat/raw/NewReplugged/src/assets/lock.png"
+                    width={20}
+                    height={20}
+                    style={{ transform: "translateY(4p)", paddingInline: 4 }}
+                />
+            )}
+        </Tooltip>
+
     );
 
 }
@@ -190,7 +200,7 @@ export default definePlugin({
 
     chatBarIcon: ErrorBoundary.wrap(ChatBarIcon, { noop: true }),
     popOverIcon: () => <PopOverIcon />,
-    indicator: Indicator
+    indicator: ErrorBoundary.wrap(Indicator, { noop: true })
 });
 
 export function encrypt(secret: string, password: string, cover: string): string {
