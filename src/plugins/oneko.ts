1db70a3eb43d1f5dc87557899706b01890ac47a7
ZerXDE "Till O <contact@swezy.dev>
2023-01-09 16:53:33 +0100

    oneko: Disable pointer events to not block below buttons (#395)
    Updated version of oneko which disables pointer Events.
    
    Co-authored-by: Ven <vendicated@riseup.net>

--- src/plugins/oneko.ts
+++ src/plugins/oneko.ts
@@ -26,7 +26,7 @@ export default definePlugin({
     authors: [Devs.Ven, Devs.adryd],
 
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.js")
+        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/5977144dce83e4d71af1de005d16e38eebeb7b72/oneko.js")
             .then(x => x.text())
             .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif"))
             .then(eval);
