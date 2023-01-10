9d68e82ab7e397361c0dd2d6d905b4c73c104942
Sofia N. Lima <me@dzshn.xyz>
2023-01-10 07:00:44 -0300

    feat(SilentTyping): add optional icon toggle

---
+++ src/plugins/silentTyping.tsx
@@ -0,0 +1,121 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { ApplicationCommandInputType, ApplicationCommandOptionType, findOption, sendBotMessage } from "@api/Commands";
+import { get, set } from "@api/DataStore";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs } from "@utils/constants";
+import { useForceUpdater } from "@utils/misc";
+import definePlugin, { OptionType } from "@utils/types";
+import { Button, ButtonLooks, ButtonWrapperClasses, FluxDispatcher, React, Tooltip } from "@webpack/common";
+import { Settings } from "Vencord";
+
+const KEY = "SilentTyping_ENABLED";
+
+let forceUpdateIcon: () => void;
+let isEnabled: boolean;
+
+function SilentTypingToggle() {
+    forceUpdateIcon = useForceUpdater();
+
+    React.useEffect(() => {
+        (async () => { isEnabled = !!await get(KEY); })();
+    }, []);
+
+    return (
+        <Tooltip text={isEnabled ? "Disable silent typing" : "Enable silent typing"} style={{ scale: "0.5" }}>
+            {(tooltipProps: any) => (
+                <Button
+                    {...tooltipProps}
+                    onClick={async () => { set(KEY, (isEnabled = !await get(KEY))); forceUpdateIcon(); }}
+                    size=""
+                    look={ButtonLooks.BLANK}
+                    innerClassName={ButtonWrapperClasses.button}
+                    style={{ margin: "0 8px 0" }}
+                >
+                    <div className={ButtonWrapperClasses.buttonWrapper}>
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <path fill="currentColor" d="M528 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h480c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM128 180v-40c0-6.627-5.373-12-12-12H76c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm-336 96v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm-336 96v-40c0-6.627-5.373-12-12-12H76c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12zm288 0v-40c0-6.627-5.373-12-12-12H172c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h232c6.627 0 12-5.373 12-12zm96 0v-40c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12z"/>
+                            {isEnabled && <path d="M13 432L590 48" stroke="var(--status-red-500)" stroke-width="72" stroke-linecap="round"/>}
+                        </svg>
+                    </div>
+                </Button>
+            )}
+        </Tooltip>
+    );
+}
+
+export default definePlugin({
+    name: "SilentTyping",
+    authors: [Devs.Ven, Devs.dzshn],
+    description: "Hide that you are typing",
+    patches: [
+        {
+            find: "startTyping:",
+            replacement: {
+                match: /startTyping:.+?,stop/,
+                replace: "startTyping:$self.startTyping,stop"
+            }
+        },
+        {
+            find: ".activeCommandOption",
+            predicate: () => Settings.plugins.SilentTyping.showIcon,
+            replacement: {
+                match: /\i=\i\.activeCommand,\i=\i\.activeCommandOption,.{1,133}(.)=\[\];/,
+                replace: "$&;$1.push($self.chatBarIcon());",
+            }
+        },
+    ],
+    dependencies: ["CommandsAPI"],
+    options: {
+        showIcon: {
+            type: OptionType.BOOLEAN,
+            default: false,
+            description: "Show an icon for toggling the plugin.",
+            restartNeeded: true,
+        }
+    },
+    commands: [{
+        name: "silenttype",
+        description: "Toggle whether you're hiding that you're typing or not.",
+        inputType: ApplicationCommandInputType.BUILT_IN,
+        options: [
+            {
+                name: "value",
+                description: "whether to hide or not what you're typing (default is toggle)",
+                required: false,
+                type: ApplicationCommandOptionType.BOOLEAN,
+            }
+        ],
+        execute: async (args, ctx) => {
+            isEnabled = !!findOption(args, "value", !await get(KEY));
+            await set(KEY, isEnabled);
+            forceUpdateIcon?.();
+            sendBotMessage(ctx.channel.id, {
+                content: isEnabled ? "Silent typing enabled!" : "Silent typing disabled!",
+            });
+        }
+    }],
+
+    async startTyping(channelId: string) {
+        if (await get(KEY)) return;
+        FluxDispatcher.dispatch({ type: "TYPING_START_LOCAL", channelId });
+    },
+
+    chatBarIcon: ErrorBoundary.wrap(SilentTypingToggle, { noop: true }),
+});
