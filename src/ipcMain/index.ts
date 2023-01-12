72420edf1360e89c87c7ae39aa4dd245e54e5ef0
Ven <contact@swezy.dev>
2023-01-12 23:15:38 +0100

    Update Windows Update patcher (#404)

--- src/ipcMain/index.ts
+++ src/ipcMain/index.ts
@@ -22,7 +22,7 @@ import "./updater";
 import { debounce } from "@utils/debounce";
 import IpcEvents from "@utils/IpcEvents";
 import { Queue } from "@utils/Queue";
-import { BrowserWindow, desktopCapturer, ipcMain, shell } from "electron";
+import { BrowserWindow, ipcMain, shell } from "electron";
 import { mkdirSync, readFileSync, watch } from "fs";
 import { open, readFile, writeFile } from "fs/promises";
 import { join } from "path";
@@ -44,9 +44,6 @@ export function readSettings() {
         return "{}";
     }
 }
-
-// Fix for screensharing in Electron >= 17
-ipcMain.handle(IpcEvents.GET_DESKTOP_CAPTURE_SOURCES, (_, opts) => desktopCapturer.getSources(opts));
 
 ipcMain.handle(IpcEvents.OPEN_QUICKCSS, () => shell.openPath(QUICKCSS_PATH));
 
