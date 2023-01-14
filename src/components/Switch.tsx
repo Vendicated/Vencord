f9cfb75627bc916e446f4c1422a790b9a2183554
Vendicated <vendicated@riseup.net>
2023-01-14 20:19:25 +0100

    Unhardcode classes

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -16,53 +16,57 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { findByPropsLazy } from "@webpack";
+
 interface SwitchProps {
     checked: boolean;
     onChange: (checked: boolean) => void;
     disabled?: boolean;
 }
 
+const SWITCH_ON = "var(--status-green-600)";
+const SWITCH_OFF = "var(--primary-dark-400)";
+const SwitchClasses = findByPropsLazy("slider", "input", "container");
+
 export function Switch({ checked, onChange, disabled }: SwitchProps) {
     return (
-        <div className="control-1fl03-">
-            <div className="container-2nx-BQ default-colors" style={{
-                backgroundColor: checked ? "var(--status-green-600)" : "var(--primary-dark-400)",
-                opacity: disabled ? 0.3 : 1
-            }}>
-                <svg
-                    className="slider-32CRPX"
-                    viewBox="0 0 28 20"
-                    preserveAspectRatio="xMinYMid meet"
-                    aria-hidden="true"
-                    style={{
-                        left: checked ? "12px" : "-3px",
-                    }}
-                >
-                    <rect fill="white" x="4" y="0" height="20" width="20" rx="10" />
-                    <svg viewBox="0 0 20 20" fill="none">
-                        {checked ? (
-                            <>
-                                <path fill="var(--status-green-600)" d="M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z" />
-                                <path fill="var(--status-green-600)" d="M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z" />
-                            </>
-                        ) : (
-                            <>
-                                <path fill="var(--primary-dark-400)" d="M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z" />
-                                <path fill="var(--primary-dark-400)" d="M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z" />
-                            </>
-                        )}
+        <div className={`${SwitchClasses.container} default-colors`} style={{
+            backgroundColor: checked ? SWITCH_ON : SWITCH_OFF,
+            opacity: disabled ? 0.3 : 1
+        }}>
+            <svg
+                className={SwitchClasses.slider}
+                viewBox="0 0 28 20"
+                preserveAspectRatio="xMinYMid meet"
+                aria-hidden="true"
+                style={{
+                    left: checked ? "12px" : "-3px",
+                }}
+            >
+                <rect fill="white" x="4" y="0" height="20" width="20" rx="10" />
+                <svg viewBox="0 0 20 20" fill="none">
+                    {checked ? (
+                        <>
+                            <path fill={SWITCH_ON} d="M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z" />
+                            <path fill={SWITCH_ON} d="M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z" />
+                        </>
+                    ) : (
+                        <>
+                            <path fill={SWITCH_OFF} d="M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z" />
+                            <path fill={SWITCH_OFF} d="M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z" />
+                        </>
+                    )}
 
-                    </svg>
                 </svg>
-                <input
-                    disabled={disabled}
-                    type="checkbox"
-                    className="input-2XRLou"
-                    tabIndex={0}
-                    checked={checked}
-                    onChange={e => onChange(e.currentTarget.checked)}
-                />
-            </div>
+            </svg>
+            <input
+                disabled={disabled}
+                type="checkbox"
+                className={SwitchClasses.input}
+                tabIndex={0}
+                checked={checked}
+                onChange={e => onChange(e.currentTarget.checked)}
+            />
         </div>
     );
 }
