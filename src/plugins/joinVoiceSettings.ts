6099dd2bcab754c955b7893d40415fcb36c8c736
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2023-01-24 01:28:57 -0500

    Use the same find as it's in the same function

--- src/plugins/joinVoiceSettings.ts
+++ src/plugins/joinVoiceSettings.ts
@@ -39,18 +39,17 @@ export default definePlugin({
     patches: [
         {
             find: ".displayName=\"MediaEngineStore\"",
-            replacement: {
-                match: /(?<pre>VOICE_CHANNEL_SELECT:function\((?<event>.{1,2})\){.*?\if\((?<var>.{1,2})\.mute\|\|\k<var>\.deaf)(?<mid>\).{0,100}?\({)deaf:!1,mute:!1(?=}\);)/,
-                replace: "$<pre>||$self.shouldOverride()$<mid>deaf:$self.shouldDeafen($<event>,$<var>),mute:$self.shouldMute($<event>,$<var>)",
-            },
+            replacement: [
+                {
+                    match: /(?<pre>VOICE_CHANNEL_SELECT:function\((?<event>.{1,2})\){.*?\if\((?<var>.{1,2})\.mute\|\|\k<var>\.deaf)(?<mid>\).{0,100}?\({)deaf:!1,mute:!1(?=}\);)/,
+                    replace: "$<pre>||$self.shouldOverride()$<mid>deaf:$self.shouldDeafen($<event>,$<var>),mute:$self.shouldMute($<event>,$<var>)",
+                },
+                {
+                    match: /(?<pre>VOICE_CHANNEL_SELECT:function\((?<event>.{1,2})\){.*?(?:var |,)(?<var>.{1,2})=\k<event>\.guildId.+?if\()(?<cond>null==\k<var>)/,
+                    replace: "$<pre>($<cond>||$self.shouldOverride())"
+                }
+            ]
         },
-        {
-            find: ".displayName=\"MediaEngineStore\"",
-            replacement: {
-                match: /(?<pre>VOICE_CHANNEL_SELECT:function\((?<event>.{1,2})\){.*?(?:var |,)(?<var>.{1,2})=\k<event>\.guildId.+?if\()(?<cond>null==\k<var>)/,
-                replace: "$<pre>($<cond>||$self.shouldOverride())"
-            }
-        }
     ],
     shouldOverride() {
         return this.settings.store.alwaysJoinMuted || this.settings.store.alwaysJoinDeafened;
