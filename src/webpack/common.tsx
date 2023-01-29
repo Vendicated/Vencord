9790698f56eeb643a918024a0604438aec6a5fca
Ven <vendicated@riseup.net>
2023-01-29 01:53:04 +0100

    Update common.tsx

--- src/webpack/common.tsx
+++ src/webpack/common.tsx
@@ -75,7 +75,6 @@ export let Button: any;
 export const ButtonLooks = findByPropsLazy("BLANK", "FILLED", "INVERTED") as Record<"FILLED" | "INVERTED" | "OUTLINED" | "LINK" | "BLANK", string>;
 export let Switch: any;
 export let Tooltip: Components.Tooltip;
-export let Timestamp: any;
 export let Router: any;
 export let TextInput: any;
 export let Text: (props: TextProps) => JSX.Element;
@@ -185,8 +184,6 @@ waitFor("getRelationshipType", m => RelationshipStore = m);
 waitFor(["Hovers", "Looks", "Sizes"], m => Button = m);
 
 waitFor(filters.byCode("tooltipNote", "ringTarget"), m => Switch = m);
-
-waitFor(filters.byCode(".Messages.MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL.format"), m => Timestamp = m);
 
 waitFor(["Positions", "Colors"], m => Tooltip = m);
 waitFor(m => m.Types?.PRIMARY === "cardPrimary", m => Card = m);
@@ -310,3 +307,4 @@ export const ContextMenu = mapMangledModuleLazy('type:"CONTEXT_MENU_OPEN"', {
 export const MaskedLinkStore = mapMangledModuleLazy('"MaskedLinkStore"', {
     openUntrustedLink: filters.byCode(".apply(this,arguments)")
 });
+
