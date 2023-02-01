ea27590bd9a73d5ebbaf9cb58405f44e64c0612f
whqwert <contact@swezy.dev>
2023-02-01 06:38:02 -0600

    feat(plugin): RevealAllSpoilers (#381)
    Co-authored-by: Ven <vendicated@riseup.net>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -192,5 +192,9 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     captain: {
         name: "Captain",
         id: 347366054806159360n
+    },
+    whqwert: {
+        name: "whqwert",
+        id: 586239091520176128n
     }
 });
