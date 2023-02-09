c80e55c8574450db5285bf704c13b8386042f7e0
Vendicated <vendicated@riseup.net>
2023-02-09 17:21:14 +0100

    impl settings ui

--- src/webpack/common/types/components.d.ts
+++ src/webpack/common/types/components.d.ts
@@ -196,7 +196,7 @@ interface SelectOption {
 }
 
 export type Select = ComponentType<PropsWithChildren<{
-    placeholder?: string;
+    placeholder: string;
     options: ReadonlyArray<SelectOption>; // TODO
 
     /**
@@ -215,9 +215,9 @@ export type Select = ComponentType<PropsWithChildren<{
     closeOnSelect?: boolean;
     hideIcon?: boolean;
 
-    select?(value: any): void;
-    isSelected?(value: any): boolean;
-    serialize?(value: any): string;
+    select(value: any): void;
+    isSelected(value: any): boolean;
+    serialize(value: any): string;
     clear?(): void;
 
     maxVisibleItems?: number;
