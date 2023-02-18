13e110e07af6579587569b3648ba7ff032d57d73
Syncxv <aruldevarackal1@gmail.com>
2023-02-18 23:58:17 +1100

    merging is hard

---
+++ src/plugins/imageUtility/components/Magnifer.tsx
@@ -0,0 +1,137 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { LazyComponent } from "@utils/misc";
+import { React } from "@webpack/common";
+
+import { waitFor } from "../utils/waitFor";
+
+interface MagniferProps {
+    zoom: number;
+    size: number,
+    instance: any;
+}
+
+// class component because i like it more
+export const Magnifer = LazyComponent(() => class Magnifer extends React.PureComponent<MagniferProps> {
+    lens = React.createRef<HTMLDivElement>();
+    imageRef = React.createRef<HTMLImageElement>();
+    currentVideoElementRef = React.createRef<HTMLVideoElement>();
+    element!: HTMLDivElement;
+    videoElement!: HTMLVideoElement;
+    constructor(props: MagniferProps) {
+        super(props);
+        this.element = document.querySelector("#bruhjuhhh")!;
+    }
+
+
+    async componentDidMount() {
+        document.addEventListener("mousemove", this.updateMousePosition);
+
+        if (this.props.instance.props.animated) {
+            await waitFor("#bruhjuhhh > video");
+            this.videoElement = this.element.querySelector("video")!;
+            this.videoElement.addEventListener("timeupdate", this.syncVidoes.bind(this));
+        }
+    }
+
+    componentWillUnmount(): void {
+        document.removeEventListener("mousemove", this.updateMousePosition);
+        this.videoElement?.removeEventListener("timeupdate", this.syncVidoes.bind(this));
+
+    }
+
+    syncVidoes(e: Event) {
+        this.currentVideoElementRef.current!.currentTime = this.videoElement.currentTime;
+    }
+
+    updateMousePosition = (e: MouseEvent) => {
+        const { instance, zoom, size } = this.props;
+        if (instance.state.mouseOver && instance.state.mouseDown) {
+            const offset = size / 2;
+            this.setLensPosition({ x: e.x - offset, y: e.y - offset });
+            const pos = { x: e.pageX, y: e.pageY };
+            const x = -((pos.x - this.element.getBoundingClientRect().left) * zoom - offset);
+            const y = -((pos.y - this.element.getBoundingClientRect().top) * zoom - offset);
+            this.setImagePosition({ x, y });
+        }
+        else this.setState({ ...this.state, opacity: 0 });
+    };
+
+    setImagePosition = (imagePosition: { x: number; y: number; }) => {
+        this.setState({ ...this.state, imagePosition });
+    };
+
+    setLensPosition = (position: { x: number; y: number; }) => {
+        this.setState({ ...this.state, opacity: 1, position: position });
+    };
+
+    state = {
+        position: { x: 0, y: 0 },
+        imagePosition: { x: 0, y: 0 },
+        opacity: 0,
+    };
+
+    render() {
+        const { size, zoom, instance: { props: { height: imageHeight, width: imageWidth, src, animated } } } = this.props;
+        const { position, opacity, imagePosition } = this.state;
+        const transformStyle = `translate(${position.x}px, ${position.y}px)`;
+        const box = document.querySelector("#bruhjuhhh")!.getBoundingClientRect();
+
+        return (
+
+            <div
+                className="lens"
+                style={{
+                    opacity,
+                    width: size + "px",
+                    height: size + "px",
+                    transform: transformStyle,
+                }}
+            >
+                {animated ?
+                    <video
+                        className="embedVideo-2ixt5A embedMedia-1mdWSP"
+                        ref={this.currentVideoElementRef}
+                        style={{
+                            position: "absolute",
+                            left: `${imagePosition.x}px`,
+                            top: `${imagePosition.y}px`
+                        }}
+                        width={`${box.width * zoom}px`}
+                        height={`${box.height * zoom}px`}
+                        poster={src}
+                        src={this.videoElement.src ?? src}
+                        autoPlay
+                        loop
+                    /> : <img
+                        ref={this.imageRef}
+                        style={{
+                            position: "absolute",
+                            left: `${imagePosition.x}px`,
+                            top: `${imagePosition.y}px`
+                        }}
+                        width={`${box.width * zoom}px`}
+                        height={`${box.height * zoom}px`}
+                        src={src} alt=""
+                    />}
+            </div>
+        );
+    }
+});
+
---
+++ src/plugins/imageUtility/index.ts
@@ -0,0 +1,109 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import "./styles.css";
+
+import definePlugin from "@utils/types";
+
+import { Magnifer } from "./components/Magnifer";
+
+
+export default definePlugin({
+    name: "ImageUtility",
+    description: "hey",
+    authors: [{
+        name: "Syncxv",
+        id: 549244932213309442n
+    },],
+    patches: [
+        {
+            find: "OPEN_IN_BROWSER",
+            replacement: {
+                match: /(return\(.{1,100}\(\)\.wrapper.{1,100})(src)/,
+                replace: "$1id: 'bruhjuhhh',$2"
+            }
+        },
+
+        {
+            find: "handleImageLoad=",
+            replacement: [
+                {
+                    match: /(render=function\(\){.{1,300}limitResponsiveWidth(.|\n){1,600})onMouseEnter:/,
+                    replace:
+                        `$1onMouseOver: () => $self.onMouseOver(this),
+                    onMouseOut: () => $self.onMouseOut(this),
+                    onMouseDown: () => $self.onMouseDown(this),
+                    onMouseUp: () => $self.onMouseUp(this),
+                    onMouseMove: () => $self.onMouseMove(this),
+                    id: this.props.id,
+                    onMouseEnter:`
+
+                },
+
+                {
+                    match: /(componentDidMount=function\(\){)/,
+                    replace: `$1
+                    if(this.props.id === 'bruhjuhhh')  {
+                        $self.what = Vencord.Webpack.Common.React.createElement($self.Magnifer, {size: 100, zoom: 2, instance: this});
+                        Vencord.Webpack.Common.ReactDOM.render($self.what, document.querySelector('.magniferContainer'))
+                    };`
+                },
+
+                {
+                    match: /(componentWillUnmount=function\(\){)/,
+                    replace: `$1
+                    if($self.what)  {
+                        Vencord.Webpack.Common.ReactDOM.unmountComponentAtNode(document.querySelector('.magniferContainer'))
+                    };`
+                }
+            ]
+        }
+    ],
+
+    Magnifer,
+
+    onMouseOver(instance) {
+        instance.setState((state: any) => ({ ...state, mouseOver: true }));
+    },
+    onMouseOut(instance) {
+        instance.setState((state: any) => ({ ...state, mouseOver: false }));
+    },
+    onMouseDown(instance) {
+        if (instance.state.mouseOver) {
+            instance.setState((state: any) => ({ ...state, mouseDown: true }));
+        }
+    },
+    onMouseUp(instance) {
+        instance.setState((state: any) => ({ ...state, mouseDown: false }));
+    },
+    onMouseMove(instance) {
+        if (instance.state.mouseOver && instance.state.mouseDown)
+            // console.log(instance.state, "HI start rendring zoom");
+            return;
+    },
+
+    start() {
+        this.element = document.createElement("div");
+        this.element.classList.add("magniferContainer");
+        document.getElementById("app-mount")!.appendChild(this.element);
+    },
+
+    stop() {
+        document.querySelector(".magniferContainer")?.remove();
+    }
+});
