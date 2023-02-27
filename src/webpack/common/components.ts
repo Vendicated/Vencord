a38c5ccf809ad571848f540a806cd07afeea32e3
Ven <vendicated@riseup.net>
2023-02-28 00:47:00 +0100

    Update components.ts

--- src/webpack/common/components.ts
+++ src/webpack/common/components.ts
@@ -46,9 +46,9 @@ export const Text = waitForComponent<t.Text>("Text", m => {
 });
 export const Select = waitForComponent<t.Select>("Select", filters.byCode("optionClassName", "popoutPosition", "autoFocus", "maxVisibleItems"));
 const searchableSelectFilter = filters.byCode("autoFocus", ".Messages.SELECT");
-export const SearchableSelect = waitForComponent<t.SearchableSelect>("SearchableSelect", m => {
-    return m.render && searchableSelectFilter(m.render);
-});
+export const SearchableSelect = waitForComponent<t.SearchableSelect>("SearchableSelect", m =>
+    m.render && searchableSelectFilter(m.render)
+);
 export const Slider = waitForComponent<t.Slider>("Slider", filters.byCode("closestMarkerIndex", "stickToMarkers"));
 export const Flex = waitForComponent<t.Flex>("Flex", ["Justify", "Align", "Wrap"]);
 
