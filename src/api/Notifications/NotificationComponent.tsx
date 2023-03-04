d1343433a3847645368457597ac425522360df4c
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2023-03-04 14:24:15 -0300

    guh

--- src/api/Notifications/NotificationComponent.tsx
+++ src/api/Notifications/NotificationComponent.tsx
@@ -90,9 +90,10 @@ export default ErrorBoundary.wrap(function NotificationComponent({
                                 width="24"
                                 height="24"
                                 viewBox="0 0 24 24"
+                                role="img"
+                                aria-labelledby="vc-notification-dismiss-title"
                             >
-								<title id="vc-notification-dismiss-title">Dismiss Notification</title>
-								<title id="vc-notification-dismiss-title">Dismiss Notification</title>
+                                <title id="vc-notification-dismiss-title">Dismiss Notification</title>
                                 <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z" />
                             </svg>
                         </button>
