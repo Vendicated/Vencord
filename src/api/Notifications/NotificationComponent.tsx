0fe4b76ccd7816be6e30810c016b6f3a072489cc
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2023-03-04 14:21:24 -0300

    Update src/api/Notifications/NotificationComponent.tsx
    Co-authored-by: Ven <vendicated@riseup.net>

--- src/api/Notifications/NotificationComponent.tsx
+++ src/api/Notifications/NotificationComponent.tsx
@@ -92,6 +92,7 @@ export default ErrorBoundary.wrap(function NotificationComponent({
                                 viewBox="0 0 24 24"
                             >
 								<title id="vc-notification-dismiss-title">Dismiss Notification</title>
+								<title id="vc-notification-dismiss-title">Dismiss Notification</title>
                                 <path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z" />
                             </svg>
                         </button>
