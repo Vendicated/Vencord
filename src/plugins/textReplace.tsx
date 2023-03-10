a54d3015b8c5fe78e9872ff138dc1a7142c6d760
AutumnVN <htuan03@gmail.com>
2023-03-10 12:23:26 +0700

    auto create new line & split string - regex

--- src/plugins/textReplace.tsx
+++ src/plugins/textReplace.tsx
@@ -24,7 +24,20 @@ import { useForceUpdater } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
 import { Button, Forms, TextInput, useState } from "@webpack/common";
 
-let rules = [] as any;
+let rulesString = [
+    {
+        find: "",
+        replace: "",
+        onlyIfIncludes: ""
+    }
+] as any;
+
+let rulesRegex = [
+    {
+        find: "",
+        replace: ""
+    }
+] as any;
 
 const settings = definePluginSettings({
     replace: {
@@ -32,123 +45,141 @@ const settings = definePluginSettings({
         description: "",
         component: () =>
             <>
-                <TextReplace />
+                <TextReplaceString />
+                <TextReplaceRegex />
             </>
     },
 });
 
-const TextReplace = () => {
-    const [find, setFind] = useState("");
-    const [replace, setReplace] = useState("");
-    const [onlyIfIncludes, setOnlyIfIncludes] = useState("");
-    const [error, setError] = useState("");
+const TextReplaceString = () => {
     const update = useForceUpdater();
 
-    async function onClickAdd() {
-        if (!find || !replace) return setError("Find and Replace must not be empty");
-        rules.push({
-            find,
-            replace,
-            onlyIfIncludes
-        });
-        await DataStore.set("TextReplace_rules", rules);
-        setFind("");
-        setReplace("");
-        setOnlyIfIncludes("");
+    async function onClickRemoveString(index: number) {
+        if (index === rulesString.length - 1) return;
+        rulesString.splice(index, 1);
+        await DataStore.set("TextReplace_rulesString", rulesString);
+        update();
     }
 
-    async function onClickRemove(index: number) {
-        rules.splice(index, 1);
-        await DataStore.set("TextReplace_rules", rules);
+    async function onChangeString(e: string, index: number, key: string) {
+        if (index === rulesString.length - 1) {
+            rulesString.push({
+                find: "",
+                replace: "",
+                onlyIfIncludes: ""
+            });
+        }
+        rulesString[index][key] = e;
+        await DataStore.set("TextReplace_rulesString", rulesString);
         update();
     }
 
     return (
         <>
-            <table>
-                <tr>
-                    <td>
-                        <TextInput
-                            value={find}
-                            onChange={
-                                e => {
-                                    setFind(e);
-                                    setError("");
-                                }
-                            }
-                            placeholder="Find"
-                            spellCheck={false}
-                        />
-                    </td>
-                    <td>
-                        <TextInput
-                            value={replace}
-                            onChange={
-                                e => {
-                                    setReplace(e);
-                                    setError("");
-                                }
-                            }
-                            placeholder="Replace"
-                            spellCheck={false}
-                        />
-                    </td>
-                    <td>
-                        <TextInput
-                            value={onlyIfIncludes}
-                            onChange={setOnlyIfIncludes}
-                            placeholder="Only if includes"
-                            spellCheck={false}
-                        />
-                    </td>
-                    <Button
-                        size={Button.Sizes.MIN}
-                        onClick={onClickAdd}
-                        style={{
-                            background: "none",
-                            bottom: "-7px",
-                            marginLeft: "3px"
-                        }}
-                    >
-                        <svg fill="#43b581" width="26px" height="26px" viewBox="2.7 3.2 26.5 27" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)">
-                            <path d="M15.5 29.5c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13zM21.938 15.938c0-0.552-0.448-1-1-1h-4v-4c0-0.552-0.447-1-1-1h-1c-0.553 0-1 0.448-1 1v4h-4c-0.553 0-1 0.448-1 1v1c0 0.553 0.447 1 1 1h4v4c0 0.553 0.447 1 1 1h1c0.553 0 1-0.447 1-1v-4h4c0.552 0 1-0.447 1-1v-1z" />
-                        </svg>
-                    </Button>
-                </tr>
-            </table>
-            <Forms.FormText type={Forms.FormText.Types.ERROR}>{error}</Forms.FormText>
+            <h2 style={{
+                color: "#fff"
+            }}>Using string</h2>
             <table>
                 {
-                    rules.map((rule: any, index: number) =>
+                    rulesString?.map((rule: any, index: number) =>
                         <tr>
                             <td>
                                 <TextInput
+                                    placeholder="Find"
                                     value={rule.find}
-                                    disabled={true}
+                                    onChange={e => onChangeString(e, index, "find")}
                                     spellCheck={false}
                                 />
                             </td>
                             <td>
                                 <TextInput
+                                    placeholder="Replace"
                                     value={rule.replace}
-                                    disabled={true}
+                                    onChange={e => onChangeString(e, index, "replace")}
                                     spellCheck={false}
                                 />
                             </td>
                             <td>
                                 <TextInput
+                                    placeholder="Only if includes"
                                     value={rule.onlyIfIncludes}
-                                    disabled={true}
+                                    onChange={e => onChangeString(e, index, "onlyIfIncludes")}
                                     spellCheck={false}
                                 />
                             </td>
                             <Button
                                 size={Button.Sizes.MIN}
-                                onClick={() => onClickRemove(index)}
+                                onClick={() => onClickRemoveString(index)}
                                 style={{
                                     background: "none",
-                                    bottom: "-7px",
-                                    marginLeft: "3px"
+                                    bottom: "-7px"
+                                }}
+                            >
+                                <svg fill="#f04747" width="26px" height="26px" viewBox="2.7 3.2 26.5 27" xmlns="http://www.w3.org/2000/svg" transform="rotate(45)">
+                                    <path d="M15.5 29.5c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13zM21.938 15.938c0-0.552-0.448-1-1-1h-4v-4c0-0.552-0.447-1-1-1h-1c-0.553 0-1 0.448-1 1v4h-4c-0.553 0-1 0.448-1 1v1c0 0.553 0.447 1 1 1h4v4c0 0.553 0.447 1 1 1h1c0.553 0 1-0.447 1-1v-4h4c0.552 0 1-0.447 1-1v-1z" />
+                                </svg>
+                            </Button>
+                        </tr>
+                    )
+                }
+            </table>
+        </>
+    );
+};
+
+const TextReplaceRegex = () => {
+    const update = useForceUpdater();
+
+    async function onClickRemoveRegex(index: number) {
+        if (index === rulesRegex.length - 1) return;
+        rulesRegex.splice(index, 1);
+        await DataStore.set("TextReplace_rulesRegex", rulesRegex);
+        update();
+    }
+
+    async function onChangeRegex(e: string, index: number, key: string) {
+        if (index === rulesRegex.length - 1) {
+            rulesRegex.push({
+                find: "",
+                replace: ""
+            });
+        }
+        rulesRegex[index][key] = e;
+        await DataStore.set("TextReplace_rulesRegex", rulesRegex);
+        update();
+    }
+
+    return (
+        <>
+            <h2 style={{
+                color: "#fff"
+            }}>Using regex</h2>
+            <table>
+                {
+                    rulesRegex?.map((rule: any, index: number) =>
+                        <tr>
+                            <td>
+                                <TextInput
+                                    placeholder="Find"
+                                    value={rule.find}
+                                    onChange={e => onChangeRegex(e, index, "find")}
+                                    spellCheck={false}
+                                />
+                            </td>
+                            <td>
+                                <TextInput
+                                    placeholder="Replace"
+                                    value={rule.replace}
+                                    onChange={e => onChangeRegex(e, index, "replace")}
+                                    spellCheck={false}
+                                />
+                            </td>
+                            <Button
+                                size={Button.Sizes.MIN}
+                                onClick={() => onClickRemoveRegex(index)}
+                                style={{
+                                    background: "none",
+                                    bottom: "-7px"
                                 }}
                             >
                                 <svg fill="#f04747" width="26px" height="26px" viewBox="2.7 3.2 26.5 27" xmlns="http://www.w3.org/2000/svg" transform="rotate(45)">
@@ -171,28 +202,56 @@ export default definePlugin({
 
     settings,
 
-    escapeRegExp(str: string) {
+    escapeRegex(str: string) {
         return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     },
 
+    stringToRegex(str: string) {
+        const m = str.match(/^([/~@;%#'])(.*?)\1([gimsuy]*)$/);
+        return m ? new RegExp(m[2], m[3]) : new RegExp(str);
+    },
+
     async start() {
-        rules = await DataStore.get("TextReplace_rules");
-        this.preSend = addPreSendListener((_, msg) => {
-            if (!rules) return;
-            for (const rule of rules) {
-                if (rule.onlyIfIncludes && !msg.content.includes(rule.onlyIfIncludes) && rule.onlyIfIncludes !== "regex") continue;
-                if (rule.onlyIfIncludes === "regex") {
-                    const regex = new RegExp(rule.find, "g");
-                    if (!regex.test(msg.content)) continue;
-                    msg.content = msg.content.replace(regex, rule.replace);
-                    continue;
+        rulesString = await DataStore.get("TextReplace_rulesString");
+        if (!rulesString) {
+            rulesString = [
+                {
+                    find: "",
+                    replace: "",
+                    onlyIfIncludes: ""
                 }
-                msg.content = msg.content.replace(new RegExp(this.escapeRegExp(rule.find), "g"), rule.replace);
+            ];
+            await DataStore.set("TextReplace_rulesString", rulesString);
+        }
+        rulesRegex = await DataStore.get("TextReplace_rulesRegex");
+        if (!rulesRegex) {
+            rulesRegex = [
+                {
+                    find: "",
+                    replace: ""
+                }
+            ];
+            await DataStore.set("TextReplace_rulesRegex", rulesRegex);
+        }
+        this.preSend = addPreSendListener((_, msg) => {
+            for (const rule of rulesString) {
+                if (!rule.find || !rule.replace) continue;
+                if (rule.onlyIfIncludes && !msg.content.includes(rule.onlyIfIncludes)) continue;
+                msg.content = msg.content.replace(new RegExp(this.escapeRegex(rule.find), "g"), rule.replace);
+            }
+            for (const rule of rulesRegex) {
+                if (!rule.find || !rule.replace) continue;
+                try {
+                    const regex = this.stringToRegex(rule.find);
+                    msg.content = msg.content.replace(regex, rule.replace);
+                } catch (e) {
+                    console.error(e);
+                }
             }
         });
     },
 
-    stop() {
+    async stop() {
         removePreSendListener(this.preSend);
     }
 });
