6f0f381c57e157137a3ce3c552ddbeef0147b27e
Damien Fajole <loupegame@gmail.com>
2023-03-14 11:21:19 +0100

    Fix some lint issues

--- src/plugins/voiceChatUtilities.tsx
+++ src/plugins/voiceChatUtilities.tsx
@@ -1,9 +1,28 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 import { addContextMenuPatch, findGroupChildrenByChildId, NavContextMenuPatchCallback, removeContextMenuPatch } from "@api/ContextMenu";
+import { definePluginSettings } from "@api/settings";
 import { WEBPACK_CHUNK } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { UserStore, Menu } from "@webpack/common";
 import { findByProps, findByPropsLazy } from "@webpack";
-import { definePluginSettings } from "@api/settings";
+import { Menu, UserStore } from "@webpack/common";
 
 
 var userToken;
@@ -13,7 +32,7 @@ function getUserToken() {
         return userToken;
     } else {
         let m;
-        let token = (window[WEBPACK_CHUNK].push([[''], {}, e => { m = []; for (let c in e.c) m.push(e.c[c]); }]), m).find(m => m?.exports?.default?.getToken !== void 0).exports.default.getToken();
+        const token = (window[WEBPACK_CHUNK].push([[""], {}, e => { m = []; for (const c in e.c) m.push(e.c[c]); }]), m).find(m => m?.exports?.default?.getToken !== void 0).exports.default.getToken();
         if (token) {
             userToken = token;
             return token;
@@ -38,31 +57,33 @@ function sendPatch(channel, body, bypass = false) {
     var usersVoice = VoiceStateStore.getVoiceStatesForChannel(channel); // Get voice states by channel id
     const myId = UserStore.getCurrentUser().id; // Get my user id
 
-    if (settings.store.delay == 0) {
+    if (settings.store.delay === 0) {
         usersVoice.forEach(userVoice => {
-            if (bypass || userVoice.member.userId != myId) {
+            if (bypass || userVoice.member.userId !== myId) {
                 var patchPayload = {
                     url: "https://discord.com/api/v9/guilds/" + userVoice.member.guildId + "/members/" + userVoice.member.userId,
                     headers: {
                         authorization: getUserToken(),
-                    }
+                    },
+                    body: {}
                 };
-                patchPayload["body"] = body;
+                patchPayload.body = body;
 
                 patch.patch(patchPayload);
             }
         });
     } else {
         usersVoice.forEach((userVoice, index) => {
-            if (bypass || userVoice.member.userId != myId) {
+            if (bypass || userVoice.member.userId !== myId) {
                 setTimeout(() => {
                     var patchPayload = {
                         url: "https://discord.com/api/v9/guilds/" + userVoice.member.guildId + "/members/" + userVoice.member.userId,
                         headers: {
                             authorization: getUserToken(),
-                        }
+                        },
+                        body: {}
                     };
-                    patchPayload["body"] = body;
+                    patchPayload.body = body;
 
                     patch.patch(patchPayload);
                 }, index * settings.store.delay);
@@ -141,7 +162,7 @@ const voiceChannelContextMenuPatch: NavContextMenuPatchCallback = (children, arg
                     id="voice-tools-move-all"
                 >
 
-                    {voiceChannels.map((channel) => {
+                    {voiceChannels.map(channel => {
                         return (
                             <Menu.MenuItem
                                 key={channel.id}
@@ -188,4 +209,4 @@ export default definePlugin({
     stop() {
         removeContextMenuPatch("channel-context", voiceChannelContextMenuPatch);
     },
-});;;
+});
