db5917f5a79205868cebb7a8f45b2bfc8dacbe45
Syncxv <aruldevarackal1@gmail.com>
2023-03-18 18:40:15 +1100

    use Context Menu API

--- src/plugins/searchReply.tsx
+++ src/plugins/searchReply.tsx
@@ -16,12 +16,13 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { addContextMenuPatch, findGroupChildrenByChildId, NavContextMenuPatchCallback, removeContextMenuPatch } from "@api/ContextMenu";
 import { Devs } from "@utils/constants";
 import { LazyComponent } from "@utils/misc";
 import definePlugin from "@utils/types";
 import { findByCode, findByCodeLazy, findLazy } from "@webpack";
 import { ChannelStore, Menu, SelectedChannelStore } from "@webpack/common";
-import type { Message } from "discord-types/general";
+import { Message } from "discord-types/general";
 
 const ReplyIcon = LazyComponent(() => findByCode("M10 8.26667V4L3 11.4667L10 18.9333V14.56C15 14.56 18.5 16.2667 21 20C20 14.6667 17 9.33333 10 8.26667Z"));
 
@@ -30,35 +31,43 @@ const i18n = findLazy(m => m.Messages?.MESSAGE_ACTION_REPLY);
 
 const replyFn = findByCodeLazy("showMentionToggle", "TEXTAREA_FOCUS", "shiftKey");
 
-export default definePlugin({
-    name: "SearchReply",
-    description: "Adds a reply button to search results",
-    authors: [Devs.Aria],
-    patches: [
-        {
-            find: 'navId:"message",onClose:',
-            replacement: {
-                match: /(function \i\(\i\){.{1,100}message.{1,100}onSelect(.|\n){1,1000}{children:)\[(?<items>.{1,50})\]/,
-                replace: "$1[$self.makeMenu(arguments[0]),$<items>]"
-            }
-        }
-    ],
+const messageContextMenuPatch: NavContextMenuPatchCallback = (children, args) => {
+    if (!args?.[0]) return;
+    const [{ message }] = args as [{ message: Message; }];
 
+    if (!message || SelectedChannelStore.getChannelId() !== (message.channel_id ?? message.getChannelId())) return;
 
-    makeMenu({ message }: { message: Message; }) {
-        if (!message || SelectedChannelStore.getChannelId() !== (message.channel_id ?? message.getChannelId())) return;
+    const channel = ChannelStore.getChannel(message.channel_id ?? message.getChannelId());
 
-        const channel = ChannelStore.getChannel(message.channel_id ?? message.getChannelId());
+    if (!channel) return;
 
-        if (!channel) return;
-        return (
+    const group = findGroupChildrenByChildId("pin", children);
+    if (group && !group.some(child => child?.props?.id === "reply")) {
+        const pinIndex = group.findIndex(c => c.props.id === "pin");
+        group.splice(pinIndex + 1, 0, (
             <Menu.MenuItem
                 id="reply"
                 label={i18n.Messages.MESSAGE_ACTION_REPLY}
                 icon={ReplyIcon}
                 action={(e: React.MouseEvent) => replyFn(channel, message, e)}
             />
-        );
+        ));
+    }
+};
 
+
+export default definePlugin({
+    name: "SearchReply",
+    description: "Adds a reply button to search results",
+    authors: [Devs.Aria],
+
+    start() {
+        addContextMenuPatch("message", messageContextMenuPatch);
+    },
+
+    stop() {
+        removeContextMenuPatch("message", messageContextMenuPatch);
     }
 });
+
+
