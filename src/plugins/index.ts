f9537078bba19dd2901d76303e2b079f3afa911c
Lewis Crichton <contact@swezy.dev>
2023-03-25 15:20:00 +0000

    fix API plugins being force enabled unconditionally (#704)
    * only enable dependencies if required
    
    * fixme note

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -43,8 +43,11 @@ export function isPluginEnabled(p: string) {
 
 const pluginsValues = Object.values(Plugins);
 
-// First roundtrip to mark and force enable dependencies
-for (const p of pluginsValues) {
+// First roundtrip to mark and force enable dependencies (only for enabled plugins)
+//
+// FIXME: might need to revisit this if there's ever nested (dependencies of dependencies) dependencies since this only
+// goes for the top level and their children, but for now this works okay with the current API plugins
+for (const p of pluginsValues) if (settings[p.name]?.enabled) {
     p.dependencies?.forEach(d => {
         const dep = Plugins[d];
         if (dep) {
