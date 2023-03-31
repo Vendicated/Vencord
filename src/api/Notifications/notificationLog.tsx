70a41a5daf7d31884eee89303f44c576234067c0
Vendicated <vendicated@riseup.net>
2023-03-31 19:42:29 +0200

    Add clear all button

--- src/api/Notifications/notificationLog.tsx
+++ src/api/Notifications/notificationLog.tsx
@@ -18,9 +18,10 @@
 
 import * as DataStore from "@api/DataStore";
 import { Settings } from "@api/settings";
+import { classNameFactory } from "@api/Styles";
 import { useAwaiter } from "@utils/misc";
-import { closeModal, ModalCloseButton, ModalContent, ModalHeader, ModalRoot, ModalSize, openModal } from "@utils/modal";
-import { Forms, moment, Text, Timestamp, useEffect, useReducer } from "@webpack/common";
+import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
+import { Alerts, Button, Forms, moment, Text, Timestamp, useEffect, useReducer } from "@webpack/common";
 import type { DispatchWithoutAction } from "react";
 
 import NotificationComponent from "./NotificationComponent";
@@ -37,6 +38,7 @@ const getLog = async () => {
     return log ?? [];
 };
 
+const cl = classNameFactory("vc-notification-log-");
 const signals = new Set<DispatchWithoutAction>();
 
 export async function persistNotification(notification: NotificationData) {
@@ -71,7 +73,7 @@ export async function deleteNotification(timestamp: number) {
     signals.forEach(x => x());
 }
 
-export function NotificationLog() {
+export function useLogs() {
     const [signal, setSignal] = useReducer(x => x + 1, 0);
 
     useEffect(() => {
@@ -84,13 +86,17 @@ export function NotificationLog() {
         deps: [signal]
     });
 
+    return [log, pending] as const;
+}
+
+export function NotificationLog({ log, pending }: { log: PersistentNotificationData[], pending: boolean; }) {
     if (pending)
         return <Forms.FormText>Loading Notification log...</Forms.FormText>;
 
     if (!log.length)
         return (
-            <div>
-                <div className="vc-notification-log-empty" />
+            <div className={cl("container")}>
+                <div className={cl("empty")} />
                 <Forms.FormText style={{ textAlign: "center" }}>
                     No notifications yet
                 </Forms.FormText>
@@ -98,18 +104,18 @@ export function NotificationLog() {
         );
 
     return (
-        <div className="vc-notification-log-container">
+        <div className={cl("container")}>
             {log.map(n => (
                 <NotificationComponent
                     {...n}
                     key={n.timestamp}
                     onClose={() => deleteNotification(n.timestamp)}
                     permanent={true}
-                    className="vc-notification-log-entry"
+                    className={cl("entry")}
                     richBody={
-                        <div className="vc-notification-log-body">
+                        <div className={cl("body")}>
                             {n.body}
-                            <Timestamp timestamp={moment(n.timestamp)} className="vc-notification-log-timestamp" />
+                            <Timestamp timestamp={moment(n.timestamp)} className={cl("timestamp")} />
                         </div>
                     }
                 />
@@ -118,17 +124,49 @@ export function NotificationLog() {
     );
 }
 
-export function openNotificationLogModal() {
-    const key = openModal(modalProps => (
+function LogModal({ modalProps, close }: { modalProps: ModalProps; close(): void; }) {
+    const [log, pending] = useLogs();
+
+    return (
         <ModalRoot {...modalProps} size={ModalSize.LARGE}>
             <ModalHeader>
                 <Text variant="heading-lg/semibold" style={{ flexGrow: 1 }}>Notification Log</Text>
-                <ModalCloseButton onClick={() => closeModal(key)} />
+                <ModalCloseButton onClick={close} />
             </ModalHeader>
 
             <ModalContent>
-                <NotificationLog />
+                <NotificationLog log={log} pending={pending} />
             </ModalContent>
+
+            <ModalFooter>
+                <Button
+                    disabled={log.length === 0}
+                    onClick={() => {
+                        Alerts.show({
+                            title: "Are you sure?",
+                            body: `This will permanently remove ${log.length} notification${log.length === 1 ? "" : "s"}. This action cannot be undone.`,
+                            async onConfirm() {
+                                await DataStore.set(KEY, []);
+                                signals.forEach(x => x());
+                            },
+                            confirmText: "Do it!",
+                            confirmColor: "vc-notification-log-danger-btn",
+                            cancelText: "Nevermind"
+                        });
+                    }}
+                >
+                    Clear Notification Log
+                </Button>
+            </ModalFooter>
         </ModalRoot>
+    );
+}
+
+export function openNotificationLogModal() {
+    const key = openModal(modalProps => (
+        <LogModal
+            modalProps={modalProps}
+            close={() => closeModal(key)}
+        />
     ));
 }
