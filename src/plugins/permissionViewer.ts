0c0bd9cd48352abb513491f38e0bc517b9b249a7
Peyton <peyton2465@outlook.com>
2023-04-05 01:59:33 -0500

    Prevent network errors

--- src/plugins/permissionViewer.ts
+++ src/plugins/permissionViewer.ts
@@ -36,12 +36,34 @@ export default definePlugin({
             find: "canAccessGuildSettings=function",
             replacement: [
                 {
-                    match: /;([A-z]+?)\.canAccessGuildSettings=function\(.+?\){.*?return.+?};/,
-                    replace: ";$1.canAccessGuildSettings=function(){return true;};"
+                    match: /(;[A-z]+?)\.canAccessGuildSettings=function\(.+?\){.*?return.+?};/,
+                    replace: "$1.canAccessGuildSettings=()=>true;"
                 },
                 {
-                    match: /;([A-z])+\.can=function\((.+?),(.+?)\)({var.+?[A-z]+?\(.+?,\1,.+?\);return .+?\(.*?\)});/g,
-                    replace: ";$1.can=function($2, $3) { if($2 == 0x0000000010000000) { return true; } else $4 };"
+                    match: /;([A-z])+\.can=function\((.+?),(.+?),(.+?)\)({var.+?[A-z]+?\(.+?,\1,.+?\);return .+?\(.*?\)});/g,
+                    replace: ";$1.can=function($2, $3, $4) { const hasPerm = (() => $5)(); if($2 == 0x0000000010000000) { window.unownedGuilds = window.unownedGuilds || {}; if (!hasPerm && window.unownedGuilds[$3.id] == null) { window.unownedGuilds[$3.id] = true; } else if (hasPerm) { window.unownedGuilds[$3.id] = null; } return true; } return hasPerm; };"
+                }
+            ]
+        },
+        {
+            find: "include_role_connections_metadata",
+            replacement: [
+                {
+                    match: /(function [A-z]+?\(([A-z]+?)\)\{)(.+?get.+?GUILD_INTEGRATIONS.+?\})/,
+                    replace: "$1 if (window.unownedGuilds != null && window.unownedGuilds[$2] == true) { return; } $3"
+                },
+                {
+                    match: /(\}function [A-z]+?\(([A-z]+?)\){)(.+\n?.+?GUILD_INTEGRATIONS.+?has_commands)/,
+                    replace: "$1 if (window.unownedGuilds != null && window.unownedGuilds[$2] == true) { return; } $3"
+                }
+            ]
+        },
+        {
+            find: "permissions.toString()",
+            replacement: [
+                {
+                    match: /(:([A-z]).id]\);[A-z]\.useEffect\(\(function\(\)\{)(null.+?==\2\?void 0:)/,
+                    replace: "$1 if (window.unownedGuilds != null && window.unownedGuilds[$2.id] == true) { return; } $3"
                 }
             ]
         }
