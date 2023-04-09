86f2a882962bac4ab19a2432037d3dddd199f8e8
ActuallyTheSun <78964224+ActuallyTheSun@users.noreply.github.com>
2023-04-09 20:26:50 +0300

    this was surprisingly easy to fix

--- src/plugins/timedLightTheme.tsx
+++ src/plugins/timedLightTheme.tsx
@@ -19,21 +19,39 @@
 import { definePluginSettings } from "@api/settings";
 import { Devs } from "@utils/constants.js";
 import definePlugin, { OptionType } from "@utils/types";
-import { findByPropsLazy, findLazy } from "@webpack";
-import { Toasts } from "@webpack/common";
+import { findByCodeLazy, findLazy } from "@webpack";
+import { FluxDispatcher, Forms, Toasts, UserStore } from "@webpack/common";
+
+// the plugin uses -1 as a fallback for no client theme. discord doesn't
+interface PreferredTheme {
+    light: number;
+    dark: number;
+}
+
+let nextChange: NodeJS.Timeout;
 
 const PreloadedUserSettings = findLazy(m => m.ProtoClass?.typeName?.includes("PreloadedUserSettings"));
-const getTheme = () => PreloadedUserSettings.getCurrentValue().appearance.theme === 1 ? "dark" : "light";
-const updateTheme: { updateTheme: (theme: "light" | "dark") => Promise<void>; } = findByPropsLazy("updateTheme");
-function updateThemeIfNecessary(theme: "light" | "dark") {
-    const currentTheme = getTheme();
+const Themes: Record<number, string> = findLazy(m => m[16] === "EASTER_EGG");
+const updateTheme: (gah: { theme: "light" | "dark", backgroundGradientPresetId?: number; }) => Promise<void> = findByCodeLazy("clientThemeSettings:{");
+
+const getBasicTheme = () =>
+    PreloadedUserSettings.getCurrentValue().appearance.theme === 1 ? "dark" : "light";
+const getClientThemeId = () =>
+    PreloadedUserSettings.getCurrentValue().appearance.clientThemeSettings?.backgroundGradientPresetId?.value as number ?? -1;
+const canUseClientThemes = () =>
+    Vencord.Plugins.isPluginEnabled("FakeNitro") || (UserStore.getCurrentUser().premiumType ?? 0) === 2;
+function updateThemeIfNecessary(theme: string) {
+    const currentTheme = getBasicTheme();
+    const themeId = settings.store.preferredTheme[theme];
     if (
         (theme === "light" && currentTheme === "dark")
         || (theme === "dark" && currentTheme === "light")
-    ) updateTheme.updateTheme(theme);
+    ) updateTheme({
+        theme,
+        backgroundGradientPresetId: canUseClientThemes() ? themeId === -1 ? undefined : themeId : undefined
+    });
 }
-let nextChange: NodeJS.Timeout;
-
+// hh:mm to unix timestamp
 function toAdjustedTimestamp(t: string): number {
     const [hours, minutes] = t.split(":").map(i => i && parseInt(i, 10));
     return new Date().setHours(hours as number, minutes || 0, 0, 0);
@@ -44,10 +62,28 @@ function showError() {
         id: Toasts.genId(),
         type: Toasts.Type.FAILURE,
         options: {
-            duration: 1500,
+            duration: 3000,
             position: Toasts.Position.BOTTOM
         }
     });
+}
+const snakeToTitleCase = (text: string) =>
+    text.split("_").map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(" ");
+
+function DisplayThemeComponent() {
+    const { light, dark }: PreferredTheme = settings.store.preferredTheme;
+    return <>
+        <Forms.FormTitle>Preferred Themes</Forms.FormTitle>
+        {canUseClientThemes()
+            ? <Forms.FormText variant="text-md/bold">
+                Light: {snakeToTitleCase(light === -1 ? "default" : Themes[light])} -
+                Dark: {snakeToTitleCase(dark === -1 ? "default" : Themes[dark])}
+            </Forms.FormText>
+            : <Forms.FormText variant="text-md/bold">
+                Can't use nitro client themes. Restart Discord if this is wrong
+            </Forms.FormText>
+        }
+    </>;
 }
 
 const settings = definePluginSettings({
@@ -67,6 +103,11 @@ const settings = definePluginSettings({
         isValid: t => /^\d{0,2}(?::\d{0,2})?$/.test(t),
         onChange: () => Vencord.Plugins.isPluginEnabled("TimedLightTheme") && (Vencord.Plugins.plugins.TimedLightTheme as any).checkForUpdate(),
     },
+    preferredTheme: {
+        description: "Store which theme to use for light and dark mode",
+        type: OptionType.COMPONENT,
+        component: DisplayThemeComponent
+    },
 });
 
 export default definePlugin({
@@ -77,16 +118,24 @@ export default definePlugin({
 
     checkForUpdate() {
         const { start, end } = settings.store;
-        if (!start || !end) {
+        if (!start || !end)
             return showError();
-        }
 
         const startTimestamp = toAdjustedTimestamp(start);
         const endTimestamp = toAdjustedTimestamp(end);
-        if (startTimestamp >= endTimestamp) {
+        if (startTimestamp >= endTimestamp)
             return showError();
-        }
+
         const now = Date.now();
+
+        const themeId = getClientThemeId();
+        if (!settings.store.preferredTheme) {
+            const theme = getBasicTheme();
+            settings.store.preferredTheme = {
+                light: theme === "light" ? themeId : -1,
+                dark: theme === "dark" ? themeId : -1
+            };
+        }
 
         if (now < startTimestamp) {
             updateThemeIfNecessary("dark");
@@ -102,11 +151,24 @@ export default definePlugin({
         }
     },
 
+    updatePreferredThemes(e) {
+        const newTheme = e?.changes?.appearance?.settings as {
+            theme?: "light" | "dark", // undefined means system theme
+            clientThemeSettings: {
+                backgroundGradientPresetId?: number;
+            };
+        };
+        if (!newTheme?.theme) return;
+        settings.store.preferredTheme[newTheme.theme] = newTheme.clientThemeSettings.backgroundGradientPresetId ?? -1;
+    },
+
     start() {
+        FluxDispatcher.subscribe("SELECTIVELY_SYNCED_USER_SETTINGS_UPDATE", this.updatePreferredThemes);
         this.checkForUpdate();
     },
 
     stop() {
+        FluxDispatcher.unsubscribe("SELECTIVELY_SYNCED_USER_SETTINGS_UPDATE", this.updatePreferredThemes);
         clearTimeout(nextChange);
     },
 });
