4adeaa3e1168e1b055c8a8fa3144287fbc91911d
TheKodeToad <TheKodeToad@proton.me>
2023-04-10 21:52:11 +0100

    Remove when not in use

--- src/plugins/lockServerLeave.tsx
+++ src/plugins/lockServerLeave.tsx
@@ -22,11 +22,14 @@ import definePlugin from "@utils/types";
 import { Menu } from "@webpack/common";
 import * as DataStore from "@api/DataStore";
 
-let locks: string[];
+let locks: string[] | null;
 const DATA_KEY = "LockServerLeave_SERVERS";
 
 function guildMenuPatch(popout: boolean): NavContextMenuPatchCallback {
     return (children, props) => {
+        if (locks === null)
+            return;
+
         const target = popout ? "leave" : "leave-guild";
         const group = findGroupChildrenByChildId(target, children);
 
@@ -44,6 +47,9 @@ function guildMenuPatch(popout: boolean): NavContextMenuPatchCallback {
                     label={locks.includes(id) ? "Unlock Leave Server" : "Lock Leave Server"}
                     action={
                         () => {
+                            if (locks === null)
+                                return;
+
                             if (locks.includes(id))
                                 locks.splice(locks.indexOf(id), 1);
                             else
@@ -68,13 +74,12 @@ export default definePlugin({
     dependencies: ["ContextMenuAPI"],
 
     async start() {
-        if (!locks)
-            locks = await DataStore.get(DATA_KEY) ?? [];
-
+        locks = await DataStore.get(DATA_KEY) ?? [];
         addContextMenuPatch("guild-context", guildContextMenuPatch);
         addContextMenuPatch("guild-header-popout", guildPopoutPatch);
     },
     stop() {
+        locks = null;
         removeContextMenuPatch("guild-context", guildContextMenuPatch);
         removeContextMenuPatch("guild-header-popout", guildPopoutPatch);
     }
