d80a57eca4ddfd7afaaa6c4b0584248aabba3439
Vendicated <vendicated@riseup.net>
2023-04-13 04:00:02 +0200

    Add missing dependency

--- src/plugins/pinDms/index.tsx
+++ src/plugins/pinDms/index.tsx
@@ -28,6 +28,8 @@ export default definePlugin({
     description: "Allows you to pin private channels to the top of your DM list. To pin/unpin or reorder pins, right click DMs",
     authors: [Devs.Ven, Devs.Strencher],
 
+    dependencies: ["ContextMenuAPI"],
+
     start: addContextMenus,
     stop: removeContextMenus,
 
