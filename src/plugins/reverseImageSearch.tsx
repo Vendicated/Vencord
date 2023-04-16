6fd5c7874f87707259b0226e671b85ae43bfa8e3
Vendicated <vendicated@riseup.net>
2023-04-17 01:39:16 +0200

    Remove ContextMenuAPIs from plugin dependencies (now required)

--- src/plugins/reverseImageSearch.tsx
+++ src/plugins/reverseImageSearch.tsx
@@ -76,7 +76,6 @@ export default definePlugin({
     name: "ReverseImageSearch",
     description: "Adds ImageSearch to image context menus",
     authors: [Devs.Ven, Devs.Nuckyz],
-    dependencies: ["ContextMenuAPI"],
     patches: [
         {
             find: ".Messages.MESSAGE_ACTIONS_MENU_LABEL",
