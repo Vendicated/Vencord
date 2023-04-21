e00a0dec65626783806cee18bc0c55dada0d9b74
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-04-21 22:09:04 +0300

    Revolutionary change

--- src/plugins/reviewDB/index.tsx
+++ src/plugins/reviewDB/index.tsx
@@ -79,10 +79,9 @@ export default definePlugin({
             const user = await getCurrentUserInfo(settings.token);
             if (settings.lastReviewId < user.lastReviewID) {
                 settings.lastReviewId = user.lastReviewID;
-                settings.userType = user.type;
-
                 user.lastReviewID !== 0 && showToast("You have new reviews on your profile!");
             }
+            settings.userType = user.type;
         }, 4000);
     },
 
