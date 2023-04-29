56dac0e6f21c3e1c7761605ffae5a7b4121a6b22
AutumnVN <htuan03@gmail.com>
2023-04-29 11:27:42 +0700

    change regex

--- src/plugins/textReplace.tsx
+++ src/plugins/textReplace.tsx
@@ -68,7 +68,7 @@ const settings = definePluginSettings({
 });
 
 function stringToRegex(str: string) {
-    const match = str.match(/^([/~@;%#'])(.*?)\1([gimsuy]*)$/); // Regex to match regex
+    const match = str.match(/^(\/)(.*?)\1([gimsuy]*)$/); // Regex to match regex
     return match
         ? new RegExp(
             match[2], // Pattern
