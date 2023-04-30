65fa79990f742ec7c44c01568e044ccac8f26ecb
Nuckyz <contact@swezy.dev>
2023-04-30 10:34:38 -0300

    Fix Plugin Reporting false dependency errors and other stuff (#1017)

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -130,7 +130,7 @@ async function printReport() {
                     },
                     {
                         title: "Discord Errors",
-                        description: toCodeBlock(report.otherErrors.join("\n")),
+                        description: report.otherErrors.length ? toCodeBlock(report.otherErrors.join("\n")) : "None",
                         color: report.otherErrors.length ? 0xff0000 : 0x00ff00
                     }
                 ]
@@ -233,7 +233,7 @@ function runTime(token: string) {
             // Needs native server to run
             if (p.name === "WebRichPresence (arRPC)") return;
 
-            p.required = true;
+            Vencord.Settings.plugins[p.name].enabled = true;
             p.patches?.forEach(patch => {
                 patch.plugin = p.name;
                 delete patch.predicate;
