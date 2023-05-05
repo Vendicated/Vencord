a16816d463cd8814787eceb1969f89b2e6007a0b
Xslash <kapimak28@gmail.com>
2023-05-05 14:43:08 +0200

    ArjixWasTaken patches

--- src/plugins/7tvemotes/index.tsx
+++ src/plugins/7tvemotes/index.tsx
@@ -179,47 +179,57 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "7TV Emotes",
     description: "Search for 7TV Emotes in your Discord Client!",
-    authors: [Devs.Xslash],
+    authors: [Devs.Xslash, Devs.Arjix],
     dependencies: [],
 
     patches: [
         {
-            find: ".activeCommandOption",
-            replacement: {
-                match: /(.)\.push.{1,30}disabled:(\i),.{1,20}\},"emoji"\)\)/,
-                replace: "$&;try{$2||$1.push($self.chatBarIcon())}catch{}",
-            }
-        },
-        {
             find: ".Messages.EXPRESSION_PICKER_GIF",
-            replacement: {
-                match: /role:"tablist",.{10,20}\.Messages\.EXPRESSION_PICKER_CATEGORIES_A11Y_LABEL,children:(\[.*?\)\]}\)}\):null,)(.*?closePopout:\w.*?:null)/,
-                replace: m => {
-                    const stickerTabRegex = /(\w)\?(\(.+?\))\((\w{1,2}),.*?isActive:(\w)==.*?children:(.{1,10}Messages.EXPRESSION_PICKER_STICKER).*?:null/;
-                    const res = m.replace(stickerTabRegex, (_m, canUseStickers, jsx, tabHeaderComp, currentTab, stickerText) => {
-                        const isActive = `${currentTab}==="7TV"`;
-                        return (
-                            `${_m},${canUseStickers}?` +
-                            `${jsx}(${tabHeaderComp},{id:"seventv-picker-tab","aria-controls":"seventv-picker-tab-panel","aria-selected":${isActive},isActive:${isActive},autoFocus:true,viewType:"7TV",children:${jsx}("div",{children:"7TV"})})` +
-                            ":null"
-                        );
-                    });
+            replacement: [
+                {
+                    match: /null==\(null===\(\w=\w\.emojis\)\|\|void 0.*?\.consolidateGifsStickersEmojis.*?(\w)\.push\((\(0,\w\.jsx\))\((\w+),{disabled:\w,type:(\w)},"emoji"\)\)/,
+                    replace: (m, btnArray, jsx, compo, type) => {
+                        const c = "arguments[0].type";
+                        return `${m};(${c}?.submit?.button||${c}?.attachments)&&${btnArray}.push(${jsx}(${compo},{disabled:!(${c}?.submit?.button||${c}?.attachments),type:${type},emojiType:"7TV"},"7TV"))`;
+                    }
+                },
+                {
+                    match: /(\.Messages\.PREMIUM_TRIAL_TUTORIAL_EMOJI_TOOLTIP.*?;return.*?,\w+\(\)\.buttonContainer.*?children:\(0,\w+\.jsx\)\()(.*?)(,.*?active:((\w).*?),)/,
+                    replace: (_, head, comp, tail, emojiActive, activeView) => {
+                        const isSevenTv = "arguments[0]?.emojiType===\"7TV\"";
+                        const isActive = `(${isSevenTv}&&${activeView}==="7TV")||${emojiActive}`;
+                        const rest = tail.replace(emojiActive, isActive);
 
-                    return res.replace(/:null,((\w)===.*?\.STICKER&&\w\?(\(.*?\)).*?(\{.*?,onSelectSticker:.*?\})\):null)/, (_, _m, currentTab, jsx, props) => {
-                        return `:null,${currentTab}==="7TV"?${jsx}($self.SevenTVComponent,${props}):null,${_m}`;
-                    });
+                        return `${head}(${isSevenTv}?$self.chatBarIcon:${comp})${rest}`;
+                    }
+                },
+                {
+                    match: /(var \w=)(\w\.useCallback\(\(function\(\)\{\(0,\w+\.\w+\)\(.*?\.EMOJI,.*?);/,
+                    replace: (_, decl, cb) => {
+                        const newCb = cb.replace(/(?<=function\(\)\{\(.*?\)\().+?\.EMOJI/, "\"7TV\"");
+                        return `${decl}arguments[0]?.emojiType?${newCb}:${cb};`;
+                    }
+                },
+                {
+                    match: /role:"tablist",.{10,20}\.Messages\.EXPRESSION_PICKER_CATEGORIES_A11Y_LABEL,children:\[.*?\)\]}\)}\):null,.*?onSelectEmoji:\w.*?:null/s,
+                    replace: m => {
+                        const stickerTabRegex = /,(\(0,\w\.jsx\))\((\w+?),\{.{50,100}isActive:(\w)===.*?\.EMOJI,\s*?viewType:.*?EXPRESSION_PICKER_EMOJI}\)/;
+                        const res = m.replace(stickerTabRegex, (_m, jsx, tabHeaderComp, currentTab, stickerText) => {
+                            const isActive = `${currentTab}==="7TV"`;
+                            return (
+                                `${_m},${jsx}(${tabHeaderComp},{id:"seventv-picker-tab","aria-controls":"seventv-picker-tab-panel","aria-selected":${isActive},isActive:${isActive},autoFocus:true,viewType:"7TV",children:${jsx}("div",{children:"7TV"})})`
+                            );
+                        });
+
+                        return res.replace(/(\w)===.{1,10}\.EMOJI\?(\(0,\w\.jsx\)).*?(\{.*?\})\):null/, (_, currentTab, jsx, props) => {
+                            return `${_},${currentTab}==="7TV"?${jsx}($self.SevenTVComponent,${props}):null`;
+                        });
+                    }
                 }
-            }
-        },
+            ]
+        }
     ],
-
     settings,
-
-    start() {
-    },
-
-    stop() {
-    },
 
     SevenTVComponent({
         channel,
@@ -317,12 +327,16 @@ export default definePlugin({
         );
     },
 
-    chatBarIcon() {
+    chatBarIcon({
+        onClick,
+        active
+    }) {
         return (
             <Tooltip text="7TV Emotes">
                 {({ onMouseEnter, onMouseLeave }) => (
                     <div style={{ display: "flex" }}>
                         <Button
+                            style={active ? { color: "black" } : undefined}
                             aria-haspopup="dialog"
                             aria-label=""
                             size=""
@@ -330,9 +344,7 @@ export default definePlugin({
                             onMouseEnter={onMouseEnter}
                             onMouseLeave={onMouseLeave}
                             innerClassName={ButtonWrapperClasses.button}
-                            onClick={() => {
-                                self.SevenTVComponent(props);
-                            }}
+                            onClick={onClick}
                             className={cl("button")}
                         >
                             <div className={ButtonWrapperClasses.buttonWrapper}>
