ad7c695705101bb2b6b84b288943cff2de09a3f3
nmsturcke <30734036+nmsturcke@users.noreply.github.com>
2023-05-06 16:40:06 +0200

    Fix z-index for avatar

--- src/plugins/reviewDB/components/ReviewComponent.tsx
+++ src/plugins/reviewDB/components/ReviewComponent.tsx
@@ -94,7 +94,7 @@ export default LazyComponent(() => {
                         className={classes(avatar, clickable)}
                         onClick={openModal}
                         src={review.sender.profilePhoto || "/assets/1f0bfc0865d324c2587920a7d80c609b.png?size=128"}
-                        style={{ left: "0px" }}
+                        style={{ left: "0px", zIndex: 0 }}
                     />
                     <span
                         className={classes(clickable, username)}
