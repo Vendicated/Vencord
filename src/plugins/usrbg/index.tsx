15222cca310c415ced9928eea9c7c2e6a45e7a32
TheKodeToad <TheKodeToad@proton.me>
2023-05-10 18:12:11 +0100

    Hide Nitro badge if banner's source is USRBG, tweaks to description

--- src/plugins/usrbg/index.tsx
+++ src/plugins/usrbg/index.tsx
@@ -47,8 +47,8 @@ const settings = definePluginSettings({
 
 export default definePlugin({
     name: "USRBG",
-    description: "USRBG is a community maintained database of Discord banners, allowing anyone to get a banner without requiring Nitro",
-    authors: [Devs.AutumnVN, Devs.pylix],
+    description: "Displays user banners from USRBG, allowing anyone to get a banner without Nitro",
+    authors: [Devs.AutumnVN, Devs.pylix, Devs.TheKodeToad],
     settings,
     patches: [
         {
@@ -61,6 +61,10 @@ export default definePlugin({
                 {
                     match: /(\i)\.bannerSrc,/,
                     replace: "$self.useBannerHook($1),"
+                },
+                {
+                    match: /\?\(0,\i\.jsx\)\(\i,{type:\i,shown/,
+                    replace: "&&$self.showBadge(arguments[0])$&"
                 }
             ]
         },
@@ -78,7 +82,7 @@ export default definePlugin({
 
     settingsAboutComponent: () => {
         return (
-            <Link href="https://github.com/AutumnVN/usrbg#how-to-request-your-own-usrbg-banner">CLICK HERE TO GET YOUR OWN BANNER</Link>
+            <Link href="https://github.com/AutumnVN/usrbg#how-to-request-your-own-usrbg-banner">How to request a banner</Link>
         );
     },
 
@@ -102,6 +106,10 @@ export default definePlugin({
 
     premiumHook({ userId }: any) {
         if (data[userId]) return 2;
+    },
+
+    showBadge({ displayProfile, user }: any) {
+        return displayProfile?.banner && (!data[user.id] || settings.store.nitroFirst);
     },
 
     async start() {
