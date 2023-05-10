01194b1178cafb67c552949a86e96f8ff52779aa
IThundxr <contact@ithundxr.dev>
2023-05-10 17:55:19 -0400

    more code

--- src/plugins/betterblockedusers/index.tsx
+++ src/plugins/betterblockedusers/index.tsx
@@ -38,26 +38,34 @@ export default definePlugin({
     patches: [{
         find: "isBeforeGroup:",
         replacement: {
-            match: /(\.collapsedReason;return\(0,\w\.jsx\)\(\w+\.Z,{.*?childrenMessageContent:\(0,\w\.jsx\)\()\w+\.Z,/,
-            replace: (_, head) => `${head}$self.actionBarComponent,`
+            match: /(\.collapsedReason;return\(0,\w\.jsx\)\(\w+\.Z,{.*?childrenMessageContent:\(0,\w\.jsx\)\()\w+\.Z,{/,
+            replace: (_, head) => `${head}$self.actionBarComponent,{...arguments[0],`
         }
     }],
 
-    actionBarComponent(props: { children: React.ReactNode[]; compact: boolean; }) {
+    actionBarComponent(props: {
+        onClick: () => void;
+        expanded: boolean;
+        count: number;
+        compact: boolean;
+        collapsedReason: unknown;
+    }) {
+        if (!props) return null;
+        const { onClick } = props;
+
         // const { showingAllowed } = settings.use(["showingAllowed"]);
+
         return (
-            <div>
-                <div className="vc-betterblockedusers-main" id="---blocked-message-bar" role="separator">
-                    <span className="vc-betterblockedusers-divider">
-                        <svg className="vc-betterblockedusers-svg" aria-hidden="true" role="img" width="8" height="13" viewBox="0 0 8 13">
-                            <path className="vc-betterblockedusers-color" stroke="currentColor" fill="transparent" d="M8.16639 0.5H9C10.933 0.5 12.5 2.067 12.5 4V9C12.5 10.933 10.933 12.5 9 12.5H8.16639C7.23921 12.5 6.34992 12.1321 5.69373 11.4771L0.707739 6.5L5.69373 1.52292C6.34992 0.86789 7.23921 0.5 8.16639 0.5Z"></path>
+            <div style={{ zIndex: -5 }}>
+                <div className="vc-betterblockedusers-main" id="---blocked-message-bar" role="separator" style={{ zIndex: -4 }}>
+                    <span className="vc-betterblockedusers-divider" style={{ zIndex: -3 }}>
+                        <svg className="vc-betterblockedusers-svg" aria-hidden="true" role="img" width="8" height="13" viewBox="0 0 8 13" style={{ zIndex: -2 }}>
+                            <path className="vc-betterblockedusers-color" stroke="currentColor" fill="transparent" style={{ zIndex: -1 }} d="M8.16639 0.5H9C10.933 0.5 12.5 2.067 12.5 4V9C12.5 10.933 10.933 12.5 9 12.5H8.16639C7.23921 12.5 6.34992 12.1321 5.69373 11.4771L0.707739 6.5L5.69373 1.52292C6.34992 0.86789 7.23921 0.5 8.16639 0.5Z"></path>
                         </svg>
-                        <span className="vc-betterblockedusers-button" role="button" tabIndex={0}>Blocked Message</span>
+                        <button className="vc-betterblockedusers-button" style={{ zIndex: 1 }} onClick={onClick}>Blocked Message</button>
+                        {/* <span className="vc-betterblockedusers-button">Blocked Message</span> */}
                     </span>
                 </div>
-                {/* {showingAllowed ? (<props.originalComponent {...props} />) : null} */}
-                {props.children}
-                {/* {self.shouldShowContent(true)} */}
             </div>
         );
     },
