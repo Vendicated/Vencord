5be324099b0a7c2872a6cc685995def3bf8b2c7f
IThundxr <contact@ithundxr.dev>
2023-05-13 20:38:09 -0400

    push fixes :3

--- src/plugins/betterBlockedUsers/index.tsx
+++ src/plugins/betterBlockedUsers/index.tsx
@@ -51,7 +51,6 @@ export default definePlugin({
         compact: boolean;
         collapsedReason: unknown;
     }) {
-        if (!props) return null;
         const { onClick } = props;
 
         const { showingAllowed } = settings.use(["showingAllowed"]);
@@ -67,12 +66,12 @@ export default definePlugin({
                             // if showing is allowed show a button and tooltip
                             <Tooltip text={props.expanded ? "Hide Blocked Message" : "Show Blocked Message"}>
                                 {(tooltipProps: any) => (
-                                    <div style={{ display: "flex" }}>
+                                    <button {...tooltipProps} className="vc-betterblockedusers-button" onClick={onClick}>
                                         <svg className="vc-betterblockedusers-svg" aria-hidden="true" role="img" width="8" height="13" viewBox="0 0 8 13">
                                             <path className="vc-betterblockedusers-color" stroke="currentColor" fill="transparent" d="M8.16639 0.5H9C10.933 0.5 12.5 2.067 12.5 4V9C12.5 10.933 10.933 12.5 9 12.5H8.16639C7.23921 12.5 6.34992 12.1321 5.69373 11.4771L0.707739 6.5L5.69373 1.52292C6.34992 0.86789 7.23921 0.5 8.16639 0.5Z"></path>
                                         </svg>
-                                        <button {...tooltipProps} className="vc-betterblockedusers-button" onClick={onClick}>Blocked Message</button>
-                                    </div>
+                                        Blocked Message
+                                    </button>
                                 )}
                             </Tooltip>
                             :
