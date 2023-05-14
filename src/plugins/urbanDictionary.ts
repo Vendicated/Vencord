a28a4672f4e0bce4ec6ab88e3c2151190e3e231b
Hunter <70922563+HunterAPI@users.noreply.github.com>
2023-05-14 11:51:04 -0400

    fixed tabs I think

--- src/plugins/urbanDictionary.ts
+++ src/plugins/urbanDictionary.ts
@@ -22,74 +22,74 @@ import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
-	name: "UrbanDictionary",
-	description: "Search for a word on Urban Dictionary via /urban slash command",
-	authors: [Devs.jewdev],
-	dependencies: ["CommandsAPI"],
-	commands: [
-		{
-			name: "urban",
-			description: "Returns the definition of a word from Urban Dictionary",
-			inputType: ApplicationCommandInputType.BUILT_IN,
-			options: [
-				{
-					type: ApplicationCommandOptionType.STRING,
-					name: "word",
-					description: "The word to search for on Urban Dictionary",
-					required: true
-				}
-			],
-			execute: async (args, ctx) => {
-				try {
-					const query = encodeURIComponent(args[0].value);
-					const { list } = await (await fetch(`https://api.urbandictionary.com/v0/define?term=${query}`)).json();
+    name: "UrbanDictionary",
+    description: "Search for a word on Urban Dictionary via /urban slash command",
+    authors: [Devs.jewdev],
+    dependencies: ["CommandsAPI"],
+    commands: [
+        {
+            name: "urban",
+            description: "Returns the definition of a word from Urban Dictionary",
+            inputType: ApplicationCommandInputType.BUILT_IN,
+            options: [
+                {
+                    type: ApplicationCommandOptionType.STRING,
+                    name: "word",
+                    description: "The word to search for on Urban Dictionary",
+                    required: true
+                }
+            ],
+            execute: async (args, ctx) => {
+                try {
+                    const query = encodeURIComponent(args[0].value);
+                    const { list } = await (await fetch(`https://api.urbandictionary.com/v0/define?term=${query}`)).json();
 
-					if (!list || list.length <= 0) {
-						return void sendBotMessage(ctx.channel.id, { content: "No results found." });
-					}
+                    if (!list || list.length <= 0) {
+                        return void sendBotMessage(ctx.channel.id, { content: "No results found." });
+                    }
 
-					const linkify = text => text
-						.replaceAll("\r\n", "\n")
-						.replace(/([*>_`~\\])/gsi, "\\$1")
-						.replace(/\[(.+?)\]/g, (_, word) => `[${word}](https://www.urbandictionary.com/define.php?term=${encodeURIComponent(word)} "Define "${word}" on Urban Dictionary")`)
-						.trim();
+                    const linkify = text => text
+                        .replaceAll("\r\n", "\n")
+                        .replace(/([*>_`~\\])/gsi, "\\$1")
+                        .replace(/\[(.+?)\]/g, (_, word) => `[${word}](https://www.urbandictionary.com/define.php?term=${encodeURIComponent(word)} "Define "${word}" on Urban Dictionary")`)
+                        .trim();
 
-					list.sort((a, b) => (b.thumbs_up / b.thumbs_down) - a.thumbs_up / a.thumbs_down);
-					const [definition] = list;
+                    list.sort((a, b) => (b.thumbs_up / b.thumbs_down) - a.thumbs_up / a.thumbs_down);
+                    const [definition] = list;
 
-					return void sendBotMessage(ctx.channel.id, {
-						embeds: [
-							{
-								type: "rich",
-								author: {
-									name: `Uploaded by "${definition.author}"`,
-									url: `https://www.urbandictionary.com/author.php?author=${encodeURIComponent(definition.author)}`
-								},
-								title: definition.word,
-								url: `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(definition.word)}`,
-								description: linkify(definition.definition),
-								fields: [
-									{
-										name: "Example",
-										value: linkify(definition.example)
-									},
-									{
-										name: "Want more definitions?",
-										value: `Check out [more definitions](https://www.urbandictionary.com/define.php?term=${query} "Define "${args[0].value}" on Urban Dictionary") on Urban Dictionary.`
-									}
-								],
-								color: 0xFF9900,
-								footer: { text: `👍 ${definition.thumbs_up.toString()} | 👎 ${definition.thumbs_down.toString()}`, icon_url: "https://www.urbandictionary.com/favicon.ico" },
-								timestamp: new Date(definition.written_on).toISOString()
-							}
-						] as any
-					});
-				} catch (error) {
-					return void sendBotMessage(ctx.channel.id, {
-						content: `Something went wrong: \`${error}\``
-					});
-				}
-			}
-		}
-	]
+                    return void sendBotMessage(ctx.channel.id, {
+                        embeds: [
+                            {
+                                type: "rich",
+                                author: {
+                                    name: `Uploaded by "${definition.author}"`,
+                                    url: `https://www.urbandictionary.com/author.php?author=${encodeURIComponent(definition.author)}`,
+                                },
+                                title: definition.word,
+                                url: `https://www.urbandictionary.com/define.php?term=${encodeURIComponent(definition.word)}`,
+                                description: linkify(definition.definition),
+                                fields: [
+                                    {
+                                        name: "Example",
+                                        value: linkify(definition.example),
+                                    },
+                                    {
+                                        name: "Want more definitions?",
+                                        value: `Check out [more definitions](https://www.urbandictionary.com/define.php?term=${query} "Define "${args[0].value}" on Urban Dictionary") on Urban Dictionary.`,
+                                    },
+                                ],
+                                color: 0xFF9900,
+                                footer: { text: `👍 ${definition.thumbs_up.toString()} | 👎 ${definition.thumbs_down.toString()}`, icon_url: "https://www.urbandictionary.com/favicon.ico" },
+                                timestamp: new Date(definition.written_on).toISOString(),
+                            },
+                        ] as any,
+                    });
+                } catch (error) {
+                    return void sendBotMessage(ctx.channel.id, {
+                        content: `Something went wrong: \`${error}\``,
+                    });
+                }
+            }
+        }
+    ]
 });
