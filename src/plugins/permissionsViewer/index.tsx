f05b2f571820dd7cb806881bfe1b0920da759f36
V <vendicated@riseup.net>
2023-05-15 02:28:06 +0200

    grammar

--- src/plugins/permissionsViewer/index.tsx
+++ src/plugins/permissionsViewer/index.tsx
@@ -146,7 +146,7 @@ function makeContextMenuPatch(childId: string, type?: MenuItemParentType): NavCo
 
 export default definePlugin({
     name: "PermissionsViewer",
-    description: "View the permissions an user or channel has, and the roles of a server",
+    description: "View the permissions a user or channel has, and the roles of a server",
     authors: [Devs.Nuckyz, Devs.Ven],
     settings,
 
