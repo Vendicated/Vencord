a4333c61781160a8af05286e15a475451ce841d1
UwU <contact@swezy.dev>
2023-05-23 03:32:27 +0200

    New plugin : Party mode 🎉 (#1161)
    Co-authored-by: ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
    Co-authored-by: V <vendicated@riseup.net>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -311,6 +311,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "outfoxxed",
         id: 837425748435796060n
     },
+    UwUDev: {
+        name: "UwU",
+        id: 691413039156690994n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
