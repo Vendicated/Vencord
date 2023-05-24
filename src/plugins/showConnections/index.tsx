4d5bc88a57ef7b6c1752cb27b0b175a887da24f8
TheKodeToad <TheKodeToad@proton.me>
2023-05-24 16:24:35 +0100

    Smol escape

--- src/plugins/showConnections/index.tsx
+++ src/plugins/showConnections/index.tsx
@@ -186,7 +186,7 @@ export default definePlugin({
         {
             find: ".CONNECTIONS_ROLE_POPOUT_VIEW_ALL",
             replacement: {
-                match: /\(0,\i\.jsxs\)\(\i\.Fragment,{children:\[\(0,\i.jsx\)\(\i.Heading,{variant:"eyebrow",className:\i\(\)\.title,children:\i\.\i\.Messages\.CONNECTIONS/,
+                match: /\(0,\i\.jsxs\)\(\i\.Fragment,{children:\[\(0,\i.jsx\)\(\i\.Heading,{variant:"eyebrow",className:\i\(\)\.title,children:\i\.\i\.Messages\.CONNECTIONS/,
                 replace: "false&&$&"
             }
         }
