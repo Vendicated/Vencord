9d80da6bda77dc8c91026c4d1fcad344cc85ad57
V <contact@swezy.dev>
2023-05-30 15:21:24 +0200

    Fix occasional freezing on firefox (cache related)

--- src/webpack/patchWebpack.ts
+++ src/webpack/patchWebpack.ts
@@ -28,21 +28,27 @@ let webpackChunk: any[];
 
 const logger = new Logger("WebpackInterceptor", "#8caaee");
 
-Object.defineProperty(window, WEBPACK_CHUNK, {
-    get: () => webpackChunk,
-    set: v => {
-        if (v?.push !== Array.prototype.push) {
-            logger.info(`Patching ${WEBPACK_CHUNK}.push`);
-            _initWebpack(v);
-            patchPush();
-            // @ts-ignore
-            delete window[WEBPACK_CHUNK];
-            window[WEBPACK_CHUNK] = v;
-        }
-        webpackChunk = v;
-    },
-    configurable: true
-});
+if (window[WEBPACK_CHUNK]) {
+    logger.info(`Patching ${WEBPACK_CHUNK}.push (was already existant, likely from cache!)`);
+    _initWebpack(window[WEBPACK_CHUNK]);
+    patchPush();
+} else {
+    Object.defineProperty(window, WEBPACK_CHUNK, {
+        get: () => webpackChunk,
+        set: v => {
+            if (v?.push !== Array.prototype.push) {
+                logger.info(`Patching ${WEBPACK_CHUNK}.push`);
+                _initWebpack(v);
+                patchPush();
+                // @ts-ignore
+                delete window[WEBPACK_CHUNK];
+                window[WEBPACK_CHUNK] = v;
+            }
+            webpackChunk = v;
+        },
+        configurable: true
+    });
+}
 
 function patchPush() {
     function handlePush(chunk: any) {
