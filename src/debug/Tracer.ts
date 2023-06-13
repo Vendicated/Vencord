5b5d4e5cffc3f51a2b6509ef0312473da0e32ea4
λ <123122904+birdlinux@users.noreply.github.com>
2023-06-14 00:32:18 +0200

    ♻️ refactor: Refactor and optimize code for better readability and performance
    - Added checking for development environment
    - Removed var declarations and replaced with const and let
    - Initialized traces and logger variables based on IS_DEV
    - Replaced empty function declarations with an arrow function (noop)
    - Used a ternary operator to conditionally assign noop or the trace functions to beginTrace, finishTrace, and noopTracer.
    - Reformatted the code to improve readability and maintainability.

--- src/debug/Tracer.ts
+++ src/debug/Tracer.ts
@@ -18,47 +18,49 @@
 
 import { Logger } from "@utils/Logger";
 
-if (IS_DEV) {
-    var traces = {} as Record<string, [number, any[]]>;
-    var logger = new Logger("Tracer", "#FFD166");
-}
+const IS_DEV = process.env.NODE_ENV === "development";
 
-const noop = function () { };
+const traces: Record<string, [number, any[]]> = {};
+const logger = IS_DEV ? new Logger("Tracer", "#FFD166") : null;
 
-export const beginTrace = !IS_DEV ? noop :
-    function beginTrace(name: string, ...args: any[]) {
-        if (name in traces)
-            throw new Error(`Trace ${name} already exists!`);
+const noop = () => {};
 
-        traces[name] = [performance.now(), args];
-    };
+export const beginTrace = IS_DEV
+  ? (name: string, ...args: any[]) => {
+      if (traces[name]) {
+        throw new Error(`Trace ${name} already exists!`);
+      }
 
-export const finishTrace = !IS_DEV ? noop : function finishTrace(name: string) {
-    const end = performance.now();
+      traces[name] = [performance.now(), args];
+    }
+  : noop;
 
-    const [start, args] = traces[name];
-    delete traces[name];
+export const finishTrace = IS_DEV
+  ? (name: string) => {
+      const end = performance.now();
+      const [start, args] = traces[name];
+      delete traces[name];
 
-    logger.debug(`${name} took ${end - start}ms`, args);
-};
+      logger.debug(`${name} took ${end - start}ms`, args);
+    }
+  : noop;
 
 type Func = (...args: any[]) => any;
 type TraceNameMapper<F extends Func> = (...args: Parameters<F>) => string;
 
-const noopTracer =
-    <F extends Func>(name: string, f: F, mapper?: TraceNameMapper<F>) => f;
+const noopTracer = <F extends Func>(name: string, f: F, mapper?: TraceNameMapper<F>) => f;
 
-export const traceFunction = !IS_DEV
-    ? noopTracer
-    : function traceFunction<F extends Func>(name: string, f: F, mapper?: TraceNameMapper<F>): F {
-        return function (this: any, ...args: Parameters<F>) {
-            const traceName = mapper?.(...args) ?? name;
+export const traceFunction = IS_DEV
+  ? <F extends Func>(name: string, f: F, mapper?: TraceNameMapper<F>): F => {
+      return function (this: any, ...args: Parameters<F>) {
+        const traceName = mapper?.(...args) ?? name;
 
-            beginTrace(traceName, ...arguments);
-            try {
-                return f.apply(this, args);
-            } finally {
-                finishTrace(traceName);
-            }
-        } as F;
-    };
+        beginTrace(traceName, ...args);
+        try {
+          return f.apply(this, args);
+        } finally {
+          finishTrace(traceName);
+        }
+      } as F;
+    }
+  : noopTracer;
