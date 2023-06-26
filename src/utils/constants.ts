5694ae57874194ded0f70fdf1c8825d47869d2e8
Default Box <134133080+thedefaultbox@users.noreply.github.com>
2023-06-26 19:58:05 +1000

    i think i fixed it

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -334,6 +334,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     HypedDomi: {
         name: "HypedDomi",
         id: 354191516979429376n
+    },
+    thedefaultbox: {
+        name: "thedefaultbox",
+        id: 796183774677041192n
     }
 } satisfies Record<string, Dev>);
 
