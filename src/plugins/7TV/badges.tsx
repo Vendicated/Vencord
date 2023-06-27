70f7c6b93512f1c7ea5a19729c1a9914f5130317
Xslash <kapimak28@gmail.com>
2023-06-27 05:03:14 +0200

    BADGE_PROPS

--- src/plugins/7TV/badges.tsx
+++ src/plugins/7TV/badges.tsx
@@ -2,16 +2,17 @@ import { BadgePosition, ProfileBadge } from "@api/Badges";
 import { hasBadge } from "./index";
 
 const STORE_URL = "https://7tv.app/store";
+const BADGE_PROPS = {
+    style: {
+        transform: "scale(0.8)"
+    }
+};
 
 const Month1Badge: ProfileBadge = {
     description: "7TV Subscriber - 1 Month",
     image: "https://cdn.7tv.app/badge/62f97c05e46eb00e438a696a/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 1 Month"),
     link: STORE_URL
 };
@@ -19,11 +20,7 @@ const Month2Badge: ProfileBadge = {
     description: "7TV Subscriber - 2 Month",
     image: "https://cdn.7tv.app/badge/62f97db2e46eb00e438a696b/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 2 Month"),
     link: STORE_URL
 };
@@ -31,11 +28,7 @@ const Month3Badge: ProfileBadge = {
     description: "7TV Subscriber - 3 Month",
     image: "https://cdn.7tv.app/badge/62f97e19e46eb00e438a696c/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 3 Month"),
     link: STORE_URL
 };
@@ -43,11 +36,7 @@ const Month6Badge: ProfileBadge = {
     description: "7TV Subscriber - 6 Month",
     image: "https://cdn.7tv.app/badge/62f97e71e46eb00e438a696d/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 6 Month"),
     link: STORE_URL
 };
@@ -55,11 +44,7 @@ const Month9Badge: ProfileBadge = {
     description: "7TV Subscriber - 9 Month",
     image: "https://cdn.7tv.app/badge/62f97ebee46eb00e438a696e/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 9 Month"),
     link: STORE_URL
 };
@@ -67,11 +52,7 @@ const Year1Badge: ProfileBadge = {
     description: "7TV Subscriber - 1 Year",
     image: "https://cdn.7tv.app/badge/62f97f19e46eb00e438a696f/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 1 Year"),
     link: STORE_URL
 };
@@ -79,11 +60,7 @@ const Year1QuarterBadge: ProfileBadge = {
     description: "7TV Subscriber - 1 Year & a Quarter",
     image: "https://cdn.7tv.app/badge/637d53962863630a2d06e27a/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 1 Year & a Quarter"),
     link: STORE_URL
 };
@@ -91,11 +68,7 @@ const Year1HalfBadge: ProfileBadge = {
     description: "7TV Subscriber - 1 Year & a Half",
     image: "https://cdn.7tv.app/badge/637d54582863630a2d06e27b/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 1 Year & a Half"),
     link: STORE_URL
 };
@@ -103,11 +76,7 @@ const Year1ThreeQuarterBadge: ProfileBadge = {
     description: "7TV Subscriber - 1 Year & Three Quarters",
     image: "https://cdn.7tv.app/badge/64344f93cda636a6910a265f/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "7TV Subscriber - 1 Year & Three Quarters"),
     link: STORE_URL
 };
@@ -116,11 +85,7 @@ const FounderBadge: ProfileBadge = {
     description: "7TV Founder",
     image: "https://cdn.7tv.app/badge/62f98190e46eb00e438a6970/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "Subscriber - Founder"),
     link: STORE_URL
 };
@@ -129,11 +94,7 @@ const XMASBadge: ProfileBadge = {
     description: "7TV XMAS Gifter",
     image: "https://cdn.7tv.app/badge/63a66ade919e3d301c52fa84/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "XMAS Gifter"),
     link: STORE_URL
 };
@@ -143,11 +104,7 @@ const TranslatorBadge: ProfileBadge = {
     description: "7TV Translator",
     image: "https://cdn.7tv.app/badge/62f99d0ce46eb00e438a6984/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "Translator"),
     link: STORE_URL
 };
@@ -155,11 +112,7 @@ const ContributorBadge: ProfileBadge = {
     description: "7TV Contributor",
     image: "https://cdn.7tv.app/badge/62f99bc1e46eb00e438a6981/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "Contributor"),
     link: STORE_URL
 };
@@ -167,11 +120,7 @@ const ModeratorBadge: ProfileBadge = {
     description: "7TV Moderator",
     image: "https://cdn.7tv.app/badge/62f98438e46eb00e438a6972/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "Moderator"),
     link: STORE_URL
 };
@@ -179,11 +128,7 @@ const AdminBadge: ProfileBadge = {
     description: "7TV Admin",
     image: "https://cdn.7tv.app/badge/62f98382e46eb00e438a6971/3x",
     position: BadgePosition.START,
-    props: {
-        style: {
-            transform: "scale(0.8)"
-        }
-    },
+    props: BADGE_PROPS,
     shouldShow: ({ user }) => hasBadge(user.id, "Admin"),
     link: STORE_URL
 };
