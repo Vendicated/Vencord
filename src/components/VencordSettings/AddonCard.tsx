c0fd12bc5dce04482f2c7f48d0a448de36f5f532
V <vendicated@riseup.net>
2023-08-04 19:06:18 +0200

    oop

--- src/components/VencordSettings/AddonCard.tsx
+++ src/components/VencordSettings/AddonCard.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import "./settingsCard.css";
+import "./addonCard.css";
 
 import { classNameFactory } from "@api/Styles";
 import { Badge } from "@components/Badge";
