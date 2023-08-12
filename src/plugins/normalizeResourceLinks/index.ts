c8b604c9cadc185892f4b82303a91eec2ccc056a
Dusk Banks <me@bb010g.com>
2023-08-11 21:02:54 -0700

    plugins(NormalizeResourceLinks): create plugin

---
+++ src/plugins/normalizeResourceLinks/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2023 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "NormalizeResourceLinks",
+    description: "Normalize resource links to match stable Discord",
+    authors: [Devs.bb010g],
+    patches: [
+        {
+            find: ".Messages.COPY_MESSAGE_LINK,",
+            replacement: {
+                match: /(\.concat\()(location\.host)(\))/,
+                replace:
+                    "$1Vencord.Plugins.plugins.NormalizeResourceLinks.normalizeResourceHost($2)$3",
+            },
+        },
+    ],
+    normalizeResourceHost(host: string) {
+        return host.replace(/(^|[^a-z-])(canary\.|ptb\.)(discord.com)$/, "$1$3");
+    },
+});
