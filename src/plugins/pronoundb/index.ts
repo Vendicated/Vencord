3e732646e5ab83f76b34ec7fdb3d9d8c68faca8d
V <vendicated@riseup.net>
2023-08-12 05:27:59 +0200

    Fix plugins broken by latest canary update (#1637)
    * Fix WebContextMenus
    * BetterGifAltText: futureproof find
    * Fix pronoundb profile patch
    * Fix NoScreensharePreview
    * Fix FakeNitro emote text patch
    
    ---------
    
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>

--- src/plugins/pronoundb/index.ts
+++ src/plugins/pronoundb/index.ts
@@ -68,7 +68,8 @@ export default definePlugin({
             find: ".USER_PROFILE_ACTIVITY",
             replacement: [
                 {
-                    match: /getGlobalName\(\i\);(?<=displayProfile.{0,200})/,
+                    /* FIXME: old name is getGlobalName, new name is getName. Remove optional Global once stable has also migrated */
+                    match: /\.get(?:Global)?Name\(\i\);(?<=displayProfile.{0,200})/,
                     replace: "$&const [vcPronounce,vcPronounSource]=$self.useProfilePronouns(arguments[0].user.id,true);if(arguments[0].displayProfile&&vcPronounce)arguments[0].displayProfile.pronouns=vcPronounce;"
                 },
                 PRONOUN_TOOLTIP_PATCH
