1fbbafb43d750b22c0ffe0978abd9ebcfa3374e6
Hugo C <lumap@duck.com>
2023-08-31 23:49:43 +0200

    better tooltip

--- src/plugins/pictureInPicture.tsx
+++ src/plugins/pictureInPicture.tsx
@@ -20,22 +20,35 @@ export default definePlugin({
         }
     }],
     renderPiPButton() {
-        return <Tooltip text={"Picture in Picture"}>
-            {this.PiPButtonContent}
+        return <Tooltip text={"Enable Picture in Picture"}>
+            {/* {this.PiPButtonContent} */}
+            {({ onMouseLeave, onMouseEnter }) => (
+                <div
+                    onMouseLeave={onMouseLeave}
+                    onMouseEnter={onMouseEnter}
+                    role="button"
+                    aria-label={"Enable Picture in Picture"}
+                    aria-disabled={false}
+                    style={{
+                        cursor: "pointer",
+                        paddingTop: "4px",
+                        paddingLeft: "4px",
+                        paddingRight: "4px",
+                    }}
+                    onClick={e => {
+                        try {
+                            // @ts-ignore
+                            e.target.parentNode.parentNode.querySelector("video").requestPictureInPicture();
+                        } catch { }
+                    }}
+                >
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: "none" }}>
+                        <g>
+                            <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
+                        </g>
+                    </svg>
+                </div>
+            )}
         </Tooltip>;
-    },
-    PiPButtonContent() {
-        return <div
-            onClick={e => { // @ts-ignore it thinks parentNode doesn't exist
-                e.target.parentNode.parentNode.querySelector("video").requestPictureInPicture();
-            }}
-            style={{ paddingTop: "4px", paddingLeft: "3px", paddingRight: "3px" }}
-        >
-            <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: "none" }}>
-                <g>
-                    <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
-                </g>
-            </svg>
-        </div>;
     }
 });
