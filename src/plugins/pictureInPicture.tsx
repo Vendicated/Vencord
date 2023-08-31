0895d3457a3691e53fcc019deb8a438635d9ef43
Hugo C <lumap@duck.com>
2023-09-01 00:09:15 +0200

    PiP: remove svg group

--- src/plugins/pictureInPicture.tsx
+++ src/plugins/pictureInPicture.tsx
@@ -36,9 +36,7 @@ export default definePlugin({
                     }}
                 >
                     <svg width="24px" height="24px" viewBox="0 0 24 24">
-                        <g>
-                            <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
-                        </g>
+                        <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
                     </svg>
                 </div>
             )}
