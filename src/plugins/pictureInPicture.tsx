42cb450c75c665e894abdaedcf0b3be440bf104a
Hugo C <lumap@duck.com>
2023-08-31 23:54:31 +0200

    PiP: use currentTarget

--- src/plugins/pictureInPicture.tsx
+++ src/plugins/pictureInPicture.tsx
@@ -37,12 +37,13 @@ export default definePlugin({
                     }}
                     onClick={e => {
                         try {
-                            // @ts-ignore
-                            e.target.parentNode.parentNode.querySelector("video").requestPictureInPicture();
-                        } catch { }
+                            e.currentTarget.parentNode!.parentNode!.querySelector("video")!.requestPictureInPicture();
+                        } catch {
+                            // this only triggers if the video hasn't finished loading, the best thing to do is simply ignore any potential error
+                        }
                     }}
                 >
-                    <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ pointerEvents: "none" }}>
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <g>
                             <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
                         </g>
