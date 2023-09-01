e5a92039afad7dd5a78bb4ba8f295cf340ba9458
DaBluLite <asphatire21reborn@gmail.com>
2023-09-01 13:01:45 +0300

    CSS cleanup

--- src/plugins/discordColorways/index.tsx
+++ src/plugins/discordColorways/index.tsx
@@ -548,7 +548,10 @@ function SelectorModal({ modalProps, colorwayProps, customColorwayProps, activeC
                                             });
                                     });
                                 });
-                            }}><div className="colorwayRefreshIcon"></div></div>;
+                            }}><div className="colorwayRefreshIcon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><g id="Frame_-_24px"><rect y="0" fill="none" width="24" height="24"></rect></g><g id="Filled_Icons"><g><path d="M6.351,6.351C7.824,4.871,9.828,4,12,4c4.411,0,8,3.589,8,8h2c0-5.515-4.486-10-10-10 C9.285,2,6.779,3.089,4.938,4.938L3,3v6h6L6.351,6.351z"></path><path d="M17.649,17.649C16.176,19.129,14.173,20,12,20c-4.411,0-8-3.589-8-8H2c0,5.515,4.486,10,10,10 c2.716,0,5.221-1.089,7.062-2.938L21,21v-6h-6L17.649,17.649z"></path></g></g></svg>
+                                </div>
+                            </div>;
                         }}
                     </Tooltip>
                     <Tooltip text="Create Colorway...">
@@ -787,7 +790,9 @@ export function ThemePreview({ accent, primary, secondary, tertiary, previewCSS 
                 <div className="colorwayPreview-channels" style={{ backgroundColor: secondary }}>
                     <div className="colorwayPreview-userArea" style={{ backgroundColor: "hsl(" + HexToHSL(secondary)[0] + " " + HexToHSL(secondary)[1] + "% " + Math.max(HexToHSL(secondary)[2] - 3.6, 0) + "%)" }}></div>
                     <div className="colorwayPreview-filler"></div>
-                    <div className="colorwayPreview-topShadow" style={{ "--primary-900-hsl": `${HexToHSL(tertiary)[0]} ${HexToHSL(tertiary)[1]}% ${Math.max(HexToHSL(tertiary)[2] - (3.6 * 6), 0)}%`, "--primary-500-hsl": `${HexToHSL(primary)[0]} ${HexToHSL(primary)[1]}% ${Math.min(HexToHSL(primary)[2] + (3.6 * 3), 100)}%` } as React.CSSProperties}></div>
+                    <div className="colorwayPreview-topShadow" style={{ "--primary-900-hsl": `${HexToHSL(tertiary)[0]} ${HexToHSL(tertiary)[1]}% ${Math.max(HexToHSL(tertiary)[2] - (3.6 * 6), 0)}%`, "--primary-500-hsl": `${HexToHSL(primary)[0]} ${HexToHSL(primary)[1]}% ${Math.min(HexToHSL(primary)[2] + (3.6 * 3), 100)}%` } as React.CSSProperties}>
+                        <Text tag="div" variant="text-md/semibold" lineClamp={1}>Discord Colorways</Text>
+                    </div>
                 </div>
                 <div className="colorwayPreview-chat" style={{ backgroundColor: primary }}>
                     <div className="colorwayPreview-chatBox" style={{ backgroundColor: "hsl(" + HexToHSL(primary)[0] + " " + HexToHSL(primary)[1] + "% " + Math.min(HexToHSL(primary)[2] + 3.6, 100) + "%)" }}></div>
