0d66604be5890a9054577bcc69bd7d9334de8e15
Lewis Crichton <lewi@lewisakura.moe>
2023-09-02 21:29:44 +0100

    style: explicit 	ype qualifier

--- src/components/VencordSettings/ThemesTab.tsx
+++ src/components/VencordSettings/ThemesTab.tsx
@@ -25,11 +25,11 @@ import { classes } from "@utils/misc";
 import { showItemInFolder } from "@utils/native";
 import { useAwaiter } from "@utils/react";
 import type { ThemeHeader } from "@utils/themes";
-import { UserThemeHeader } from "@utils/themes/bd";
+import type { UserThemeHeader } from "@utils/themes/bd";
 import { findByCodeLazy, findByPropsLazy, findLazy } from "@webpack";
 import { Button, Card, FluxDispatcher, Forms, React, showToast, TabBar, TextArea, useEffect, useRef, useState } from "@webpack/common";
 import type { ComponentType, Ref, SyntheticEvent } from "react";
-import { UserstyleHeader } from "usercss-meta";
+import type { UserstyleHeader } from "usercss-meta";
 
 import { AddonCard } from "./AddonCard";
 import { SettingsTab, wrapTab } from "./shared";
