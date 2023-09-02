a6d483e05473dbd4729e3ebc16729629ec7e630d
Hugo C <lumap@duck.com>
2023-09-02 21:16:30 +0200

    PiP: use ErrorBoundary + definePluginSettings

--- src/plugins/pictureInPicture.tsx
+++ src/plugins/pictureInPicture.tsx
@@ -4,23 +4,26 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Settings } from "@api/Settings";
+import { definePluginSettings, Settings } from "@api/Settings";
+import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { React, Tooltip } from "@webpack/common";
+
+const pluginSettings = definePluginSettings({
+    loop: {
+        description: "Whether to make the PiP video loop or not",
+        type: OptionType.BOOLEAN,
+        default: true,
+        restartNeeded: false
+    }
+});
 
 export default definePlugin({
     name: "PictureInPicture",
     description: "Adds picture in picture to videos (next to the Download button)",
     authors: [Devs.Lumap],
-    options: {
-        loop: {
-            description: "Whether to make the PiP video loop or not",
-            type: OptionType.BOOLEAN,
-            default: true,
-            restartNeeded: false
-        }
-    },
+    settings: pluginSettings,
     patches: [
         {
             find: ".onRemoveAttachment,",
@@ -31,36 +34,38 @@ export default definePlugin({
         },
     ],
     renderPiPButton() {
-        return <Tooltip text="Toggle Picture in Picture">
-            {tooltipProps => (
-                <div
-                    {...tooltipProps}
-                    role="button"
-                    style={{
-                        cursor: "pointer",
-                        paddingTop: "4px",
-                        paddingLeft: "4px",
-                        paddingRight: "4px",
-                    }}
-                    onClick={e => {
-                        const PiPVideo = e.currentTarget.parentNode!.parentNode!.querySelector("video")!.cloneNode(true);
-                        const newVid = document.body.appendChild(PiPVideo) as HTMLVideoElement;
-                        if (Settings.plugins.PictureInPicture.loop) newVid.style.display = "none";
-                        newVid.loop = true;
-                        newVid.onleavepictureinpicture = () => {
-                            newVid.remove();
-                        };
-                        newVid.onloadedmetadata = () => {
-                            newVid.requestPictureInPicture();
-                            newVid.play();
-                        };
-                    }}
-                >
-                    <svg width="24px" height="24px" viewBox="0 0 24 24">
-                        <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
-                    </svg>
-                </div>
-            )}
-        </Tooltip >;
+        return <ErrorBoundary noop>
+            <Tooltip text="Toggle Picture in Picture">
+                {tooltipProps => (
+                    <div
+                        {...tooltipProps}
+                        role="button"
+                        style={{
+                            cursor: "pointer",
+                            paddingTop: "4px",
+                            paddingLeft: "4px",
+                            paddingRight: "4px",
+                        }}
+                        onClick={e => {
+                            const PiPVideo = e.currentTarget.parentNode!.parentNode!.querySelector("video")!.cloneNode(true);
+                            const newVid = document.body.appendChild(PiPVideo) as HTMLVideoElement;
+                            if (Settings.plugins.PictureInPicture.loop) newVid.style.display = "none";
+                            newVid.loop = true;
+                            newVid.onleavepictureinpicture = () => {
+                                newVid.remove();
+                            };
+                            newVid.onloadedmetadata = () => {
+                                newVid.requestPictureInPicture();
+                                newVid.play();
+                            };
+                        }}
+                    >
+                        <svg width="24px" height="24px" viewBox="0 0 24 24">
+                            <path fill="var(--interactive-normal)" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4z" />
+                        </svg>
+                    </div>
+                )}
+            </Tooltip >
+        </ErrorBoundary>;
     }
 });
