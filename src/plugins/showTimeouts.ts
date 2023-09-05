c439249b383685bacf36434286685a02f5f1b84c
dolfies <contact@swezy.dev>
2023-09-05 13:42:35 -0400

    feat(plugin): ShowTimeouts (#1687)
    Co-authored-by: TheKodeToad <TheKodeToad@proton.me>
    Co-authored-by: V <vendicated@riseup.net>

---
+++ src/plugins/showTimeouts.ts
@@ -0,0 +1,35 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "ShowTimeouts",
+    description: "Display member timeout icons in chat regardless of permissions.",
+    authors: [Devs.Dolfies],
+    patches: [
+        {
+            find: "showCommunicationDisabledStyles",
+            replacement: {
+                match: /&&\i\.\i\.canManageUser\(\i\.\i\.MODERATE_MEMBERS,\i\.author,\i\)/,
+                replace: "",
+            },
+        },
+    ],
+});
