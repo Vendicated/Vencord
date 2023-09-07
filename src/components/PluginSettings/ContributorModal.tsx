3d3a9d6d5ccbfc1750f141293dd1fa2e7f97bf34
V <vendicated@riseup.net>
2023-09-07 03:32:01 +0200

    add github and website buttons

--- src/components/PluginSettings/ContributorModal.tsx
+++ src/components/PluginSettings/ContributorModal.tsx
@@ -9,16 +9,20 @@ import "./contributorModal.css";
 import { useSettings } from "@api/Settings";
 import { classNameFactory } from "@api/Styles";
 import ErrorBoundary from "@components/ErrorBoundary";
-import { Link } from "@components/Link";
 import { DevsById } from "@utils/constants";
-import { getTheme, Theme } from "@utils/discord";
+import { fetchUserProfile, getTheme, Theme } from "@utils/discord";
 import { ModalContent, ModalRoot, openModal } from "@utils/modal";
-import { Forms, showToast, useMemo } from "@webpack/common";
+import { Forms, MaskedLink, showToast, useEffect, useMemo, UserProfileStore, useStateFromStores } from "@webpack/common";
 import { User } from "discord-types/general";
 
 import Plugins from "~plugins";
 
 import { PluginCard } from ".";
+
+const WebsiteIconDark = "/assets/e1e96d89e192de1997f73730db26e94f.svg";
+const WebsiteIconLight = "/assets/730f58bcfd5a57a5e22460c445a0c6cf.svg";
+const GithubIconLight = "/assets/3ff98ad75ac94fa883af5ed62d17c459.svg";
+const GithubIconDark = "/assets/6a853b4c87fce386cbfef4a2efbacb09.svg";
 
 const cl = classNameFactory("vc-author-modal-");
 
@@ -34,19 +38,28 @@ export function openContributorModal(user: User) {
     );
 }
 
-const GithubLight = "/assets/3ff98ad75ac94fa883af5ed62d17c459.svg";
-const GithubDark = "/assets/6a853b4c87fce386cbfef4a2efbacb09.svg";
-
 function GithubIcon() {
-    const src = getTheme() === Theme.Light ? GithubLight : GithubDark;
+    const src = getTheme() === Theme.Light ? GithubIconLight : GithubIconDark;
     return <img src={src} alt="GitHub" />;
+}
+
+function WebsiteIcon() {
+    const src = getTheme() === Theme.Light ? WebsiteIconLight : WebsiteIconDark;
+    return <img src={src} alt="Website" />;
 }
 
 function ContributorModal({ user }: { user: User; }) {
     useSettings();
 
-    // TODO: unhardcode - Decide whether we want to hardcode in devs object or fetch from discord connections
-    const githubName = "Vendicated";
+    const profile = useStateFromStores([UserProfileStore], () => UserProfileStore.getUserProfile(user.id));
+
+    useEffect(() => {
+        if (!profile && !user.bot && user.id)
+            fetchUserProfile(user.id);
+    }, [user.id]);
+
+    const githubName = profile?.connectedAccounts?.find(a => a.type === "github")?.name;
+    const website = profile?.connectedAccounts?.find(a => a.type === "domain")?.name;
 
     const plugins = useMemo(() => {
         const allPlugins = Object.values(Plugins);
@@ -70,10 +83,17 @@ function ContributorModal({ user }: { user: User; }) {
                 <Forms.FormTitle tag="h2" className={cl("name")}>{user.username}</Forms.FormTitle>
 
                 <div className={cl("links")}>
+                    {website && (
+                        <MaskedLink
+                            href={"https://" + website}
+                        >
+                            <WebsiteIcon />
+                        </MaskedLink>
+                    )}
                     {githubName && (
-                        <Link href={`https://github.com/${githubName}`}>
+                        <MaskedLink href={`https://github.com/${githubName}`}>
                             <GithubIcon />
-                        </Link>
+                        </MaskedLink>
                     )}
                 </div>
             </div>
