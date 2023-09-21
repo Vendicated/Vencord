b9cd1cab2388d165f5cb3302300d74d139f1b337
Hugo C <lumap@duck.com>
2023-09-21 18:02:15 +0200

    Ven code review

---
+++ src/plugins/loopVideos/README.md
@@ -0,0 +1,5 @@
+# LoopVideos
+
+Adds a loop button next to the volume slider on embedded videos.
+
+![image](https://cdn.discordapp.com/attachments/1020339327831658629/1154446995633737778/image.png)
--- src/plugins/loopVideos/index.tsx
+++ src/plugins/loopVideos/index.tsx
@@ -15,22 +15,21 @@ const settings = definePluginSettings({
     loop: {
         description: "Whether to enable looping by default or not",
         type: OptionType.BOOLEAN,
-        default: true,
-        restartNeeded: false
+        default: true
     }
 });
 
 export default definePlugin({
     name: "LoopVideos",
-    description: "Adds a loop button next to the volume slider for embedded videos",
+    description: "Adds a loop button next to the volume slider on embedded videos",
     authors: [Devs.Lumap],
     settings,
 
     patches: [
         {
-            find: "().videoControls:",
+            find: ".videoControls:",
             replacement: {
-                match: /children:\[this\.renderPlayIcon\(\),.{0,200}\.setDurationRef}\),/gm,
+                match: /children:\[this\.renderPlayIcon\(\),.{0,200}\.setDurationRef}\),/,
                 replace: "$&$self.renderLoopButton(),"
             },
         },
@@ -39,6 +38,7 @@ export default definePlugin({
     renderLoopButton: ErrorBoundary.wrap(() => {
         const [isEnabled, setIsEnabled] = useState(false);
         const elementRef = useRef<HTMLDivElement>(null);
+
         useEffect(() => {
             if (elementRef.current) {
                 const elt = elementRef.current;
@@ -47,6 +47,7 @@ export default definePlugin({
                 setIsEnabled(settings.store.loop);
             }
         }, []);
+
         return <div ref={elementRef}>
             <Tooltip text="Toggle Loop">
                 {tooltipProps => (
@@ -65,7 +66,7 @@ export default definePlugin({
                             setIsEnabled(video.loop);
                         }}
                     >
-                        <svg width="19" height="19" viewBox="0 0 19 19" fill={isEnabled ? "#98C379" : "#A0A0A1"}>
+                        <svg width="19" height="19" viewBox="0 0 19 19" fill={isEnabled ? "var(--green-200)" : "var(--primary-300)"}>
                             <path d="M14.3353 5.93745H12.4687C11.8119 5.93745 11.2812 6.46811 11.2812 7.12495C11.2812 7.78179 11.8119 8.31245 12.4687 8.31245H17.2187C17.8755 8.31245 18.4062 7.78179 18.4062 7.12495V2.37495C18.4062 1.71812 17.8755 1.18745 17.2187 1.18745C16.5619 1.18745 16.0312 1.71812 16.0312 2.37495V4.27495L15.3781 3.62183C12.131 0.374756 6.8689 0.374756 3.62183 3.62183C0.374756 6.8689 0.374756 12.131 3.62183 15.3781C6.8689 18.6251 12.131 18.6251 15.3781 15.3781C15.8419 14.9142 15.8419 14.1609 15.3781 13.697C14.9142 13.2332 14.1609 13.2332 13.697 13.697C11.3777 16.0164 7.61851 16.0164 5.29917 13.697C2.97983 11.3777 2.97983 7.61851 5.29917 5.29917C7.61851 2.97983 11.3777 2.97983 13.697 5.29917L14.3353 5.93745Z" />
                         </svg>
                     </div>
