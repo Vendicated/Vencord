6fbe24a268c6b0039e4cb0acc88dd2a9c5cfbd11
Lewis Crichton <lewi@lewisakura.moe>
2023-09-25 19:06:36 +0100

    feat: don't parse if not able to compile

--- src/components/ThemeSettings/ThemesTab.tsx
+++ src/components/ThemeSettings/ThemesTab.tsx
@@ -228,7 +228,7 @@ function ThemesTab() {
         for (const { fileName, content } of themes) {
             if (!fileName.endsWith(".css")) continue;
 
-            if (fileName.endsWith(".user.css")) {
+            if ((!IS_WEB || "armcord" in window) && fileName.endsWith(".user.css")) {
                 // handle it as usercss
                 const header = await usercssParse(content, fileName);
 
