5a143666318cd104f03df51519b56ac07e6d7f38
TheKodeToad <TheKodeToad@proton.me>
2023-09-26 19:00:28 +0100

    Remove ReportIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -255,18 +255,3 @@ export function DeleteIcon(props: IconProps) {
         </Icon>
     );
 }
-
-export function ReportIcon(props: IconProps) {
-    return (
-        <Icon
-            {...props}
-            className={classes(props.className, "vc-report-icon")}
-            viewBox="0 0 24 24"
-        >
-            <path
-                fill="currentColor"
-                d="M20,6.002H14V3.002C14,2.45 13.553,2.002 13,2.002H4C3.447,2.002 3,2.45 3,3.002V22.002H5V14.002H10.586L8.293,16.295C8.007,16.581 7.922,17.011 8.076,17.385C8.23,17.759 8.596,18.002 9,18.002H20C20.553,18.002 21,17.554 21,17.002V7.002C21,6.45 20.553,6.002 20,6.002Z"
-            />
-        </Icon>
-    );
-}
--- src/plugins/reviewDB/components/MessageButton.tsx
+++ src/plugins/reviewDB/components/MessageButton.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { DeleteIcon, ReportIcon } from "@components/Icons";
+import { DeleteIcon } from "@components/Icons";
 import { classes } from "@utils/misc";
 import { findByPropsLazy } from "@webpack";
 import { Tooltip } from "@webpack/common";
@@ -48,7 +48,12 @@ export function ReportButton({ onClick }: { onClick(): void; }) {
                     className={iconClasses.button}
                     onClick={onClick}
                 >
-                    <ReportIcon width={20} height={20} />
+                    <svg width={20} height={20} viewBox="0 0 24 24">
+                        <path
+                            fill="currentColor"
+                            d="M20,6.002H14V3.002C14,2.45 13.553,2.002 13,2.002H4C3.447,2.002 3,2.45 3,3.002V22.002H5V14.002H10.586L8.293,16.295C8.007,16.581 7.922,17.011 8.076,17.385C8.23,17.759 8.596,18.002 9,18.002H20C20.553,18.002 21,17.554 21,17.002V7.002C21,6.45 20.553,6.002 20,6.002Z"
+                        />
+                    </svg>
                 </div>
             )}
         </Tooltip>
