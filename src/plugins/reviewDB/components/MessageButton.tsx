5334fa5d1e8e975c0112c900e67e84f4881a8664
TheKodeToad <TheKodeToad@proton.me>
2023-09-26 20:46:47 +0100

    {} -> ""

--- src/plugins/reviewDB/components/MessageButton.tsx
+++ src/plugins/reviewDB/components/MessageButton.tsx
@@ -32,7 +32,7 @@ export function DeleteButton({ onClick }: { onClick(): void; }) {
                     className={classes(iconClasses.button, iconClasses.dangerous)}
                     onClick={onClick}
                 >
-                    <DeleteIcon width={20} height={20} />
+                    <DeleteIcon width="20" height="20" />
                 </div>
             )}
         </Tooltip>
@@ -48,7 +48,7 @@ export function ReportButton({ onClick }: { onClick(): void; }) {
                     className={iconClasses.button}
                     onClick={onClick}
                 >
-                    <svg width={20} height={20} viewBox="0 0 24 24">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
                         <path
                             fill="currentColor"
                             d="M20,6.002H14V3.002C14,2.45 13.553,2.002 13,2.002H4C3.447,2.002 3,2.45 3,3.002V22.002H5V14.002H10.586L8.293,16.295C8.007,16.581 7.922,17.011 8.076,17.385C8.23,17.759 8.596,18.002 9,18.002H20C20.553,18.002 21,17.554 21,17.002V7.002C21,6.45 20.553,6.002 20,6.002Z"
