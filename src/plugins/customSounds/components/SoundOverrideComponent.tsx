9efd0f02cfc76086984b8de99f78ade23bb61eac
TheKodeToad <TheKodeToad@proton.me>
2023-09-29 22:36:28 +0100

    Extract SoundOverrideComponent

---
+++ src/plugins/customSounds/components/SoundOverrideComponent.tsx
@@ -0,0 +1,68 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2023 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { makeRange } from "@components/PluginSettings/components";
+import { Margins } from "@utils/margins";
+import { findByCodeLazy } from "@webpack";
+import { Button, Card, Forms, Slider, Switch, TextInput, useRef } from "@webpack/common";
+
+import { SoundOverride, SoundPlayer, SoundType } from "../types";
+
+const playSound: (id: string) => SoundPlayer = findByCodeLazy(".playWithListener().then");
+
+export function SoundOverrideComponent({ type, override }: { type: SoundType; override: SoundOverride; }) {
+    const sound: React.MutableRefObject<SoundPlayer | null> = useRef(null);
+    return (
+        <Card style={{ padding: "1em 1em 0" }}>
+            <Switch
+                value={override.enabled}
+                onChange={value => override.enabled = value}
+                hideBorder={true}
+            >
+                {type.name} <span style={{ color: "var(--text-muted)" }}>({type.id})</span>
+            </Switch>
+            <Button
+                color={Button.Colors.PRIMARY}
+                className={Margins.bottom16}
+                onClick={() => {
+                    if (sound.current != null)
+                        sound.current.stop();
+                    sound.current = playSound(type.id);
+                }}
+                disabled={!override.enabled}
+            >
+                Preview
+            </Button>
+            <Forms.FormTitle>File</Forms.FormTitle>
+            <TextInput
+                type="text"
+                value={override.url}
+                onChange={value => override.url = value}
+                placeholder="Use default"
+                className={Margins.bottom16}
+                disabled={!override.enabled}
+            />
+            <Button
+                color={Button.Colors.PRIMARY}
+                className={Margins.bottom16}
+                size={Button.Sizes.MEDIUM}
+                onClick={() => {
+                }}
+                disabled={!override.enabled}
+            >
+                Preview
+            </Button>
+            <Forms.FormTitle>Volume</Forms.FormTitle>
+            <Slider
+                markers={makeRange(0, 100, 10)}
+                initialValue={override.volume}
+                onValueChange={value => override.volume = value}
+                className={Margins.bottom16}
+                disabled={!override.enabled}
+            />
+        </Card>
+    );
+}
--- src/plugins/customSounds/settings.tsx
+++ src/plugins/customSounds/settings.tsx
@@ -5,15 +5,10 @@
  */
 
 import { definePluginSettings } from "@api/Settings";
-import { makeRange } from "@components/PluginSettings/components";
-import { Margins } from "@utils/margins";
 import { OptionType } from "@utils/types";
-import { findByCodeLazy } from "@webpack";
-import { Button, Card, Forms, Slider, Switch, TextInput, useRef } from "@webpack/common";
 
-import { SoundOverride, SoundPlayer, SoundType, soundTypes } from "./types";
-
-const playSound: (id: string) => SoundPlayer = findByCodeLazy(".playWithListener().then");
+import { SoundOverride, soundTypes } from "./types";
+import { SoundOverrideComponent } from "./components/SoundOverrideComponent";
 
 export const settings = definePluginSettings({
     overrides: {
@@ -22,7 +17,7 @@ export const settings = definePluginSettings({
         default: {},
         component: () =>
             <>{soundTypes.map(type =>
-                <SoundComponent
+                <SoundOverrideComponent
                     type={type}
                     override={
                         settings.store.overrides[type.id] ??= {
@@ -42,57 +37,4 @@ export function findOverride(id: string): SoundOverride | null {
         return null;
 
     return result;
-}
-
-function SoundComponent({ type, override }: { type: SoundType; override: SoundOverride; }) {
-    const sound: React.MutableRefObject<SoundPlayer | null> = useRef(null);
-    return (
-        <Card style={{ padding: "1em 1em 0" }}>
-            <Switch
-                value={override.enabled}
-                onChange={value => override.enabled = value}
-                hideBorder={true}
-            >
-                {type.name} <span style={{ color: "var(--text-muted)" }}>({type.id})</span>
-            </Switch>
-            <Button
-                color={Button.Colors.PRIMARY}
-                className={Margins.bottom16}
-                onClick={() => {
-                    if (sound.current != null)
-                        sound.current.stop();
-                    sound.current = playSound(type.id);
-                }}
-                disabled={!override.enabled}
-            >
-                Preview
-            </Button>
-            <Forms.FormTitle>File</Forms.FormTitle>
-            <TextInput
-                type="text"
-                value={override.url}
-                onChange={value => override.url = value}
-                placeholder="Use default"
-                className={Margins.bottom16}
-                disabled={!override.enabled}
-            />
-            <Button
-                color={Button.Colors.PRIMARY}
-                className={Margins.bottom16}
-                onClick={() => {
-                }}
-                disabled={!override.enabled}
-            >
-                Preview
-            </Button>
-            <Forms.FormTitle>Volume</Forms.FormTitle>
-            <Slider
-                markers={makeRange(0, 100, 10)}
-                initialValue={override.volume}
-                onValueChange={value => override.volume = value}
-                className={Margins.bottom16}
-                disabled={!override.enabled}
-            />
-        </Card>
-    );
 }
