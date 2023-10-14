ed6cea4d72ef5226128c1f193bb5ecb2aeef61b4
TheKodeToad <TheKodeToad@proton.me>
2023-10-14 13:20:46 +0100

    Prevent videos playing in modal with NoMosaic

--- src/plugins/noMosaic/index.ts
+++ src/plugins/noMosaic/index.ts
@@ -4,17 +4,28 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { definePluginSettings } from "@api/Settings";
 import { disableStyle, enableStyle } from "@api/Styles";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 
 import style from "./styles.css?managed";
 
+const settings = definePluginSettings({
+    preventVideoModal: {
+        type: OptionType.BOOLEAN,
+        description: "Prevent videos from playing in a modal",
+        default: true,
+        restartNeeded: true
+    }
+});
+
 export default definePlugin({
     name: "NoMosaic",
-    authors: [Devs.AutumnVN],
+    authors: [Devs.AutumnVN, Devs.TheKodeToad],
     description: "Removes Discord new image mosaic",
     tags: ["image", "mosaic", "media"],
+    settings,
     patches: [{
         find: "Media Mosaic",
         replacement: [
@@ -30,6 +41,11 @@ export default definePlugin({
                 match: /null!==\(\i=\i\.get\(\i\)\)&&void 0!==\i\?\i:"INVALID"/,
                 replace: '"INVALID"',
             },
+            {
+                match: /\i\.length>1\?\(0,\i\.\i\)\(\i,\i\):{}/,
+                replace: "{}",
+                predicate: () => settings.store.preventVideoModal
+            }
         ],
     }],
     start() {
