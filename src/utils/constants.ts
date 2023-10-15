a7e205382e3f8bb5ba587a7349ca61c67b29b10a
Skully <contact@skully.tech>
2023-10-16 00:31:59 +0100

    update constants.ts Devs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -379,6 +379,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "ProffDea",
         id: 609329952180928513n
     },
+    Skully: {
+        name: "Skully",
+        id: 150298098516754432n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
