891e5d95992b6a7c9eb8400c3b1a4338e2f9b8f3
Jax121243 <101913901+Jax121243@users.noreply.github.com>
2023-10-21 22:24:24 +0800

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -379,6 +379,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "ProffDea",
         id: 609329952180928513n
     },
+    Jaxx: {
+        name: "Jaxx",
+        id: 901016640253227059n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
