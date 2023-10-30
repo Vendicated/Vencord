0f1563295e4f8c60e692421d5ed2e96e0170c566
ryan-0324 <77452312+ryan-0324@users.noreply.github.com>
2023-10-30 01:15:29 -0400

    Removed unnecessary </br> tag in settingsAboutComponent

--- src/plugins/fakeProfileThemesAndEffects/index.tsx
+++ src/plugins/fakeProfileThemesAndEffects/index.tsx
@@ -207,7 +207,7 @@ export default definePlugin({
             <Forms.FormSection>
                 <Forms.FormTitle tag={"h3"}>{"Usage"}</Forms.FormTitle>
                 <Forms.FormText>
-                    {"After enabling this plugin, you will see custom theme colors and effects in the profiles of other people using this plugin."}<br />
+                    {"After enabling this plugin, you will see custom theme colors and effects in the profiles of other people using this plugin."}
                     <div className={Margins.top8}>
                         <b>{"To set your own profile theme colors and effect:"}</b>
                     </div>
