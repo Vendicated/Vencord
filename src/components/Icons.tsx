872de2643f51efb5e7fe413688d1c81136eeefa7
Jack Matthews <jm5112356@gmail.com>
2023-10-31 18:53:35 -0400

    Lint

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -264,7 +264,7 @@ export function PlusIcon(props: IconProps) {
             viewBox="0 0 18 18"
         >
             <polygon
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 fill="currentColor"
                 points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"
             />
