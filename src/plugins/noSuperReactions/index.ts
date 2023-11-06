385cfc54b135f3c2e04dfc5f8d86378c2d9d00d6
throwaway-123456 <150086817+throwaway-123456@users.noreply.github.com>
2023-11-06 23:21:27 +0100

    Add missing import

--- src/plugins/noSuperReactions/index.ts
+++ src/plugins/noSuperReactions/index.ts
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { MessageStore } from "@webpack/common";
 
