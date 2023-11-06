Vendicated/Vencord

commit 4c75d7759677391977051a56303f42f75faf84eb

Fixed eslint errors

src/plugins/fakeProfileThemesAndEffects/index.tsx
@@ -1,30 +1,19 @@
 /*
- * Vencord, a modification for Discord's desktop app
+ * Vencord, a Discord client mod
  * Copyright (c) 2023 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
-import definePlugin, { OptionType } from "@utils/types";
 import { Margins } from "@utils/margins";
 import { copyWithToast } from "@utils/misc";
-import { Button, Forms, showToast, Slider, Switch, Text, Toasts, useState } from "@webpack/common";
+import definePlugin, { OptionType } from "@utils/types";
+import { Button, Forms, showToast, Switch, Text, Toasts, useState } from "@webpack/common";
+import { User } from "discord-types/general";
+
 import { openColorPickerModal } from "./components/ColorPickerModal";
 import { openProfileEffectModal } from "./components/ProfileEffectModal";
-import { User } from "discord-types/general";
 
 interface UserProfile extends User {
     themeColors: [number, number] | undefined;
@@ -145,7 +134,7 @@ function decode3y3(bio: string): [string, string, string] {
             if (i > 1) break;
             tempCPs = [];
             i++;
-        } else if (0xE0000 < currCP && currCP < 0xE007E) {
+        } else if (currCP < 0xE0000 && currCP < 0xE007E) {
             tempCPs.push(currCP - 0xE0000);
             if (j === bioCPs.length - 1)
                 decoded3y3[i] = String.fromCodePoint(...tempCPs);
@@ -263,7 +252,7 @@ function generate3y3(primary: number, accent: number, effect: string, legacy: bo
         // If the accent color is set and different from the primary color, it
         // will be encoded and added to the string prefixed by one separator.
         if (accent !== -1 && primary !== accent) {
-            str += sep + encode3y3(base10NumToBase125Str(accent))
+            str += sep + encode3y3(base10NumToBase125Str(accent));
 
             // If the effect is set, it will be encoded and added to the string prefixed by one separator.
             if (effect !== "")
@@ -292,7 +281,7 @@ function fetchProfileEffects(callback: (v: any) => void): void {
         .then((data: any) => {
             callback(data?.profile_effect_configs);
         })
-        .catch((e) => {
+        .catch(e => {
             console.error(e);
             showToast("Unable to retrieve the list of profile effects.", Toasts.Type.FAILURE);
         });
@@ -365,7 +354,7 @@ export default definePlugin({
                     </div>
                     <ol
                         className={Margins.bottom8}
-                        style={{listStyle: "decimal", paddingLeft: "40px"}}
+                        style={{ listStyle: "decimal", paddingLeft: "40px" }}
                     >
                         <li>{"Go to your profile settings"}</li>
                         <li>{"Use the 3y3 Builder to choose your profile theme colors and effect"}</li>
@@ -428,7 +417,7 @@ export default definePlugin({
                 <Text
                     tag={"h3"}
                     variant={"eyebrow"}
-                    style={{display: "inline"}}
+                    style={{ display: "inline" }}
                 >
                     {"3y3 Builder"}
                 </Text>
@@ -537,7 +526,7 @@ export default definePlugin({
                             paddingRight: "0"
                         }}
                         onClick={() => {
-                           const stringToCopy: string = generate3y3(primaryColor, accentColor, effectID, buildLegacy3y3);
+                            const stringToCopy: string = generate3y3(primaryColor, accentColor, effectID, buildLegacy3y3);
                             if (stringToCopy === "")
                                 showToast("3y3 Builder is empty; nothing to copy!");
                             else
@@ -550,7 +539,7 @@ export default definePlugin({
                 <Switch
                     value={buildLegacy3y3}
                     note={"Will use more characters"}
-                    onChange={(value: boolean) => {setBuildLegacy3y3(value)}}
+                    onChange={(value: boolean) => { setBuildLegacy3y3(value); }}
                 >
                     {"Build backwards compatible 3y3"}
                 </Switch>

src/plugins/fakeProfileThemesAndEffects/components/ColorPickerModal.tsx
@@ -1,19 +1,7 @@
 /*
- * Vencord, a modification for Discord's desktop app
+ * Vencord, a Discord client mod
  * Copyright (c) 2023 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
@@ -33,10 +21,10 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
         b /= 255;
         const xmax: number = Math.max(r, g, b);
         const xmin: number = Math.min(r, g, b);
-        const chroma: number  = xmax - xmin;
-        const v: number  = xmax;
-        let h: number  = 0;
-        let s: number  = 0;
+        const chroma: number = xmax - xmin;
+        const v: number = xmax;
+        let h: number = 0;
+        let s: number = 0;
         if (chroma > 0) {
             if (xmax === r) h = (g - b) / chroma;
             if (xmax === g) h = (b - r) / chroma + 2;
@@ -45,7 +33,7 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
         }
         h *= 60;
         return [h < 0 ? h + 360 : h, s, v];
-    }
+    };
 
     const HSVtoRGB = (h: number, s: number, v: number): [number, number, number] => {
         let chroma = s * v;
@@ -59,29 +47,29 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
             Math.round([x, chroma, chroma, x, m, m][index] * 255),
             Math.round([m, m, x, chroma, chroma, x][index] * 255)
         ];
-    }
+    };
 
     const RGBto24BitColor = (r: number, g: number, b: number): number => {
         return r * 65_536 + g * 256 + b;
-    }
+    };
 
     const _24BitColorToRGB = (c: number): [number, number, number] => {
         return [Math.trunc(c / 65_536), Math.trunc(c % 65_536 / 256), c % 65_536 % 256];
-    }
+    };
 
     const updateColor = (newSliderPos: number, newMarkerPos: [number, number]): void => {
         const newColor: number = RGBto24BitColor(...HSVtoRGB(newSliderPos * 360, newMarkerPos[0], 1 - newMarkerPos[1]));
         setColor(newColor);
         setInputColor(newColor.toString(16).padStart(6, "0"));
-    }
+    };
 
     const startDrag = (onMouseMoveFunc: (e: MouseEvent) => void): void => {
         document.addEventListener("mousemove", onMouseMoveFunc);
         document.addEventListener("mouseup",
-            () => {document.removeEventListener("mousemove", onMouseMoveFunc)},
+            () => { document.removeEventListener("mousemove", onMouseMoveFunc); },
             { once: true }
         );
-    }
+    };
 
     const colorAreaRef = useRef<HTMLDivElement>(null);
     const sliderBarRef = useRef<HTMLDivElement>(null);
@@ -104,14 +92,14 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
                         width: "100%"
                     }}
                 >
-                    <Text style={{color: "var(--header-primary)", fontSize: "20px", fontWeight: "600"}}>
+                    <Text style={{ color: "var(--header-primary)", fontSize: "20px", fontWeight: "600" }}>
                         {"Color Picker"}
                     </Text>
                     <ModalCloseButton onClick={onClose} />
                 </div>
             </ModalHeader>
             <ModalContent>
-                <div style={{width: "220px", padding: "16px"}}>
+                <div style={{ width: "220px", padding: "16px" }}>
                     <div
                         ref={colorAreaRef}
                         style={{
@@ -135,7 +123,7 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
                                     ];
                                     setMarkerPos(newMarkerPos);
                                     updateColor(sliderPos, newMarkerPos);
-                                }
+                                };
                                 startDrag(onMouseMoveFunc);
                             }
                         }}
@@ -173,7 +161,7 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
                                     const newSliderPos: number = e.pageX < rect.left ? 0 : e.pageX > rect.right ? 1 : (e.pageX - rect.left) / rect.width;
                                     setSliderPos(newSliderPos);
                                     updateColor(newSliderPos, markerPos);
-                                }
+                                };
                                 startDrag(onMouseMoveFunc);
                             }
                         }}
@@ -252,7 +240,7 @@ export function ColorPickerModal({ modalProps, onClose, onSubmit, initialColor =
                 <Button
                     color={Button.Colors.PRIMARY}
                     size={Button.Sizes.MEDIUM}
-                    onClick={() => {onSubmit(color)}}
+                    onClick={() => { onSubmit(color); }}
                 >
                     {"Apply"}
                 </Button>
@@ -265,9 +253,9 @@ export function openColorPickerModal(onSubmit: (v: number) => void, initialColor
     const key = openModal(modalProps =>
         <ColorPickerModal
             modalProps={modalProps}
-            onClose={() => {closeModal(key)}}
+            onClose={() => { closeModal(key); }}
             onSubmit={onSubmit}
             initialColor={initialColor}
         />
     );
-};
+}

src/plugins/fakeProfileThemesAndEffects/components/ProfileEffectModal.tsx
@@ -1,19 +1,7 @@
 /*
- * Vencord, a modification for Discord's desktop app
+ * Vencord, a Discord client mod
  * Copyright (c) 2023 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
@@ -40,7 +28,7 @@ export function ProfileEffectModal({ modalProps, onClose, onSubmit, profileEffec
                         width: "100%"
                     }}
                 >
-                    <Text style={{color: "var(--header-primary)", fontSize: "20px", fontWeight: "600"}}>
+                    <Text style={{ color: "var(--header-primary)", fontSize: "20px", fontWeight: "600" }}>
                         {"Add Profile Effect"}
                     </Text>
                     <ModalCloseButton onClick={onClose} />
@@ -64,7 +52,7 @@ export function ProfileEffectModal({ modalProps, onClose, onSubmit, profileEffec
                             width: "80px",
                             height: "80px"
                         }}
-                        onClick={() => {setSelected(i)}}
+                        onClick={() => { setSelected(i); }}
                     />
                 )}
             </ModalContent>
@@ -77,7 +65,7 @@ export function ProfileEffectModal({ modalProps, onClose, onSubmit, profileEffec
                         width: "100%"
                     }}
                 >
-                    <Text style={{color: "var(--header-primary)", fontSize: "20px", fontWeight: "600"}}>
+                    <Text style={{ color: "var(--header-primary)", fontSize: "20px", fontWeight: "600" }}>
                         {selected === -1 ? "" : profileEffects[selected].title}
                     </Text>
                     <Button
@@ -102,9 +90,9 @@ export function openProfileEffectModal(onSubmit: (i: string, n: string) => void,
     const key = openModal(modalProps =>
         <ProfileEffectModal
             modalProps={modalProps}
-            onClose={() => {closeModal(key)}}
+            onClose={() => { closeModal(key); }}
             onSubmit={onSubmit}
             profileEffects={profileEffects}
         />
     );
-};
+}

commit c291f75b48f01364dec3adf631b5a775b8b65e8f

Fixed a regression in https://github.com/Vendicated/Vencord/commit/4c75d7759677391977051a56303f42f75faf84eb

src/plugins/fakeProfileThemesAndEffects/index.tsx
@@ -134,7 +134,7 @@ function decode3y3(bio: string): [string, string, string] {
             if (i > 1) break;
             tempCPs = [];
             i++;
-        } else if (currCP < 0xE0000 && currCP < 0xE007E) {
+        } else if (currCP > 0xE0000 && currCP < 0xE007E) {
             tempCPs.push(currCP - 0xE0000);
             if (j === bioCPs.length - 1)
                 decoded3y3[i] = String.fromCodePoint(...tempCPs);
@@ -274,7 +274,7 @@ function generate3y3(primary: number, accent: number, effect: string, legacy: bo
 }
 
 function fetchProfileEffects(callback: (v: any) => void): void {
-    fetch("/api/v9/user-profile-effects", { mode:"same-origin", cache: "only-if-cached" })
+    fetch("/api/v9/user-profile-effects", { mode: "same-origin", cache: "only-if-cached" })
         .then((response: Response): Promise<any> => {
             return response.json();
         })