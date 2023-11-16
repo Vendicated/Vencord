2efbf75452d51edcf595034a54d11c35e0c42bdf
adryd <me@adryd.com>
2023-11-15 19:26:35 -0500

    oneko: update script version (#1994)

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -26,15 +26,13 @@ export default definePlugin({
     authors: [Devs.Ven, Devs.adryd],
 
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/5977144dce83e4d71af1de005d16e38eebeb7b72/oneko.js")
+        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
             .then(x => x.text())
             .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif"))
             .then(eval);
     },
 
     stop() {
-        clearInterval(window.onekoInterval);
-        delete window.onekoInterval;
         document.getElementById("oneko")?.remove();
     }
 });
