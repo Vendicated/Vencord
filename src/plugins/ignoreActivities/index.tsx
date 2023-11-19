4812c8ad6e73e6825c497e788a26b59fac8837cf
Nuckyz <contact@swezy.dev>
2023-11-18 22:53:50 -0300

    IgnoreActivities: fix and improvements

--- src/plugins/ignoreActivities/index.tsx
+++ src/plugins/ignoreActivities/index.tsx
@@ -8,7 +8,6 @@ import * as DataStore from "@api/DataStore";
 import { definePluginSettings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
-import { useForceUpdater } from "@utils/react";
 import definePlugin from "@utils/types";
 import { findStoreLazy } from "@webpack";
 import { StatusSettingsStores, Tooltip } from "webpack/common";
@@ -27,14 +26,12 @@ interface IgnoredActivity {
 const RunningGameStore = findStoreLazy("RunningGameStore");
 
 function ToggleIcon(activity: IgnoredActivity, tooltipText: string, path: string, fill: string) {
-    const forceUpdate = useForceUpdater();
-
     return (
         <Tooltip text={tooltipText}>
             {tooltipProps => (
                 <button
                     {...tooltipProps}
-                    onClick={e => handleActivityToggle(e, activity, forceUpdate)}
+                    onClick={e => handleActivityToggle(e, activity)}
                     style={{ all: "unset", cursor: "pointer", display: "flex", justifyContent: "center", alignItems: "center" }}
                 >
                     <svg
@@ -54,11 +51,14 @@ const ToggleIconOn = (activity: IgnoredActivity, fill: string) => ToggleIcon(act
 const ToggleIconOff = (activity: IgnoredActivity, fill: string) => ToggleIcon(activity, "Enable Activity", "m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z", fill);
 
 function ToggleActivityComponent(activity: IgnoredActivity, isPlaying = false) {
-    if (getIgnoredActivities().some(act => act.id === activity.id)) return ToggleIconOff(activity, "var(--status-danger)");
+    const s = settings.use(["ignoredActivities"]);
+    const { ignoredActivities = [] } = s;
+
+    if (ignoredActivities.some(act => act.id === activity.id)) return ToggleIconOff(activity, "var(--status-danger)");
     return ToggleIconOn(activity, isPlaying ? "var(--green-300)" : "var(--primary-400)");
 }
 
-function handleActivityToggle(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, activity: IgnoredActivity, forceUpdateButton: () => void) {
+function handleActivityToggle(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, activity: IgnoredActivity) {
     e.stopPropagation();
 
     const ignoredActivityIndex = getIgnoredActivities().findIndex(act => act.id === activity.id);
@@ -67,7 +67,6 @@ function handleActivityToggle(e: React.MouseEvent<HTMLButtonElement, MouseEvent>
 
     // Trigger activities recalculation
     StatusSettingsStores.ShowCurrentGame.updateSetting(old => old);
-    forceUpdateButton();
 }
 
 const settings = definePluginSettings({}).withPrivateSettings<{
@@ -90,8 +89,8 @@ export default definePlugin({
             find: '.displayName="LocalActivityStore"',
             replacement: [
                 {
-                    match: /LISTENING.+?}\),(?<=(\i)\.push.+?)/,
-                    replace: (m, activities) => `${m}${activities}=${activities}.filter($self.isActivityNotIgnored),`
+                    match: /HANG_STATUS.+?(?=!\i\(\i,\i\)&&)(?<=(\i)\.push.+?)/,
+                    replace: (m, activities) => `${m}${activities}=${activities}.filter($self.isActivityNotIgnored);`
                 }
             ]
         },
