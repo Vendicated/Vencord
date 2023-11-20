d5c8ab4d68d508da740d9ef0f10abddae89e2d46
nmsturcke <30734036+nmsturcke@users.noreply.github.com>
2023-11-20 18:34:07 +0100

    Ignore deleted messages (MessageLogger)

--- src/plugins/messageClickActions/index.ts
+++ src/plugins/messageClickActions/index.ts
@@ -72,6 +72,7 @@ export default definePlugin({
                 if (event.detail < 2) return;
                 if (settings.store.requireModifier && !event.ctrlKey && !event.shiftKey) return;
                 if (channel.guild_id && !PermissionStore.can(PermissionsBits.SEND_MESSAGES, channel)) return;
+                if (msg.deleted === true) return;
 
                 if (isMe) {
                     if (!settings.store.enableDoubleClickToEdit || EditStore.isEditing(channel.id, msg.id)) return;
