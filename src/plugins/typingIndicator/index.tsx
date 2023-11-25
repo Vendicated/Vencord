867730a478d0b7a87b46ecc9f8080d66dde2cd68
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2023-11-24 23:14:18 -0300

    Simplify some components finds; Make undo of patch groups more clear

--- src/plugins/typingIndicator/index.tsx
+++ src/plugins/typingIndicator/index.tsx
@@ -20,17 +20,12 @@ import { definePluginSettings, Settings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { find, findStoreLazy, LazyComponentWebpack } from "@webpack";
+import { findExportedComponentLazy, findStoreLazy } from "@webpack";
 import { ChannelStore, GuildMemberStore, i18n, RelationshipStore, Tooltip, UserStore, useStateFromStores } from "@webpack/common";
 
 import { buildSeveralUsers } from "../typingTweaks";
 
-const ThreeDots = LazyComponentWebpack(() => {
-    // This doesn't really need to explicitly find Dots' own module, but it's fine
-    const res = find(m => m.Dots && !m.Menu);
-
-    return res?.Dots;
-});
+const ThreeDots = findExportedComponentLazy("Dots", "AnimatedDots");
 
 const TypingStore = findStoreLazy("TypingStore");
 const UserGuildSettingsStore = findStoreLazy("UserGuildSettingsStore");
