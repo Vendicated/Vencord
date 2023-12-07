6eb26fdc729b99e61eb45ebedb0484798629a887
LordElias <kerlinelias@gmail.com>
2023-12-07 15:45:06 +0100

    fix errors

--- src/plugins/userVoiceShow/components/VoiceChannelSection.tsx
+++ src/plugins/userVoiceShow/components/VoiceChannelSection.tsx
@@ -19,16 +19,14 @@
 import "./VoiceChannelSection.css";
 
 import { Flex } from "@components/Flex";
-import { findByCodeLazy, findByPropsLazy, findLazy } from "@webpack";
-import { Button, Forms, PermissionStore, Toasts } from "@webpack/common";
+import { findByCodeLazy, findByPropsLazy } from "@webpack";
+import { Button, Forms, NavigationRouter, PermissionsBits, PermissionStore, Toasts } from "@webpack/common";
 import { Channel } from "discord-types/general";
 
 import eyeSvg from "./eye.svg";
 
 const ChannelActions = findByPropsLazy("selectChannel", "selectVoiceChannel");
 const UserPopoutSection = findByCodeLazy(".lastSection", "children:");
-const DiscordPermissions = findLazy(m => m.VIEW_CREATOR_MONETIZATION_ANALYTICS && !m.A11Y_ROLE_SWITCH);
-const transitionTo = findByCodeLazy("transitionTo -");
 
 
 interface VoiceChannelFieldProps {
@@ -37,61 +35,61 @@ interface VoiceChannelFieldProps {
     showHeader: boolean;
 }
 
-export const VoiceChannelSection = ({ channel, label, showHeader }: VoiceChannelFieldProps) => (console.log(ChannelActions),
-<UserPopoutSection>
-    {showHeader && <Forms.FormTitle className="vc-uvs-header">In a voice channel</Forms.FormTitle>}
-    <Flex
-        flexDirection="row"
-        style={{
-            flexShrink: 0,
-            flexGrow: 0,
-            gap: "0.5em",
-        }}
-    >
-        <Button
-            className="vc-uvs-button"
-            color={Button.Colors.TRANSPARENT}
-            size={Button.Sizes.SMALL}
-
-            onClick={() => {
-                if (PermissionStore.can(DiscordPermissions.CONNECT, channel))
-                    ChannelActions.selectVoiceChannel(channel.id);
-                else
-                    Toasts.show({
-                        message: "Insufficient permissions to enter the channel.",
-                        id: "user-voice-show-insufficient-permissions",
-                        type: Toasts.Type.FAILURE,
-                        options: {
-                            position: Toasts.Position.BOTTOM,
-                        }
-                    });
+export const VoiceChannelSection = ({ channel, label, showHeader }: VoiceChannelFieldProps) => (
+    <UserPopoutSection>
+        {showHeader && <Forms.FormTitle className="vc-uvs-header">In a voice channel</Forms.FormTitle>}
+        <Flex
+            flexDirection="row"
+            style={{
+                flexShrink: 0,
+                flexGrow: 0,
+                gap: "0.5em",
             }}
         >
-            {label}
-        </Button>
-        <Button
-            className="vc-uvs-button-view"
-            color={Button.Colors.TRANSPARENT}
-            size={Button.Sizes.SMALL}
+            <Button
+                className="vc-uvs-button"
+                color={Button.Colors.TRANSPARENT}
+                size={Button.Sizes.SMALL}
 
-            onClick={() => {
-                if (PermissionStore.can(DiscordPermissions.VIEW_CHANNEL, channel))
-                    transitionTo(getChannelPath(channel));
-                else
-                    Toasts.show({
-                        message: "Insufficient permissions to view the channel.",
-                        id: "user-voice-show-insufficient-permissions",
-                        type: Toasts.Type.FAILURE,
-                        options: {
-                            position: Toasts.Position.BOTTOM,
-                        }
-                    });
-            }}
-        >
-            {eyeSvg()}
-        </Button>
-    </Flex>
-</UserPopoutSection>
+                onClick={() => {
+                    if (PermissionStore.can(PermissionsBits.CONNECT, channel))
+                        ChannelActions.selectVoiceChannel(channel.id);
+                    else
+                        Toasts.show({
+                            message: "Insufficient permissions to enter the channel.",
+                            id: "user-voice-show-insufficient-permissions",
+                            type: Toasts.Type.FAILURE,
+                            options: {
+                                position: Toasts.Position.BOTTOM,
+                            }
+                        });
+                }}
+            >
+                {label}
+            </Button>
+            <Button
+                className="vc-uvs-button-view"
+                color={Button.Colors.TRANSPARENT}
+                size={Button.Sizes.SMALL}
+
+                onClick={() => {
+                    if (PermissionStore.can(PermissionsBits.VIEW_CHANNEL, channel))
+                        NavigationRouter.transitionTo(getChannelPath(channel));
+                    else
+                        Toasts.show({
+                            message: "Insufficient permissions to view the channel.",
+                            id: "user-voice-show-insufficient-permissions",
+                            type: Toasts.Type.FAILURE,
+                            options: {
+                                position: Toasts.Position.BOTTOM,
+                            }
+                        });
+                }}
+            >
+                {eyeSvg()}
+            </Button>
+        </Flex>
+    </UserPopoutSection>
 );
 
 const getChannelPath = (c: Channel) => `/channels/${c.guild_id ?? "@me"}/${c.id}`;
