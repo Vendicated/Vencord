5eb392bce5241985cb7784a01fc20a498878228c
AutumnVN <contact@swezy.dev>
2023-12-10 08:05:40 +0700

    oneko: allow oneko in reduced motion (#2018)

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -28,7 +28,8 @@ export default definePlugin({
     start() {
         fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
             .then(x => x.text())
-            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif"))
+            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+                .replace("(isReducedMotion)", "(false)"))
             .then(eval);
     },
 
