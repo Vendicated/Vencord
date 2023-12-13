2c3bb4e9e9b4a55f98edd5e01022d23d3845fb9b
Andrew Grant <andrew.grant@mail.rit.edu>
2023-12-13 00:21:56 -0500

    Optimize and reposition SVG
    Updated src/plugins/previewMessage/index.tsx

--- src/plugins/previewMessage/index.tsx
+++ src/plugins/previewMessage/index.tsx
@@ -110,15 +110,18 @@ export function PreviewButton(chatBoxProps: Props) {
                         )}
                     size=""
                     look={ButtonLooks.BLANK}
-                    innerClassName={ButtonWrapperClasses.button}
-                    style={{ padding: "0 2px 0 0", height: "100%" }}
+                    innerClassName={ButtonWrapperClasses.button + " " + ButtonWrapperClasses.buttonWrapper}
                 >
-                    <div className={ButtonWrapperClasses.buttonWrapper}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M15.56 11.77c.2-.1.44.02.44.23a4 4 0 1 1-4-4c.21 0 .33.25.23.44a2.5 2.5 0 0 0 3.32 3.32Z"></path>
-                        <path fill="currentColor" fill-rule="evenodd" d="M22.89 11.7c.07.2.07.4 0 .6C22.27 13.9 19.1 21 12 21c-7.11 0-10.27-7.11-10.89-8.7a.83.83 0 0 1 0-.6C1.73 10.1 4.9 3 12 3c7.11 0 10.27 7.11 10.89 8.7Zm-4.5-3.62A15.11 15.11 0 0 1 20.85 12c-.38.88-1.18 2.47-2.46 3.92C16.87 17.62 14.8 19 12 19c-2.8 0-4.87-1.38-6.39-3.08A15.11 15.11 0 0 1 3.15 12c.38-.88 1.18-2.47 2.46-3.92C7.13 6.38 9.2 5 12 5c2.8 0 4.87 1.38 6.39 3.08Z" clip-rule="evenodd"></path>
-                        </svg>
-                    </div>
+                    <svg
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        style={{ padding: "4px", margin: "0 4px" }}
+                    >
+                        <path d="M22.89 11.7c.07.2.07.4 0 .6C22.27 13.9 19.1 21 12 21c-7.11 0-10.27-7.11-10.89-8.7a.83.83 0 0 1 0-.6C1.73 10.1 4.9 3 12 3c7.11 0 10.27 7.11 10.89 8.7Zm-4.5-3.62A15.11 15.11 0 0 1 20.85 12c-.38.88-1.18 2.47-2.46 3.92C16.87 17.62 14.8 19 12 19c-2.8 0-4.87-1.38-6.39-3.08A15.11 15.11 0 0 1 3.15 12c.38-.88 1.18-2.47 2.46-3.92C7.13 6.38 9.2 5 12 5c2.8 0 4.87 1.38 6.39 3.08ZM15.56 11.77c.2-.1.44.02.44.23a4 4 0 1 1-4-4c.21 0 .33.25.23.44a2.5 2.5 0 0 0 3.32 3.32Z" />
+                    </svg>
                 </Button>
             )}
         </Tooltip>
