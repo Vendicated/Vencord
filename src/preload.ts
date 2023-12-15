faed5b57e7d8f926bbffee27f9498deee531533f
Ashley :3 <iamashley@tuta.io>
2023-12-15 22:02:38 +0300

    Chore: Refactor Code

--- src/preload.ts
+++ src/preload.ts
@@ -23,42 +23,49 @@ import { join } from "path";
 
 import VencordNative from "./VencordNative";
 
+// Expose VencordNative in the main world
 contextBridge.exposeInMainWorld("VencordNative", VencordNative);
 
 // Discord
 if (location.protocol !== "data:") {
     // #region cssInsert
     const rendererCss = join(__dirname, IS_VESKTOP ? "vencordDesktopRenderer.css" : "renderer.css");
-
+    
+    // Create and append style element
     const style = document.createElement("style");
     style.id = "vencord-css-core";
     style.textContent = readFileSync(rendererCss, "utf-8");
+    
+    // Append style element to the document when ready
+    const appendStyleToDocument = () => document.documentElement.appendChild(style);
 
     if (document.readyState === "complete") {
-        document.documentElement.appendChild(style);
+        appendStyleToDocument();
     } else {
-        document.addEventListener("DOMContentLoaded", () => document.documentElement.appendChild(style), {
-            once: true
-        });
+        document.addEventListener("DOMContentLoaded", appendStyleToDocument, { once: true });
     }
 
     if (IS_DEV) {
-        // persistent means keep process running if watcher is the only thing still running
-        // which we obviously don't want
+        // Watch for changes in rendererCss and update style accordingly
         watch(rendererCss, { persistent: false }, () => {
-            document.getElementById("vencord-css-core")!.textContent = readFileSync(rendererCss, "utf-8");
+            const coreStyleElement = document.getElementById("vencord-css-core");
+            if (coreStyleElement) {
+                coreStyleElement.textContent = readFileSync(rendererCss, "utf-8");
+            }
         });
     }
     // #endregion
 
     if (IS_DISCORD_DESKTOP) {
+        // Execute JavaScript and require preload in Discord desktop environment
         webFrame.executeJavaScript(readFileSync(join(__dirname, "renderer.js"), "utf-8"));
         require(process.env.DISCORD_PRELOAD!);
     }
-} // Monaco popout
-else {
+} else {
+    // Monaco popout
     contextBridge.exposeInMainWorld("setCss", debounce(VencordNative.quickCss.set));
     contextBridge.exposeInMainWorld("getCurrentCss", VencordNative.quickCss.get);
     // shrug
     contextBridge.exposeInMainWorld("getTheme", () => "vs-dark");
 }
+
