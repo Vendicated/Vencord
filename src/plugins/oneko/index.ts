5f7c18a5666f5ecdb768d484b451e0cca087310f
varram <53030931+not-varram@users.noreply.github.com>
2024-01-13 10:06:33 +0530

    fix issues based on comments :3

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -16,9 +16,35 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+
+import { definePluginSettings } from "@api/Settings";
 import { Settings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+export const onekoSettings = definePluginSettings({
+    speed: {
+        description: "Speed of Da Cat :3",
+        type: OptionType.NUMBER,
+        default: 10,
+        isValid: (value: number) => {
+            if (value && value < 0) return "The number must be bigger than 0";
+            return true;
+        },
+        onChange: () => {
+            // note: cant call the start() function from here. so i just copy pasted it (This was pointed out in the last commit i made. So this is to just clear stuff up for any future devs that work on this :D )
+            if (Settings.plugins.oneko.enabled) {
+                document.getElementById("oneko")?.remove();
+                fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+                    .then(x => x.text())
+                    .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
+                    .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+                        .replace("(isReducedMotion)", "(false)"))
+                    .then(eval);
+            }
+            return;
+        }
+    },
+});
 
 export default definePlugin({
     name: "oneko",
@@ -26,34 +52,12 @@ export default definePlugin({
     // Listing adryd here because this literally just evals her script
     authors: [Devs.Ven, Devs.adryd, Devs.varram],
 
-    options: {
-        randomisedLength: {
-            description: "Speed",
-            type: OptionType.NUMBER,
-            default: 10,
-            isValid: (value: number) => {
-                if (value && value < 0) return "The number must be bigger than 0";
-                return true;
-            },
-            onChange: () => {
-                if (Settings.plugins.oneko.enabled) {
-                    document.getElementById("oneko")?.remove();
-                    fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
-                        .then(x => x.text())
-                        .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${Settings.plugins.oneko.randomisedLength};`))
-                        .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
-                            .replace("(isReducedMotion)", "(false)"))
-                        .then(eval);
-                }
-                return;
-            }
-        },
-    },
+    settings: onekoSettings,
 
     start() {
         fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
             .then(x => x.text())
-            .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${Settings.plugins.oneko.randomisedLength};`))
+            .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
             .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
                 .replace("(isReducedMotion)", "(false)"))
             .then(eval);
