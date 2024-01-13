cb1040d517c74133267cab3359c0ffee7de9c57d
varram <53030931+not-varram@users.noreply.github.com>
2024-01-13 10:10:23 +0530

    fix merge conflict

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -399,6 +399,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "maisy",
         id: 257109471589957632n,
     },
+    Grzesiek11: {
+        name: "Grzesiek11",
+        id: 368475654662127616n,
+    },
     varram: {
         name: "varram",
         id: 588937914423967766n
