2a63d8da541c79fdf8523b90852494a74591ad1f
David L <77948280+TechFun4@users.noreply.github.com>
2024-01-15 06:28:46 -0800

    better fully fix now hehe

--- src/plugins/NoMoreRacism/index.ts
+++ src/plugins/NoMoreRacism/index.ts
@@ -24,7 +24,7 @@ import definePlugin from "@utils/types";
 const change = async (_, message) => {
     if (!message.content) return;
     message.content = message.content
-    .replace(/\b(digger|nigger)\b/gi, function(match) {
+    .replace(/\b([dn]iggers?)\b/gi, function(match) {
         return match.replace(/./g, function(char, index) {
           if (match[index] == "d") return "n"
           if (match[index] == "D") return "N"
@@ -33,7 +33,7 @@ const change = async (_, message) => {
           return match[index];
         });
     })
-    .replace(/\b(digga|nigga)\b/gi, function(match) {
+    .replace(/\b([dn]iggas?)\b/gi, function(match) {
         return match.replace(/./g, function(char, index) {
             if (match[index] == "d") return "n"
             if (match[index] == "D") return "N"
