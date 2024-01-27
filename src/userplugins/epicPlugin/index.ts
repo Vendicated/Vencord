4cfef5cc66ea541c041235c46ff1b71fb1708eb0
Riham M <118399375+Rcxz0@users.noreply.github.com>
2024-01-27 20:50:32 +0300

    Update index.ts

--- src/userplugins/epicPlugin/index.ts
+++ src/userplugins/epicPlugin/index.ts
@@ -1,1 +1,16 @@
+import definePlugin from "@utils/types";
 
+export default definePlugin({
+    name: "Plugin test",
+    description: "Plugin test",
+    authors: [
+        {
+            id: 570717618323980321n,
+            name: "Riham",
+        },
+    ],
+    patches: [],
+    // Delete these two below if you are only using code patches
+    start() {},
+    stop() {},
+});
