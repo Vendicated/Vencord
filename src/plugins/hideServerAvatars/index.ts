c12312a7f49fb234d7bf5fe22c7039e2762f6337
crwn1337 <38888474+crwn1337@users.noreply.github.com>
2024-02-03 19:41:21 +0200

    add a plugin that hides server avatars

---
+++ src/plugins/hideServerAvatars/index.ts
@@ -0,0 +1,43 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "HideServerAvatars",
+    description: "Disables server avatars.",
+    authors: [Devs.crwn],
+    patches: [
+        // for chat messages
+        {
+            find: ".default.getGuildMemberAvatarURLSimple",
+            replacement: {
+                match: /this\.guildMemberAvatars\[\w+\]/,
+                replace: "null",
+            },
+        },
+        // for replies
+        {
+            find: "renderSingleLineMessage:",
+            replacement: {
+                match: /t\.guildMemberAvatar/,
+                replace: "null",
+            },
+        }],
+});
