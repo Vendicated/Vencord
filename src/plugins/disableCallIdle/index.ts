6c8a398ce7319f1d589e367c6ae11311aeb7b4b2
Nuckyz <contact@swezy.dev>
2024-02-09 17:59:40 -0300

    DisableDMCallIdle -> DisableCallIdle; add more functionality

--- src/plugins/disableCallIdle/index.ts
+++ src/plugins/disableCallIdle/index.ts
@@ -16,12 +16,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { migratePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
+migratePluginSettings("DisableCallIdle", "DisableDMCallIdle");
 export default definePlugin({
-    name: "DisableDMCallIdle",
-    description: "Disables automatically getting kicked from a DM voice call after 3 minutes.",
+    name: "DisableCallIdle",
+    description: "Disables automatically getting kicked from a DM voice call after 3 minutes and being moved to an AFK voice channel.",
     authors: [Devs.Nuckyz],
     patches: [
         {
@@ -29,6 +31,13 @@ export default definePlugin({
             replacement: {
                 match: /,?(?=this\.idleTimeout=new \i\.Timeout)/,
                 replace: ";return;"
+            }
+        },
+        {
+            find: "handleIdleUpdate(){",
+            replacement: {
+                match: /(?<=_initialize\(\){)/,
+                replace: "return;"
             }
         }
     ]
