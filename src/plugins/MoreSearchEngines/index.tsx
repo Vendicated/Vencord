7993d8edf0c4aff3737657472317bb2b8fb3693e
unknown <gplanet.online123@gmail.com>
2024-03-01 21:27:43 +0200

    update and bug fix

--- src/plugins/MoreSearchEngines/index.tsx
+++ src/plugins/MoreSearchEngines/index.tsx
@@ -5,35 +5,71 @@
  */
 
 import { addContextMenuPatch, findGroupChildrenByChildId, NavContextMenuPatchCallback, removeContextMenuPatch } from "@api/ContextMenu";
+import { definePluginSettings } from "@api/Settings";
 import { Flex } from "@components/Flex";
+import PluginModal from "@components/PluginSettings/PluginModal";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
-import { Menu, Toasts } from "@webpack/common";
+import { openModal } from "@utils/modal";
+import definePlugin, { OptionType } from "@utils/types";
+import { Menu, SettingsRouter } from "@webpack/common";
+
+const testieWestie: boolean = true;
 
 const Engines = {
-    Google: "https://www.google.com/search?q=",
     DuckDuckGo: "https://duckduckgo.com/",
     Bing: "https://www.bing.com/search?q=",
     Yahoo: "https://search.yahoo.com/search?p=",
+    Github: "https://github.com/search?q=",
+    Kagi: "https://kagi.com/search?q=",
     Yandex: "https://yandex.com/search/?text=",
     AOL: "https://search.aol.com/aol/search?q=",
-    BaiDu: "https://www.baidu.com/s?wd=",
+    Baidu: "https://www.baidu.com/s?wd=",
     Wikipedia: "https://wikipedia.org/w/index.php?search="
 } as const;
+
+function getSettings(): Record<string, any> {
+    const settings = {};
+
+    Object.keys(Engines).map((engine, index) => {
+        settings[engine] = {
+            type: OptionType.BOOLEAN,
+            description: `Add ${engine} to to the context menu`,
+            default: true
+        };
+    });
+
+    return settings;
+}
+
+const settings = definePluginSettings(getSettings());
 
 function search(src: string, engine: string) {
     open(engine + encodeURIComponent(src), "_blank");
 }
 
+function areBrowsersDisabled(): boolean {
+    for (const engine in Engines) {
+        if (settings.store[engine]) {
+            return false;
+        };
+    }
+
+    return true;
+}
+
+
 function makeSearchItem(src: string) {
+    const browsersDisabled = areBrowsersDisabled();
+
     return (
         <Menu.MenuItem
-            label="Search Content"
+            label="More Search Engines"
             key="search-content"
             id="search-content"
         >
             {Object.keys(Engines).map((engine, i) => {
                 const key = "search-content-" + engine;
+                if (!settings.store[engine]) return null; // Return null for engines not selected
                 return (
                     <Menu.MenuItem
                         key={key}
@@ -42,7 +78,7 @@ function makeSearchItem(src: string) {
                             <Flex style={{ alignItems: "center", gap: "0.5em" }}>
                                 <img
                                     style={{
-                                        borderRadius: i >= 3 // Do not round Google, Yandex & SauceNAO
+                                        borderRadius: i >= 3
                                             ? "50%"
                                             : void 0
                                     }}
@@ -58,14 +94,36 @@ function makeSearchItem(src: string) {
                     />
                 );
             })}
+            {browsersDisabled && (
+                <Menu.MenuItem
+                    key="search-content-none"
+                    id="search-content-none"
+                    label={
+                        <Flex style={{ alignItems: "center", gap: "0.5em" }}>
+                            Add engines
+                        </Flex>
+                    }
+
+                    action={() => {
+                        SettingsRouter.open("VencordPlugins");
+                    }}
+                />
+            )}
         </Menu.MenuItem>
     );
 }
 
 const messageContextMenuPatch: NavContextMenuPatchCallback = (children, props) => () => {
-    const message = props.message.content;
+    let message = props.message.content;
+    const selection = document.getSelection()?.toString();
+
     if (!message) return;
-    const group = findGroupChildrenByChildId("copy-link", children);
+    const group = findGroupChildrenByChildId("search-google", children);
+
+    if (selection) {
+        message = selection;
+    }
+
     group?.push(makeSearchItem(message));
 };
 
@@ -73,6 +131,7 @@ export default definePlugin({
     name: "MoreSearchEngines",
     authors: [Devs.Moxxie, Devs.Ethan],
     description: "Allows you to search messages in diffrent search engines!",
+    settings,
 
     start() {
         addContextMenuPatch("message", messageContextMenuPatch);
