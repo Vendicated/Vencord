ab1df777002db696b1a5717a658c7bc1709cb057
dolfies <jeyalfie47@gmail.com>
2024-03-03 13:42:10 -0500

    feat(plugin): ResurrectHome

---
+++ src/plugins/resurrectHome/README.md
@@ -0,0 +1,5 @@
+# ResurrectHome
+
+Brings back the phased out [Server Home](https://support.discord.com/hc/en-us/articles/6156116949911-Server-Home-Beta) feature!
+
+![]()
---
+++ src/plugins/resurrectHome/index.ts
@@ -0,0 +1,60 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "ResurrectHome",
+    description: "Reenables the Server Home tab when there isn't a Server Guide.",
+    authors: [Devs.Dolfies],
+    patches: [
+        // Force home deprecation override
+        {
+            find: "GuildFeatures.GUILD_HOME_DEPRECATION_OVERRIDE",
+            all: true,
+            noWarn: true,
+            replacement: [
+                {
+                    match: /\i\.hasFeature\(\i\.GuildFeatures\.GUILD_HOME_DEPRECATION_OVERRIDE\)/g,
+                    replace: "true",
+                },
+            ],
+        },
+        // Disable feedback prompts
+        {
+            find: "GuildHomeFeedbackExperiment.definition.id",
+            replacement: [
+                {
+                    match: /return{showFeedback:\i,setOnDismissedFeedback:(\i)}/g,
+                    replace: "return{showFeedback:false,setOnDismissedFeedback:$1}",
+                },
+            ],
+        },
+        // Enable guild feed render mode selector
+        {
+            find: "2022-01_home_feed_toggle",
+            replacement: [
+                {
+                    match: /showSelector:!1/,
+                    replace: "showSelector:true",
+                },
+            ],
+        },
+    ]
+});
