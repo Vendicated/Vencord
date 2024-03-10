d23acffcf4de53e11061211514228d78d1d18f62
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-03-10 01:44:36 +0000

    add myself as author

--- src/plugins/userVoiceShow/index.tsx
+++ src/plugins/userVoiceShow/index.tsx
@@ -91,7 +91,7 @@ const VoiceChannelField = ErrorBoundary.wrap(({ user }: UserProps) => {
 export default definePlugin({
     name: "UserVoiceShow",
     description: "Shows whether a User is currently in a voice channel somewhere in their profile",
-    authors: [Devs.LordElias],
+    authors: [Devs.LordElias, Devs.Johannes7k75],
     settings,
 
     patchModal({ user }: UserProps) {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -422,6 +422,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Kyuuhachi: {
         name: "Kyuuhachi",
         id: 236588665420251137n,
+    },
+    Johannes7k75: {
+        name: "Johannes7k75",
+        id: 587701169103699994n
     }
 } satisfies Record<string, Dev>);
 
