26c71f27dde5243b6e8da4cc66b96acb08efd173
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2024-03-10 08:16:40 -0400

    Fix types in HolyNotes namespace

--- src/plugins/holynotes/types.ts
+++ src/plugins/holynotes/types.ts
@@ -11,37 +11,37 @@ export declare namespace Discord {
         format_type: number;
         id: string;
         name: string;
-      }
+    }
 
-      export interface Attachment extends MessageAttachment {
+    export interface Attachment extends MessageAttachment {
         sensitive: boolean;
-      }
+    }
 
-      export interface Reaction extends MessageReaction {
+    export interface Reaction extends MessageReaction {
         burst_colors: string[];
         borst_count: number;
-        count_details: { burst: number; normal: number };
+        count_details: { burst: number; normal: number; };
         me_burst: boolean;
-      }
+    }
 }
 
 export declare namespace HolyNotes {
     export interface Note {
-      id: string;
-      channel_id: string;
-      guild_id: string;
-      content: string;
-      author: {
         id: string;
-        avatar: string;
-        discriminator: string;
-        username: string;
-      };
-      flags: number;
-      timestamp: string;
-      attachments: Discord.Attachment[];
-      embeds: Embed[];
-      reactions: Discord.Reaction[];
-      stickerItems: Discord.Sticker[];
+        channel_id: string;
+        guild_id: string;
+        content: string;
+        author: {
+            id: string;
+            avatar: string;
+            discriminator: string;
+            username: string;
+        };
+        flags: number;
+        timestamp: string;
+        attachments: Discord.Attachment[];
+        embeds: Embed[];
+        reactions: Discord.Reaction[];
+        stickerItems: Discord.Sticker[];
     }
-  }
+}
