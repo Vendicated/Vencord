fcf45151d2bcd4af1699d1f591a5cb52b8b0f968
sphirye <ronielcontreras70@gmail.com>
2024-03-12 16:14:22 -0500

    Add NoGenericFilenames plugin

---
+++ src/plugins/NoGenericFilenames/index.ts
@@ -0,0 +1,106 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+import { saveFile } from "@utils/web";
+
+const genericFilenames: string[] = [
+    "image",
+    "video",
+];
+
+async function fetchImage(url: string) {
+    const res = await fetch(url);
+    if (res.status !== 200) return;
+
+    return await res.blob();
+}
+
+function isGenericFilename(filename: string) {
+    let rex = /^(bruh)\s*(?:\(([\d]+)\))?$/g;
+    rex = new RegExp(rex.source.replace("bruh", genericFilenames.join("|")));
+    return rex.test(filename);
+}
+
+function getFilenameData(filename: string): { name: string, extension: string; } {
+    const regex = /^(.+)(\.[^.]+)$/;
+    const result = regex.exec(filename);
+
+    if (!result) {
+        throw new Error(`Invalid filename: ${filename}`);
+    }
+
+    return {
+        name: result[1],
+        extension: result[2].substring(1),
+    };
+}
+
+const settings = definePluginSettings({
+    includeMilis: {
+        name: "Include milis",
+        description: "Include miliseconds in the timestamp",
+        type: OptionType.BOOLEAN,
+        default: true,
+    }
+});
+
+export default definePlugin({
+    name: "NoGenericFilenames",
+    description: "Prevent discord downloads from overwriting files with generic names by adding a timestamp.",
+    settings: settings,
+    authors: [
+        {
+            id: 858410144349945867n,
+            name: "Kirigamium",
+        },
+    ],
+
+    patches: [
+        {
+            find: 'id:"save-image"',
+            replacement: [
+                {
+                    match: /(?<=SAVE_IMAGE_MENU_ITEM,)action:/,
+                    replace: "action:()=>$self.saveImage(arguments[0]),"
+                },
+            ]
+        }
+    ],
+
+    getCurrentDate(): string {
+        const date = new Date();
+        const hour = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        const seconds = date.getSeconds().toString().padStart(2, "0");
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+
+        return `${hour}:${minutes}:${seconds} ${day}-${month}-${year}`;
+    },
+
+    async saveImage(url: string) {
+        const data = await fetchImage(url);
+
+        if (!data) return;
+
+        const filenameData = getFilenameData(new URL(url).pathname.split("/").pop()!);
+        var name: string = "";
+
+        console.log(isGenericFilename(filenameData.name));
+        console.log(filenameData);
+
+        if (isGenericFilename(filenameData.name)) {
+            name = `${filenameData.name} ${this.getCurrentDate()}.${filenameData.extension}`;
+        } else {
+            name = `${filenameData.name}.${filenameData.extension}`;
+        }
+
+        saveFile(new File([data], name, { type: data.type }));
+    },
+});
