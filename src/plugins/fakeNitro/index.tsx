c55e4e32ebf09eb19274590d6e8657c2d50c1b40
Vendicated <contact@swezy.dev>
2024-03-13 00:36:42 +0100

    rename Devs.obscurity => Devs.fawn :3

--- src/plugins/fakeNitro/index.tsx
+++ src/plugins/fakeNitro/index.tsx
@@ -185,7 +185,7 @@ const hasAttachmentPerms = (channelId: string) => hasPermission(channelId, Permi
 
 export default definePlugin({
     name: "FakeNitro",
-    authors: [Devs.Arjix, Devs.D3SOX, Devs.Ven, Devs.obscurity, Devs.captain, Devs.Nuckyz, Devs.AutumnVN],
+    authors: [Devs.Arjix, Devs.D3SOX, Devs.Ven, Devs.fawn, Devs.captain, Devs.Nuckyz, Devs.AutumnVN],
     description: "Allows you to stream in nitro quality, send fake emojis/stickers and use client themes.",
     dependencies: ["MessageEventsAPI"],
 
