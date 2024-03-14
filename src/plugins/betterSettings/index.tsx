5a67675f56208456357328d9e71219e69c26e5f6
Kyuuhachi <caagr98@gmail.com>
2024-03-14 01:58:03 +0100

    Fix formatting

--- src/plugins/betterSettings/index.tsx
+++ src/plugins/betterSettings/index.tsx
@@ -22,19 +22,19 @@ const settings = definePluginSettings({
         description: "Disable the crossfade animation",
         type: OptionType.BOOLEAN,
         default: true,
-        restartNeeded: true,
+        restartNeeded: true
     },
     organizeMenu: {
         description: "Organizes the settings cog context menu",
         type: OptionType.BOOLEAN,
-        default: true,
+        default: true
     },
     eagerLoad: {
         description: "Eagerly load menu contents",
         type: OptionType.BOOLEAN,
         default: true,
-        restartNeeded: true,
-    },
+        restartNeeded: true
+    }
 });
 
 export default definePlugin({
@@ -50,14 +50,14 @@ export default definePlugin({
                 { // Fade in on layer
                     match: /(?<=(\i)\.contextType=\i\.AccessibilityPreferencesContext;)/,
                     replace: "$1=$self.Layer;",
-                    predicate: () => settings.store.disableFade,
+                    predicate: () => settings.store.disableFade
                 },
                 { // Lazy-load contents
                     match: /createPromise:\(\)=>([^:}]*?),webpackId:"\d+",name:(?!="CollectiblesShop")"[^"]+"/g,
                     replace: "$&,_:$1",
-                    predicate: () => settings.store.eagerLoad,
-                },
-            ],
+                    predicate: () => settings.store.eagerLoad
+                }
+            ]
         },
         { // For some reason standardSidebarView also has a small fade-in
             find: "DefaultCustomContentScroller:function()",
@@ -68,10 +68,10 @@ export default definePlugin({
                 },
                 {
                     match: /\i\.animated\.div/,
-                    replace: "\"div\""
-                },
+                    replace: '"div"'
+                }
             ],
-            predicate: () => settings.store.disableFade,
+            predicate: () => settings.store.disableFade
         },
         { // Load menu stuff on hover, not on click
             find: "Messages.USER_SETTINGS_WITH_BUILD_OVERRIDE.format",
@@ -79,7 +79,7 @@ export default definePlugin({
                 match: /(?<=handleOpenSettingsContextMenu.{0,250}?\i\.el\(("\d+")\)\.then.*?Messages\.USER_SETTINGS,)(?=onClick:)/,
                 replace: "onMouseEnter(){let r=Vencord.Webpack.wreq;r.el($1).then(r.bind(r,$1));},"
             },
-            predicate: () => settings.store.eagerLoad,
+            predicate: () => settings.store.eagerLoad
         },
         { // Settings cog context menu
             find: "Messages.USER_SETTINGS_ACTIONS_MENU_LABEL",
@@ -106,23 +106,23 @@ export default definePlugin({
             className={cl({
                 [Classes.layer]: true,
                 [Classes.baseLayer]: baseLayer,
-                "stop-animations": hidden,
+                "stop-animations": hidden
             })}
             style={{ opacity: hidden ? 0 : undefined }}
             {...props}
         />;
-        if(baseLayer) return node;
+        if (baseLayer) return node;
         else return <FocusLock containerRef={containerRef}>{node}</FocusLock>;
     },
 
     wrapMenu(list: SettingsEntry[]) {
-        if(!settings.store.organizeMenu) return list;
+        if (!settings.store.organizeMenu) return list;
 
         const items = [{ label: null as string|null, items: [] as SettingsEntry[] }];
-        for(const item of list) {
-            if(item.section === "HEADER") {
+        for (const item of list) {
+            if (item.section === "HEADER") {
                 items.push({ label: item.label, items: [] });
-            } else if(item.section === "DIVIDER") {
+            } else if (item.section === "DIVIDER") {
                 items.push({ label: i18n.Messages.OTHER_OPTIONS, items: [] });
             } else {
                 items.at(-1)!.items.push(item);
@@ -130,7 +130,7 @@ export default definePlugin({
         }
         return {
             filter(predicate: (item: SettingsEntry) => boolean) {
-                for(const category of items) {
+                for (const category of items) {
                     category.items = category.items.filter(predicate);
                 }
                 return this;
@@ -140,7 +140,7 @@ export default definePlugin({
                     .filter(a => a.items.length > 0)
                     .map(({ label, items }) => {
                         const children = items.map(render);
-                        if(label) {
+                        if (label) {
                             return <Menu.MenuItem
                                 id={label.replace(/\W/, "_")}
                                 label={label}
@@ -151,7 +151,7 @@ export default definePlugin({
                             return children;
                         }
                     });
-            },
+            }
         };
     }
 });
