b13a801f85605a7ef76c61d80738fbca949722cf
AntoKek <152296803+AntoKek@users.noreply.github.com>
2024-03-21 22:47:29 +0100

    Update constants.ts
    added myself to the Devs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -426,6 +426,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Elvyra: {
         name: "Elvyra",
         id: 708275751816003615n,
+    },
+    AntoKek: {
+        name: "AntoKek",
+        id: 224864947480297472n,
     }
 } satisfies Record<string, Dev>);
 
