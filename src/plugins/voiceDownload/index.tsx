42f1a8293724b8dbcf6a0655504a3bcea36f5c48
puv <dev@puv.bar>
2024-03-22 08:43:06 +0000

    Fix linting, header errors

--- src/plugins/voiceDownload/index.tsx
+++ src/plugins/voiceDownload/index.tsx
@@ -1,7 +1,13 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import "./style.css";
 
-import { Devs } from "@utils/constants";
 import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
@@ -22,7 +28,7 @@ export default definePlugin({
         return (
             <ErrorBoundary>
                 <a
-                    className="voiceDownload"
+                    className="voice-download"
                     href={src}
                     target="_blank"
                     onClick={e => e.stopPropagation()}
