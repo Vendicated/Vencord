580673e83bf9be553b6630f720d21798ccaaca1f
KrystalSkull <150982280+KrstlSkll69@users.noreply.github.com>
2024-03-25 08:56:17 -0400

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -426,6 +426,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "newwares",
         id: 421405303951851520n
     }
+    KrystalSkull: {
+        name: "krystalskull"
+        id: 929208515883569182
+    
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
