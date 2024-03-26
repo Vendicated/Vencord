73ec09760202922c687309b7ebb2cc7e952bce21
Kyuuhachi <caagr98@gmail.com>
2024-03-26 18:51:12 +0100

    Add TeX plugin

---
+++ src/plugins/tex/index.tsx
@@ -0,0 +1,108 @@
+import "./style.css";
+
+import { Devs } from "@utils/constants";
+import { makeLazy } from "@utils/lazy";
+import { classes } from "@utils/misc";
+import definePlugin from "@utils/types";
+import { React, Tooltip, useMemo } from "@webpack/common";
+import { useEffect, useState } from "@webpack/common";
+
+const SCRIPT_URL = "https://unpkg.com/katex@0.16.9/dist/katex.mjs";
+const STYLE_URL = "https://unpkg.com/katex@0.16.9/dist/katex.min.css";
+
+let theKatex: undefined | any = undefined;
+export const loadKatex = makeLazy(async () => {
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", STYLE_URL);
+    document.head.appendChild(style);
+    return theKatex = (await import(SCRIPT_URL)).default;
+});
+
+export function useKatex() {
+    const [katex, setKatex] = useState(theKatex);
+    useEffect(() => {
+        if(katex === undefined)
+            loadKatex().then(setKatex);
+    });
+    return katex;
+}
+
+export default definePlugin({
+    name: "TeX",
+    description: "Typesets math in messages, written as `$x$` or `$$x$$`.",
+    authors: [Devs.Kyuuhachi],
+
+    patches: [
+        {
+            find: "inlineCode:{react",
+            replacement: {
+                match: /inlineCode:\{react:\((\i,\i,\i)\)=>/,
+                replace: "$&$self.render($1)??"
+            },
+        },
+    ],
+
+    render({ content }) {
+        const displayMatch = /^\$\$(.*)\$\$$/.exec(content);
+        const inlineMatch = /^\$(.*)\$$/.exec(content);
+        if(displayMatch)
+            return <LazyLatex displayMode formula={displayMatch[1]} delim="$$" />;
+        if(inlineMatch)
+            return <LazyLatex formula={inlineMatch[1]} delim="$" />;
+    }
+});
+
+function LazyLatex(props) {
+    const { formula, delim } = props;
+    const katex = useKatex();
+    return katex
+        ? <Latex {...props} katex={katex} />
+        : <LatexPlaceholder className="tex-loading" delim={delim}>{formula}</LatexPlaceholder>;
+}
+
+function Latex({ katex, formula, displayMode, delim }) {
+    const result = useMemo(() => {
+        try {
+            const html = katex.renderToString(formula, { displayMode });
+            return { html };
+        } catch(error) {
+            return { error };
+        }
+    }, [formula, displayMode]);
+
+    return result.html
+        ? <span className="tex" dangerouslySetInnerHTML={{ __html: result.html }} />
+        : <LatexError formula={formula} delim={delim} error={result.error} />;
+}
+
+function LatexError({ formula, delim, error }) {
+    const { rawMessage, position, length } = error;
+    const pre = formula.slice(0, position);
+    const mid = formula.slice(position, position+length);
+    const suf = formula.slice(position+length);
+    return (
+        <Tooltip text={rawMessage}>
+            {({ onMouseLeave, onMouseEnter }) => (
+                <LatexPlaceholder
+                    onMouseLeave={onMouseLeave}
+                    onMouseEnter={onMouseEnter}
+                    delim={delim}
+                    className="tex-error"
+                >
+                    {pre}<strong>{mid}</strong>{suf}
+                </LatexPlaceholder>
+            )}
+        </Tooltip>
+    );
+}
+
+function LatexPlaceholder({ className, delim, children, ...props }) {
+    return (
+        <code className={classes(className, "tex-placeholder inline")} {...props}>
+            {delim}
+            {children}
+            {delim}
+        </code>
+    );
+}
