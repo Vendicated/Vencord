c24abb0414e5ba6d66d6cfe099c1df47863e7857
Trey <47907719+trwy7@users.noreply.github.com>
2024-03-26 10:34:33 -0600

    Add friends icon to popout
    Adds the discord friends icon to the Friends Since popout

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -47,8 +47,8 @@ export default definePlugin({
                 <Heading variant="eyebrow" className={clydeMoreInfo.title}>
                     Friends Since
                 </Heading>
-
                 <div className={container.memberSinceContainer}>
+                    <svg aria-label="Friends" aria-hidden="false" role="img" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24"><path fill="lightgrey" d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"></path><path fill="lightgrey" d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z"></path></svg>
                     <Text variant="text-sm/normal" className={clydeMoreInfo.body}>
                         {getCreatedAtDate(friendsSince, locale.getLocale())}
                     </Text>
