d03098583e1b8877dac60bb79e192346035bcd02
Trey <47907719+trwy7@users.noreply.github.com>
2024-03-27 15:32:12 -0600

    Remove raw dom

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -41,14 +41,13 @@ export default definePlugin({
     friendsSince: ErrorBoundary.wrap(({ userId }: { userId: string; }) => {
         const friendsSince = RelationshipStore.getSince(userId);
         if (!friendsSince) return null;
-
         return (
             <div className={lastSection.section}>
                 <Heading variant="eyebrow" className={clydeMoreInfo.title}>
                     Friends Since
                 </Heading>
                 <div className={container.memberSinceContainer}>
-                    {Array.from(document.getElementsByClassName("discordIcon_feeef1")).length === 1 && (
+                    {document.querySelectorAll('[class^="discordIcon_"]')[0] && (
                         <svg aria-hidden="true" width="18" height="18" viewBox="0 0 24 24"><path fill="lightgrey" d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"></path><path fill="lightgrey" d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z"></path></svg>
                     )}
                     <Text variant="text-sm/normal" className={clydeMoreInfo.body}>
