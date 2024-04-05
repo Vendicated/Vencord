de5c4d04477af405bc52ead52b8f7324bf1423cd
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-04-06 10:57:15 +1100

    formatting comments

--- src/plugins/allCallTimers/timerText.tsx
+++ src/plugins/allCallTimers/timerText.tsx
@@ -6,10 +6,12 @@
 
 export function TimerText({ text, className }: Readonly<{ text: string; className: string; }>) {
     return <div className={`timeCounter ${className}`} style={{
-        marginTop: -6,  // this margin value doesn't change the default size of the user container
+        // this margin value doesn't change the default size of the user container
+        marginTop: -6,
         fontWeight: "bold",
         fontFamily: "monospace",
-        fontSize: 11,  // good size that doesn't touch username
+        // good size that doesn't touch username
+        fontSize: 11,
         position: "relative",
     }}>{text}</div>;
 }
