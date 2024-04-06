f961b562f71c760d3daff6fe8c2b43e9ea799702
varram <53030931+not-varram@users.noreply.github.com>
2024-04-06 10:48:37 +0530

    Updates as directed by nuckyz

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -21,46 +21,39 @@ import { definePluginSettings } from "@api/Settings";
 import { Settings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-export const settings = definePluginSettings({
+function runner() {
+    fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+        .then(x => x.text())
+        .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
+        .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+            .replace("(isReducedMotion)", "(false)"))
+        .then(eval);
+}
+export const onekoSettings = definePluginSettings({
     speed: {
         description: "Speed of Da Cat :3",
-        type: OptionType.NUMBER,
+        type: OptionType.SLIDER,
         default: 10,
-        isValid: (value: number) => {
-            if (value && value < 0) return "The number must be bigger than 0";
-            return true;
-        },
+        markers: [1, 10, 15, 20, 25, 30, 35, 40, 45, 50],
         onChange: () => {
-            // note: cant call the start() function from here. so i just copy pasted it (This was pointed out in the last commit i made. So this is to just clear stuff up for any future devs that work on this :D )
             if (Settings.plugins.oneko.enabled) {
                 document.getElementById("oneko")?.remove();
-                fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
-                    .then(x => x.text())
-                    .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
-                    .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
-                        .replace("(isReducedMotion)", "(false)"))
-                    .then(eval);
+                runner();
             }
             return;
         }
     },
 });
-
 export default definePlugin({
     name: "oneko",
     description: "cat follow mouse (real)",
     // Listing adryd here because this literally just evals her script
     authors: [Devs.Ven, Devs.adryd, Devs.varram],
 
-    settings,
+    settings: onekoSettings,
 
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
-            .then(x => x.text())
-            .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
-            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
-                .replace("(isReducedMotion)", "(false)"))
-            .then(eval);
+        runner();
     },
 
     stop() {
