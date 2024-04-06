8e386603e4c223a95bb737d2a17aded60fd5f39c
varram <53030931+not-varram@users.noreply.github.com>
2024-04-06 16:13:17 +0530

    im dumb

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -23,7 +23,7 @@ import definePlugin, { OptionType } from "@utils/types";
 function runner() {
     fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
         .then(x => x.text())
-        .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${onekoSettings.store.speed};`))
+        .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${settings.store.speed};`))
         .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
             .replace("(isReducedMotion)", "(false)"))
         .then(eval);
