f4f27348067b070676d66188133d42600f97ca61
Nick <29809309+PrinceBunBun981@users.noreply.github.com>
2024-04-08 12:50:20 -0400

    Voice Chat Mention

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -425,6 +425,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     newwares: {
         name: "newwares",
         id: 421405303951851520n
+    },
+    nickwoah: {
+        name: "nickwoah",
+        id: 644298972420374528n
     }
 } satisfies Record<string, Dev>);
 
