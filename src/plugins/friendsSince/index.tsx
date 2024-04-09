12b19f38d7c9acbb90b9afc705b61f931966b364
Vendicated <contact@swezy.dev>
2024-04-09 02:52:21 +0200

    FriendsSince: Add icon to be consistent with "member since"
    Co-authored-by: Trey <47907719+trwy7@users.noreply.github.com>

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -6,6 +6,7 @@
 
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
+import { getCurrentChannel } from "@utils/discord";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
 import { React, RelationshipStore } from "@webpack/common";
@@ -49,6 +50,18 @@ export default definePlugin({
                 </Heading>
 
                 <div className={container.memberSinceContainer}>
+                    {!!getCurrentChannel()?.guild_id && (
+                        <svg
+                            aria-hidden="true"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="var(--interactive-normal)"
+                        >
+                            <path d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" />
+                            <path d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z" />
+                        </svg>
+                    )}
                     <Text variant="text-sm/normal" className={clydeMoreInfo.body}>
                         {getCreatedAtDate(friendsSince, locale.getLocale())}
                     </Text>
