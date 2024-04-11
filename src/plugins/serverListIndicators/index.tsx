08856e9b38ea8e2aebe65f27f4e577cd113e0647
Panniku <hashimdxb07@gmail.com>
2024-04-11 16:06:34 +0400

    feat: consistency

--- src/plugins/serverListIndicators/index.tsx
+++ src/plugins/serverListIndicators/index.tsx
@@ -18,11 +18,14 @@
 
 import { addServerListElement, removeServerListElement, ServerListRenderPosition } from "@api/ServerList";
 import { Settings } from "@api/Settings";
+import { enableStyle } from "@api/Styles";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import { useForceUpdater } from "@utils/react";
 import definePlugin, { OptionType } from "@utils/types";
-import { GuildStore, PresenceStore, RelationshipStore } from "@webpack/common";
+import { GuildStore, PresenceStore, RelationshipStore, Tooltip } from "@webpack/common";
+
+import style from "./styles.css?managed";
 
 const enum IndicatorType {
     SERVER = 1 << 0,
@@ -39,17 +42,24 @@ function FriendsIndicator() {
     forceUpdateFriendCount = useForceUpdater();
 
     return (
-        <span id="vc-friendcount" style={{
-            display: "inline-block",
-            width: "100%",
-            fontSize: "12px",
-            fontWeight: "600",
-            color: "var(--header-secondary)",
-            textTransform: "uppercase",
-            textAlign: "center",
-        }}>
-            {onlineFriends} <span id="vc-friendcount-text">friends</span>
-        </span>
+        <div id="vc-friendcount">
+            <svg
+                id="vc-friendcount-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                fill="currentColor"
+                viewBox="0 0 24 24">
+                <path fill="currentColor"
+                    d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" />
+                <path
+                    fill="currentColor"
+                    d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z">
+                </path></svg>
+            <span id="vc-friendcount-text">
+                {onlineFriends}
+            </span>
+        </div>
     );
 }
 
@@ -57,17 +67,26 @@ function ServersIndicator() {
     forceUpdateGuildCount = useForceUpdater();
 
     return (
-        <span id="vc-guildcount" style={{
-            display: "inline-block",
-            width: "100%",
-            fontSize: "12px",
-            fontWeight: "600",
-            color: "var(--header-secondary)",
-            textTransform: "uppercase",
-            textAlign: "center",
-        }}>
-            {guildCount} <span id="vc-guildcount-text">servers</span>
-        </span>
+        <div id="vc-guildcount">
+            <div id="vc-guildcount-dot">
+                <svg
+                    id="vc-guildcount-icon"
+                    aria-hidden="true"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    viewBox="0 0 24 24">
+                    <path
+                        fill="currentColor"
+                        d="M19.73 4.87a18.2 18.2 0 0 0-4.6-1.44c-.21.4-.4.8-.58 1.21-1.69-.25-3.4-.25-5.1 0-.18-.41-.37-.82-.59-1.2-1.6.27-3.14.75-4.6 1.43A19.04 19.04 0 0 0 .96 17.7a18.43 18.43 0 0 0 5.63 2.87c.46-.62.86-1.28 1.2-1.98-.65-.25-1.29-.55-1.9-.92.17-.12.32-.24.47-.37 3.58 1.7 7.7 1.7 11.28 0l.46.37c-.6.36-1.25.67-1.9.92.35.7.75 1.35 1.2 1.98 2.03-.63 3.94-1.6 5.64-2.87.47-4.87-.78-9.09-3.3-12.83ZM8.3 15.12c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.89 2.27-2 2.27Zm7.4 0c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.88 2.27-2 2.27Z">
+                    </path></svg>
+            </div>
+            <span id="vc-guildcount-text">
+                {guildCount}
+            </span>
+        </div>
     );
 }
 
@@ -110,9 +129,18 @@ export default definePlugin({
     renderIndicator: () => {
         const { mode } = Settings.plugins.ServerListIndicators;
         return <ErrorBoundary noop>
-            <div id="vc-serverlist-indicators" style={{ marginBottom: "4px" }}>
-                {!!(mode & IndicatorType.FRIEND) && <FriendsIndicator />}
-                {!!(mode & IndicatorType.SERVER) && <ServersIndicator />}
+            <div id="vc-indicators-container">
+                <Tooltip text={`${onlineFriends} Friends, ${guildCount} Servers`} position="right">
+                    {({ onMouseEnter, onMouseLeave }) => (
+                        <div
+                            id="vc-serverlist-indicators"
+                            onMouseEnter={onMouseEnter}
+                            onMouseLeave={onMouseLeave}>
+                            {!!(mode & IndicatorType.FRIEND) && <FriendsIndicator />}
+                            {!!(mode & IndicatorType.SERVER) && <ServersIndicator />}
+                        </div>
+                    )}
+                </Tooltip>
             </div>
         </ErrorBoundary>;
     },
@@ -126,9 +154,10 @@ export default definePlugin({
 
     start() {
         addServerListElement(ServerListRenderPosition.Above, this.renderIndicator);
-
+        enableStyle(style);
         handlePresenceUpdate();
         handleGuildUpdate();
+
     },
 
     stop() {
