adefdde15352c46441ed578c5585d249a03643fe
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-04-12 15:02:51 +0200

    .

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -423,6 +423,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Kyuuhachi",
         id: 236588665420251137n,
     },
+    Elvyra: {
+        name: "Elvyra",
+        id: 708275751816003615n,
+    },
     Johannes7k75: {
         name: "Johannes7k75",
         id: 587701169103699994n
