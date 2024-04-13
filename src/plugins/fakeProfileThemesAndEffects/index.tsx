7a32bc34a0d1ce8fd81d064eb5a59573abf1d233
ryan-0324 <77452312+ryan-0324@users.noreply.github.com>
2024-04-13 19:54:50 -0400

    Fixed the find for `ProfileEffectModalList` + added error reporting for the replaces

--- src/plugins/fakeProfileThemesAndEffects/index.tsx
+++ src/plugins/fakeProfileThemesAndEffects/index.tsx
@@ -9,12 +9,25 @@ import { Devs } from "@utils/constants";
 import { canonicalizeMatch, canonicalizeReplace } from "@utils/patches";
 import definePlugin, { OptionType } from "@utils/types";
 import { useMemo } from "@webpack/common";
-import type { ComponentProps } from "react";
 
 import { Builder, BuilderProps, setCustomColorPicker, setCustomizationSection, setProfileEffectModal, settingsAboutComponent, setUseAvatarColors } from "./components";
 import { ProfileEffectRecord, ProfileEffectStore, setProfileEffectRecord, setProfileEffectStore } from "./lib/profileEffects";
 import { profilePreviewHook } from "./lib/profilePreview";
 import { decodeUserBioFPTEHook } from "./lib/userProfile";
+
+function replaceHelper(string: string, replaceArgs: [searchRegExp: RegExp, replaceString: string][]) {
+    let result = string;
+    replaceArgs.forEach(([searchRegExp, replaceString]) => {
+        const beforeReplace = result;
+        result = result.replace(
+            canonicalizeMatch(searchRegExp),
+            canonicalizeReplace(replaceString, "FakeProfileThemesAndEffects") as string
+        );
+        if (beforeReplace === result)
+            throw new Error("Replace had no effect: " + searchRegExp);
+    });
+    return result;
+}
 
 export const settings = definePluginSettings({
     prioritizeNitro: {
@@ -31,8 +44,6 @@ export const settings = definePluginSettings({
         default: false
     }
 });
-
-const pluginName = "FakeProfileThemesAndEffects";
 
 export default definePlugin({
     name: "FakeProfileThemesAndEffects",
@@ -118,56 +129,59 @@ export default definePlugin({
                 match: /default:function\(\){return (\i)}(?=.+?(function \1\((?:.(?!function |}$))+\.jsxs?\)\((\i),.+?})(?:function |}$)).+?(function \3\(.+?})(?=function |}$).*(?=}$)/,
                 replace: (wpModule, modalRootName, ModalRoot, _modalInnerName, ModalInner) => (
                     `${wpModule}{$self.ProfileEffectModal=${modalRootName};`
-                    + ModalRoot
+                    + replaceHelper(ModalRoot, [
                         // Required for the profile preview to show profile effects
-                        .replace(
-                            canonicalizeMatch(/(?<=[{,]purchases:.+?}=).+?(?=,\i=|,{\i:|;)/),
-                            canonicalizeReplace("{isFetching:!1,categories:new Map,purchases:$self.getPurchases()}", pluginName) as string
-                        )
-                    + ModalInner
+                        [
+                            /(?<=[{,]purchases:.+?}=).+?(?=,\i=|,{\i:|;)/,
+                            "{isFetching:!1,categories:new Map,purchases:$self.getPurchases()}"
+                        ]
+                    ])
+                    + replaceHelper(ModalInner, [
                         // Required to show the apply button
-                        .replace(
-                            canonicalizeMatch(/(?<=[{,]purchase:.+?}=).+?(?=,\i=|,{\i:|;)/),
+                        [
+                            /(?<=[{,]purchase:.+?}=).+?(?=,\i=|,{\i:|;)/,
                             "{purchase:{purchasedAt:new Date}}"
-                        )
+                        ],
                         // Replaces the profile effect list with the modified version
-                        .replace(
+                        [
                             /(?<=\.jsxs?\)\()[^,]+(?=,{(?:(?:.(?!\.jsxs?\)))+,)?onSelect:)/,
-                            canonicalizeReplace("$self.ProfileEffectModalList", pluginName) as string
-                        )
+                            "$self.ProfileEffectModalList"
+                        ],
                         // Replaces the apply profile effect function with the modified version
-                        .replace(
-                            canonicalizeMatch(/(?<=[{,]onApply:).+?\.setNewPendingProfileEffectId\)\((\i).+?(?=,\i:|}\))/),
-                            canonicalizeReplace("()=>$self.onApply($1)", pluginName) as string
-                        )
+                        [
+                            /(?<=[{,]onApply:).+?\.setNewPendingProfileEffectId\)\((\i).+?(?=,\i:|}\))/,
+                            "()=>$self.onApply($1)"
+                        ],
                         // Required to show the apply button
-                        .replace(
-                            canonicalizeMatch(/(?<=[{,]canUseCollectibles:).+?(?=,\i:|}\))/),
+                        [
+                            /(?<=[{,]canUseCollectibles:).+?(?=,\i:|}\))/,
                             "!0"
-                        )
+                        ],
                         // Required to enable the apply button
-                        .replace(
-                            canonicalizeMatch(/(?<=[{,]disableApplyButton:).+?(?=,\i:|}\))/),
+                        [
+                            /(?<=[{,]disableApplyButton:).+?(?=,\i:|}\))/,
                             "!1"
-                        )
+                        ]
+                    ])
                     + "}"
                 )
             }
         },
         // ProfileEffectModalList
         {
-            find: ".Section.PREMIUM_PURCHASE",
+            find: "selectedProfileEffectRef",
             replacement: {
                 match: /function\(\i,(\i),.+[,;}]\1\.default=([^=].+?})(?=;|}$).*(?=}$)/,
                 replace: (wpModule, _wpModuleVar, List) => (
                     `${wpModule};$self.ProfileEffectModalList=`
-                    + List
+                    + replaceHelper(List, [
                         // Removes the "Exclusive to Nitro" and "Preview The Shop" sections
                         // Adds every profile effect to the "Your Decorations" section and removes the "Shop" button
-                        .replace(
-                            canonicalizeMatch(/(?<=[ ,](\i)=).+?(?=(?:,\i=|,{\i:|;).+?:\1\.map\()/),
-                            canonicalizeReplace("$self.getListSections($&)", pluginName) as string
-                        )
+                        [
+                            /(?<=[ ,](\i)=).+?(?=(?:,\i=|,{\i:|;).+?:\1\.map\()/,
+                            "$self.getListSections($&)"
+                        ]
+                    ])
                 )
             }
         }
@@ -175,10 +189,10 @@ export default definePlugin({
 
     addFPTEBuilder: (guildId?: BuilderProps["guildId"]) => settings.store.hideBuilder ? null : <Builder guildId={guildId} />,
 
-    onApply(_effectId: string) { },
+    onApply(_effectId: string | undefined) { },
     set ProfileEffectModal(comp: Parameters<typeof setProfileEffectModal>[0]) {
-        setProfileEffectModal((props: ComponentProps<typeof comp>) => {
-            this.onApply = (effectId: string | undefined) => {
+        setProfileEffectModal(props => {
+            this.onApply = effectId => {
                 props.onApply(effectId ? ProfileEffectStore.getProfileEffectById(effectId)!.config : null);
                 props.onClose();
             };
@@ -189,12 +203,10 @@ export default definePlugin({
     ProfileEffectModalList: () => null,
 
     getPurchases: () => useMemo(
-        () => (
-            new Map(ProfileEffectStore.profileEffects.map(effect => [
-                effect.id,
-                { items: new ProfileEffectRecord(effect) }
-            ]))
-        ),
+        () => new Map(ProfileEffectStore.profileEffects.map(effect => [
+            effect.id,
+            { items: new ProfileEffectRecord(effect) }
+        ])),
         [ProfileEffectStore.profileEffects]
     ),
 
