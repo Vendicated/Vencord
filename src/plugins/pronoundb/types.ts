7fb709a4153813001fa2b455422b9c11c55fb6fe
EdVraz <88881326+EdVraz@users.noreply.github.com>
2024-04-14 05:10:04 +0200

    pronounDB: Update to API v2

--- src/plugins/pronoundb/types.ts
+++ src/plugins/pronoundb/types.ts
@@ -26,31 +26,40 @@ export interface UserProfilePronounsProps {
 }
 
 export interface PronounsResponse {
-    [id: string]: PronounCode;
+    [id: string]: {
+        sets: {
+            [locale: string]: PronounCode[];
+        }
+    }
+}
+
+export interface CachePronouns {
+    sets: {
+        [locale: string]: PronounCode[];
+    }
 }
 
 export type PronounCode = keyof typeof PronounMapping;
 
 export const PronounMapping = {
-    hh: "He/Him",
-    hi: "He/It",
-    hs: "He/She",
-    ht: "He/They",
-    ih: "It/Him",
-    ii: "It/Its",
-    is: "It/She",
-    it: "It/They",
-    shh: "She/He",
-    sh: "She/Her",
-    si: "She/It",
-    st: "She/They",
-    th: "They/He",
-    ti: "They/It",
-    ts: "They/She",
-    tt: "They/Them",
+    he: "He/Him",
+    it: "It/Its",
+    she: "She/Her",
+    they: "They/Them",
     any: "Any pronouns",
     other: "Other pronouns",
     ask: "Ask me my pronouns",
     avoid: "Avoid pronouns, use my name",
-    unspecified: "Unspecified"
+    unspecified: "No pronouns specified.",
+    // neither avoid nor unspecified can occur when there is any other pronoun set, hence there is no short for needed.
+
+    // S for short form, used for when there is multiple different pronouns.
+    // pronounDB actually allows you to select any/other/ask as 2nd or 3rd set, so these are included here.
+    anyS: "Any",
+    otherS: "Other",
+    askS: "Ask",
+    heS: "He",
+    itS: "It",
+    sheS: "She",
+    theyS: "They",
 } as const;
