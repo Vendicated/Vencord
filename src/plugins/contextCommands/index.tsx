f7fe90104f4064b70389d842f875b9435d2bf672
aequabit <contact@aequabit.de>
2024-04-17 08:08:38 +0200

    ContextCommands: Add channel limit option

--- src/plugins/contextCommands/index.tsx
+++ src/plugins/contextCommands/index.tsx
@@ -20,13 +20,17 @@ import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findStoreLazy } from "@webpack";
-import { Menu, RestAPI, SelectedChannelStore, SnowflakeUtils } from "@webpack/common";
+import { Menu, RestAPI, SelectedChannelStore, SnowflakeUtils, UserStore } from "@webpack/common";
 import type { Channel, User } from "discord-types/general";
 
 interface UserContextProps {
     channel: Channel;
     guildId?: string;
     user: User;
+}
+
+interface ChannelContextProps {
+    channel: Channel;
 }
 
 const VoiceStateStore = findStoreLazy("VoiceStateStore");
@@ -47,8 +51,7 @@ function sendMessage(channelId: string, content: string) {
 }
 
 const UserContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: UserContextProps) => {
-    // if (!user || user.id === UserStore.getCurrentUser().id) return;
-    if (!user) return;
+    if (!user || user.id === UserStore.getCurrentUser().id) return;
 
     // Get our current voice channel
     const voiceChannelId = SelectedChannelStore.getVoiceChannelId();
@@ -60,8 +63,6 @@ const UserContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: U
 
     // Check if the selected user is in the same channel as us
     if (userVoiceState.channelId !== voiceChannelId) return;
-
-    // const voiceChannel = ChannelStore.getChannel(voiceChannelId);
 
     children.push(
         <Menu.MenuItem
@@ -79,11 +80,152 @@ const UserContextMenuPatch: NavContextMenuPatchCallback = (children, { user }: U
     );
 };
 
+const ChannelContextMenuPatch: NavContextMenuPatchCallback = (children, { channel }: ChannelContextProps) => {
+    if (!channel) return;
+
+    // Ignore non-voice channels
+    if (channel.type !== 2) return;
+
+    children.push(
+        <Menu.MenuItem
+            id="vc-change-limit"
+            label="Change user limit"
+        >
+            <Menu.MenuItem
+                id={"vc-change-limit-1"}
+                label="1"
+                action={() => sendMessage(channel.id, "!voice-limit 1")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-2"}
+                label="2"
+                action={() => sendMessage(channel.id, "!voice-limit 2")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-3"}
+                label="3"
+                action={() => sendMessage(channel.id, "!voice-limit 3")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-4"}
+                label="4"
+                action={() => sendMessage(channel.id, "!voice-limit 4")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-5"}
+                label="5"
+                action={() => sendMessage(channel.id, "!voice-limit 5")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-6"}
+                label="6"
+                action={() => sendMessage(channel.id, "!voice-limit 6")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-7"}
+                label="7"
+                action={() => sendMessage(channel.id, "!voice-limit 7")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-8"}
+                label="8"
+                action={() => sendMessage(channel.id, "!voice-limit 8")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-9"}
+                label="9"
+                action={() => sendMessage(channel.id, "!voice-limit 9")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-10"}
+                label="10"
+                action={() => sendMessage(channel.id, "!voice-limit 10")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-11"}
+                label="11"
+                action={() => sendMessage(channel.id, "!voice-limit 11")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-12"}
+                label="12"
+                action={() => sendMessage(channel.id, "!voice-limit 12")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-13"}
+                label="13"
+                action={() => sendMessage(channel.id, "!voice-limit 13")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-14"}
+                label="14"
+                action={() => sendMessage(channel.id, "!voice-limit 14")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-15"}
+                label="15"
+                action={() => sendMessage(channel.id, "!voice-limit 15")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-16"}
+                label="16"
+                action={() => sendMessage(channel.id, "!voice-limit 16")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-17"}
+                label="17"
+                action={() => sendMessage(channel.id, "!voice-limit 17")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-18"}
+                label="18"
+                action={() => sendMessage(channel.id, "!voice-limit 18")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-19"}
+                label="19"
+                action={() => sendMessage(channel.id, "!voice-limit 19")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-20"}
+                label="20"
+                action={() => sendMessage(channel.id, "!voice-limit 20")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-21"}
+                label="21"
+                action={() => sendMessage(channel.id, "!voice-limit 21")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-22"}
+                label="22"
+                action={() => sendMessage(channel.id, "!voice-limit 22")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-23"}
+                label="23"
+                action={() => sendMessage(channel.id, "!voice-limit 23")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-24"}
+                label="24"
+                action={() => sendMessage(channel.id, "!voice-limit 24")}
+            />
+            <Menu.MenuItem
+                id={"vc-change-limit-25"}
+                label="25"
+                action={() => sendMessage(channel.id, "!voice-limit 25")}
+            />
+        </Menu.MenuItem>
+    );
+};
+
 export default definePlugin({
     name: "ContextCommands",
-    description: "Adds /voice-kick and /voice-ban shortcuts to user context menus",
+    description: "Adds command shortcuts to context menus (!voice-kick, !voice-ban, !voice-limit)",
     authors: [Devs.Nobody],
     contextMenus: {
-        "user-context": UserContextMenuPatch
+        "user-context": UserContextMenuPatch,
+        "channel-context": ChannelContextMenuPatch
     }
 });
