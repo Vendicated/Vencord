7fee537ea1e9150832ef9a274bfb60650cb90a5b
Nico <nico@d3sox.me>
2024-04-18 08:21:47 +0200

    chore(memberListActivities): fix typo in comment

--- src/plugins/memberListActivities/index.tsx
+++ src/plugins/memberListActivities/index.tsx
@@ -64,7 +64,7 @@ const TimeBar: React.ComponentType<React.PropsWithChildren<{
     className: string;
 }>> = findComponentByCodeLazy("isSingleLine");
 
-// if discord one day decides changes their icon this needs to be updated
+// if discord one day decides to change their icon this needs to be updated
 const DefaultActivityIcon = findComponentByCodeLazy("M6,7 L2,7 L2,6 L6,6 L6,7 Z M8,5 L2,5 L2,4 L8,4 L8,5 Z M8,3 L2,3 L2,2 L8,2 L8,3 Z M8.88888889,0 L1.11111111,0 C0.494444444,0 0,0.494444444 0,1.11111111 L0,8.88888889 C0,9.50253861 0.497461389,10 1.11111111,10 L8.88888889,10 C9.50253861,10 10,9.50253861 10,8.88888889 L10,1.11111111 C10,0.494444444 9.5,0 8.88888889,0 Z");
 
 const fetchedApplications = new Map<string, Application | null>();
