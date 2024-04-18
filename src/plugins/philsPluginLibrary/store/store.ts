b9dff8fe8e772e75b59194937ff8573a91513211
Phil <letsphil12@gmail.com>
2024-04-18 16:12:07 +0200

    fix: apply correct formatting

--- src/plugins/philsPluginLibrary/store/store.ts
+++ src/plugins/philsPluginLibrary/store/store.ts
@@ -38,11 +38,11 @@ function createObjectProxy<T extends object>(obj1: T, onUpdate: (updatedObject: 
             const success = Reflect.set(target, property, value, receiver);
             const nestedObj = target[property];
 
-            if (typeof nestedObj == 'object') {
-                target[property] = createObjectProxy(nestedObj, () => { onUpdate(obj1); }); //On update will call itself until the top level object
+            if (typeof nestedObj === "object") {
+                target[property] = createObjectProxy(nestedObj, () => { onUpdate(obj1); }); // On update will call itself until the top level object
             }
 
-            onUpdate(obj1); //This will recursively call on nested objects
+            onUpdate(obj1); // This will recursively call on nested objects
             return success;
         }
     };
@@ -60,15 +60,15 @@ export function createPluginStore<Z extends PluginSettings = {}>(pluginName: str
     if (!Settings.plugins[pluginName].stores)
         Settings.plugins[pluginName].stores = {};
 
-    if (!Settings.plugins[pluginName].stores[storeName]) //Just incase the store doesn't exist we create it here (otherwise we crash)
+    if (!Settings.plugins[pluginName].stores[storeName]) // Just incase the store doesn't exist we create it here (otherwise we crash)
         Settings.plugins[pluginName].stores[storeName] = {};
 
     const get: PluginGet<Z> = () => {
         const storeSettings = settingStorage.get(storeName);
 
-        if (!startupStates[storeName]) { //We do this so that we can load all the saved data without the proxy attempting to overwrite it
+        if (!startupStates[storeName]) { // We do this so that we can load all the saved data without the proxy attempting to overwrite it
             const startupInfo = Settings.plugins[pluginName].stores[storeName];
-            Object.keys(startupInfo).forEach((prop) => storeSettings[prop] = startupInfo[prop]);
+            Object.keys(startupInfo).forEach(prop => storeSettings[prop] = startupInfo[prop]);
 
             startupStates[storeName] = true;
         }
@@ -79,17 +79,17 @@ export function createPluginStore<Z extends PluginSettings = {}>(pluginName: str
     const set: PluginSet<Z> = (s: ((settings: Z) => Z | undefined) | Z) =>
         (typeof s === "function" ? s(get()) : s) || get();
 
-    const use: PluginUse<Z> = () => { useSettings().plugins[pluginName].stores[storeName]; return get(); }; //useSettings is called to update renderer (after settings change)
+    const use: PluginUse<Z> = () => { useSettings().plugins[pluginName].stores[storeName]; return get(); }; // useSettings is called to update renderer (after settings change)
 
     const initialSettings: Z = f(set, get);
-    const proxiedSettings = createObjectProxy(initialSettings as unknown, updateCallback); //Setup our proxy that allows us connections to the datastore
+    const proxiedSettings = createObjectProxy(initialSettings as unknown, updateCallback); // Setup our proxy that allows us connections to the datastore
 
     function updateCallback(updatedObject: any) {
-        if (!startupStates[storeName]) return; //Wait for the startup information to overwrite the blank proxy
+        if (!startupStates[storeName]) return; // Wait for the startup information to overwrite the blank proxy
         Settings.plugins[pluginName].stores[storeName] = JSON.parse(JSON.stringify(updatedObject));
     }
 
-    for (const key of Object.keys(initialSettings)) { proxiedSettings[key] = initialSettings[key]; } //Set them so the nested objects also become proxies
+    for (const key of Object.keys(initialSettings)) { proxiedSettings[key] = initialSettings[key]; } // Set them so the nested objects also become proxies
     settingStorage.set(storeName, proxiedSettings);
 
     updateCallback(initialSettings);
