ff0e07b519afd921448329e95bfc9995c0ebc7a4
varram <53030931+not-varram@users.noreply.github.com>
2024-05-01 11:02:07 +0530

    Update src/plugins/oneko/index.ts
    Co-authored-by: ryan-0324 <77452312+ryan-0324@users.noreply.github.com>

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -23,10 +23,9 @@ import definePlugin, { OptionType } from "@utils/types";
 function runner() {
     fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
         .then(x => x.text())
-        .then(s => s.replace("const nekoSpeed = 10;", `const nekoSpeed = ${settings.store.speed};`))
-        .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
-            .replace("(isReducedMotion)", "(false)"))
-        .then(eval);
+        .then(s => eval(s.replace(/(?<=const\s+nekoSpeed\s*=\s*)\S+?(?=\s*;)/, `${settings.store.speed}`)
+            .replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+            .replace("(isReducedMotion)", "(false)")));
 }
 export const settings = definePluginSettings({
     speed: {
