7f36d8569f45fdad0be7aebcd17d4ae417ef6c12
byron <47872200+byeoon@users.noreply.github.com>
2024-05-01 18:07:11 -0400

    fixed lint

--- src/plugins/WebhookManager/index.tsx
+++ src/plugins/WebhookManager/index.tsx
@@ -32,7 +32,7 @@ export default definePlugin({
             ],
             execute: async (option, ctx) => {
                 try {
-                    await fetch("" + findOption(option, "url"), { method: 'DELETE' }).then(() => sendBotMessage(ctx.channel.id, { content: "The webhook has deleted successfully." }));
+                    await fetch("" + findOption(option, "url"), { method: "DELETE" }).then(() => sendBotMessage(ctx.channel.id, { content: "The webhook has deleted successfully." }));
                 }
                 catch (error) {
                     sendBotMessage(ctx.channel.id, {
@@ -64,7 +64,7 @@ export default definePlugin({
                                 {
                                     title: "Webhook Information",
                                     color: "1323",
-                                    //@ts-ignore
+                                    // @ts-ignore
                                     author: {
                                         name: response.name,
                                         url: ""
