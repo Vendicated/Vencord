617de5d04c3fb79c86953d44f188684f462c149b
thororen <78185467+thororen1234@users.noreply.github.com>
2024-05-09 17:27:04 -0400

    fix

--- src/plugins/_core/supportHelper.tsx
+++ src/plugins/_core/supportHelper.tsx
@@ -170,7 +170,7 @@ ${makeCodeblock(enabledPlugins.join(", "))}
             }
 
             const repo = await VencordNative.updater.getRepo();
-            if (repo.ok && !repo.value.includes("Vendicated/Vencord")) {
+            if (repo.ok && !repo.value.includes("Vendicated/Vencord") && Vencord.Plugins.isPluginEnabled("VCSupport")) {
                 return Alerts.show({
                     title: "Hold on!",
                     body: <div>
