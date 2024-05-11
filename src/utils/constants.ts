4bcf1f7eeff31b04ca53f6623ae2bdd924e11006
Im_Banana <67657872+ImBonana@users.noreply.github.com>
2024-05-11 23:49:00 +0300

    SilentTyping: add chat input context menu option to toggle (#2386)
    Co-authored-by: vee <vendicated@riseup.net>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -461,6 +461,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     GabiRP: {
         name: "GabiRP",
         id: 507955112027750401n
+    },
+    ImBanana: {
+        name: "Im_Banana",
+        id: 635250116688871425n
     }
 } satisfies Record<string, Dev>);
 
