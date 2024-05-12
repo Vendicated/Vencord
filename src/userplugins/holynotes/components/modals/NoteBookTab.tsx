76b6006a5616188f637aa9e4df4677ad82b4e3c3
AzraelHerself <scyyebiz@gmail.com>
2024-05-12 19:18:22 -0400

    feat(plugin): HolyNotes

---
+++ src/userplugins/holynotes/components/modals/NoteBookTab.tsx
@@ -0,0 +1,185 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { classes } from "@utils/misc";
+import { findByProps } from "@webpack";
+import { Button, Clickable, Menu, Popout, React } from "@webpack/common";
+
+import { SvgOverFlowIcon } from "../icons/overFlowIcon";
+
+
+
+export function NoteBookTabs({ tabs, selectedTabId, onSelectTab }: { tabs: string[], selectedTabId: string, onSelectTab: (tab: string) => void }) {
+    const tabBarRef = React.useRef<HTMLDivElement>(null);
+    const widthRef = React.useRef<number>(0);
+    const tabWidthMapRef = React.useRef(new Map());
+    const [overflowedTabs, setOverflowedTabs] = React.useState<string[]>([]);
+    const resizeObserverRef = React.useRef<ResizeObserver | null>(null);
+    const [show, setShow] = React.useState(false);
+
+    const { isNotNullish } = findByProps("isNotNullish");
+    const { overflowIcon } = findByProps("overflowIcon");
+
+    const handleResize = React.useCallback(() => {
+        if (!tabBarRef.current) return;
+        const overflowed = [] as string[];
+
+        const totalWidth = tabBarRef.current.clientWidth;
+        if (totalWidth !== widthRef.current) {
+
+            // Thanks to daveyy1 for the help with this
+            let width = 0;
+            for (let i = 0; i < tabs.length; i++) {
+                const tab = tabs[i];
+                const tabRef = tabWidthMapRef.current.get(tab);
+
+                if (!tabRef) continue;
+                width += tabRef.width;
+
+                if (width > totalWidth) {
+                    overflowed.push(tab);
+                }
+
+            }
+
+            setOverflowedTabs(overflowed);
+        }
+    }, [tabs, selectedTabId]);
+
+
+    React.useEffect(() => {
+        handleResize();
+
+        resizeObserverRef.current = new ResizeObserver(handleResize);
+
+        if (tabBarRef.current) resizeObserverRef.current.observe(tabBarRef.current);
+        return () => {
+            if (resizeObserverRef.current) resizeObserverRef.current.disconnect();
+        };
+    }, [handleResize]);
+
+    const TabItem = React.forwardRef(function ({ id, selected, onClick, children }: { id: string, selected: boolean, onClick: () => void, children: React.ReactNode }, ref) {
+        return (
+            <Clickable
+                className={classes("vc-notebook-tabbar-item", selected ? "vc-notebook-selected" : "")}
+                data-tab-id={id}
+                // @ts-expect-error
+                innerRef={ref}
+                onClick={onClick}
+            >
+                {children}
+            </Clickable>
+        );
+    });
+
+    const renderOverflowMenu = React.useCallback((closePopout: () => void) => {
+        return (
+            <Menu.Menu
+                navId="notebook-tabs"
+                aria-label="Notebook Tabs"
+                variant="fixed"
+                onClose={closePopout}
+                onSelect={closePopout}
+            >
+                {tabs.map(tab => {
+                    return overflowedTabs.includes(tab) && selectedTabId !== tab ? (
+                        <Menu.MenuItem
+                            id={tab}
+                            label={tab}
+                            action={() => onSelectTab(tab)}
+                        />
+                    ) : null;
+                }).filter(isNotNullish)}
+
+            </Menu.Menu>
+        );
+    }, [tabs, selectedTabId, onSelectTab, overflowedTabs]);
+
+    return (
+        <div
+            className={classes("vc-notebook-tabbar")}
+            ref={tabBarRef}
+        >
+
+            {tabs.map(tab => {
+                if (!overflowedTabs.includes(tab)) {
+                    return (
+                        <TabItem
+                            id={tab}
+                            selected={selectedTabId === tab}
+                            ref={(el: HTMLElement | null) => {
+                                const width = tabWidthMapRef.current.get(tab)?.width ?? 0;
+                                tabWidthMapRef.current.set(tab, {
+                                    node: el,
+                                    width: el ? el.getBoundingClientRect().width : width
+                                });
+                            }}
+                            onClick={selectedTabId !== tab ? () => onSelectTab(tab) : () => {}}
+                        >
+                            {tab}
+                        </TabItem>
+                    );
+                }
+                return null;
+            }).filter(isNotNullish)}
+            {overflowedTabs.length > 0 && (
+                <Popout
+                    shouldShow={show}
+                    onRequestClose={() => setShow(false)}
+                    renderPopout={() => renderOverflowMenu(() => setShow(false))}
+                    position="bottom"
+                    align="right"
+                    spacing={0}
+                >
+                    {props => (
+                        <Button
+                            {...props}
+                            className={"vc-notebook-overflow-chevron"}
+                            size={Button.Sizes.ICON}
+                            look={Button.Looks.BLANK}
+                            onClick={() => setShow(v => !v)}
+                        >
+                            <SvgOverFlowIcon className={classes(overflowIcon)} width={16} height={16}/>
+                        </Button>
+                    )}
+                </Popout>
+
+            )}
+        </div >
+    );
+}
+
+export function CreateTabBar({ tabs, firstSelectedTab, onChangeTab }) {
+    const tabKeys = Object.keys(tabs);
+    const mainTabIndex = tabKeys.indexOf("Main");
+    if (mainTabIndex !== -1 && mainTabIndex !== 0) {
+        tabKeys.splice(mainTabIndex, 1);
+        tabKeys.unshift("Main");
+    }
+
+    const [selectedTab, setSelectedTab] = React.useState(
+        firstSelectedTab || (tabKeys.length > 0 ? tabKeys[0] : null)
+    );
+
+
+
+    const renderSelectedTab = React.useCallback(() => {
+        const selectedTabId = tabKeys.find(tab => tab === selectedTab);
+        return selectedTabId;
+    }, [tabs, selectedTab]);
+
+    return {
+        TabBar: <NoteBookTabs
+            tabs={tabKeys}
+            selectedTabId={selectedTab}
+            onSelectTab={tab => {
+                setSelectedTab(tab);
+                if (onChangeTab) onChangeTab(tab);
+            }} />,
+        renderSelectedTab,
+        selectedTab
+    };
+}
---
+++ src/userplugins/holynotes/components/modals/Notebook.tsx
@@ -0,0 +1,174 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import ErrorBoundary from "@components/ErrorBoundary";
+import { classes } from "@utils/misc";
+import { ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
+import { findByProps } from "@webpack";
+import { ContextMenuApi, Flex, FluxDispatcher, Menu, React, Text, TextInput } from "@webpack/common";
+import noteHandler from "../../noteHandler";
+import { HolyNotes } from "../../types";
+
+import HelpIcon from "../icons/HelpIcon";
+import Errors from "./Error";
+import HelpModal from "./HelpModal";
+import ManageNotebookButton from "./ManageNotebookButton";
+import { CreateTabBar } from "./NoteBookTab";
+import { RenderMessage } from "./RenderMessage";
+
+const renderNotebook = ({
+    notes, notebook, updateParent, sortDirection, sortType, searchInput, closeModal
+}: {
+    notes: Record<string, HolyNotes.Note>;
+    notebook: string;
+    updateParent: () => void;
+    sortDirection: boolean;
+    sortType: boolean;
+    searchInput: string;
+    closeModal: () => void;
+}) => {
+    const messageArray = Object.values(notes).map(note => (
+        <RenderMessage
+            note={note}
+            notebook={notebook}
+            updateParent={updateParent}
+            fromDeleteModal={false}
+            closeModal={closeModal}
+        />
+    ));
+
+    if (sortType)
+        messageArray.sort(
+            (a, b) =>
+                new Date(b.props.note?.timestamp)?.getTime() - new Date(a.props.note?.timestamp)?.getTime(),
+        );
+
+    if (sortDirection) messageArray.reverse();
+
+    const filteredMessages = messageArray.filter(message =>
+        message.props.note?.content?.toLowerCase().includes(searchInput.toLowerCase()),
+    );
+
+    return filteredMessages.length > 0 ? filteredMessages : <Errors />;
+};
+
+
+
+export const NoteModal = (props: ModalProps & { onClose: () => void; }) => {
+    const [sortType, setSortType] = React.useState(true);
+    const [searchInput, setSearch] = React.useState("");
+    const [sortDirection, setSortDirection] = React.useState(true);
+    const [currentNotebook, setCurrentNotebook] = React.useState("Main");
+
+    const { quickSelect, quickSelectLabel, quickSelectQuick, quickSelectValue, quickSelectArrow } = findByProps("quickSelect");
+
+    const forceUpdate = React.useReducer(() => ({}), {})[1] as () => void;
+
+    const notes = noteHandler.getNotes(currentNotebook);
+    if (!notes) return <></>;
+
+    const { TabBar, selectedTab } = CreateTabBar({ tabs: noteHandler.getAllNotes(), firstSelectedTab: currentNotebook, onChangeTab: setCurrentNotebook });
+
+    return (
+        <ErrorBoundary>
+            <ModalRoot {...props} className={classes("vc-notebook")} size={ModalSize.LARGE}>
+                <Flex className={classes("vc-notebook-flex")} direction={Flex.Direction.VERTICAL} style={{ width: "100%" }}>
+                    <div className={classes("vc-notebook-top-section")}>
+                        <ModalHeader className={classes("vc-notebook-header-main")}>
+                            <Text
+                                variant="heading-lg/semibold"
+                                style={{ flexGrow: 1 }}
+                                className={classes("vc-notebook-heading")}>
+                                NOTEBOOK
+                            </Text>
+                            <div className={classes("vc-notebook-flex", "vc-help-icon")} onClick={() => openModal(HelpModal)}>
+                                <HelpIcon />
+                            </div>
+                            <div style={{ marginBottom: "10px" }} className={classes("vc-notebook-search")}>
+                                <TextInput
+                                    autoFocus={false}
+                                    placeholder="Search for a message..."
+                                    onChange={e => setSearch(e)}
+                                />
+                            </div>
+                            <ModalCloseButton onClick={props.onClose} />
+                        </ModalHeader>
+                        <div className={classes("vc-notebook-tabbar-container")}>
+                            {TabBar}
+                        </div>
+                    </div>
+                    <ModalContent style={{ marginTop: "20px" }}>
+                        <ErrorBoundary>
+                            {renderNotebook({
+                                notes,
+                                notebook: currentNotebook,
+                                updateParent: () => forceUpdate(),
+                                sortDirection: sortDirection,
+                                sortType: sortType,
+                                searchInput: searchInput,
+                                closeModal: props.onClose,
+                            })}
+                        </ErrorBoundary>
+                    </ModalContent>
+                </Flex>
+                <ModalFooter>
+                    <ManageNotebookButton notebook={currentNotebook} setNotebook={setCurrentNotebook} />
+                    <div className={classes("sort-button-container", "vc-notebook-display-left")}>
+                        <Flex
+                            align={Flex.Align.CENTER}
+                            className={quickSelect}
+                            onClick={(event: React.MouseEvent<HTMLDivElement>) => {
+                                ContextMenuApi.openContextMenu(event, () => (
+                                    <Menu.Menu
+                                        navId="sort-menu"
+                                        onClose={() => FluxDispatcher.dispatch({ type: "CONTEXT_MENU_CLOSE" })}
+                                        aria-label="Sort Menu"
+                                    >
+                                        <Menu.MenuItem
+                                            label="Ascending / Date Added"
+                                            id="ada"
+                                            action={() => {
+                                                setSortDirection(true);
+                                                setSortType(true);
+                                            }} /><Menu.MenuItem
+                                            label="Ascending / Message Date"
+                                            id="amd"
+                                            action={() => {
+                                                setSortDirection(true);
+                                                setSortType(false);
+                                            }} /><Menu.MenuItem
+                                            label="Descending / Date Added"
+                                            id="dda"
+                                            action={() => {
+                                                setSortDirection(false);
+                                                setSortType(true);
+                                            }} /><Menu.MenuItem
+                                            label="Descending / Message Date"
+                                            id="dmd"
+                                            action={() => {
+                                                setSortDirection(false);
+                                                setSortType(false);
+                                            }} />
+                                    </Menu.Menu>
+
+                                ));
+                            }}
+                        >
+                            <Text className={quickSelectLabel}>Change Sorting:</Text>
+                            <Flex grow={0} align={Flex.Align.CENTER} className={quickSelectQuick}>
+                                <Text className={quickSelectValue}>
+                                    {sortDirection ? "Ascending" : "Descending"} /{" "}
+                                    {sortType ? "Date Added" : "Message Date"}
+                                </Text>
+                                <div className={quickSelectArrow} />
+                            </Flex>
+                        </Flex>
+                    </div>
+                </ModalFooter>
+            </ModalRoot>
+        </ErrorBoundary>
+    );
+};
