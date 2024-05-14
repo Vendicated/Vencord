88179537eebe72d2c2d566100e1908b74a3f3291
Vendicated <contact@swezy.dev>
2024-05-14 18:52:35 +0200

    fix settings ui on canary

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -26,7 +26,7 @@ import UpdaterTab from "@components/VencordSettings/UpdaterTab";
 import VencordTab from "@components/VencordSettings/VencordTab";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { React } from "@webpack/common";
+import { i18n, React } from "@webpack/common";
 
 import gitHash from "~git-hash";
 
@@ -36,41 +36,55 @@ export default definePlugin({
     authors: [Devs.Ven, Devs.Megu],
     required: true,
 
-    patches: [{
-        find: ".versionHash",
-        replacement: [
-            {
-                match: /\[\(0,.{1,3}\.jsxs?\)\((.{1,10}),(\{[^{}}]+\{.{0,20}.versionHash,.+?\})\)," "/,
-                replace: (m, component, props) => {
-                    props = props.replace(/children:\[.+\]/, "");
-                    return `${m},$self.makeInfoElements(${component}, ${props})`;
+    patches: [
+        {
+            find: ".versionHash",
+            replacement: [
+                {
+                    match: /\[\(0,.{1,3}\.jsxs?\)\((.{1,10}),(\{[^{}}]+\{.{0,20}.versionHash,.+?\})\)," "/,
+                    replace: (m, component, props) => {
+                        props = props.replace(/children:\[.+\]/, "");
+                        return `${m},$self.makeInfoElements(${component}, ${props})`;
+                    }
                 }
+            ]
+        },
+        // Discord Stable
+        // FIXME: remove once change merged to stable
+        {
+            find: "Messages.ACTIVITY_SETTINGS",
+            replacement: {
+                get match() {
+                    switch (Settings.plugins.Settings.settingsLocation) {
+                        case "top": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.USER_SETTINGS/;
+                        case "aboveNitro": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.BILLING_SETTINGS/;
+                        case "belowNitro": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.APP_SETTINGS/;
+                        case "belowActivity": return /(?<=\{section:(\i\.\i)\.DIVIDER},)\{section:"changelog"/;
+                        case "bottom": return /\{section:(\i\.\i)\.CUSTOM,\s*element:.+?}/;
+                        case "aboveActivity":
+                        default:
+                            return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.ACTIVITY_SETTINGS/;
+                    }
+                },
+                replace: "...$self.makeSettingsCategories($1),$&"
             }
-        ]
-    }, {
-        find: "Messages.ACTIVITY_SETTINGS",
-        replacement: {
-            get match() {
-                switch (Settings.plugins.Settings.settingsLocation) {
-                    case "top": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.USER_SETTINGS/;
-                    case "aboveNitro": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.BILLING_SETTINGS/;
-                    case "belowNitro": return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.APP_SETTINGS/;
-                    case "belowActivity": return /(?<=\{section:(\i\.\i)\.DIVIDER},)\{section:"changelog"/;
-                    case "bottom": return /\{section:(\i\.\i)\.CUSTOM,\s*element:.+?}/;
-                    case "aboveActivity":
-                    default:
-                        return /\{section:(\i\.\i)\.HEADER,\s*label:(\i)\.\i\.Messages\.ACTIVITY_SETTINGS/;
-                }
-            },
-            replace: "...$self.makeSettingsCategories($1),$&"
+        },
+        // Discord Canary
+        {
+            find: "Messages.ACTIVITY_SETTINGS",
+            replacement: {
+                match: /(?<=section:(.{0,50})\.DIVIDER\}\))([,;])(?=.{0,200}(\i)\.push.{0,100}label:(\i)\.header)/,
+                replace: (_, sectionTypes, commaOrSemi, elements, element) => `${commaOrSemi} $self.addSettings(${elements}, ${element}, ${sectionTypes}) ${commaOrSemi}`
+            }
+        },
+        {
+            find: "Messages.USER_SETTINGS_ACTIONS_MENU_LABEL",
+            replacement: {
+                match: /(?<=function\((\i),\i\)\{)(?=let \i=Object.values\(\i.UserSettingsSections\).*?(\i)\.default\.open\()/,
+                replace: "$2.default.open($1);return;"
+            }
         }
-    }, {
-        find: "Messages.USER_SETTINGS_ACTIONS_MENU_LABEL",
-        replacement: {
-            match: /(?<=function\((\i),\i\)\{)(?=let \i=Object.values\(\i.UserSettingsSections\).*?(\i)\.default\.open\()/,
-            replace: "$2.default.open($1);return;"
-        }
-    }],
+    ],
 
     customSections: [] as ((SectionTypes: Record<string, unknown>) => any)[],
 
@@ -130,19 +144,43 @@ export default definePlugin({
         ].filter(Boolean);
     },
 
+    isRightSpot({ header, settings }: { header?: string; settings?: string[]; }) {
+        const firstChild = settings?.[0];
+        // lowest two elements... sanity backup
+        if (firstChild === "LOGOUT" || firstChild === "SOCIAL_LINKS") return true;
+
+        const { settingsLocation } = Settings.plugins.Settings;
+
+        if (settingsLocation === "bottom") return firstChild === "LOGOUT";
+        if (settingsLocation === "belowActivity") return firstChild === "CHANGELOG";
+
+        const names = {
+            top: i18n.Messages.USER_SETTINGS,
+            aboveNitro: i18n.Messages.BILLING_SETTINGS,
+            belowNitro: i18n.Messages.APP_SETTINGS,
+            aboveActivity: i18n.Messages.ACTIVITY_SETTINGS
+        };
+        return header === names[settingsLocation];
+    },
+
+    addSettings(elements: any[], element: { header?: string; settings: string[]; }, sectionTypes: Record<string, unknown>) {
+        if (!this.isRightSpot(element)) return;
+
+        elements.push(...this.makeSettingsCategories(sectionTypes));
+    },
+
     options: {
         settingsLocation: {
             type: OptionType.SELECT,
             description: "Where to put the Vencord settings section",
             options: [
                 { label: "At the very top", value: "top" },
-                { label: "Above the Nitro section", value: "aboveNitro" },
+                { label: "Above the Nitro section", value: "aboveNitro", default: true },
                 { label: "Below the Nitro section", value: "belowNitro" },
-                { label: "Above Activity Settings", value: "aboveActivity", default: true },
+                { label: "Above Activity Settings", value: "aboveActivity" },
                 { label: "Below Activity Settings", value: "belowActivity" },
                 { label: "At the very bottom", value: "bottom" },
-            ],
-            restartNeeded: true
+            ]
         },
     },
 
