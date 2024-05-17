def4cb05678012df1b57cf18bee1c0a40375aec6
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2024-05-17 04:07:14 -0300

    Clean up

--- src/plugins/automodContext/index.tsx
+++ src/plugins/automodContext/index.tsx
@@ -8,64 +8,66 @@ import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
-import { Button, NavigationRouter, SelectedGuildStore, Text } from "@webpack/common";
-const Kangaroo = findByPropsLazy("jumpToMessage");
+import { Button, ChannelStore, Text } from "@webpack/common";
 
-import "./style.css";
+const { selectChannel } = findByPropsLazy("selectChannel", "selectVoiceChannel");
+
+function jumpToMessage(channelId: string, messageId: string) {
+    const guildId = ChannelStore.getChannel(channelId)?.guild_id;
+
+    selectChannel({
+        guildId,
+        channelId,
+        messageId,
+        jumpType: "INSTANT"
+    });
+}
+
+function findChannelId(message: any): string | null {
+    const { embeds: [embed] } = message;
+    const channelField = embed.fields.find(({ rawName }) => rawName === "channel_id");
+
+    if (!channelField) {
+        return null;
+    }
+
+    return channelField.rawValue;
+}
 
 export default definePlugin({
     name: "AutomodContext",
     description: "Allows you to jump to the messages surrounding an automod hit.",
     authors: [Devs.JohnyTheCarrot],
+
     patches: [
         {
-            find: "Messages.GUILD_AUTOMOD_REPORT_ISSUES",
+            find: ".Messages.GUILD_AUTOMOD_REPORT_ISSUES",
             replacement: {
-                match: /([^.]+\.[^.]+}\)]\}\)\}\),(\(0,.\.[^.]+\)\("div",{[^:]+:.\..{3}}\),))/,
-                replace: "$1 $self.renderJumpButton($self.findChannelId(arguments[0].message), arguments[0].message.id),$2"
+                match: /\.Messages\.ACTIONS.+?}\)(?=,(\(0.{0,40}\.dot.*?}\)),)/,
+                replace: (m, dot) => `${m},${dot},$self.renderJumpButton({message:arguments[0].message})`
             }
         }
     ],
 
-    findChannelId(message) {
-        const { embeds: [embed] } = message;
-        const channelField = embed.fields.find(({ rawName }) => rawName === "channel_id");
-        if (!channelField)
+    renderJumpButton: ErrorBoundary.wrap(({ message }: { message: any; }) => {
+        const channelId = findChannelId(message);
+
+        if (!channelId) {
             return null;
-
-        return channelField.rawValue;
-    },
-
-    jumpToMessage(channelId: string, messageId: string) {
-        const guildId = SelectedGuildStore.getGuildId();
-
-        NavigationRouter.transitionTo(`/channels/${guildId}/${channelId}/${messageId}`);
-        Kangaroo.jumpToMessage({
-            channelId,
-            messageId,
-            flash: false,
-            jumpType: "INSTANT"
-        });
-    },
-
-    renderJumpButton(channelId: string | undefined, messageId: string) {
-        if (!channelId)
-            return null;
+        }
 
         return (
-            <ErrorBoundary noop>
-                <Button
-                    className="automod-context-jump-button"
-                    look={Button.Looks.LINK}
-                    size={Button.Sizes.SMALL}
-                    color={Button.Colors.LINK}
-                    onClick={() => this.jumpToMessage(channelId, messageId)}
-                >
-                    <Text color="text-link" variant="text-xs/normal">
-                        Jump to Surrounding
-                    </Text>
-                </Button>
-            </ErrorBoundary>
+            <Button
+                style={{ padding: "2px 8px" }}
+                look={Button.Looks.LINK}
+                size={Button.Sizes.SMALL}
+                color={Button.Colors.LINK}
+                onClick={() => jumpToMessage(channelId, message.id)}
+            >
+                <Text color="text-link" variant="text-xs/normal">
+                    Jump to Surrounding
+                </Text>
+            </Button>
         );
-    }
+    }, { noop: true })
 });
