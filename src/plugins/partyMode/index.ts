2362aeed67b7bac7dd266d2a726bb1ed7af3e0b1
Nuckyz <contact@swezy.dev>
2024-05-17 04:51:59 -0300

    chore: Fix non standard plugin names

--- src/plugins/partyMode/index.ts
+++ src/plugins/partyMode/index.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { definePluginSettings } from "@api/Settings";
+import { definePluginSettings, migratePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { FluxDispatcher } from "@webpack/common";
@@ -41,8 +41,9 @@ const settings = definePluginSettings({
     },
 });
 
+migratePluginSettings("PartyMode", "Party mode 🎉");
 export default definePlugin({
-    name: "Party mode 🎉",
+    name: "PartyMode",
     description: "Allows you to use party mode cause the party never ends ✨",
     authors: [Devs.UwUDev],
     settings,
