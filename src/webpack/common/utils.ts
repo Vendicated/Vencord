dd85b98e97ac439b9305db32a4291211bf1ad406
ScattrdBlade <61068313+ScattrdBlade@users.noreply.github.com>
2024-05-17 23:58:37 -0400

    Add UploadStore to Webpack Common

--- src/webpack/common/utils.ts
+++ src/webpack/common/utils.ts
@@ -120,6 +120,7 @@ export function showToast(message: string, type = ToastType.MESSAGE) {
 
 export const UserUtils = findByPropsLazy("getUser", "fetchCurrentUser") as { getUser: (id: string) => Promise<User>; };
 
+export const UploadStore = findByPropsLazy("getUploads");
 export const UploadManager = findByPropsLazy("clearAll", "addFile");
 export const UploadHandler = findByPropsLazy("showUploadFileSizeExceededError", "promptToUpload") as {
     promptToUpload: (files: File[], channel: Channel, draftType: Number) => void;
