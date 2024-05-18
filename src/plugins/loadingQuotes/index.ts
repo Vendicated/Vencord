1b5b8cca0368ff7294e54a0ddd4c37aff5814c03
Ulysses Zhan <ulysseszhan@gmail.com>
2024-05-17 22:23:01 -0700

    loadingQuotes: use quotes.txt instead of quotes.js

--- src/plugins/loadingQuotes/index.ts
+++ src/plugins/loadingQuotes/index.ts
@@ -20,7 +20,10 @@ import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 
-import { noQuotesQuote, presetQuotes } from "./quotes";
+import presetQuotesText from "~fileContent/quotes.txt";
+
+const presetQuotes = presetQuotesText.split("\n").map(quote => /^\s*[^#\s]/.test(quote) && quote.trim()).filter(Boolean) as string[];
+const noQuotesQuote = "Did you really disable all loading quotes? What a buffoon you are...";
 
 const settings = definePluginSettings({
     replaceEvents: {
