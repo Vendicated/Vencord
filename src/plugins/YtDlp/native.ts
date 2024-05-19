a087ef9f1dfd531c6adcba7b6aac9d1b4c8b6a39
TheColorman <thoren.alex@gmail.com>
2024-05-19 13:28:55 +0200

    Remove: yt-dlp download

--- src/plugins/YtDlp/native.ts
+++ src/plugins/YtDlp/native.ts
@@ -16,18 +16,10 @@ type DownloadOptions = {
     max_file_size?: number;
 };
 
-const WIN = "https://github.com/yt-dlp/yt-dlp/releases/download/2024.04.09/yt-dlp.exe";
-const LINUX = "https://github.com/yt-dlp/yt-dlp/releases/download/2024.04.09/yt-dlp_linux";
-const LINUX_AARCH64 = "https://github.com/yt-dlp/yt-dlp/releases/download/2024.04.09/yt-dlp_linux_aarch64";
-const LINUX_ARMV7L = "https://github.com/yt-dlp/yt-dlp/releases/download/2024.04.09/yt-dlp_linux_armv7l";
-const MACOS = "https://github.com/yt-dlp/yt-dlp/releases/download/2024.04.09/yt-dlp_macos";
-
-let ytdlpBin: ArrayBuffer | null = null;
 let workdir: string | null = null;
 
 let stdout_global: string = "";
 
-const setYtDlp = (ytDlp: ArrayBuffer) => (ytdlpBin = ytDlp);
 const getdir = () => workdir ?? process.cwd();
 const p = (file: string) => path.join(getdir(), file);
 const cleanVideoFiles = () => {
@@ -85,16 +77,6 @@ function argsFromFormat(format?: "video" | "audio" | "gif", max_file_size?: numb
     }
 }
 
-async function setup(buffer: ArrayBuffer) {
-    setYtDlp(buffer);
-
-    workdir = fs.mkdtempSync(path.join(os.tmpdir(), "vencord_ytdlp_"));
-    console.log("[Plugin:yt-dlp] Using workdir: ", workdir);
-
-    // okay so windows is the only system that throws a fit if you don't include the .exe, so I'm just adding it for all of them.
-    fs.writeFileSync(p("yt-dlp.exe"), Buffer.from(buffer));
-}
-
 async function _download(url: string, { format, additional_arguments, max_file_size }: DownloadOptions): Promise<{
     buffer: Buffer;
     title: string;
@@ -116,9 +98,9 @@ async function _download(url: string, { format, additional_arguments, max_file_s
     } catch (e: any) {
         console.error(e);
         return {
-            error: "Failed to get video metadata:\n" + e?.stderr?.toString()
-                ? `\`\`\`\n${e?.stderr?.toString()}\n\`\`\``
-                : "An unknown error occurred."
+            error:
+                "Failed to get video metadata:\n" +
+                `\`\`\`\n${e?.stderr?.toString() || e?.toString()}\n\`\`\``
         };
     }
 
@@ -141,45 +123,16 @@ async function _download(url: string, { format, additional_arguments, max_file_s
     } catch (e: any) {
         console.error(e);
         return {
-            error: "Failed to download or remux video:\n" + (e?.stderr?.toString()
-                ? `\`\`\`\n${e?.stderr?.toString()}\n\`\`\``
-                : `\`\`\`\n${e?.toString()}\n\`\`\``)
+            error:
+                "Failed to download or remux video:\n" +
+                `\`\`\`\n${e?.stderr?.toString() || e?.toString()}\n\`\`\``
         };
     }
 }
 
-export async function start(_: IpcMainInvokeEvent, ytDlp: ArrayBuffer | null) {
-    try {
-        if (ytDlp) {
-            await setup(ytDlp);
-            return "reused";
-        }
-
-        const platform = os.platform();
-        let url: string;
-        switch (platform) {
-            case "win32":
-                url = WIN;
-                break;
-            case "linux":
-                url = os.arch() === "arm" ? LINUX_ARMV7L : os.arch() === "arm64" ? LINUX_AARCH64 : LINUX;
-                break;
-            case "darwin":
-                url = MACOS;
-                break;
-            default:
-                throw new Error("Unsupported platform.");
-        }
-
-        const blob = await (await fetch(url)).blob();
-        const buffer = await blob.arrayBuffer();
-
-        setup(buffer);
-        return buffer;
-    } catch (e) {
-        console.error(e);
-        return "error";
-    }
+export async function start(_: IpcMainInvokeEvent) {
+    workdir = fs.mkdtempSync(path.join(os.tmpdir(), "vencord_ytdlp_"));
+    console.log("[Plugin:yt-dlp] Using workdir: ", workdir);
 }
 export async function stop(_: IpcMainInvokeEvent) {
     if (workdir) {
@@ -211,5 +164,21 @@ export function checkffmpeg(_?: IpcMainInvokeEvent) {
         return true;
     } catch (e) {
         return false;
+    }
+}
+export async function checkytdlp(_?: IpcMainInvokeEvent) {
+    const base = () => execFileSync("yt-dlp", ["--version"]);
+    const python = () => execFileSync("python", ["-m", "yt_dlp", "--version"]);
+    try {
+        base();
+        return true;
+    } catch (e) {
+        // how do I unnest this shit
+        try {
+            python();
+            return true;
+        } catch (e) {
+            return false;
+        }
     }
 }
