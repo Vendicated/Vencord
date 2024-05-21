e026a60458254dd97e0fcce6271ead20c92439bd
AzraelHerself <scyyebiz@gmail.com>
2024-05-21 11:58:29 -0400

    Add plugin

--- src/userplugins/pluralKitEdit/index.tsx
+++ src/userplugins/pluralKitEdit/index.tsx
@@ -19,57 +19,90 @@
 import { addButton, removeButton } from "@api/MessagePopover";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { copyWithToast } from "@utils/misc";
-import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalRoot, ModalSize, openModal } from "@utils/modal";
-import definePlugin, { OptionType } from "@utils/types";
-import { Button, ChannelStore, FluxDispatcher, Forms, Text, Toasts } from "@webpack/common";
-import { Message } from "discord-types/general";
+import {
+    closeModal,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalRoot,
+    ModalSize,
+    openModal
+} from "@utils/modal";
+import definePlugin, { StartAt } from "@utils/types";
+import { Button, ChannelStore, FluxDispatcher, Forms, Text } from "@webpack/common";
+import { Message, User } from "discord-types/general";
 import { insertTextIntoChatInputBox } from "@utils/discord";
 import { CheckedTextInput } from "@components/CheckedTextInput";
 import { EdgeIcon } from "../../plugins/betterSessions/components/icons";
+import { Member, PKAPI, Switch } from "pkapi.js";
+import { Badges } from "@api/index";
+import { BadgeUserArgs } from "@api/Badges";
 import { definePluginSettings } from "@api/Settings";
 
-const settings = definePluginSettings({
-    modal: {
-        type: OptionType.BOOLEAN,
-        description: "Use modal to edit messages",
-        default: false
-    }
+const api = new PKAPI({
 });
+
+function isPk(msg: Message) {
+    // If the message is null, early return
+    return (msg && msg.applicationId === "466378653216014359")
+}
+
+const settings = definePluginSettings({
+
+});
+
 
 export default definePlugin({
     name: "Plural Kit Edit",
     description: "Allows easier editing of pluralkit messages",
     authors: [{ id: 553652308295155723n, name: "Scyye" }],
-    settings: settings,
-    start() {
-        addButton("EditPluralkit", msg => {
-            const handleClick = () => {
-                const pk = msg.author.bot && msg.author.discriminator === "0000";
-                if (pk) {
-                    if (settings.store.modal)
-                        openViewRawModal(msg);
-                    else {
-                        FluxDispatcher.dispatch({
-                            type: "CREATE_PENDING_REPLY",
-                            channel: ChannelStore.getChannel(msg.channel_id),
-                            message: msg,
-                            shouldMention: false,
-                            showMentionToggle: false,
-                        });
-                        insertTextIntoChatInputBox("pk;edit " + msg.content)
-                    }
-                } else {
-                    Toasts.show({
-                        message: "This message was not sent by PluralKit",
-                        id: Toasts.genId(),
-                        type: Toasts.Type.FAILURE
-                    });
-                }
-            };
+    startAt: StartAt.WebpackReady,
+    patches: [
+        {
+            find: ".useCanSeeRemixBadge)",
+            replacement: {
+                match: /(?<=onContextMenu:\i,children:).*?\)}/,
+                replace: "$self.renderUsername(arguments[0])}"
+            }
+        },
+    ],
 
-            const handleContextMenu = e => {
-                e.preventDefault();
-                e.stopPropagation();
+    renderUsername: ({ author, message, isRepliedMessage, withMentionPrefix, userOverride }) => {
+        try {
+            const user: User = userOverride ?? message.author;
+            let { username } = user;
+            username = (user as any).globalName || username;
+
+            const { nick } = author;
+            const prefix = withMentionPrefix ? "@" : "";
+            return <>{message.id}</>
+            return api.getMessage({message:message.id}).then((msg) => {
+                if (msg === undefined) {
+                    return <>{username}</>;
+                }
+                return <>{message.id}</>;
+            });
+            /*
+            if ((username === nick || isRepliedMessage) && !isPk(message))
+                return <>{prefix}{nick}</>;
+
+            let fronters = await (await api.getSystem({ system: user.id })).getFronters();
+            if (fronters === undefined) {
+                return <>Error</>;
+            }
+            return <>{prefix}{username} ({(fronters as Switch).members})</>*/
+        } catch {
+            return <>{author?.nick}</>;
+        }
+    },
+
+    async start() {
+        addButton("pk-edit", msg => {
+            if (!msg) return null;
+            if (!isPk(msg)) return null;
+            const handleClick = () => {
+                replyToMessage(msg, false, true, "pk;edit " + msg.content);
             };
 
             return {
@@ -80,51 +113,46 @@ export default definePlugin({
                 message: msg,
                 channel: ChannelStore.getChannel(msg.channel_id),
                 onClick: handleClick,
-                onContextMenu: handleContextMenu
+                onContextMenu: (e) => {}
             };
+        });
+
+        const system = await api.getSystem({ system: "318902553024659456"});
+
+        if (system === undefined) {
+            console.log("1")
+            return;
+        }
+        // @ts-ignore
+        (await system.getFronters()).members.forEach((member:Member) => {
+            console.log(member)
+            Badges.addBadge({
+                key: "pk-badge-" + system.id + "-" + member.id,
+                image: member.avatar_url??undefined,
+                description: member.name,
+                link: "https://discord.com/users/" + member.id,
+                shouldShow(userInfo: BadgeUserArgs): boolean {
+                    return userInfo.user.id === "318902553024659456";
+                }
+            });
         });
     },
     stop() {
-        removeButton("EditPluralkit");
+        removeButton("pk-edit");
     },
 });
 
-function openViewRawModal(msg?: Message) {
-    if (!msg) return;
-    const message = msg;
-    var result: string = message.content;
-    const key = openModal(props => (
-        <ErrorBoundary>
-            <ModalRoot {...props} size={ModalSize.MEDIUM}>
-                <ModalHeader>
-                    <Text variant="heading-lg/semibold" style={{ flexGrow: 1 }}>EDIT MESSAGE</Text>
-                    <ModalCloseButton onClick={() => closeModal(key)}/>
-                </ModalHeader>
-                <ModalContent>
-                    <div style={{ padding: "16px 0" }}>
-                        {!!message.content && (
-                            <>
-                                <Forms.FormTitle tag="h5">Content</Forms.FormTitle>
-                                <CheckedTextInput value={message.content} onChange={newValue => result = newValue}
-                                                  validate={() => true}></CheckedTextInput>
-                            </>
-                        )}
-                    </div>
-                </ModalContent>
-                <ModalFooter>
-                    <Button onClick={() => copyWithToast(message.content, `Content copied to clipboard!`)}>
-                        Copy
-                    </Button>
-                    <Button onClick={() => {
-                        closeModal(key)
-                        insertTextIntoChatInputBox("pk;edit " + getMessageLink(message) + " " + result)
-                    }}>
-                        Submit
-                    </Button>
-                </ModalFooter>
-            </ModalRoot>
-        </ErrorBoundary>
-    ));
+function replyToMessage(msg: Message, mention: boolean, hideMention: boolean, content?: string | undefined) {
+    FluxDispatcher.dispatch({
+        type: "CREATE_PENDING_REPLY",
+        channel: ChannelStore.getChannel(msg.channel_id),
+        message: msg,
+        shouldMention: mention,
+        showMentionToggle: !hideMention,
+    });
+    if (content) {
+        insertTextIntoChatInputBox(content);
+    }
 }
 
 function getMessageLink(msg: Message) {
