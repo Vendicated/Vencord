60d0929472fc516f126e99b6618c3f4fc61f3b93
cloudskies <cloudcloudskies@gmail.com>
2024-05-22 14:32:46 +0300

    Update to new docs

--- src/plugins/reactErrorDecoder/index.ts
+++ src/plugins/reactErrorDecoder/index.ts
@@ -25,12 +25,12 @@ let ERROR_CODES: any;
 export default definePlugin({
     name: "ReactErrorDecoder",
     description: 'Replaces "Minifed React Error" with the actual error.',
-    authors: [Devs.Cyn, Devs.maisymoe],
+    authors: [Devs.Cyn, Devs.maisymoe, Devs.cloudskies],
     patches: [
         {
-            find: '"https://reactjs.org/docs/error-decoder.html?invariant="',
+            find: '"https://react.dev/errors?invariant="',
             replacement: {
-                match: /(function .\(.\)){(for\(var .="https:\/\/reactjs\.org\/docs\/error-decoder\.html\?invariant="\+.,.=1;.<arguments\.length;.\+\+\).\+="&args\[\]="\+encodeURIComponent\(arguments\[.\]\);return"Minified React error #"\+.\+"; visit "\+.\+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings.")}/,
+                match: /(function .\(.\)){(for\(var .="https:\/\/react\.dev\/errors\/?invariant="\+.,.=1;.<arguments\.length;.\+\+\).\+="&args\[\]="\+encodeURIComponent\(arguments\[.\]\);return"Minified React error #"\+.\+"; visit "\+.\+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings.")}/,
                 replace: (_, func, original) =>
                     `${func}{var decoded=$self.decodeError.apply(null, arguments);if(decoded)return decoded;${original}}`,
             },
@@ -38,7 +38,7 @@ export default definePlugin({
     ],
 
     async start() {
-        const CODES_URL = `https://raw.githubusercontent.com/facebook/react/v${React.version}/scripts/error-codes/codes.json`;
+        const CODES_URL = `https://raw.githubusercontent.com/facebook/react/main/scripts/error-codes/codes.json`;
 
         ERROR_CODES = await fetch(CODES_URL)
             .then(res => res.json())
