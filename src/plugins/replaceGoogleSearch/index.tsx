ad6acc2216413608a59b70cdcd2cf1beb3533af9
Mylloon <kennel.anri@tutanota.com>
2024-05-22 18:56:50 +0200

    also fix newline at start

--- src/plugins/replaceGoogleSearch/index.tsx
+++ src/plugins/replaceGoogleSearch/index.tsx
@@ -37,7 +37,7 @@ const settings = definePluginSettings({
 });
 
 function search(src: string, engine: string) {
-    open(engine + encodeURIComponent(src.trimEnd()), "_blank");
+    open(engine + encodeURIComponent(src.trim()), "_blank");
 }
 
 function makeSearchItem(src: string) {
