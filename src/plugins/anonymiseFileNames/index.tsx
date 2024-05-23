a5f790a1a89af144412bbac98321e766c87ee647
Vendicated <contact@swezy.dev>
2024-05-23 03:26:23 +0200

    fix AnonymiseFilenames

--- src/plugins/anonymiseFileNames/index.tsx
+++ src/plugins/anonymiseFileNames/index.tsx
@@ -73,13 +73,13 @@ export default definePlugin({
         {
             find: "instantBatchUpload:function",
             replacement: {
-                match: /uploadFiles:(.{1,2}),/,
+                match: /uploadFiles:(\i),/,
                 replace:
                     "uploadFiles:(...args)=>(args[0].uploads.forEach(f=>f.filename=$self.anonymise(f)),$1(...args)),",
             },
         },
         {
-            find: "message.attachments",
+            find: 'addFilesTo:"message.attachments"',
             replacement: {
                 match: /(\i.uploadFiles\((\i),)/,
                 replace: "$2.forEach(f=>f.filename=$self.anonymise(f)),$1"
