d7552300eb80bca9f9b9251ce32f4aa8f8c81a39
JokerJosh1234 <103841613+JokerJosh1234@users.noreply.github.com>
2024-05-26 13:48:12 +0800

    Add files via upload

---
+++ src/plugins/hideMessage/index.tsx
@@ -0,0 +1,44 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { addButton, removeButton } from "@api/MessagePopover";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { ChannelStore } from "@webpack/common";
+
+export default definePlugin({
+    name: "Hide Message",
+    description: "Adds a button to hide a message from the chat",
+    authors: [Devs.JokerJosh],
+
+    start() {
+        addButton("HideMessage", message => ({
+            key: message.id,
+            label: "Hide Message",
+            icon: HideIcon,
+            message: message,
+            channel: ChannelStore.getChannel(message.channel_id),
+            onClick: () => hideMessage(message),
+        }));
+    },
+    stop() {
+        removeButton("HideMessage");
+    }
+});
+
+function hideMessage(msg) {
+    const messageElement = document.querySelector(`[id='chat-messages-${msg.channel_id}-${msg.id}']`);
+
+    if (messageElement) {
+        messageElement.remove();
+    }
+}
+
+const HideIcon = () => (
+    <svg aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 226 226">
+        <path fill="currentColor" d="M37.57471,28.15804c-3.83186,0.00101 -7.28105,2.32361 -8.72295,5.87384c-1.4419,3.55022 -0.58897,7.62011 2.15703,10.29267l16.79183,16.79183c-18.19175,14.60996 -29.9888,32.52303 -35.82747,43.03711c-3.12633,5.63117 -3.02363,12.41043 0.03678,18.07927c10.87625,20.13283 42.14532,66.10058 100.99007,66.10058c19.54493,0 35.83986,-5.13463 49.36394,-12.65365l19.31152,19.31152c2.36186,2.46002 5.8691,3.45098 9.16909,2.5907c3.3,-0.86028 5.87708,-3.43736 6.73736,-6.73736c0.86028,-3.3 -0.13068,-6.80724 -2.5907,-9.16909l-150.66666,-150.66667c-1.77289,-1.82243 -4.20732,-2.8506 -6.74984,-2.85075zM113,37.66667c-11.413,0 -21.60375,1.88068 -30.91683,4.81869l24.11182,24.11182c2.23175,-0.32958 4.47909,-0.6805 6.80501,-0.6805c25.99942,0 47.08333,21.08392 47.08333,47.08333c0,2.32592 -0.35092,4.57326 -0.6805,6.80501l32.29623,32.29623c10.1135,-11.22467 17.51573,-22.61015 21.94157,-30.18115c3.3335,-5.68767 3.32011,-12.67425 0.16553,-18.4655c-11.00808,-20.27408 -42.2439,-65.78792 -100.80615,-65.78792zM73.77002,87.08577l13.77555,13.77556c-1.77707,3.67147 -2.79557,7.77466 -2.79557,12.13867c0,15.60342 12.64658,28.25 28.25,28.25c4.364,0 8.46719,-1.01851 12.13867,-2.79557l13.79395,13.79395c-9.356,6.20362 -21.03043,9.17606 -33.4733,7.24642c-19.75617,-3.06983 -35.88427,-19.19794 -38.9541,-38.9541c-1.92879,-12.43739 1.0665,-24.10096 7.26481,-33.45491z"></path>
+    </svg>
+);
