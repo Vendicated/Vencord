affd527bc13503aa906286c0d23f99bc64fe5550
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2024-05-26 05:19:52 -0300

    Make factory wrapper a little more future proof

--- src/utils/misc.tsx
+++ src/utils/misc.tsx
@@ -102,3 +102,8 @@ export function pluralise(amount: number, singular: string, plural = singular + 
 
 /** Unconfigurable properties for proxies */
 export const UNCONFIGURABLE_PROPERTIES = ["arguments", "caller", "prototype"];
+
+export function interpolateIfDefined(strings: TemplateStringsArray, ...args: any[]) {
+    if (args.some(arg => arg == null)) return "";
+    return strings.reduce((acc, str, i) => `${acc}${str}${args[i] ?? ""}`, "");
+}
