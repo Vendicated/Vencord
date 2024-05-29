d7b8afa2215dd4a1998bf0aa563e1504a4a94ee9
vishnyanetchereshnya <151846235+vishnyanetchereshnya@users.noreply.github.com>
2024-05-29 14:30:49 +0300

    Update index.tsx

--- src/plugins/CopyEmojiAsFormattedString/index.tsx
+++ src/plugins/CopyEmojiAsFormattedString/index.tsx
@@ -1,27 +1,15 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin, { OptionType } from "@utils/types";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
-import { Menu } from "@webpack/common";
 import { copyWithToast } from "@utils/misc";
+import definePlugin, { OptionType } from "@utils/types";
 import { findByPropsLazy } from "@webpack";
+import { Menu } from "@webpack/common";
 
 const { convertNameToSurrogate } = findByPropsLazy("convertNameToSurrogate");
 
@@ -41,7 +29,7 @@ function removeCountingPostfix(name: string): string {
 }
 
 function getEmojiFormattedString(target: Target, copyUnicode: boolean): string {
-	const { id: emojiId, name: emojiName } = target.dataset;
+    const { id: emojiId, name: emojiName } = target.dataset;
 
     if (!emojiId) {
         return (
