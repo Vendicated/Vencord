b60ce3d926f272ee136243d5a367a8153d7fd14d
lucifer <snawalt420@proton.me>
2024-05-30 13:11:43 +0200

    Integrated QuickPin plugin into MessageClickActions

--- src/plugins/messageClickActions/index.ts
+++ src/plugins/messageClickActions/index.ts
@@ -1,6 +1,6 @@
 /*
  * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2023 Vendicated and contributors
+ * Copyright (c) 2023 Vendicated, Prism and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@ import { FluxDispatcher, PermissionsBits, PermissionStore, UserStore } from "@we
 
 const MessageActions = findByPropsLazy("deleteMessage", "startEditMessage");
 const EditStore = findByPropsLazy("isEditing", "isEditingAny");
+const pinModule = findByPropsLazy("pinMessage", "unpinMessage");
 
 let isDeletePressed = false;
 const keydown = (e: KeyboardEvent) => e.key === "Backspace" && (isDeletePressed = true);
@@ -46,6 +47,11 @@ const settings = definePluginSettings({
         description: "Enable double click to reply",
         default: true
     },
+    enableCtrlShiftClickToPinUnpin: {
+        type: OptionType.BOOLEAN,
+        description: "Enable Ctrl+Shift click to pin/unpin messages",
+        default: true
+    },
     requireModifier: {
         type: OptionType.BOOLEAN,
         description: "Only do double click actions when shift/ctrl is held",
@@ -53,10 +59,20 @@ const settings = definePluginSettings({
     }
 });
 
+async function pinMessage(channel: any, message: any): Promise<void> {
+    if (!pinModule) return;
+    await pinModule.pinMessage(channel, message.id);
+}
+
+async function unpinMessage(channel: any, message: any): Promise<void> {
+    if (!pinModule) return;
+    await pinModule.unpinMessage(channel, message.id);
+}
+
 export default definePlugin({
     name: "MessageClickActions",
-    description: "Hold Backspace and click to delete, double click to edit/reply",
-    authors: [Devs.Ven],
+    description: "Hold Backspace and click to delete, double click to edit/reply, ctrl+shift click to pin/unpin",
+    authors: [Devs.Ven, Devs.Prism],
     dependencies: ["MessageEventsAPI"],
 
     settings,
@@ -65,9 +81,15 @@ export default definePlugin({
         document.addEventListener("keydown", keydown);
         document.addEventListener("keyup", keyup);
 
-        this.onClick = addClickListener((msg: any, channel, event) => {
+        this.onClick = addClickListener((msg: any, channel: any, event: MouseEvent) => {
             const isMe = msg.author.id === UserStore.getCurrentUser().id;
-            if (!isDeletePressed) {
+
+            if (settings.store.enableCtrlShiftClickToPinUnpin && event.ctrlKey && event.shiftKey) {
+                if (msg.pinned) unpinMessage(channel, msg);
+                else pinMessage(channel, msg);
+                event.preventDefault();
+            }
+            else if (!isDeletePressed) {
                 if (event.detail < 2) return;
                 if (settings.store.requireModifier && !event.ctrlKey && !event.shiftKey) return;
                 if (channel.guild_id && !PermissionStore.can(PermissionsBits.SEND_MESSAGES, channel)) return;
@@ -75,15 +97,12 @@ export default definePlugin({
 
                 if (isMe) {
                     if (!settings.store.enableDoubleClickToEdit || EditStore.isEditing(channel.id, msg.id)) return;
-
                     MessageActions.startEditMessage(channel.id, msg.id, msg.content);
                     event.preventDefault();
                 } else {
                     if (!settings.store.enableDoubleClickToReply) return;
-
                     const EPHEMERAL = 64;
                     if (msg.hasFlag(EPHEMERAL)) return;
-
                     const isShiftPress = event.shiftKey && !settings.store.requireModifier;
                     const NoReplyMention = Vencord.Plugins.plugins.NoReplyMention as any as typeof import("../noReplyMention").default;
                     const shouldMention = Vencord.Plugins.isPluginEnabled("NoReplyMention")
