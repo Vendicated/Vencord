bf6848dffb3d4465079e89c58cb5c6e72195bc39
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-06-01 14:38:39 -0400

    Formatting

--- src/equicordplugins/customAppIcons/index.tsx
+++ src/equicordplugins/customAppIcons/index.tsx
@@ -100,12 +100,12 @@ export default definePlugin({
             <><Forms.FormTitle>
                 <Forms.FormTitle>How to use?</Forms.FormTitle>
             </Forms.FormTitle>
-                <Forms.FormText>
-                    <Forms.FormText>Go to <Link href="/settings/appearance" onClick={e => { e.preventDefault(); closeAllModals(); FluxDispatcher.dispatch({ type: "USER_SETTINGS_MODAL_SET_SECTION", section: "Appearance" }); }}>Appearance Settings</Link> tab.</Forms.FormText>
-                    <Forms.FormText>Scroll down to "In-app Icons" and click on "Preview App Icon".</Forms.FormText>
-                    <Forms.FormText>And upload your own custom icon!</Forms.FormText>
-                    <Forms.FormText>You can only use links when you are uploading your Custom Icon.</Forms.FormText>
-                </Forms.FormText></>
+            <Forms.FormText>
+                <Forms.FormText>Go to <Link href="/settings/appearance" onClick={e => { e.preventDefault(); closeAllModals(); FluxDispatcher.dispatch({ type: "USER_SETTINGS_MODAL_SET_SECTION", section: "Appearance" }); }}>Appearance Settings</Link> tab.</Forms.FormText>
+                <Forms.FormText>Scroll down to "In-app Icons" and click on "Preview App Icon".</Forms.FormText>
+                <Forms.FormText>And upload your own custom icon!</Forms.FormText>
+                <Forms.FormText>You can only use links when you are uploading your Custom Icon.</Forms.FormText>
+            </Forms.FormText></>
         );
     }
 });
