bc95679eb3bf2e544630e9a3f07dbedfe99d4fff
Vendicated <vendicated@riseup.net>
2024-06-06 01:18:11 +0200

    pretend im not dumb

--- src/components/PluginSettings/index.tsx
+++ src/components/PluginSettings/index.tsx
@@ -261,9 +261,9 @@ export default function PluginSettings() {
         plugins = [];
         requiredPlugins = [];
 
-        const showApi = searchValue.value !== "API";
+        const showApi = searchValue.value === "API";
         for (const p of sortedPlugins) {
-            if (p.hidden || (!p.options && p.name.endsWith("API") && showApi))
+            if (p.hidden || (!p.options && p.name.endsWith("API") && !showApi))
                 continue;
 
             if (!pluginFilter(p)) continue;
