2d50ef2405b3b990b01af7e38366165702604f79
Korbo <korbin.m.marshall@gmail.com>
2024-06-06 10:01:31 -0500

    Add speed slider, update oneko.js

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -19,17 +19,29 @@
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
+const settings = definePluginSettings({
+    speed: {
+        description: "Cat movement speed",
+        type: OptionType.SLIDER,
+        markers: makeRange(5, 30, 1),
+        default: 10,
+        stickToMarkers: yes,
+	restartNeeded: true
+    },
+});
+
 export default definePlugin({
     name: "oneko",
     description: "cat follow mouse (real)",
     // Listing adryd here because this literally just evals her script
-    authors: [Devs.Ven, Devs.adryd],
+    authors: [Devs.Ven, Devs.adryd, Devs.Korbo],
 
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/8fa8a1864aa71cd7a794d58bc139e755e96a236c/oneko.js")
+	    fetch("https://raw.githubusercontent.com/adryd325/oneko.js/d7e5e3249206a61011978945e72a9c652d449ef3/oneko.js")
             .then(x => x.text())
             .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
-                .replace("(isReducedMotion)", "(false)"))
+                .replace("(isReducedMotion)", "(false)")
+		        .replace("nekoSpeed = 10", `nekoSpeed = ${nekoSpeed}`))
             .then(eval);
     },
 
