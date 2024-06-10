588a40c43f9c37e4026c658fe2aa46633d5791a4
byeoon <47872200+byeoon@users.noreply.github.com>
2024-06-10 13:53:58 -0400

    fuck lint.

--- src/plugins/webhookManager.desktop/index.tsx
+++ src/plugins/webhookManager.desktop/index.tsx
@@ -6,8 +6,8 @@
 
 import { ApplicationCommandInputType, ApplicationCommandOptionType, findOption, sendBotMessage } from "@api/Commands";
 import { Devs } from "@utils/constants";
-import definePlugin, { PluginNative } from "@utils/types";
 import { ModalContent, ModalProps, ModalRoot, ModalSize } from "@utils/modal";
+import definePlugin, { PluginNative } from "@utils/types";
 
 const Native = VencordNative.pluginHelpers.WebhookManager as PluginNative<typeof import("./native")>;
 
