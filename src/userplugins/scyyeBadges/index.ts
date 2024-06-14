ab5bcd5b7bc95cba9612b66671799e186fd36ee1
AzraelHerself <scyyebiz@gmail.com>
2024-06-14 07:59:12 -0400

    style(badges): Reformat code.

--- src/userplugins/scyyeBadges/index.ts
+++ src/userplugins/scyyeBadges/index.ts
@@ -23,9 +23,16 @@ import { GuildMemberStore, GuildStore, UserStore } from "@webpack/common";
 import { User } from "discord-types/general/index.js";
 import React from "react";
 
-var registered: ProfileBadge[] = [];
+let registered: ProfileBadge[] = [];
 
-function badge(key: string, badge: Badge) {
+
+interface ScyyeBadge {
+    image: string;
+    shouldShow(info: BadgeUserArgs): boolean;
+}
+
+function badge(key: string, badge: ScyyeBadge, position: BadgePosition = BadgePosition.START, link: string = "",
+    onClick: () => void = () => {}) {
     const b: ProfileBadge = new class implements ProfileBadge {
         component: React.ComponentType<ProfileBadge & BadgeUserArgs> | undefined;
         description: string | undefined;
@@ -36,7 +43,7 @@ function badge(key: string, badge: Badge) {
         props: React.HTMLProps<HTMLImageElement> | undefined;
 
         onClick(): void {
-            badge?.onClick();
+            onClick();
         }
 
         shouldShow(userInfo: BadgeUserArgs): boolean {
@@ -44,28 +51,31 @@ function badge(key: string, badge: Badge) {
         }
     };
 
-    b.key = badge.key;
+    b.key = key;
     b.image = badge.image;
-    b.description = badge.description;
-    b.link = badge.link;
-    b.position = badge.position;
+    b.description = format(key);
+    b.link = link;
+    b.position = position;
 
-    if (b.position===undefined)
-        b.position=BadgePosition.START;
     Badges.addBadge(badge);
     registered.push(badge);
 }
 
-class Badge {
-    key: string | undefined;
-    image: string | undefined;
-    description: string | undefined;
-    shouldShow(_: BadgeUserArgs): boolean {
-        return false;
+function format(id: string) {
+    let result = "";
+    let startOfWord = true;
+    for (const c of id) {
+        if (c === "_") {
+            startOfWord = true;
+            result += " ";
+        } else if (startOfWord) {
+            result += c.toUpperCase();
+            startOfWord = false;
+        } else {
+            result += c;
+        }
     }
-    link: string | undefined;
-    onClick(): void {}
-    position: BadgePosition | undefined;
+    return result;
 }
 
 export default definePlugin({
@@ -89,37 +99,25 @@ function removeScyyeBadges() {
 
 function addScyyeBadges() {
     badge("scyye", {
-        key: undefined, link: undefined, position: undefined, onClick(): void {
-        },
-        description: "Scyye",
         image: "https://i.imgur.com/u8fTrP9.png",
         shouldShow(userInfo: BadgeUserArgs): boolean {
             return userInfo.userId==="553652308295155723";
         }
     });
-    badge("scyye_dms", {
-        key: undefined, link: undefined, position: undefined, onClick(): void {
-        },
-        description: "In my DM server",
+    badge("in_my_DM_server", {
         image: GuildStore.getGuild("1116093904266211358").getIconURL(500, true),
         shouldShow(userInfo: BadgeUserArgs): boolean {
             return GuildMemberStore.isMember("1116093904266211358", userInfo.userId);
         }
     });
     badge("root", {
-        key: undefined, link: undefined, position: undefined, onClick(): void {
-        },
-        description: "Root",
         image: UserStore.getUser("318902553024659456").getAvatarURL(),
         shouldShow(userInfo: BadgeUserArgs): boolean {
             const user: User = UserStore.getUser(userInfo.userId);
             return (user.globalName??user.username).includes("』");
         }
     });
-    badge("cc", {
-        key: undefined, link: undefined, position: undefined, onClick(): void {
-        },
-        description: "Card Creator",
+    badge("card_creator", {
         image: "https://cdn.discordapp.com/role-icons/945402769525858355/e95ef5364010c2ff97e3dcce45b9aa80.webp?size=24&quality=lossless",
         shouldShow(userInfo: BadgeUserArgs): boolean {
             return GuildMemberStore.isMember("844974450927730738", userInfo.userId)
