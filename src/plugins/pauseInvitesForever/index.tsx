f1e236973ded71529d00eec6997d7696766b32dc
Amia <9750071+aamiaa@users.noreply.github.com>
2024-06-19 02:41:24 +0200

    fix experiment find

--- src/plugins/pauseInvitesForever/index.tsx
+++ src/plugins/pauseInvitesForever/index.tsx
@@ -19,10 +19,10 @@
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { findByPropsLazy } from "@webpack";
+import { findLazy } from "@webpack";
 import { Constants, GuildStore, i18n, RestAPI } from "@webpack/common";
 
-const { InvitesDisabledExperiment } = findByPropsLazy("InvitesDisabledExperiment");
+const InvitesDisabledExperiment = findLazy(m => m.definition?.id === "2022-07_invites_disabled");
 
 function showDisableInvites(guildId: string) {
     // Once the experiment is removed, this should keep working
