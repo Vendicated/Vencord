3738edff48274b9d96f38336ef2b895d9540ab34
Zuxi <gpg@mail.imzuxi.com>
2024-06-23 15:39:06 -0400

    added ability to add ignored words to message logger

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -529,6 +529,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     nekohaxx: {
         name: "nekohaxx",
         id: 1176270221628153886n
+    },
+    Zuxi: {
+        name: "Zuxi",
+        id: 459742547305299981n
     }
 } satisfies Record<string, Dev>);
 
