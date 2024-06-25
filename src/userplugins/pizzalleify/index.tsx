f7d37d2dd3e5d87acc15d0394ddae7e23665d00f
doeimospng <137697062+doeimospng@users.noreply.github.com>
2024-06-25 14:56:10 +0300

    yeag

--- src/userplugins/pizzalleify/index.tsx
+++ src/userplugins/pizzalleify/index.tsx
@@ -24,7 +24,7 @@ export default definePlugin({
     patches: [],
 
     start() {
-
+        console.log("ok");
         this.preSend = addPreSendListener((channelId, msg) => {
             msg.content = pizzalleify(msg.content);
         });
