fb73730e3a42298ed3e6205c86df0b6adef7d665
doeimospng <137697062+doeimospng@users.noreply.github.com>
2024-06-25 14:41:09 +0300

    pizzalleify plugin

---
+++ src/userplugins/ok/index.tsx
@@ -0,0 +1,34 @@
+import definePlugin from "@utils/types";
+import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
+import { findByPropsLazy } from "@webpack";
+import { ChannelStore, FluxDispatcher, i18n, Menu, MessageStore, Parser, Timestamp, UserStore, useStateFromStores } from "@webpack/common";
+
+function pizzalleify(str: string): string {
+    let result = str.toUpperCase();
+
+    if (!result.endsWith(".")) {
+        result += ".";
+    }
+
+    return result;
+}
+
+export default definePlugin({
+    name: "Pizzalleify",
+    description: "Makes all of your messages Pizzalle-like",
+    authors: [{
+        name: "doeimos.png",
+        id: 1105436709669314600n
+    }],
+
+    async start() {
+
+        this.preSend = addPreSendListener((channelId, msg) => {
+            msg.content = pizzalleify(msg.content);
+        });
+    },
+
+    stop() {
+        removePreSendListener(this.preSend);
+    }
+});
