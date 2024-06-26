88e4357ebcd95004e628c45ea51be261dedebb7e
vee <vendicated@riseup.net>
2024-06-26 12:57:50 +0200

    Update index.tsx

--- src/plugins/showMeYourName/index.tsx
+++ src/plugins/showMeYourName/index.tsx
@@ -67,12 +67,15 @@ export default definePlugin({
             const { nick } = author;
             const prefix = withMentionPrefix ? "@" : "";
 
-            if (username === nick.toLowerCase() || isRepliedMessage && !settings.store.inReplies)
+            if (isRepliedMessage && !settings.store.inReplies || username === nick.toLowerCase())
                 return <>{prefix}{nick}</>;
+
             if (settings.store.mode === "user-nick")
                 return <>{prefix}{username} <span className="vc-smyn-suffix">{nick}</span></>;
+
             if (settings.store.mode === "nick-user")
                 return <>{prefix}{nick} <span className="vc-smyn-suffix">{username}</span></>;
+
             return <>{prefix}{username}</>;
         } catch {
             return <>{author?.nick}</>;
