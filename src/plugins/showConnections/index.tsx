a8f8852135a3f57657ff2f856b36b65aa92f3eb4
Vendicated <contact@swezy.dev>
2024-06-26 13:27:23 +0200

    fix ShowConnections

--- src/plugins/showConnections/index.tsx
+++ src/plugins/showConnections/index.tsx
@@ -211,7 +211,7 @@ export default definePlugin({
             }
         },
         {
-            find: ".BITE_SIZE,onOpenProfile",
+            find: '"BiteSizeProfileBody"',
             replacement: {
                 match: /currentUser:\i,guild:\i,onOpenProfile:.+?}\)(?=])(?<=user:(\i),bio:null==(\i)\?.+?)/,
                 replace: "$&,$self.profilePopoutComponent({ user: $1, displayProfile: $2, simplified: true })"
