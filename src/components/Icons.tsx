4cbbf1a7239eb15d779d616e3237e95ad14171d2
Vendicated <contact@swezy.dev>
2024-06-26 14:38:50 +0200

    PermissionsViewer: add to simplified profiles

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -290,3 +290,21 @@ export function NoEntrySignIcon(props: IconProps) {
         </Icon>
     );
 }
+
+export function SafetyIcon(props: IconProps) {
+    return (
+        <Icon
+            {...props}
+            className={classes(props.className, "vc-safety-icon")}
+            viewBox="0 0 24 24"
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4.27 5.22A2.66 2.66 0 0 0 3 7.5v2.3c0 5.6 3.3 10.68 8.42 12.95.37.17.79.17 1.16 0A14.18 14.18 0 0 0 21 9.78V7.5c0-.93-.48-1.78-1.27-2.27l-6.17-3.76a3 3 0 0 0-3.12 0L4.27 5.22ZM6 7.68l6-3.66V12H6.22C6.08 11.28 6 10.54 6 9.78v-2.1Zm6 12.01V12h5.78A11.19 11.19 0 0 1 12 19.7Z"
+            />
+        </Icon>
+
+    );
+}
--- src/plugins/permissionsViewer/index.tsx
+++ src/plugins/permissionsViewer/index.tsx
@@ -20,14 +20,21 @@ import "./styles.css";
 
 import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { definePluginSettings } from "@api/Settings";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { SafetyIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
+import { classes } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
-import { ChannelStore, GuildMemberStore, GuildStore, Menu, PermissionsBits, UserStore } from "@webpack/common";
+import { findByPropsLazy } from "@webpack";
+import { Button, ChannelStore, Dialog, GuildMemberStore, GuildStore, Menu, PermissionsBits, Popout, TooltipContainer, UserStore } from "@webpack/common";
 import type { Guild, GuildMember } from "discord-types/general";
 
 import openRolesAndUsersPermissionsModal, { PermissionType, RoleOrUserPermission } from "./components/RolesAndUsersPermissions";
 import UserPermissions from "./components/UserPermissions";
 import { getSortedRoles, sortPermissionOverwrites } from "./utils";
+
+const PopoutClasses = findByPropsLazy("container", "scroller", "list");
+const RoleButtonClasses = findByPropsLazy("button", "buttonInner", "icon", "text");
 
 export const enum PermissionsSortOrder {
     HighestRole,
@@ -168,10 +175,45 @@ export default definePlugin({
                 match: /showBorder:(.{0,60})}\),(?<=guild:(\i),guildMember:(\i),.+?)/,
                 replace: (m, showBoder, guild, guildMember) => `${m}$self.UserPermissions(${guild},${guildMember},${showBoder}),`
             }
+        },
+        {
+            find: ".VIEW_ALL_ROLES,",
+            replacement: {
+                match: /children:"\+"\.concat\(\i\.length-\i\.length\).{0,20}\}\),/,
+                replace: "$&$self.ViewPermissionsButton(arguments[0]),"
+            }
         }
     ],
 
-    UserPermissions: (guild: Guild, guildMember: GuildMember | undefined, showBoder: boolean) => !!guildMember && <UserPermissions guild={guild} guildMember={guildMember} showBorder={showBoder} />,
+    UserPermissions: (guild: Guild, guildMember: GuildMember | undefined, showBorder: boolean) =>
+        !!guildMember && <UserPermissions guild={guild} guildMember={guildMember} showBorder={showBorder} />,
+
+    ViewPermissionsButton: ErrorBoundary.wrap(({ guild, guildMember }: { guild: Guild; guildMember: GuildMember; }) => (
+        <Popout
+            position="bottom"
+            align="center"
+            renderPopout={() => (
+                <Dialog className={PopoutClasses.container} style={{ width: "500px" }}>
+                    <UserPermissions guild={guild} guildMember={guildMember} showBorder forceOpen />
+                </Dialog>
+            )}
+        >
+            {popoutProps => (
+                <TooltipContainer text="View Permissions">
+                    <Button
+                        {...popoutProps}
+                        color={Button.Colors.CUSTOM}
+                        look={Button.Looks.FILLED}
+                        size={Button.Sizes.NONE}
+                        innerClassName={classes(RoleButtonClasses.buttonInner, RoleButtonClasses.icon)}
+                        className={classes(RoleButtonClasses.button, RoleButtonClasses.icon, "vc-permviewer-role-button")}
+                    >
+                        <SafetyIcon height="16" width="16" />
+                    </Button>
+                </TooltipContainer>
+            )}
+        </Popout>
+    ), { noop: true }),
 
     contextMenus: {
         "user-context": makeContextMenuPatch("roles", MenuItemParentType.User),
