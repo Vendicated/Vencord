84a7439ee1ceafa8ee8cb212a284cece9775fa2f
Nuckyz <contact@swezy.dev>
2024-06-28 18:17:38 -0300

    Obliterate Sentry

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -289,6 +289,8 @@ page.on("console", async e => {
 
 page.on("error", e => console.error("[Error]", e.message));
 page.on("pageerror", e => {
+    if (e.message.includes("Sentry successfully disabled")) return;
+
     if (!e.message.startsWith("Object") && !e.message.includes("Cannot find module")) {
         console.error("[Page Error]", e.message);
         report.otherErrors.push(e.message);
