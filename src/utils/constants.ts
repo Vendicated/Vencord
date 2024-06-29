fb280bb9621159e50782e0f760ee47d8ce444555
Oliver Anderson <144633084+OtterCodes101@users.noreply.github.com>
2024-06-29 17:53:44 -0600

    fix merge conflicts(i think?)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -530,6 +530,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "nekohaxx",
         id: 1176270221628153886n
     },
+    Antti: {
+        name: "Antti",
+        id: 312974985876471810n
+    },
     OtterDev: {
         name: "OtterDev",
         id: 833087679063130162n
