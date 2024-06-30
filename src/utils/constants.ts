c39b4cd715e08fe58b4064b508fce2d463cecb73
SerStars <90714930+SerStars@users.noreply.github.com>
2024-07-01 01:01:08 +0200

    Add SerStars (me)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -533,6 +533,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Antti: {
         name: "Antti",
         id: 312974985876471810n
+    },
+    SerStars: {
+        name: "SerStars",
+        id: 861631850681729045n
     }
 } satisfies Record<string, Dev>);
 
