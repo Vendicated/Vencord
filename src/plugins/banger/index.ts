833a570d143232098070ba19542d46ea2970148c
Vendicated <contact@swezy.dev>
2024-07-03 21:38:21 +0200

    BetterNotes: fix crashing

--- src/plugins/banger/index.ts
+++ src/plugins/banger/index.ts
@@ -16,28 +16,34 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+
+const settings = definePluginSettings({
+    source: {
+        description: "Source to replace ban GIF with (Video or Gif)",
+        type: OptionType.STRING,
+        default: "https://i.imgur.com/wp5q52C.mp4",
+        restartNeeded: true,
+    }
+});
 
 export default definePlugin({
     name: "BANger",
     description: "Replaces the GIF in the ban dialogue with a custom one.",
     authors: [Devs.Xinto, Devs.Glitch],
+    settings,
     patches: [
         {
             find: "BAN_CONFIRM_TITLE.",
             replacement: {
                 match: /src:\i\("?\d+"?\)/g,
-                replace: "src: Vencord.Settings.plugins.BANger.source"
+                replace: "src:$self.source"
             }
         }
     ],
-    options: {
-        source: {
-            description: "Source to replace ban GIF with (Video or Gif)",
-            type: OptionType.STRING,
-            default: "https://i.imgur.com/wp5q52C.mp4",
-            restartNeeded: true,
-        }
+    get source() {
+        return settings.store.source;
     }
 });
