b8633bd3e85b9d4bd70c648b9343bd5a9e2e937e
Vendicated <contact@swezy.dev>
2024-07-05 18:43:51 +0200

    fix(MessageLogger): don't break attachment edits

--- src/plugins/messageLogger/index.tsx
+++ src/plugins/messageLogger/index.tsx
@@ -337,7 +337,7 @@ export default definePlugin({
                     // Pass through editHistory & deleted & original attachments to the "edited message" transformer
                     match: /(?<=null!=\i\.edited_timestamp\)return )\i\(\i,\{reactions:(\i)\.reactions.{0,50}\}\)/,
                     replace:
-                        "Object.assign($&,{ deleted:$1.deleted, editHistory:$1.editHistory, attachments:$1.attachments })"
+                        "Object.assign($&,{ deleted:$1.deleted, editHistory:$1.editHistory })"
                 },
 
                 {
