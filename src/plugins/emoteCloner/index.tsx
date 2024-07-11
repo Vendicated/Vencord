850b940306830c338c5d80befc7180a6e40d7347
sadan <117494111+sadan4@users.noreply.github.com>
2024-07-10 22:35:27 -0400

    fix regex and remove log

--- src/plugins/emoteCloner/index.tsx
+++ src/plugins/emoteCloner/index.tsx
@@ -363,9 +363,8 @@ const expressionPickerPatch: NavContextMenuPatchCallback = (children, props: { t
 function imageContextPatch(c, p: {
     src: string
 }){
-    console.log("passed", p);
     if ("src" in p
-    && /https:\/\/cdn\.discordapp\.com\/emojis\/(\d+)\.(gif|png).*/gm.test(p.src)
+    && /https:\/\/cdn\.discordapp\.com\/emojis\/(\d+)\.[a-zA-Z]{3,4}.*/gm.test(p.src)
     ){
         const matches = [...p.src.matchAll(/https:\/\/cdn\.discordapp\.com\/emojis\/(\d+)\.(gif|png).*/gm)];
         if (!matches) return;
