116532406a4b4bec4b44e1d7298c7ca800144f82
Hen <contact@swezy.dev>
2024-07-13 19:17:00 +0200

    InvisibleChat: fix embeds missing decrypted content (#2655)

--- src/plugins/invisibleChat.desktop/index.tsx
+++ src/plugins/invisibleChat.desktop/index.tsx
@@ -133,10 +133,12 @@ export default definePlugin({
                     message: message,
                     channel: ChannelStore.getChannel(message.channel_id),
                     onClick: async () => {
-                        await iteratePasswords(message).then((res: string | false) => {
-                            if (res) return void this.buildEmbed(message, res);
-                            return void buildDecModal({ message });
-                        });
+                        const res = await iteratePasswords(message);
+
+                        if (res)
+                            this.buildEmbed(message, res);
+                        else
+                            buildDecModal({ message });
                     }
                 }
                 : null;
@@ -169,9 +171,9 @@ export default definePlugin({
 
         message.embeds.push({
             type: "rich",
-            title: "Decrypted Message",
+            rawTitle: "Decrypted Message",
             color: "0x45f5f5",
-            description: revealed,
+            rawDescription: revealed,
             footer: {
                 text: "Made with ❤️ by c0dine and Sammy!",
             },
