e8f4fbef732127e72778dab5d5c6f25ef301c319
deimos <137697062+doeimospng@users.noreply.github.com>
2024-07-15 13:01:16 +0300

    moldy,, hes DEAD,,,

--- src/userplugins/imageFavouriter/ImageFavouriter.ts
+++ src/userplugins/imageFavouriter/ImageFavouriter.ts
@@ -3,16 +3,14 @@ import definePlugin from "@utils/types";
 export default definePlugin({
     name: "ImageFavouriter",
     description: "Adds the favourite button to all kinds of media.",
-    authors: [
-        {
-            id: 834469760686489620n,
-            name: "NavaShield",
-        },
-    ],
+    authors: [{
+        name: "NavaShield",
+        id: 834469760686489620n,
+    }],
     patches: [],
     start() {
         (RegExp as any)._test ??= RegExp.prototype.test;
-        RegExp.prototype.test = function(str) { return ((RegExp as any)._test as Function).call(this.source === "\\.gif($|\\?|#)" ? /\.(gif|png|jpe?g|webp)($|\?|#)/i : this, str); }
+        RegExp.prototype.test = function (str) { return ((RegExp as any)._test as Function).call(this.source === "\\.gif($|\\?|#)" ? /\.(gif|png|jpe?g|webp)($|\?|#)/i : this, str); };
     },
     stop() {
         RegExp.prototype.test = (RegExp as any)._test;
