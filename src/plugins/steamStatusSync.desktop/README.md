9d9fa18eb4ed3330a01d3bbaab16faffa5dd26f7
niko <57009359+hauntii@users.noreply.github.com>
2024-07-16 00:26:24 -0400

    Add SteamStatusSync

---
+++ src/plugins/steamStatusSync.desktop/README.md
@@ -0,0 +1,5 @@
+# SteamStatusSync
+
+Syncs your status on Discord to Steam via protocol URIs with configurable options
+
+![Animated Demo](./steamsyncdemo.webp)
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -533,6 +533,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Antti: {
         name: "Antti",
         id: 312974985876471810n
+    },
+    niko: {
+        name: "niko",
+        id: 341377368075796483n
     }
 } satisfies Record<string, Dev>);
 
