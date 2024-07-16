5fc6a079b23359188a457e8a4c5a85907433a02a
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-07-16 13:54:07 -0400

    List our plugins as well

--- scripts/generatePluginList.ts
+++ scripts/generatePluginList.ts
@@ -242,7 +242,7 @@ function isPluginFile({ name }: { name: string; }) {
     const plugins = [] as PluginData[];
     const readmes = {} as Record<string, string>;
 
-    await Promise.all(["src/plugins", "src/plugins/_core"].flatMap(dir =>
+    await Promise.all(["src/plugins", "src/plugins/_core", "src/equicordplugins"].flatMap(dir =>
         readdirSync(dir, { withFileTypes: true })
             .filter(isPluginFile)
             .map(async dirent => {
