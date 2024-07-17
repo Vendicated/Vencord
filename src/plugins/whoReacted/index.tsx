94e48b1828226cd75e9b584384652563de3d6d78
Vendicated <contact@swezy.dev>
2024-07-17 21:30:10 +0200

    WhoReacted: fix errors

--- src/plugins/whoReacted/index.tsx
+++ src/plugins/whoReacted/index.tsx
@@ -43,14 +43,23 @@ function fetchReactions(msg: Message, emoji: ReactionEmoji, type: number) {
         },
         oldFormErrors: true
     })
-        .then(res => FluxDispatcher.dispatch({
-            type: "MESSAGE_REACTION_ADD_USERS",
-            channelId: msg.channel_id,
-            messageId: msg.id,
-            users: res.body,
-            emoji,
-            reactionType: type
-        }))
+        .then(res => {
+            for (const user of res.body) {
+                FluxDispatcher.dispatch({
+                    type: "USER_UPDATE",
+                    user
+                });
+            }
+
+            FluxDispatcher.dispatch({
+                type: "MESSAGE_REACTION_ADD_USERS",
+                channelId: msg.channel_id,
+                messageId: msg.id,
+                users: res.body,
+                emoji,
+                reactionType: type
+            });
+        })
         .catch(console.error)
         .finally(() => sleep(250));
 }
@@ -147,13 +156,6 @@ export default definePlugin({
 
         const reactions = getReactionsWithQueue(message, emoji, type);
         const users = Object.values(reactions).filter(Boolean) as User[];
-
-        for (const user of users) {
-            FluxDispatcher.dispatch({
-                type: "USER_UPDATE",
-                user
-            });
-        }
 
         return (
             <div
