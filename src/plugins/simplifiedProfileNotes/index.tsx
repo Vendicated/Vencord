0eff5c352d9913cf3f6019e04edf75522885f2b3
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2024-07-19 18:33:02 +1200

    SimplifiedProfileNotes: Explicitly place after FriendsSince

--- src/plugins/simplifiedProfileNotes/index.tsx
+++ src/plugins/simplifiedProfileNotes/index.tsx
@@ -96,8 +96,8 @@ export default definePlugin({
             // DM Sidebar
             find: /getRelationshipType.{0,800}\.Overlay.{0,200}Messages\.USER_POPOUT_ABOUT_ME/,
             replacement: {
-                match: /(\(0,.{0,50}?Messages\.BOT_PROFILE_CREATED_ON.{0,100}?userId:(\i)\.id}\)\}\))/,
-                replace: "$1,$self.NotesSection({ headingColor: 'header-primary', user: $2, ...vencordNotesHook })"
+                match: /(\(0,.{0,50}?Messages\.BOT_PROFILE_CREATED_ON.{0,100}?userId:(\i)\.id}\)\}\))(.{0,200}?)\]\}/,
+                replace: "$1$3,$self.NotesSection({ headingColor: 'header-primary', user: $2, ...vencordNotesHook })]}"
             }
         }
     ].map(p => ({
