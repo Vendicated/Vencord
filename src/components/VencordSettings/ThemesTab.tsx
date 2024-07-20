42353373db15f72159761bb6ba7ec23fb8080854
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2024-07-20 19:17:05 -0300

    mapMangledModule: Properly support component finds

--- src/components/VencordSettings/ThemesTab.tsx
+++ src/components/VencordSettings/ThemesTab.tsx
@@ -276,7 +276,7 @@ function ThemesTab() {
                             />
                         ))}
                     </div>
-                </Forms.FormSection >
+                </Forms.FormSection>
             </>
         );
     }
