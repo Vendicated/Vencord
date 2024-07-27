a3f203f078c25773668086fb0237915cf10995c6
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2024-07-26 22:44:35 -0600

    Patch context menu

--- src/plugins/noReactOverLimit/index.ts
+++ src/plugins/noReactOverLimit/index.ts
@@ -6,13 +6,28 @@
 
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { ReactionEmoji } from "discord-types/general";
+
+interface MessageReaction {
+    emoji: ReactionEmoji;
+    count: number;
+    count_details: {
+        burst: number;
+        normal: number;
+    };
+    burst_colors: any[];
+    me_burst: boolean;
+    burst_me: boolean;
+    me: boolean;
+    burst_count: number;
+}
 
 export default definePlugin({
     name: "NoReactOverLimit",
     description: "Hides the react button when the limit has already been reached",
     authors: [Devs.MrDiamond],
 
-    countReactions(reactions: { count: number; burst_count: number; }[]) {
+    countReactions(reactions: MessageReaction[]) {
         let count = 0;
         reactions.forEach(reaction => {
             if (reaction.count > 0) count++;
@@ -22,6 +37,7 @@ export default definePlugin({
     },
 
     patches: [
+        // reaction group button
         {
             find: ".Messages.ADD_BURST_REACTION",
             replacement: {
@@ -29,10 +45,27 @@ export default definePlugin({
                 replace: "$1 if ($self.countReactions(this.props.message.reactions) >= 20) return null; $4"
             }
         },
+        // message accessory
         {
             find: ",\"add-reaction\"",
             replacement: {
                 match: /(let\{channel:\i,message:(\i),togglePopout:\i,renderEmojiPicker:\i,shouldShow:\i}=\i;)(return\(0,)/,
+                replace: "$1 if ($self.countReactions($2.reactions) >= 20) return null; $3"
+            }
+        },
+        // quick react buttons
+        {
+            find: "id:\"quickreact-\".concat",
+            replacement: {
+                match: /(function \i\((\i),\i\)\{)(let \i=\(0,)/,
+                replace: "$1 if ($self.countReactions($2.reactions) >= 20) return null; $3"
+            }
+        },
+        // add reaction context menu button
+        {
+            find: "id:\"add-reaction\",",
+            replacement: {
+                match: /(function \i\((\i),\i\)\{)(let\{reducedMotion:)/,
                 replace: "$1 if ($self.countReactions($2.reactions) >= 20) return null; $3"
             }
         }
