258bc36945d9020de31c62fa61f0c4ae9612d50a
NeoNyaa <56982408+NeoNyaa@users.noreply.github.com>
2024-07-29 19:30:58 +0100

    Added a toggle to disable link embed

--- src/plugins/maskedLinkPaste/index.ts
+++ src/plugins/maskedLinkPaste/index.ts
@@ -4,18 +4,29 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants.js";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 import { findByPropsLazy } from "@webpack";
 
 const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const SlateTransforms = findByPropsLazy("insertText", "selectCommandOption");
 
+const settings = definePluginSettings({
+    disableEmbed: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Wrap the link inbetween < > to prevent it from embedding.",
+        restartNeeded: false
+    }
+});
+
 export default definePlugin({
     name: "MaskedLinkPaste",
-    authors: [Devs.TheSun],
+    authors: [Devs.TheSun, Devs.Neo],
     description: "Pasting a link while having text selected will paste a hyperlink",
+    settings,
     patches: [
         {
             find: ".selection,preventEmojiSurrogates:",
@@ -23,15 +34,22 @@ export default definePlugin({
                 match: /(?<=\i.delete.{0,50})(\i)\.insertText\((\i)\)/,
                 replace: "$self.handlePaste($1, $2, () => $&)"
             }
-        }
+        },
     ],
 
     handlePaste(editor, content: string, originalBehavior: () => void) {
         if (content && linkRegex.test(content) && editor.operations?.[0]?.type === "remove_text") {
-            SlateTransforms.insertText(
-                editor,
-                `[${editor.operations[0].text}](${content})`
-            );
+            if (settings.store.disableEmbed) {
+                SlateTransforms.insertText(
+                    editor,
+                    `[${editor.operations[0].text}](<${content}>)`
+                );
+            } else {
+                SlateTransforms.insertText(
+                    editor,
+                    `[${editor.operations[0].text}](${content})`
+                );
+            }
         }
         else originalBehavior();
     }
