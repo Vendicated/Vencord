3905e5278bda1054e35cf3074ab28a94700f2821
AshtonMemer <53633401+AshtonMemer@users.noreply.github.com>
2024-07-31 03:29:40 -0500

    Switch native function from http to fetch API

--- src/plugins/translate/native.ts
+++ src/plugins/translate/native.ts
@@ -5,32 +5,26 @@
  */
 
 import { IpcMainInvokeEvent } from "electron";
-import { request } from "https";
 
 interface DeepLResponse {
     status?: number;
     data: string;
 }
 
-export function makeRequest(_: IpcMainInvokeEvent, pro: boolean, apiKey: string, payload: string): Promise<DeepLResponse> {
+export async function makeRequest(_: IpcMainInvokeEvent, pro: boolean, apiKey: string, payload: string): Promise<DeepLResponse> {
     const url = pro
         ? "https://api.deepl.com/v2/translate"
         : "https://api-free.deepl.com/v2/translate";
 
-    return new Promise<DeepLResponse>((resolve, reject) => {
-        const req = request(new URL(url), {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `DeepL-Auth-Key ${apiKey}`
-            }
-        }, res => {
-            let responseBody = "";
-            res.on("data", chunk => responseBody += chunk);
-            res.on("end", () => resolve({ status: res.statusCode, data: responseBody }));
-        });
-        req.on("error", err => reject({ data: err.message }));
-        req.write(payload);
-        req.end();
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `DeepL-Auth-Key ${apiKey}`
+        },
+        body: payload
     });
+
+    const data = await res.text();
+    return { status: res.status, data };
 }
