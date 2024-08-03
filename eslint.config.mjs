Vendicated/Vencord

commit 9e9d4657148389b2e3ccf217557caccd2d36c89b

refactor: misc. changes

src/utils/mergeDefaults.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { isObject } from "./misc";
+
 /**
  * Recursively merges defaults into an object and returns the same object
  * @param obj Object
@@ -13,7 +15,7 @@
 export function mergeDefaults<T>(obj: T, defaults: T): T {
     for (const key in defaults) {
         const v = defaults[key];
-        if (typeof v === "object" && v !== null && !Array.isArray(v)) {
+        if (isObject(v)) {
             obj[key] ??= {} as any;
             mergeDefaults(obj[key], v);
         } else {

packages/discord-types/src/flux/FluxActionHandlersGraph.ts
@@ -7,7 +7,7 @@
 import type { DepGraph } from "dependency-graph";
 import type { UnionToIntersection } from "type-fest";
 
-import type { Nullish } from "../internal";
+import type { IsAny, Nullish } from "../internal";
 import type { ExtractAction, FluxAction, FluxActionHandler, FluxActionType } from "./fluxActions";
 import type { FluxDispatchBand } from "./FluxDispatcher";
 
@@ -55,7 +55,7 @@ export type FluxActionHandlerMap<Action extends FluxAction = FluxAction>
     = UnionToIntersection<
         Action extends unknown
             ? PropertyKey extends keyof Action
-                ? 0 extends 1 & Action[PropertyKey]
+                ? IsAny<Action[string]> & IsAny<Action[number]> & IsAny<Action[symbol]> extends unknown
                     ? { [ActionType in Action["type"]]: (action: any) => void; }
                     : { [ActionType in Action["type"]]: (action: Action & { type: ActionType; }) => void; }
                 : { [ActionType in Action["type"]]: (action: Action & { type: ActionType; }) => void; }

packages/discord-types/src/flux/fluxActions.ts
@@ -4,10 +4,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import type { IsAny } from "../internal";
+
 export type FluxActionHandler<Action extends FluxAction = FluxAction, Return = void>
     = Action extends unknown
         ? PropertyKey extends keyof Action
-            ? 0 extends 1 & Action[PropertyKey]
+            ? IsAny<Action[string]> & IsAny<Action[number]> & IsAny<Action[symbol]> extends unknown
                 ? (action: any) => Return
                 : (action: Action) => Return
             : (action: Action) => Return

packages/discord-types/src/general/messages/MessageRecord.ts
@@ -164,6 +164,13 @@ export interface MessageReference {
     channel_id: string;
     guild_id?: string;
     message_id?: string;
+    type: MessageReferenceType;
+}
+
+// Original name: MessageReferenceTypes
+export enum MessageReferenceType {
+    DEFAULT = 0,
+    FORWARD = 1,
 }
 
 export interface MessagePoll {

packages/discord-types/src/internal.ts
@@ -16,6 +16,9 @@ export type Defined<T> = Exclude<T, undefined>;
 export type GenericConstructor = new (...args: any[]) => unknown;
 
 /** @internal */
+export type IsAny<T> = 0 extends 1 & T ? unknown : never;
+
+/** @internal */
 export type Nullish = null | undefined;
 
 /** @internal */

src/api/Commands/commandHelpers.ts
@@ -17,10 +17,10 @@
 */
 
 import { mergeDefaults } from "@utils/mergeDefaults";
-import type { ChannelType, InteractionType, MessageActivity, MessageAttachment, MessageEmbedType, MessageFlags, MessageInteractionMetadata, MessagePoll, MessageRoleSubscriptionData, MessageType, Sticker, StickerItem, UserFlags, UserPremiumType } from "@vencord/discord-types";
+import type { ChannelType, InteractionType, MessageActivity, MessageAttachment, MessageEmbedType, MessageFlags, MessageInteractionMetadata, MessagePoll, MessageReference, MessageRoleSubscriptionData, MessageType, Sticker, StickerItem, UserClanData, UserFlags, UserPremiumType } from "@vencord/discord-types";
 import { findByCodeLazy } from "@webpack";
 import { MessageActionCreators, SnowflakeUtils } from "@webpack/common";
-import type { LiteralToPrimitive, PartialDeep } from "type-fest";
+import type { LiteralToPrimitive, PartialDeep, SnakeCasedProperties } from "type-fest";
 
 import type { Argument } from "./types";
 
@@ -39,7 +39,7 @@ export const generateId = () => `-${SnowflakeUtils.fromTimestamp(Date.now())}`;
  * @param channelId ID of the channel to send the message to
  * @param message The message to send
  */
-export function sendBotMessage(channelId: string, message: PartialDeep<MessageJSON, { recurseIntoArrays: true; }>) {
+export function sendBotMessage(channelId: string, message: PartialDeep<MessageJSON>) {
     const botMessage = createBotMessage({ channelId, content: "" });
 
     MessageActionCreators.receiveMessage(channelId, mergeDefaults(message, botMessage));
@@ -96,12 +96,7 @@ export interface MessageJSON {
     mention_everyone: boolean;
     mention_roles: string[];
     mentions: UserJSON[];
-    message_reference?: {
-        channel_id?: string;
-        fail_if_not_exists?: boolean;
-        guild_id?: string;
-        message_id?: string;
-    };
+    message_reference?: MessageReference;
     nonce?: string | number;
     pinned: boolean;
     poll?: Omit<MessagePoll, "expiry"> & { expiry: string | null; };
@@ -150,6 +145,7 @@ export interface UserJSON {
     };
     banner?: string | null;
     bot?: boolean;
+    clan: SnakeCasedProperties<UserClanData> | null;
     discriminator: string;
     email?: string | null;
     flags?: UserFlags;

commit 1cd4473edc8159e55c99b6f466ab4da65fdb6e16

fix: regressions in 9e9d465 and 04ae33e where VencordDesktopMain errors when it imports from `src/utils/misc.ts`

eslint.config.mjs
@@ -11,7 +11,7 @@ import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    { ignores: ["browser", "dist", "packages", "src/**/*.?(c|m)js?(x)", "eslint.config.mjs"] },
+    { ignores: ["browser", "dist", "packages", "src/**/*.?(c|m)js?(x)", "*.*"] },
     {
         files: ["**/*.?(c|m)[jt]s?(x)"],
         languageOptions: {

src/shared/SettingsStore.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { isObject } from "@utils";
 import type { LiteralUnion } from "type-fest";
 
 // Resolves a possibly nested prop in the form of "some.nested.prop" to type of T.some.nested.prop
@@ -68,7 +67,9 @@ export class SettingsStore<T extends object> {
                     });
                 }
 
-                if (isObject(v))
+                // This cannot be replaced with a call to 'isObject' from '@utils/misc'.
+                // '@utils/misc' will error when imported in the main process.
+                if (typeof v === "object" && v !== null && !Array.isArray(v))
                     return self.makeProxy(v, root, `${path}${path && "."}${key}`);
 
                 return v;

src/utils/mergeDefaults.ts
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { isObject } from "./misc";
-
 /**
  * Recursively merges defaults into an object and returns the same object
  * @param obj Object
@@ -15,7 +13,9 @@ import { isObject } from "./misc";
 export function mergeDefaults<T>(obj: T, defaults: T): T {
     for (const key in defaults) {
         const v = defaults[key];
-        if (isObject(v)) {
+        // This cannot be replaced with a call to 'isObject' from '@utils/misc'.
+        // '@utils/misc' will error when imported in the main process.
+        if (typeof v === "object" && v !== null && !Array.isArray(v)) {
             obj[key] ??= {} as any;
             mergeDefaults(obj[key], v);
         } else {