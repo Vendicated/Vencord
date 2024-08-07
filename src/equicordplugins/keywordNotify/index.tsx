1e5e6d222b66a539e866b9fb615c7cfa64fcdb21
thororen <78185467+thororen1234@users.noreply.github.com>
2024-08-07 02:36:31 -0400

    Womp Womp 3

--- src/equicordplugins/keywordNotify/index.tsx
+++ src/equicordplugins/keywordNotify/index.tsx
@@ -34,7 +34,7 @@ const tabClass = findByPropsLazy("tab");
 const buttonClass = findByPropsLazy("size36");
 
 const MenuHeader = findByCodeLazy(".getMessageReminders()).length");
-const Popout = findByCodeLazy("e.get(e.jumpTargetId");
+const Popout = findByCodeLazy(".Messages.UNBLOCK_TO_JUMP_TITLE", "canCloseAllMessages:");
 const createMessageRecord = findByCodeLazy(".createFromServer(", ".isBlockedForMessage", "messageReference:");
 const KEYWORD_ENTRIES_KEY = "KeywordNotify_keywordEntries";
 const KEYWORD_LOG_KEY = "KeywordNotify_log";
