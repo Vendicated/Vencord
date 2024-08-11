28dcd2bc3e947158ce77e034a22758ab67c5acbe
Inbestigator <inbestigatoryt@gmail.com>
2024-08-10 20:10:57 -0700

    Initial commit

---
+++ src/plugins/secureToken/README.md
@@ -0,0 +1,5 @@
+# SecureToken
+
+Stores your Discord token safely. This is meant to combat hackers from simply stealing your token from the traditional location.
+
+Note that this plugin requires you to re-login to an account every time you want to switch to it.
---
+++ src/plugins/secureToken/index.ts
@@ -0,0 +1,97 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+
+const tokenUtils = findByPropsLazy("getToken");
+const key = crypto.randomUUID();
+
+const settings = definePluginSettings({
+    encrypted: {
+        type: OptionType.BOOLEAN,
+        description: "Whether the token should be encrypted, will require a login every restart",
+        restartNeeded: true,
+        default: true
+    },
+    token: {
+        type: OptionType.STRING,
+        hidden: true,
+        description: "Discord token"
+    }
+});
+
+function setToken(t: string) {
+    if (settings.store.encrypted) {
+        settings.store.token = xorEncryptDecrypt(t, key);
+    } else {
+        settings.store.token = settings.store.token;
+    }
+    tokenUtils.setToken("secured", undefined);
+};
+
+function xorEncryptDecrypt(input: string, key: string): string {
+    const encoder = new TextEncoder();
+    const inputBytes = encoder.encode(input);
+    const keyBytes = encoder.encode(key);
+
+    const outputBytes = new Uint8Array(inputBytes.length);
+
+    for (let i = 0; i < inputBytes.length; i++) {
+        outputBytes[i] = inputBytes[i] ^ keyBytes[i % keyBytes.length];
+    }
+
+    const decoder = new TextDecoder();
+    return decoder.decode(outputBytes);
+}
+
+function handleGetToken(res: any) {
+    if (res() !== "secured" && res()) {
+        setToken(res());
+        return res();
+    } else if (settings.store.token && res()) {
+        if (settings.store.encrypted) {
+            return xorEncryptDecrypt(settings.store.token, key);
+        } else {
+            return settings.store.token;
+        }
+    } else {
+        return undefined;
+    }
+};
+
+function handleSetToken(v: any, res: any) {
+    if (v === "secured") { res(); } else { setToken(v); }
+};
+
+export default definePlugin({
+    name: "SecureToken",
+    description: "Stores your Discord token safely.",
+    authors: [Devs.Inbestigator],
+    handleGetToken,
+    handleSetToken,
+    settings,
+    patches: [
+        {
+            find: "decryptedToken",
+            replacement: [
+                {
+                    match: /return\((\i)\(\),null!=(\i)\)\?_\[(\i)\]:(\i)/,
+                    replace: "return $self.handleGetToken((()=>($1(),null!=$2)?_[$3]:$4))"
+                },
+                {
+                    match: /if\(null==(\i)\)\{\i\(\i\);return\}\i=\i,null!=\i&&\(_\[\i\]=\i\),\i\?\i\(\):\(\i=\i,\i=_,\i\(\)\)/,
+                    replace: "$self.handleSetToken($1,()=>{$&})"
+                }
+            ]
+        }
+    ],
+    start: () => {
+        tokenUtils.getToken();
+    },
+});
