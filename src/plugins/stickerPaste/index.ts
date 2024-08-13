a4bf87a050290f3458bbcf954580ab9dcb61d978
ImBonana <eladtairo@gmail.com>
2024-08-13 22:46:15 +0300

    Init

---
+++ src/plugins/stickerPaste/index.ts
@@ -0,0 +1,22 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "StickerPaste",
+    description: "When picking a sticker, it inserts it into the chatpox instead of sending it instantly.",
+    authors: [Devs.ImBanana],
+
+    patches: [{
+        find: ".stickers,previewSticker:",
+        replacement: {
+            match: /(if\(.{1,5}\.getUploadCount)/,
+            replace: "return true;$1",
+        }
+    }]
+});
