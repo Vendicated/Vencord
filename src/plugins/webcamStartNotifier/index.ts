7d4fb35cd361566c6dd3d9828f18f55bdc2c3042
redbaron2k7 <76609332+redbaron2k7@users.noreply.github.com>
2024-08-13 16:24:36 -0600

    Update index.ts

--- src/plugins/webcamStartNotifier/index.ts
+++ src/plugins/webcamStartNotifier/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import definePlugin from "@utils/types";
 import { SelectedChannelStore } from "@webpack/common";
 import { Devs } from "@utils/constants";
