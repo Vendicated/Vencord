eeb71f43e9f690ce17047a01380845963d5403f8
The Master Kitty <109026259+TheMasterKitty@users.noreply.github.com>
2024-08-16 15:22:52 -0400

    Modified Friend Invites
    Allowed slash commands to work everywhere (dms, servers, etc)
    Changed commands to start with /fvite followed by create/list/revoke

--- src/plugins/friendInvites/index.ts
+++ src/plugins/friendInvites/index.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { ApplicationCommandInputType, sendBotMessage } from "@api/Commands";
+import { ApplicationCommandInputType, ApplicationCommandOptionType, sendBotMessage } from "@api/Commands";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
@@ -25,16 +25,15 @@ const FriendInvites = findByPropsLazy("createFriendInvite");
 
 export default definePlugin({
     name: "FriendInvites",
-    description: "Create and manage friend invite links via slash commands (/create friend invite, /view friend invites, /revoke friend invites).",
-    authors: [Devs.afn, Devs.Dziurwa],
+    description: "Create and manage friend invite links via slash commands (/fvite create, /fvite list, /fvite revoke).",
+    authors: [ Devs.afn, Devs.Dziurwa, Devs.TheMasterKitty ],
     dependencies: ["CommandsAPI"],
     commands: [
         {
-            name: "create friend invite",
+            name: "fvite create",
             description: "Generates a friend invite link.",
-            inputType: ApplicationCommandInputType.BOT,
-
-            execute: async (args, ctx) => {
+            inputType: ApplicationCommandInputType.BUILT_IN,
+            execute: async (_, ctx) => {
                 const invite = await FriendInvites.createFriendInvite();
 
                 sendBotMessage(ctx.channel.id, {
@@ -47,14 +46,14 @@ export default definePlugin({
             }
         },
         {
-            name: "view friend invites",
-            description: "View a list of all generated friend invites.",
-            inputType: ApplicationCommandInputType.BOT,
+            name: "fvite list",
+            description: "Lists all friend invite link.",
+            inputType: ApplicationCommandInputType.BUILT_IN,
             execute: async (_, ctx) => {
                 const invites = await FriendInvites.getAllFriendInvites();
                 const friendInviteList = invites.map(i =>
                     `
-                    _discord.gg/${i.code}_ ·
+                    discord.gg/${i.code} ·
                     Expires: <t:${new Date(i.expires_at).getTime() / 1000}:R> ·
                     Times used: \`${i.uses}/${i.max_uses}\`
                     `.trim().replace(/\s+/g, " ")
@@ -63,19 +62,19 @@ export default definePlugin({
                 sendBotMessage(ctx.channel.id, {
                     content: friendInviteList.join("\n") || "You have no active friend invites!"
                 });
-            },
+            }
         },
         {
-            name: "revoke friend invites",
-            description: "Revokes all generated friend invites.",
-            inputType: ApplicationCommandInputType.BOT,
+            name: "fvite revoke",
+            description: "Revokes all friend invite link.",
+            inputType: ApplicationCommandInputType.BUILT_IN,
             execute: async (_, ctx) => {
                 await FriendInvites.revokeFriendInvites();
 
                 sendBotMessage(ctx.channel.id, {
                     content: "All friend invites have been revoked."
                 });
-            },
-        },
+            }
+        }
     ]
 });
