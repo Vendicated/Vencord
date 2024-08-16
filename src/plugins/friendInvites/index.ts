2c1867238b7ea1b5cb40473315654d480717bcb2
The Master Kitty <109026259+TheMasterKitty@users.noreply.github.com>
2024-08-16 15:37:10 -0400

    Fixed Unused Import

--- src/plugins/friendInvites/index.ts
+++ src/plugins/friendInvites/index.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { ApplicationCommandInputType, ApplicationCommandOptionType, sendBotMessage } from "@api/Commands";
+import { ApplicationCommandInputType, sendBotMessage } from "@api/Commands";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByPropsLazy } from "@webpack";
