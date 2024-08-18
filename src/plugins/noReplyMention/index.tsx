0593bcf70f7c00451ed0dc9c89c969867c7bfcaf
Antidisestablishmentarianism <43791401+AntidisestablishmentarianismIsReal@users.noreply.github.com>
2024-08-18 05:16:30 -0400

    Revert "Add no reply role to NoReplyMention"
    This reverts commit 6fade75e4eb65759942241757deb477f09efcc5b.

--- src/plugins/noReplyMention/index.tsx
+++ src/plugins/noReplyMention/index.tsx
@@ -1,13 +1,24 @@
 /*
- * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { ChannelStore, GuildMemberStore } from "@webpack/common";
 import type { Message } from "discord-types/general";
 
 const settings = definePluginSettings({
@@ -17,22 +28,16 @@ const settings = definePluginSettings({
         type: OptionType.STRING,
         default: "1234567890123445,1234567890123445",
     },
-    roleList: {
-        description:
-            "List of roles to allow or exempt pings for (separated by commas or spaces)",
-        type: OptionType.STRING,
-        default: "1234567890123445,1234567890123445",
-    },
     shouldPingListed: {
         description: "Behaviour",
         type: OptionType.SELECT,
         options: [
             {
-                label: "Do not ping the listed users/roles",
+                label: "Do not ping the listed users",
                 value: false,
             },
             {
-                label: "Only ping the listed users/roles",
+                label: "Only ping the listed users",
                 value: true,
                 default: true,
             },
@@ -52,19 +57,7 @@ export default definePlugin({
     settings,
 
     shouldMention(message: Message, isHoldingShift: boolean) {
-        const channel = ChannelStore.getChannel(message.channel_id);
-        let isListed = settings.store.userList.includes(message.author.id);
-
-        if (channel) {
-            const roles = GuildMemberStore.getMember(channel.guild_id, message.author.id)?.roles;
-
-            roles.forEach(role => {
-                if (settings.store.roleList.includes(role)) {
-                    isListed = true;
-                }
-            });
-        }
-
+        const isListed = settings.store.userList.includes(message.author.id);
         const isExempt = settings.store.shouldPingListed ? isListed : !isListed;
         return settings.store.inverseShiftReply ? isHoldingShift !== isExempt : !isHoldingShift && isExempt;
     },
