53fa510ed82d21ed8af02cd7f545b039fa91c353
Obsidian <108832807+Obsidianninja11@users.noreply.github.com>
2024-08-30 04:10:04 -0800

    Formatting

--- src/plugins/CopyFileContents/index.tsx
+++ src/plugins/CopyFileContents/index.tsx
@@ -13,7 +13,7 @@ import definePlugin from "@utils/types";
 import { Clipboard, Tooltip, useState } from "@webpack/common";
 
 const CheckMarkIcon = () => {
-    return <svg aria-hidden="true" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    return <svg width="24" height="24" viewBox="0 0 24 24">
         <path fill="currentColor" d="M21.7 5.3a1 1 0 0 1 0 1.4l-12 12a1 1 0 0 1-1.4 0l-6-6a1 1 0 1 1 1.4-1.4L9 16.58l11.3-11.3a1 1 0 0 1 1.4 0Z"></path>
     </svg>;
 };
@@ -31,6 +31,7 @@ export default definePlugin({
             }
         }
     ],
+
     addCopyButton: ErrorBoundary.wrap(({ fileContents, bytesLeft }: { fileContents: string, bytesLeft: number; }) => {
         const [recentlyCopied, setRecentlyCopied] = useState(false);
         return (
