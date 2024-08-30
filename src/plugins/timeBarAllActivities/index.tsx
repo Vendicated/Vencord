602b573eb825ea381f930d9a49a345015b4a1dd3
niko <57009359+hauntii@users.noreply.github.com>
2024-08-30 09:28:34 -0400

    Update src/plugins/timeBarAllActivities/index.tsx
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>

--- src/plugins/timeBarAllActivities/index.tsx
+++ src/plugins/timeBarAllActivities/index.tsx
@@ -65,7 +65,7 @@ export default definePlugin({
     ],
 
     isActivityTimestamped(activity: Activity) {
-        return activity.timestamps && activity.timestamps.start && activity.timestamps.end;
+        return activity.timestamps != null && activity.timestamps.start != null && activity.timestamps.end != null
     },
 
     getTimeBar(activity: Activity) {
