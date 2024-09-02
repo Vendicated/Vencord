eb054f3f5731d14ac53d49268a8d49bbee116cdc
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-09-01 22:22:58 -0400

    Fix Page Error

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -290,11 +290,11 @@ page.on("console", async e => {
 });
 
 page.on("error", e => {
-    if (e.message.includes("the network is offline")) return;
     console.error("[Error]", e.message);
 });
 page.on("pageerror", e => {
     if (e.message.includes("Sentry successfully disabled")) return;
+    if (e.message.includes("the network is offline")) return;
 
     if (!e.message.startsWith("Object") && !e.message.includes("Cannot find module")) {
         console.error("[Page Error]", e.message);
