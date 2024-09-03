5a39189c6e163b04f19194da5cc1bb029bad4a2a
Ramzi <ramziah.git.recolor221@passmail.net>
2024-09-02 20:08:17 -0500

    Changed to use definePluginSettings & moved 2nd replace to the first find patch

--- src/plugins/secretRingTone/index.ts
+++ src/plugins/secretRingTone/index.ts
@@ -4,9 +4,18 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Settings } from "@api/Settings";
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+
+const settings = definePluginSettings({
+    onlySnow: {
+        type: OptionType.BOOLEAN,
+        description: "Only play the Snow Halation Theme?",
+        default: false,
+        restartNeeded: true
+    }
+});
 
 export default definePlugin({
     name: "SecretRingToneEnabler",
@@ -15,26 +24,18 @@ export default definePlugin({
     patches: [
         {
             find: '"call_ringing_beat"',
-            replacement: {
-                match: /500!==\i\(\)\.random\(1,1e3\)/,
-                replace: "false",
-            }
-        },
-        {
-            find: '"call_ringing_beat"',
-            predicate: ()=> Settings.plugins.SecretRingToneEnabler.onlySnow,
-            replacement: {
-                match: /"call_ringing_beat",/,
-                replace: "",
-            }
+            replacement: [
+                {
+                    match: /500!==\i\(\)\.random\(1,1e3\)/,
+                    replace: "false",
+                },
+                {
+                    predicate: ()=> settings.store.onlySnow,
+                    match: /"call_ringing_beat",/,
+                    replace: "",
+                }
+            ]
         },
     ],
-    options: {
-        onlySnow: {
-            type: OptionType.BOOLEAN,
-            description: "Only play the Snow Halation Theme?",
-            default: false,
-            restartNeeded: true
-        }
-    },
+    settings,
 });
