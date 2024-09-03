08f936fe7ed498be4b3c1df4421e64ec93691d1b
Maddie <contact@swezy.dev>
2024-09-03 03:53:03 +0200

    NoServerEmojis: Fix detecting server emojis (#2835)

--- src/plugins/noServerEmojis/index.ts
+++ src/plugins/noServerEmojis/index.ts
@@ -36,7 +36,7 @@ export default definePlugin({
         }
     ],
     shouldSkip(guildId: string, emoji: any) {
-        if (emoji.type !== "GUILD_EMOJI") {
+        if (emoji.type !== 1) {
             return false;
         }
         if (settings.store.shownEmojis === "onlyUnicode") {
