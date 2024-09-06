5b0734415aa7c18e434de386203eff661105b791
niko <57009359+hauntii@users.noreply.github.com>
2024-09-06 00:12:24 -0400

    TimeBarAllActivities: Fix patch

--- src/plugins/timeBarAllActivities/index.tsx
+++ src/plugins/timeBarAllActivities/index.tsx
@@ -40,7 +40,7 @@ export default definePlugin({
     settings,
     patches: [
         {
-            find: ".Messages.USER_ACTIVITY_PLAYING",
+            find: 'location:"UserProfileActivityCard"',
             replacement: [
                 // Insert Spotify time bar component
                 {
