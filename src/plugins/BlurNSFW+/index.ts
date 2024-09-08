c02ee89b120a3037eaef8b6267c17a68615b320d
MiguVT <miguyt1@gmail.com>
2024-09-08 18:02:20 +0200

    Made BlurNSFW+

---
+++ src/plugins/BlurNSFW+/index.ts
@@ -0,0 +1,81 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Settings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+let style: HTMLStyleElement;
+
+function setCss() {
+    style.textContent = `
+        .vc-nsfw-img [class^=imageWrapper] img,
+        .vc-nsfw-img [class^=wrapperPaused] video {
+            filter: blur(${Settings.plugins["BlurNSFW"].blurAmount}px);
+            transition: filter ${Settings.plugins["BlurNSFW"].transitionTime}ms;
+        }
+        .vc-nsfw-img [class^=imageWrapper]:hover img,
+        .vc-nsfw-img [class^=wrapperPaused]:hover video {
+            filter: unset;
+        }
+        `;
+}
+
+export default definePlugin({
+    name: "BlurNSFW+",
+    description: "Up-to-date fork of BlurNSFW, blur attachments in NSFW channels until hovered with additional options. Original by Ven.",
+    authors: [Devs.MiguVT],
+
+    patches: [
+        {
+            find: ".embedWrapper,embed",
+            replacement: [{
+                match: /\.embedWrapper(?=.+?channel_id:(\i)\.id)/g,
+                replace: "$&+($1.nsfw?' vc-nsfw-img':'')"
+            }]
+        }
+    ],
+
+    options: {
+        blurAmount: {
+            type: OptionType.NUMBER,
+            description: "Blur Amount",
+            default: 10,
+            onChange: setCss
+        },
+        transitionTime: {
+            type: OptionType.NUMBER,
+            description: "Transition time for blur effect (in milliseconds)",
+            default: 200,
+            min: 0,
+            max: 1000
+        },
+    },
+
+    start() {
+        style = document.createElement("style");
+        style.id = "VcBlurNsfw";
+        document.head.appendChild(style);
+
+        setCss();
+    },
+
+    stop() {
+        style?.remove();
+    }
+});
