844b9c3dd2e71fc788a9f9a9c5da63b7814e4e7e
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-09-09 12:10:42 -0400

    Fix Nyx Dev

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -523,7 +523,7 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     },
     nyx: {
         name: "verticalsync",
-        id: 328165170536775680n
+        id: 1207087393929171095n
     },
     nekohaxx: {
         name: "nekohaxx",
