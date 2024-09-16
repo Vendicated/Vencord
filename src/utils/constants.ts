74b03c0962d67fe0b705b293f7c2976f44dde125
sadan4 <117494111+sadan4@users.noreply.github.com>
2024-09-15 21:43:14 -0400

    Discard changes to src/utils/constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -530,13 +530,13 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Antti",
         id: 312974985876471810n
     },
-    sadan: {
-        name: "sadan",
-        id: 521819891141967883n,
-    },
     Joona: {
         name: "Joona",
         id: 297410829589020673n
+    },
+    sadan: {
+        name: "sadan",
+        id: 521819891141967883n,
     },
     Kylie: {
         name: "Cookie",
