e15f72cc7d82abe5e3f36115dac7f73d1545161a
Vendicated <vendicated@riseup.net>
2024-09-17 17:38:46 +0200

    style

--- src/plugins/betterSettings/PluginsSubmenu.tsx
+++ src/plugins/betterSettings/PluginsSubmenu.tsx
@@ -20,45 +20,49 @@ function onRestartNeeded() {
     });
 }
 
-export default function() {
+export default function PluginsSubmenu() {
     const sortedPlugins = useMemo(() => Object.values(Plugins)
         .sort((a, b) => a.name.localeCompare(b.name)), []);
     const [query, setQuery] = useState("");
 
     const search = query.toLowerCase();
     const include = (p: typeof Plugins[keyof typeof Plugins]) => (
-        (
+        Vencord.Plugins.isPluginEnabled(p.name)
+        && p.options && !isObjectEmpty(p.options)
+        && (
             p.name.toLowerCase().includes(search)
             || p.description.toLowerCase().includes(search)
             || p.tags?.some(t => t.toLowerCase().includes(search))
         )
-        && Vencord.Plugins.isPluginEnabled(p.name)
-        && p.options && !isObjectEmpty(p.options)
     );
 
     const plugins = sortedPlugins.filter(include);
-    return [
-        <Menu.MenuControlItem
-            key={"vc-plugins-search"}
-            id={"vc-plugins-search"}
-            control={(props, ref) => (
-                <Menu.MenuSearchControl
-                    {...props}
-                    query={query}
-                    onChange={setQuery}
-                    ref={ref}
-                    placeholder={i18n.Messages.SEARCH}
-                />
-            )}
-        />,
-        !!plugins.length && <Menu.MenuSeparator key={"vc-plugins-separator"} />,
-        ...plugins.map(p => (
-            <Menu.MenuItem
-                key={p.name}
-                id={p.name}
-                label={p.name}
-                action={() => openPluginModal(p, onRestartNeeded)}
+
+    return (
+        <>
+            <Menu.MenuControlItem
+                id="vc-plugins-search"
+                control={(props, ref) => (
+                    <Menu.MenuSearchControl
+                        {...props}
+                        query={query}
+                        onChange={setQuery}
+                        ref={ref}
+                        placeholder={i18n.Messages.SEARCH}
+                    />
+                )}
             />
-        ))
-    ];
+
+            {!!plugins.length && <Menu.MenuSeparator />}
+
+            {plugins.map(p => (
+                <Menu.MenuItem
+                    key={p.name}
+                    id={p.name}
+                    label={p.name}
+                    action={() => openPluginModal(p, onRestartNeeded)}
+                />
+            ))}
+        </>
+    );
 }
