e3929587361babda8daba061c6a78aa4f9413f10
sadan <117494111+sadan4@users.noreply.github.com>
2024-09-18 23:07:44 -0400

    create plugin

---
+++ src/plugins/copyStatusUrls/index.ts
@@ -0,0 +1,66 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { findByCodeLazy } from "@webpack";
+import { Clipboard, Toasts } from "@webpack/common";
+import { User } from "discord-types/general";
+
+interface MakeContextMenuProps {
+    user: User,
+    activity: any
+}
+
+// This is an API call if the result is not cached
+// i looked for an hour and did not find a better way to do this
+const getMetadataFromApi: (activity: any, userId: string) => Promise<any> = findByCodeLazy("null/undefined");
+
+export default definePlugin({
+    name: "CopyStatusUrls",
+    description: "Copy the users status url when you right-click it",
+    authors: [Devs.sadan],
+    patches: [
+        {
+            find: "?\"PRESS_WATCH_ON_CRUNCHYROLL_BUTTON\"",
+            replacement: {
+                match: /(?=onClick)(?=.*index:(\i))/,
+                replace: "onContextMenu: $self.makeContextMenu(arguments[0], $1),"
+            }
+        }
+    ],
+
+    makeContextMenu(props: MakeContextMenuProps, index: number) {
+        return async () => {
+        try {
+                const { button_urls } = await getMetadataFromApi(props.activity, props.user.id);
+                if (!button_urls[index]) {
+                    throw new Error("button_urls does not contain index");
+                }
+                Clipboard.copy(button_urls[index]);
+                Toasts.show({
+                    id: Toasts.genId(),
+                    message: "Copied URL",
+                    type: Toasts.Type.SUCCESS,
+                    options: {
+                        position: Toasts.Position.TOP
+                    }
+                });
+            } catch (e) {
+                console.error(e);
+                Toasts.show({
+                    id: Toasts.genId(),
+                    message: "Error copying URL, check console for more info",
+                    type: Toasts.Type.FAILURE,
+                    options: {
+                        position: Toasts.Position.TOP
+                    }
+                });
+            }
+        };
+    }
+});
+
