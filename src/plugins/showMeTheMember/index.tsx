569c30fdcfa6f6eaf7ea9fbf0a0017c6c557717f
Masterjoona <69722179+Masterjoona@users.noreply.github.com>
2024-09-23 14:55:30 +0300

    rework a bit

--- src/plugins/showMeTheMember/index.tsx
+++ src/plugins/showMeTheMember/index.tsx
@@ -4,11 +4,14 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import "./styles.css";
+
+import { definePluginSettings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
-import { findByCodeLazy, findComponentByCodeLazy } from "@webpack";
-import { Popout } from "@webpack/common";
+import definePlugin, { OptionType } from "@utils/types";
+import { extractAndLoadChunksLazy, filters, findByCodeLazy, findComponentByCodeLazy, mapMangledModuleLazy } from "@webpack";
+import { ChannelStore, ContextMenuApi, Popout, SelectedChannelStore } from "@webpack/common";
 import { Guild, User } from "discord-types/general";
 
 
@@ -16,9 +19,27 @@ import { Guild, User } from "discord-types/general";
 const UserProfile = findComponentByCodeLazy("UserProfilePopoutWrapper: user cannot be undefined");
 const preloadUser = findByCodeLazy("preloadUserBanner");
 
+const { UserContext } = mapMangledModuleLazy('navId:"user-context"', {
+    UserContext: filters.byCode("children:")
+});
+const { GuildContext } = mapMangledModuleLazy('navId:"guild-context"', {
+    GuildContext: filters.byCode("children:")
+});
+
+const requireUserContextMenu = extractAndLoadChunksLazy([".userIds.length>0).reverse("]);
+const requireGuildContextMenu = extractAndLoadChunksLazy([".Messages.NO_MUTUAL_GUILDS})]"]);
+
 let shiftPressed = false;
 const keydown = (e: KeyboardEvent) => e.key === "Shift" && (shiftPressed = true);
 const keyup = (e: KeyboardEvent) => e.key === "Shift" && (shiftPressed = false);
+
+const settings = definePluginSettings({
+    ReverseShift: {
+        description: "Reverse the shift key behavior",
+        type: OptionType.BOOLEAN,
+        default: false,
+    },
+});
 
 export default definePlugin({
     name: "ShowMeTheMember",
@@ -27,15 +48,31 @@ export default definePlugin({
     patches: [
         {
             find: ".Messages.NO_MUTUAL_GUILDS})]",
+            replacement: [
+                {
+                    match: /return\(0,\i\.jsxs?\)\((\i),({.+?}),\i\.id\)/,
+                    replace: "return $self.renderGuild({Original: $1, ...$2})"
+                },
+                {
+                    match: /onClick:\i,onContextMenu:.+?children:/,
+                    replace: "children:"
+                }
+            ]
+        },
+        {
+            find: 'section:"MUTUAL_FRIENDS"',
             replacement: {
-                match: /return\(0,\i\.jsxs?\)\((\i),({.+?}),\i\.id\)(?<=\.NO_MUTUAL_GUILDS.{1,200})/,
+                match: /return\(0,\i\.jsxs?\)\((\i\.\i),({.+?}),\i\.id\)/,
                 replace: "return $self.renderGuild({Original: $1, ...$2})"
-            },
+            }
         }
     ],
-    start() {
+    settings,
+    async start() {
         document.addEventListener("keydown", keydown);
         document.addEventListener("keyup", keyup);
+        await requireUserContextMenu();
+        await requireGuildContextMenu();
     },
     stop() {
         document.removeEventListener("keydown", keydown);
@@ -61,7 +98,28 @@ export default definePlugin({
                     {props => (
                         <div
                             {...props}
-                            onClick={e => shiftPressed ? onSelect() : props.onClick(e)}
+                            onClick={e => {
+                                (shiftPressed !== settings.store.ReverseShift) ? onSelect() : props.onClick(e);
+                            }}
+                            onContextMenu={e => {
+                                if (shiftPressed !== settings.store.ReverseShift) {
+                                    ContextMenuApi.openContextMenu(e, () => (
+                                        <GuildContext
+                                            guild={guild}
+                                        />
+                                    ));
+                                    return;
+                                }
+                                ContextMenuApi.openContextMenu(e, () => (
+                                    <div className="vc-smtm-menu">
+                                        <UserContext
+                                            user={user}
+                                            guildId={guild.id}
+                                            channel={ChannelStore.getChannel(SelectedChannelStore.getChannelId())}
+                                        />
+                                    </div>
+                                ));
+                            }}
                         >
                             <Original
                                 guild={guild}
