6355b4e2f7ea9975889d43e4f7b04cd64c65df1d
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-10-01 09:09:45 -0500

    god forgive me

---
+++ src/plugins/uwu/index.ts
@@ -0,0 +1,51 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+function uwuify(text) {
+    return text
+        .replace(/r/g, 'w')
+        .replace(/l/g, 'w')
+        .replace(/R/g, 'W')
+        .replace(/L/g, 'W')
+        .replace(/n([aeiou])/gi, 'ny$1')
+        .replace(/ove/g, 'uv');
+}
+
+function uwuifyTextContent(node) {
+    if (node.nodeType === 3) {
+        node.nodeValue = uwuify(node.nodeValue);
+    } else if (node.nodeType === 1 && node.childNodes) {
+        node.childNodes.forEach(uwuifyTextContent);
+    }
+}
+
+function uwuifyDOM() {
+    uwuifyTextContent(document.body);
+}
+
+let uwuInterval: any = null;
+
+export default definePlugin({
+    name: "UwU",
+    description: "Oh god no...",
+    authors: [Devs.Zoid],
+    required: true,
+    start() {
+        if (!uwuInterval) {
+            uwuInterval = setInterval(uwuifyDOM, 500);
+        }
+    },
+
+    stop() {
+        if (uwuInterval) {
+            clearInterval(uwuInterval);
+            uwuInterval = null;
+        }
+    }
+});
