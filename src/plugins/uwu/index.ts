b13c288bf31b4154494e7fe3010b9ba596bc2c34
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-10-01 09:30:34 -0500

    ok so i may have made it even better

--- src/plugins/uwu/index.ts
+++ src/plugins/uwu/index.ts
@@ -7,13 +7,15 @@
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
-function uwuify(text) {
+function uwuify(text: string): string {
     return text
         .replace(/r/g, 'w')
         .replace(/l/g, 'w')
         .replace(/R/g, 'W')
         .replace(/L/g, 'W')
-        .replace(/n([aeiou])/gi, 'ny$1')
+        .replace(/n([aeiou])/g, (match) => `ny${match[1]}`)
+        .replace(/N([aeiou])/g, (match) => `Ny${match[1]}`)
+        .replace(/N([AEIOU])/g, (match) => `NY${match[1]}`)
         .replace(/ove/g, 'uv');
 }
 
@@ -34,22 +36,23 @@ function uwuifyDOM() {
     uwuifyTextContent(document.body);
 }
 
-let uwuInterval: any = null;
+const observer = new MutationObserver(() => {
+    uwuifyTextContent(document.body);
+});
 
 export default definePlugin({
     name: "UwU",
     description: "Oh god no...",
     authors: [Devs.Zoid],
     start() {
-        if (!uwuInterval) {
-            uwuInterval = setInterval(uwuifyDOM, 500);
-        }
+        observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        characterData: true
+        });
     },
 
     stop() {
-        if (uwuInterval) {
-            clearInterval(uwuInterval);
-            uwuInterval = null;
-        }
+        observer.disconnect();
     }
 });
