cbc254e959f69cda7f6dfac7c12fa3ce33c369bc
Cortex <126973723+verysillycat@users.noreply.github.com>
2024-10-01 21:12:29 -0600

    reflactor(PluginsTab): reformat

--- src/components/PluginSettings/PluginStatCards.tsx
+++ src/components/PluginSettings/PluginStatCards.tsx
@@ -27,33 +27,46 @@ export function StockPluginsCard({ totalStockPlugins, enabledStockPlugins }) {
 }
 
 export function UserPluginsCard({ totalUserPlugins, enabledUserPlugins }) {
-    if (totalUserPlugins <= 1) return (
-        <div className="vc-plugin-stats vc-stockplugins-stats-card">
-            <div className="vc-plugin-stats-card-container ">
-                <div className="vc-plugin-stats-card-section">
-                    <Text variant="text-md/semibold">Total UserPlugins</Text>
-                    <Tooltip text={<img src="https://files.catbox.moe/x4plqa.png" style={{ width: "40px", height: "40px" }} />}>
-                        {tooltipProps => (
-                            <Text variant="heading-xl/bold" {...tooltipProps}>{totalUserPlugins}</Text>
-                        )}
-                    </Tooltip>
+    if (totalUserPlugins <= 1)
+        return (
+            <div className="vc-plugin-stats vc-stockplugins-stats-card">
+                <div className="vc-plugin-stats-card-container ">
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Total UserPlugins</Text>
+                        <Tooltip
+                            text={
+                                <img
+                                    src="https://discord.com/assets/ab6835d2922224154ddf.svg"
+                                    style={{ width: "40px", height: "40px" }}
+                                />
+                            }
+                        >
+                            {tooltipProps => (
+                                <span style={{ display: "inline", position: "relative" }}>
+                                    <Text variant="heading-xl/bold" {...tooltipProps}>
+                                        {totalUserPlugins}
+                                    </Text>
+                                </span>
+                            )}
+                        </Tooltip>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-    else return (
-        <div className="vc-plugin-stats vc-stockplugins-stats-card">
-            <div className="vc-plugin-stats-card-container">
-                <div className="vc-plugin-stats-card-section">
-                    <Text variant="text-md/semibold">Enabled UserPlugins</Text>
-                    <Text variant="heading-xl/bold">{enabledUserPlugins}</Text>
-                </div>
-                <div className="vc-plugin-stats-card-divider"></div>
-                <div className="vc-plugin-stats-card-section">
-                    <Text variant="text-md/semibold">Total UserPlugins</Text>
-                    <Text variant="heading-xl/bold">{totalUserPlugins}</Text>
+        );
+    else
+        return (
+            <div className="vc-plugin-stats vc-stockplugins-stats-card">
+                <div className="vc-plugin-stats-card-container">
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Enabled UserPlugins</Text>
+                        <Text variant="heading-xl/bold">{enabledUserPlugins}</Text>
+                    </div>
+                    <div className="vc-plugin-stats-card-divider"></div>
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Total UserPlugins</Text>
+                        <Text variant="heading-xl/bold">{totalUserPlugins}</Text>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 }
