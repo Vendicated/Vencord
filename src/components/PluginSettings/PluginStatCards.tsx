a92bae80e070439f307a7863d0d920cd2cebeb5d
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-10-02 12:32:40 -0400

    Fix Userplugincard error

--- src/components/PluginSettings/PluginStatCards.tsx
+++ src/components/PluginSettings/PluginStatCards.tsx
@@ -9,64 +9,64 @@ import "./styles.css";
 import { Text, Tooltip } from "@webpack/common";
 
 export function StockPluginsCard({ totalStockPlugins, enabledStockPlugins }) {
-  return (
-    <div className="vc-plugin-stats vc-stockplugins-stats-card">
-      <div className="vc-plugin-stats-card-container">
-        <div className="vc-plugin-stats-card-section">
-          <Text variant="text-md/semibold">Enabled Plugins</Text>
-          <Text variant="heading-xl/bold">{enabledStockPlugins}</Text>
+    return (
+        <div className="vc-plugin-stats vc-stockplugins-stats-card">
+            <div className="vc-plugin-stats-card-container">
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Enabled Plugins</Text>
+                    <Text variant="heading-xl/bold">{enabledStockPlugins}</Text>
+                </div>
+                <div className="vc-plugin-stats-card-divider"></div>
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Total Plugins</Text>
+                    <Text variant="heading-xl/bold">{totalStockPlugins}</Text>
+                </div>
+            </div>
         </div>
-        <div className="vc-plugin-stats-card-divider"></div>
-        <div className="vc-plugin-stats-card-section">
-          <Text variant="text-md/semibold">Total Plugins</Text>
-          <Text variant="heading-xl/bold">{totalStockPlugins}</Text>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export function UserPluginsCard({ totalUserPlugins, enabledUserPlugins }) {
-  if (totalUserPlugins <= 1)
-    return (
-      <div className="vc-plugin-stats vc-stockplugins-stats-card">
-        <div className="vc-plugin-stats-card-container ">
-          <div className="vc-plugin-stats-card-section">
-            <Text variant="text-md/semibold">Total Userplugins</Text>
-            <Tooltip
-              text={
-                <img
-                  src="https://discord.com/assets/ab6835d2922224154ddf.svg"
-                  style={{ width: "40px", height: "40px" }}
-                />
-              }
-            >
-              {(tooltipProps) => (
-                <span style={{ display: "inline", position: "relative" }}>
-                  <Text variant="heading-xl/bold" {...tooltipProps}>
-                    {totalUserPlugins}
-                  </Text>
-                </span>
-              )}
-            </Tooltip>
-          </div>
-        </div>
-      </div>
-    );
-  else
-    return (
-      <div className="vc-plugin-stats vc-stockplugins-stats-card">
-        <div className="vc-plugin-stats-card-container">
-          <div className="vc-plugin-stats-card-section">
-            <Text variant="text-md/semibold">Enabled Userplugins</Text>
-            <Text variant="heading-xl/bold">{enabledUserPlugins}</Text>
-          </div>
-          <div className="vc-plugin-stats-card-divider"></div>
-          <div className="vc-plugin-stats-card-section">
-            <Text variant="text-md/semibold">Total Userplugins</Text>
-            <Text variant="heading-xl/bold">{totalUserPlugins}</Text>
-          </div>
-        </div>
-      </div>
-    );
+    if (totalUserPlugins === 0)
+        return (
+            <div className="vc-plugin-stats vc-stockplugins-stats-card">
+                <div className="vc-plugin-stats-card-container ">
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Total Userplugins</Text>
+                        <Tooltip
+                            text={
+                                <img
+                                    src="https://discord.com/assets/ab6835d2922224154ddf.svg"
+                                    style={{ width: "40px", height: "40px" }}
+                                />
+                            }
+                        >
+                            {tooltipProps => (
+                                <span style={{ display: "inline", position: "relative" }}>
+                                    <Text variant="heading-xl/bold" {...tooltipProps}>
+                                        {totalUserPlugins}
+                                    </Text>
+                                </span>
+                            )}
+                        </Tooltip>
+                    </div>
+                </div>
+            </div>
+        );
+    else
+        return (
+            <div className="vc-plugin-stats vc-stockplugins-stats-card">
+                <div className="vc-plugin-stats-card-container">
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Enabled Userplugins</Text>
+                        <Text variant="heading-xl/bold">{enabledUserPlugins}</Text>
+                    </div>
+                    <div className="vc-plugin-stats-card-divider"></div>
+                    <div className="vc-plugin-stats-card-section">
+                        <Text variant="text-md/semibold">Total Userplugins</Text>
+                        <Text variant="heading-xl/bold">{totalUserPlugins}</Text>
+                    </div>
+                </div>
+            </div>
+        );
 }
