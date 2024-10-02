58c7eea90a49a6de7a8c17bbbe83364c39f849c0
Cortex <126973723+verysillycat@users.noreply.github.com>
2024-10-01 20:50:12 -0600

    feat(PluginsTab): make it alot more prettier

--- src/components/PluginSettings/PluginStatCards.tsx
+++ src/components/PluginSettings/PluginStatCards.tsx
@@ -6,24 +6,54 @@
 
 import "./styles.css";
 
-import { Text } from "@webpack/common";
+import { Text, Tooltip } from "@webpack/common";
 
 export function StockPluginsCard({ totalStockPlugins, enabledStockPlugins }) {
     return (
         <div className="vc-plugin-stats vc-stockplugins-stats-card">
-            <Text variant="text-md/bold">Enabled Stock Plugins</Text>
-            <Text variant="heading-xxl/bold" style={{ textAlign: "center" }}>{enabledStockPlugins}</Text>
-            <Text variant="text-sm/normal">Total Stock Plugins: {totalStockPlugins} </Text>
+            <div className="vc-plugin-stats-card-container">
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Enabled Plugins</Text>
+                    <Text variant="heading-xl/bold">{enabledStockPlugins}</Text>
+                </div>
+                <div className="vc-plugin-stats-card-divider"></div>
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Total Plugins</Text>
+                    <Text variant="heading-xl/bold">{totalStockPlugins}</Text>
+                </div>
+            </div>
         </div>
     );
 }
 
 export function UserPluginsCard({ totalUserPlugins, enabledUserPlugins }) {
-    return (
-        <div className="vc-plugin-stats vc-userplugins-stats-card">
-            <Text variant="text-md/bold">Enabled UserPlugins</Text>
-            <Text variant="heading-xxl/bold" style={{ textAlign: "center" }}>{totalUserPlugins}</Text>
-            <Text variant="text-sm/normal">Total UserPlugins: {enabledUserPlugins} </Text>
+    if (totalUserPlugins <= 1) return (
+        <div className="vc-plugin-stats vc-stockplugins-stats-card">
+            <div className="vc-plugin-stats-card-container ">
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Total UserPlugins</Text>
+                    <Tooltip text={<img src="https://files.catbox.moe/x4plqa.png" style={{ width: "40px", height: "40px" }} />}>
+                        {tooltipProps => (
+                            <Text variant="heading-xl/bold" {...tooltipProps}>{totalUserPlugins}</Text>
+                        )}
+                    </Tooltip>
+                </div>
+            </div>
+        </div>
+    );
+    else return (
+        <div className="vc-plugin-stats vc-stockplugins-stats-card">
+            <div className="vc-plugin-stats-card-container">
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Enabled UserPlugins</Text>
+                    <Text variant="heading-xl/bold">{enabledUserPlugins}</Text>
+                </div>
+                <div className="vc-plugin-stats-card-divider"></div>
+                <div className="vc-plugin-stats-card-section">
+                    <Text variant="text-md/semibold">Total UserPlugins</Text>
+                    <Text variant="heading-xl/bold">{totalUserPlugins}</Text>
+                </div>
+            </div>
         </div>
     );
 }
