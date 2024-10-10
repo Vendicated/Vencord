8f99a0e8a32c37985e01e4cf563d0e9a8d880c0f
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-10-09 19:10:52 -0500

    YIPPEE MORE MARKDOWN IS BACK ( IMPORTANT )

---
+++ src/plugins/moreMarkdown/index.tsx
@@ -0,0 +1,153 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { parse } from "path";
+import { ReactNode } from "react";
+
+const blockReact = (data, output, className) => {
+    return (
+        <span className={className}>
+            {output(data.content)}
+        </span>
+    );
+};
+
+const characterReact = (data, output, className) => {
+    let offset = 0;
+    const traverse = (raw) => {
+        const children = !Array.isArray(raw) ? [raw] : raw;
+        let modified = false;
+
+        let j = -1;
+        for (const child of children) {
+            j++;
+            if (typeof child === "string") {
+                modified = true;
+                children[j] = child.split("").map((x, i) => (
+                    <span key={i}>
+                        <span
+                            className={className}
+                            style={{
+                                animationDelay: `${((offset++) * 25) % 1200}ms`,
+                            }}
+                        >
+                            {x}
+                        </span>
+                    </span>
+                ));
+            } else if (child?.props?.children) {
+                child.props.children = traverse(child.props.children);
+            }
+        }
+
+        return modified ? children : raw;
+    };
+
+    return traverse(output(data.content));
+};
+
+const createRule = (name, order, charList, className, type) => {
+    const regex = new RegExp(`^${charList[0]}([\\s\\S]+?)${charList[1]}`);
+    const reactFunction = type === "block" ? blockReact : characterReact;
+    const rule = {
+        name: name,
+        order: order,
+        match: (source) => source.match(regex),
+        parse: (capture, transform, state) => ({
+            content: transform(capture[1], state)
+        }),
+        react: (data, output) => reactFunction(data, output, className),
+    };
+    return rule;
+};
+
+let styles: HTMLStyleElement;
+const updateStyles = () => {
+    styles.textContent = `
+.wiggly {
+    position: relative;
+    top: 0;
+    left: 0;
+    animation: 0.6s wiggle-x alternate ease-in-out infinite, 1.2s wiggle-y linear infinite;
+}
+
+@keyframes wiggle-x {
+    from {
+        left: -5px;
+    }
+
+    to {
+        left: 5px;
+    }
+}
+
+@keyframes wiggle-y {
+    0% {
+        top: 0px;
+        animation-timing-function: ease-out;
+    }
+
+    25% {
+        top: -5px;
+        animation-timing-function: ease-in;
+    }
+
+    50% {
+        top: 0px;
+        animation-timing-function: ease-out;
+    }
+
+    75% {
+        top: 5px;
+        animation-timing-function: ease-in;
+    }
+`;
+};
+
+const rules = [
+    createRule("wiggly", 24, ["~", "~"], "wiggly", "character"),
+];
+
+const rulesByName = {};
+rules.forEach(rule => {
+    rulesByName[rule.name] = rule;
+});
+
+let patch = "";
+rules.forEach(rule => {
+    patch += `${rule.name}:$self.rulesByName["${rule.name}"],`;
+});
+
+export default definePlugin({
+    name: "MoreMarkdown",
+    description: "More markdown capabilities for Zoidcord",
+    authors: [Devs.Zoid],
+    required: true,
+    rulesByName: rulesByName,
+
+    patches: [
+        {
+            find: "parseToAST:",
+            replacement: {
+                match: /(parse[\w]*):(.*?)\((\i)\),/g,
+                replace: `$1:$2({...$3,${patch}}),`,
+            },
+        },
+    ],
+    start: () => {
+        styles = document.createElement("style");
+        styles.id = "moreMarkdownStyles";
+        document.head.appendChild(styles);
+
+        updateStyles();
+        console.log(patch);
+    },
+
+    stop: () => styles.remove()
+});
+
