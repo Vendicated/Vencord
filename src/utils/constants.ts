f967ac3370dc4265a110a25fb2b627eca35eb769
Benjamín <iakzs@protonmail.com>
2024-10-18 18:09:58 -0300

    Added k_z to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -575,6 +575,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "RamziAH",
         id: 1279957227612147747n,
     },
+    k_z: {
+        name: "k_z.",
+        id: 622795838032314388n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
