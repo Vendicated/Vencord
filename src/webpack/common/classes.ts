dc96e3ddfdd60884a3c40c86acd682d3a27d7f85
sadan <117494111+sadan4@users.noreply.github.com>
2024-10-22 09:34:17 -0400

    fix lint

--- src/webpack/common/classes.ts
+++ src/webpack/common/classes.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { findByPropsLazy, findLazy } from "@webpack";
+import { findByPropsLazy } from "@webpack";
 
 import * as t from "./types/classes";
 
