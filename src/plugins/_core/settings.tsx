f2082492d030535a1af860fae9d3973e2082aae2
Nuckyz <contact@swezy.dev>
2024-11-03 15:47:19 -0300

    Fix settings & updater for Canary

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -57,7 +57,7 @@ export default definePlugin({
             ]
         },
         {
-            find: "Messages.ACTIVITY_SETTINGS",
+            find: ".SEARCH_NO_RESULTS&&0===",
             replacement: [
                 {
                     match: /(?<=section:(.{0,50})\.DIVIDER\}\))([,;])(?=.{0,200}(\i)\.push.{0,100}label:(\i)\.header)/,
@@ -148,13 +148,17 @@ export default definePlugin({
 
         if (!header) return;
 
-        const names = {
-            top: i18n.Messages.USER_SETTINGS,
-            aboveNitro: i18n.Messages.BILLING_SETTINGS,
-            belowNitro: i18n.Messages.APP_SETTINGS,
-            aboveActivity: i18n.Messages.ACTIVITY_SETTINGS
-        };
-        return header === names[settingsLocation];
+        try {
+            const names = {
+                top: i18n.Messages.USER_SETTINGS,
+                aboveNitro: i18n.Messages.BILLING_SETTINGS,
+                belowNitro: i18n.Messages.APP_SETTINGS,
+                aboveActivity: i18n.Messages.ACTIVITY_SETTINGS
+            };
+            return header === names[settingsLocation];
+        } catch {
+            return firstChild === "PREMIUM";
+        }
     },
 
     patchedSettings: new WeakSet(),
