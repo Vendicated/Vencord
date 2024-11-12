2bed9d22f484f81d5ef8f5458f456a521edeacf6
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-11-12 15:42:55 -0500

    Fix Name In CrashHandler

--- src/plugins/crashHandler/index.ts
+++ src/plugins/crashHandler/index.ts
@@ -107,7 +107,7 @@ export default definePlugin({
             try {
                 if (!hasCrashedOnce) {
                     hasCrashedOnce = true;
-                    maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Vencord update available that might fix this issue! Would you like to update now?", true);
+                    maybePromptToUpdate("Uh oh, Discord has just crashed... but good news, there is a Equicord update available that might fix this issue! Would you like to update now?", true);
                 }
             } catch { }
 
