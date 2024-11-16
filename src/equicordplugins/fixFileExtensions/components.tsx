e04fc4e17b17a445632b1660bc6e3ae04a1f191b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-11-16 08:52:39 -0500

    Dont Auto Correct m4a Files

--- src/equicordplugins/fixFileExtensions/components.tsx
+++ src/equicordplugins/fixFileExtensions/components.tsx
@@ -8,7 +8,7 @@ const extensionMap = {
     "ogg": [".ogv", ".oga", ".ogx", ".ogm", ".spx", ".opus"],
     "jpg": [".jpg", ".jpeg", ".jfif", ".jpe", ".jif", ".jfi", ".pjpeg", ".pjp"],
     "svg": [".svgz"],
-    "mp4": [".m4v", ".m4a", ".m4r", ".m4b", ".m4p"],
+    "mp4": [".m4v", ".m4r", ".m4b", ".m4p"],
     "mov": [".movie", ".qt"],
 };
 
