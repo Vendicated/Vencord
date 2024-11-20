8b41753689f014d2227925261cc8f24c3134d7a2
sadan <117494111+sadan4@users.noreply.github.com>
2024-11-20 18:34:18 -0500

    EmoteCloner: fix recognizing non-animated emojis

--- src/plugins/emoteCloner/index.tsx
+++ src/plugins/emoteCloner/index.tsx
@@ -310,7 +310,8 @@ function buildMenuItem(type: "Emoji" | "Sticker", fetchData: () => Promisable<Om
 }
 
 function isGifUrl(url: string) {
-    return new URL(url).pathname.endsWith(".gif");
+    const u = new URL(url);
+    return u.pathname.endsWith(".gif") || u.searchParams.has("animated");
 }
 
 const messageContextMenuPatch: NavContextMenuPatchCallback = (children, props) => {
