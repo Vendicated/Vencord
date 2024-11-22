ee339a0ee77ca27820d126ef9c486c561352ef17
EnergoStalin <ens.stalin@gmail.com>
2024-11-22 13:06:43 +0300

    refactor(roleColorEverywhere): make it more fun and easy to pull patches from upstream

---
+++ src/plugins/roleColorEverywhere/components/RolesModal.tsx
@@ -0,0 +1,28 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { ModalProps } from "@utils/modal";
+import { GuildStore, React } from "@webpack/common";
+import { Guild } from "discord-types/general";
+
+import { toggleRole } from "../storeHelper";
+import { RoleModalList } from "./RolesView";
+
+export function RoleModal({ modalProps, guild, colorsStore }: { modalProps: ModalProps, guild: Guild, colorsStore: Record<string, string[]> }) {
+    const [ids, setIds] = React.useState(colorsStore[guild.id]);
+    const roles = React.useMemo(() => ids.map(id => GuildStore.getRole(guild.id, id)), [ids]);
+
+    return <RoleModalList
+        modalProps={modalProps}
+        roleList={roles}
+        header={`${guild.name} highlighted roles.`}
+        onRoleRemove={id => {
+            toggleRole(colorsStore, guild.id, id);
+            setIds(colorsStore[guild.id]);
+        }}
+    />;
+}
+
