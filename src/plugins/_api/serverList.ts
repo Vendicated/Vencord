2afca6fe2fa5bf84f1d66b03979dd3492798c3ff
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-11-25 21:47:20 -0500

    Fix HideSevers Patch

--- src/plugins/_api/serverList.ts
+++ src/plugins/_api/serverList.ts
@@ -39,7 +39,7 @@ export default definePlugin({
                     replace: "Vencord.Api.ServerList.renderAll(Vencord.Api.ServerList.ServerListRenderPosition.In).concat($&)"
                 },
                 {
-                    match: /lastTargetNode.{0,300}\{\}\)\]\}\)\]/,
+                    match: /fixedDiscoveryIcon\}\)\}\)\]/,
                     replace: "$&.concat(Vencord.Api.ServerList.renderAll(Vencord.Api.ServerList.ServerListRenderPosition.Below))"
                 }
             ]
