bfa5d2e2e5ab541d41d807f4fcd8f2d447ca4507
KrystalSkull <150982280+KrstlSkll69@users.noreply.github.com>
2024-12-03 18:13:05 -0500

    added disclamer to quest completer, remove warning from RemixMe (#103)

--- src/equicordplugins/remixMe/index.tsx
+++ src/equicordplugins/remixMe/index.tsx
@@ -4,12 +4,9 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import "./style.css";
-
 import { addPreSendListener, MessageExtra, MessageObject, removePreSendListener } from "@api/MessageEvents";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { Forms } from "@webpack/common";
 
 const handleMessage = (channelID: string, message: MessageObject, messageEx: MessageExtra) => messageEx.uploads && messageEx.uploads.forEach(att => (att as any).isRemix = true);
 
@@ -17,11 +14,6 @@ export default definePlugin({
     name: "RemixMe",
     description: "Turns every single message with attachment to have remix tag",
     authors: [EquicordDevs.kvba],
-    settingsAboutComponent: () => <>
-        <Forms.FormText className="remixme-warning">
-            We can't guarantee this plugin won't get you warned or banned.
-        </Forms.FormText>
-    </>,
     start: () => addPreSendListener(handleMessage),
     stop: () => removePreSendListener(handleMessage)
 });
