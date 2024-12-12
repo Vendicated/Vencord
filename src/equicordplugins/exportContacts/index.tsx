62c0ad394bc4c1993519b2b041c9e2fa68ca8b04
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2024-12-12 15:15:44 -0500

    ExportContacts Temp Patch Update

--- src/equicordplugins/exportContacts/index.tsx
+++ src/equicordplugins/exportContacts/index.tsx
@@ -68,20 +68,15 @@ export default definePlugin({
         },
         {
             find: "[role=\"tab\"][aria-disabled=\"false\"]",
-            replacement: [
-                {
-                    match: /this\.props;(return\(.*?"aria-label":(\i))/,
-                    replace: "this.props;console.log($2?.Children);$1"
-                },
-                {
-                    match: /("aria-label":(\i),children:)(\w+)\.Children\.map\((\w+),\s*this\.renderChildren\)/,
-                    replace:
-                        "$1($3 && $3.Children ?" +
-                        "($2 === 'Friends' ?" +
-                        "[...$3.Children.map($4, this.renderChildren), $self.addExportButton()]" +
-                        ": [...$3.Children.map($4, this.renderChildren)]) : $3.map($4, this.renderChildren))"
-                }
-            ]
+            replacement: {
+                match: /("aria-label":(\i).{0,25})(\i)\.Children\.map\((\i),this\.renderChildren\)/,
+                replace:
+                    "$1($3 && $3.Children" +
+                    "? ($2 === 'Friends'" +
+                    "? [...$3.Children.map($4, this.renderChildren), $self.addExportButton()]" +
+                    ": [...$3.Children.map($4, this.renderChildren)])" +
+                    ": $3.map($4, this.renderChildren))"
+            }
         }
     ],
 
