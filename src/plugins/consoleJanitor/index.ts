c73916607660704de99f0b713f686683719174f9
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-11 22:04:11 -0500

    fix console janitor

--- src/plugins/consoleJanitor/index.ts
+++ src/plugins/consoleJanitor/index.ts
@@ -74,9 +74,9 @@ export default definePlugin({
             }
         },
         {
-            find: 'react-spring: The "interpolate" function',
+            find: 'The "interpolate" function is deprecated in v10 (use "to" instead)',
             replacement: {
-                match: /,console.warn\('react-spring: The "interpolate" function is deprecated in v10 \(use "to" instead\)'\)/,
+                match: /,console.warn\(\i\+'The "interpolate" function is deprecated in v10 \(use "to" instead\)'\)/,
                 replace: ""
             }
         },
