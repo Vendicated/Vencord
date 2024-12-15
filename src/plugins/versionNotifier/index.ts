c073daeb427894210d8fce30dc7b04d88f128d0a
bluejutzu <kalebdaniel98@gmail.com>
2024-12-15 08:03:07 +0100

    rename once again

---
+++ src/plugins/versionNotifier/index.ts
@@ -0,0 +1,22 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+import { checkForVencordUpdate } from "./versionChecker";
+
+export default definePlugin({
+    name: "VersionNotifier",
+    description: "Notifies you of recent changelogs from the Vencord Repository when the version changes",
+    authors: [Devs.bluejutzu],
+    enabledByDefault: true,
+    flux: {
+        async POST_CONNECTION_OPEN() {
+            await checkForVencordUpdate();
+        }
+    }
+});
---
+++ src/plugins/versionNotifier/versionChecker.ts
@@ -0,0 +1,21 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { DataStore } from "@api/index";
+
+import { openVersionModal } from "./VersionModal";
+
+const DATA_KEY = "VencordVersion_LastKnown";
+
+export async function checkForVencordUpdate(): Promise<void> {
+    // Output: xx.xx.xx
+    const lastKnownVersion = await DataStore.get(DATA_KEY) as string;
+    console.log(lastKnownVersion, VERSION, IS_DEV);
+    if (lastKnownVersion !== VERSION || IS_DEV) {
+        openVersionModal(lastKnownVersion);
+        await DataStore.set(DATA_KEY, VERSION);
+    }
+}
