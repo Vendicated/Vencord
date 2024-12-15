a06fb096bf972740b7c6bd330b2ef0904241b3ad
Cooper <coop@coopeeo.xyz>
2024-12-15 12:07:20 -0600

    Create connections.ts

---
+++ src/plugins/extraConnectionLinks/connections.ts
@@ -0,0 +1,10 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Cooper/coopeeo, Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+// This is a file that can be used and imported by other plugins
+// This file is for types of the built-in Discord connections
+
+// coming soon when i make it lol
