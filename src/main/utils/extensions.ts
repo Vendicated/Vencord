e7ae711e8ba7ef06e3bb483865fac423eb515060
Julien Bolly <julienraptor01@gmail.com>
2024-12-19 23:23:50 +0100

    Update the prodversion in the extension download URL

--- src/main/utils/extensions.ts
+++ src/main/utils/extensions.ts
@@ -72,7 +72,7 @@ export async function installExt(id: string) {
             // v4.27 is broken in Electron, see https://github.com/facebook/react/issues/25843
             // Unfortunately, Google does not serve old versions, so this is the only way
             ? "https://raw.githubusercontent.com/Vendicated/random-files/f6f550e4c58ac5f2012095a130406c2ab25b984d/fmkadmapgofadopljbjfkapdkoienihi.zip"
-            : `https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&x=id%3D${id}%26uc&prodversion=32`;
+            : `https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&x=id%3D${id}%26uc&prodversion=2147483647`;
         const buf = await get(url, {
             headers: {
                 "User-Agent": "Vencord (https://github.com/Vendicated/Vencord)"
