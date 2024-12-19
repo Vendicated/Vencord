ef4918b2a5f9cd4517e7f7c29f54563fa331c72f
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2024-12-19 10:55:05 +0100

    add to contributors :eyes:

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -579,6 +579,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "jamesbt365",
         id: 158567567487795200n,
     },
+    mathu_lmn: {
+        name: "Mathu_lmn",
+        id: 381894752649478144n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
