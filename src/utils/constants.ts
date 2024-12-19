d7b3ba1a17ac7242055202490509efe2afbbcf2b
Void48 <85196642+Void48@users.noreply.github.com>
2024-12-19 04:56:35 +0000

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -579,6 +579,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "jamesbt365",
         id: 158567567487795200n,
     },
+    Void: {
+        name: "Void",
+        id: 605764872856403978n
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
