ed5772ee1ccaeebecf7e7ddd708bf8381ed5f97c
million1156 <million@alyocord.com>
2024-12-19 16:26:16 -0600

    CrashHandler: revert attemptRecover

--- src/plugins/crashHandler/index.ts
+++ src/plugins/crashHandler/index.ts
@@ -55,7 +55,7 @@ const settings = definePluginSettings({
 
 let hasCrashedOnce = false;
 let isRecovering = false;
-let shouldAttemptRecover = false;// true;
+let shouldAttemptRecover = true;
 
 export default definePlugin({
     name: "CrashHandler",
