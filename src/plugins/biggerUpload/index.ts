baf1e7c64c26f08f668ca3bc10672f5290b15d4a
Kaydax <kaydax@kaydax.xyz>
2024-12-19 22:05:55 -0500

    Fix linting errors

--- src/plugins/biggerUpload/index.ts
+++ src/plugins/biggerUpload/index.ts
@@ -1,7 +1,19 @@
 /*
-* Vencord, a Discord client mod
-* Copyright (c) 2024 Vendicated and contributors*
-* SPDX-License-Identifier: GPL-3.0-or-later
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import definePlugin from "@utils/types";
@@ -9,20 +21,20 @@ import definePlugin from "@utils/types";
 export default definePlugin({
     name: "Bigger Upload",
     description: "Reverts the free upload size limit back to 25MB",
-    authors: [{ name: 'Kaydax', id: 142782417994907648n }],
+    authors: [{ name: "Kaydax", id: 142782417994907648n }],
     patches: [
         {
-            find: 'GLOBAL_DISCOVERY:"/discovery"',
+            find: "GLOBAL_DISCOVERY:\"/discovery\"",
             replacement: {
                 match: /10485760/,
-                replace: '26214400'
+                replace: "26214400"
             }
         },
         {
-            find: 'client_build_number',
+            find: "client_build_number",
             replacement: {
                 match: /client_build_number=[a-zA-Z]+/,
-                replace: 'client_build_number=10000'
+                replace: "client_build_number=10000"
             }
         }
     ]
