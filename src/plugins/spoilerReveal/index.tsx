d91cf4b99779a6b49042c75e503bf3bdcdc22f7d
Nebula <114772385+nebulamoe@users.noreply.github.com>
2024-12-27 06:27:22 +0100

    add license header

--- src/plugins/spoilerReveal/index.tsx
+++ src/plugins/spoilerReveal/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { React } from "@webpack/common";
