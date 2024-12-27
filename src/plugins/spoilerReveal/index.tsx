b940d8813e5c889b48827185b032f44279f66ff6
Nebula <114772385+nebulamoe@users.noreply.github.com>
2024-12-27 05:57:28 +0100

    add SpoilerReveal plugin

---
+++ src/plugins/spoilerReveal/index.tsx
@@ -0,0 +1,72 @@
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { React } from "@webpack/common";
+import { definePluginSettings } from "@api/Settings";
+
+const settings = definePluginSettings({
+    enableHoverReveal: {
+        type: OptionType.BOOLEAN,
+        description: "Enable spoiler reveal on hover",
+        default: true,
+    },
+});
+
+export default definePlugin({
+    name: "SpoilerReveal",
+    description: "Reveal spoilers on hover, ctrl+click to hide them again",
+    authors: [Devs.rushy],
+    settings,
+
+    patches: [
+        {
+            find: ".removeObscurity,",
+            replacement: {
+                match: /super\(\.\.\.e\),/,
+                replace: 'super(...e),Vencord.Plugins.plugins.SpoilerReveal.createHoverWrapper(this),'
+            }
+        }
+    ],
+
+    createHoverWrapper(component: any) {
+        const originalRenderObscuredText = component.renderObscuredText;
+
+        component.renderObscuredText = function () {
+            const result = originalRenderObscuredText.call(this);
+            if (!settings.store.enableHoverReveal) {
+                return result;
+            }
+
+            const handleSpoilerClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+                if (event.ctrlKey && this.state.visible && this._wasClickRevealed) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    this._wasClickRevealed = false;
+                    this.setState({ visible: false });
+                    return;
+                }
+
+                if (!this._wasClickRevealed) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    this._wasClickRevealed = true;
+                    this.setState({ visible: true });
+                    return;
+                }
+            };
+
+            return React.createElement("span", {
+                onMouseEnter: () => {
+                    if (!this.state.visible) {
+                        this.setState({ visible: true });
+                    }
+                },
+                onMouseLeave: () => {
+                    if (this.state.visible && !this._wasClickRevealed) {
+                        this.setState({ visible: false });
+                    }
+                },
+                onClick: handleSpoilerClick.bind(this)
+            }, result);
+        };
+    }
+});
