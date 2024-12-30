83486dbd18279e63f9f25965df8e2c313b25ecb1
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2024-12-30 01:30:59 -0300

    Use original component in fallback

--- src/plugins/fullUserInChatbox/index.tsx
+++ src/plugins/fullUserInChatbox/index.tsx
@@ -8,7 +8,7 @@ import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findComponentByCodeLazy } from "@webpack";
-import { UserStore } from "@webpack/common";
+import { ReactNode } from "react";
 
 const UserMentionComponent = findComponentByCodeLazy(".USER_MENTION)");
 
@@ -16,42 +16,32 @@ interface UserMentionComponentProps {
     id: string;
     channelId: string;
     guildId: string;
+    OriginalComponent: ReactNode;
 }
 
 export default definePlugin({
     name: "FullUserInChatbox",
-    description: "Makes the user mention in the chatbox have more functionalities, like right clicking",
+    description: "Makes the user mention in the chatbox have more functionalities, like left/right clicking",
     authors: [Devs.sadan],
 
     patches: [
         {
             find: ':"text":',
             replacement: {
-                match: /(hidePersonalInformation.*?)return/,
-                replace: "$1return $self.UserMentionComponent(arguments[0]);"
+                match: /(hidePersonalInformation\).+?)(if\(null!=\i\){.+?return \i)(?=})/,
+                replace: "$1return $self.UserMentionComponent({...arguments[0],OriginalComponent:(()=>{$2})()});"
             }
         }
     ],
 
-    UserMentionComponent: ErrorBoundary.wrap((props: UserMentionComponentProps) => {
-        return <UserMentionComponent
+    UserMentionComponent: ErrorBoundary.wrap((props: UserMentionComponentProps) => (
+        <UserMentionComponent
             // This seems to be constant
             className="mention"
             userId={props.id}
             channelId={props.channelId}
-        />;
-    }, {
-        fallback: ({ wrappedProps }) => {
-            let username: string | null = null;
-            try {
-                username = UserStore.getUser(wrappedProps.id)?.username;
-            } catch (e) {
-                console.error(e);
-            }
-            username ||= "Unknown User";
-            return <span style={{
-                color: "red",
-            }}>@{username}</span>;
-        }
-    }),
+        />
+    ), {
+        fallback: ({ wrappedProps }) => wrappedProps.OriginalComponent
+    })
 });
