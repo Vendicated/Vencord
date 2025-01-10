4a53216e27bd4f52263d087755d0d3b347b827c6
Zoid <zoid@zoid.one>
2025-01-09 19:54:49 -0600

    shi mb linter

--- src/plugins/trolUtils/index.ts
+++ src/plugins/trolUtils/index.ts
@@ -7,9 +7,8 @@ import { Devs } from "@utils/constants";
 import { relaunch } from "@utils/native";
 import definePlugin, { PluginNative } from "@utils/types";
 import { checkForUpdates, update } from "@utils/updater";
-import { findByProps } from "@webpack";
 import { RestAPI, UserStore } from "@webpack/common";
-import { Message, User } from "discord-types/general";
+import { Message } from "discord-types/general";
 import kernex from "plugins/kernex";
 
 interface IMessageCreate {
--- src/plugins/trolUtils/native.ts
+++ src/plugins/trolUtils/native.ts
@@ -1,3 +1,8 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 import { app } from "electron/main";
 
 export function close() {
