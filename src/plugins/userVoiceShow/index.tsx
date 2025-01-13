aed4e5828ee0bc9431c7dfca1084391336a2d434
Johannes7k75 <Jonashebenstreit1@gmail.com>
2025-01-13 13:39:08 +0100

    add live indicator

--- src/plugins/userVoiceShow/index.tsx
+++ src/plugins/userVoiceShow/index.tsx
@@ -26,7 +26,7 @@ import definePlugin, { OptionType } from "@utils/types";
 
 import { VoiceChannelIndicator } from "./components";
 
-const settings = definePluginSettings({
+export const settings = definePluginSettings({
     showInUserProfileModal: {
         type: OptionType.BOOLEAN,
         description: "Show a user's Voice Channel indicator in their profile next to the name",
@@ -42,6 +42,12 @@ const settings = definePluginSettings({
     showInMessages: {
         type: OptionType.BOOLEAN,
         description: "Show a user's Voice Channel indicator in messages",
+        default: true,
+        restartNeeded: true
+    },
+    showLiveIndicator: {
+        type: OptionType.BOOLEAN,
+        description: "Show a Live indicator next to a user's Voice Channel indicator",
         default: true,
         restartNeeded: true
     }
