5b6c5c1fdb408280ba2622ee66dfe0e7cbce35c3
TheKodeToad <TheKodeToad@proton.me>
2025-01-14 20:53:00 +0000

    Rework sound replacement (display file name)

--- src/plugins/customSounds/components/SoundOverrideComponent.tsx
+++ src/plugins/customSounds/components/SoundOverrideComponent.tsx
@@ -60,52 +60,57 @@ export function SoundOverrideComponent({ type, override, onChange }: { type: Sou
             >
                 Preview
             </Button>
-            <Forms.FormTitle>Replacement Sound</Forms.FormTitle>
-            <Button
-                color={Button.Colors.PRIMARY}
-                disabled={!override.enabled}
-                className={classes(Margins.right8, Margins.bottom16, cl("upload"))}
-            >
-                Upload
-                <FileInput
-                    ref={fileInputRef}
+            <Forms.FormTitle>Sound File</Forms.FormTitle>
+            <div className={classes(cl("file"), Margins.bottom16)} >
+                <Forms.FormText className={cl("file-name")}>{override.url.length === 0 ? "Discord Default" : (override.fileName || "Custom Sound")}</Forms.FormText>
+                <Button
+                    color={Button.Colors.PRIMARY}
+                    size={Button.Sizes.SMALL}
                     disabled={!override.enabled}
-                    onChange={event => {
-                        event.stopPropagation();
-                        event.preventDefault();
+                    className={cl("file-replace")}
+                >
+                    Replace
+                    <FileInput
+                        ref={fileInputRef}
+                        disabled={!override.enabled}
+                        onChange={event => {
+                            event.stopPropagation();
+                            event.preventDefault();
 
-                        if (!event.currentTarget?.files?.length)
-                            return;
+                            if (!event.currentTarget?.files?.length)
+                                return;
 
-                        const { files } = event.currentTarget;
-                        const file = files[0];
+                            const { files } = event.currentTarget;
+                            const file = files[0];
 
-                        // Set override URL to a data URI
-                        const reader = new FileReader;
-                        reader.onload = () => {
-                            override.url = reader.result as string;
-                            onChange();
-                            update();
-                        };
-                        reader.readAsDataURL(file);
+                            // Set override URL to a data URI
+                            const reader = new FileReader;
+                            reader.onload = () => {
+                                override.url = reader.result as string;
+                                override.fileName = file.name;
+                                onChange();
+                                update();
+                            };
+                            reader.readAsDataURL(file);
+                        }}
+                        // Sorry .caf lovers, https://en.wikipedia.org/wiki/HTML5_audio#Supported_audio_coding_formats
+                        filters={[{ extensions: ["mp3", "wav", "ogg", "webm", "flac"] }]}
+                    />
+                </Button>
+                <Button
+                    color={Button.Colors.RED}
+                    size={Button.Sizes.SMALL}
+                    onClick={() => {
+                        override.url = "";
+                        onChange();
+                        update();
                     }}
-                    // Sorry .caf lovers, https://en.wikipedia.org/wiki/HTML5_audio#Supported_audio_coding_formats
-                    filters={[{ extensions: ["mp3", "wav", "ogg", "webm", "flac"] }]}
-                />
-            </Button>
-            <Button
-                color={Button.Colors.RED}
-                onClick={() => {
-                    override.url = "";
-                    onChange();
-                    update();
-                }}
-                disabled={!(override.enabled && override.url.length !== 0)}
-                style={{ display: "inline" }}
-                className={classes(Margins.right8, Margins.bottom16)}
-            >
-                Clear
-            </Button>
+                    disabled={!(override.enabled && override.url.length !== 0)}
+                    className={cl("file-reset")}
+                >
+                    Reset
+                </Button>
+            </div>
             <Forms.FormTitle>Volume</Forms.FormTitle>
             <Slider
                 markers={makeRange(0, 100, 10)}
