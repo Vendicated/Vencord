ba4a9ae354a2dbe1be60c2200fba011eb1741c43
Loukious <Loukious@users.noreply.github.com>
2025-01-14 19:29:58 +0100

    chore: questCompleter rewrite to work on more tasks.

--- src/plugins/questCompleter/index.tsx
+++ src/plugins/questCompleter/index.tsx
@@ -20,20 +20,20 @@ import { showNotification } from "@api/Notifications";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { findByCode, findByProps, findStoreLazy } from "@webpack";
-import { Text, UserStore } from "@webpack/common";
+import { ChannelStore, FluxDispatcher, GuildChannelStore, RestAPI, Text, UserStore } from "@webpack/common";
 
 import { IconWithTooltip, QuestIcon } from "./components/Icons";
 
 
 
 const ApplicationStreamingStore = findStoreLazy("ApplicationStreamingStore");
-const questAssetsBaseUrl = "https://cdn.discordapp.com/assets/quests/";
+const questAssetsBaseUrl = "https://cdn.discordapp.com/quests/";
 
 
 function getLeftQuests() {
     const QuestsStore = findByProps("getQuest");
     // check if user still has incompleted quests
-    const quest = [...QuestsStore.quests.values()].find(quest => quest.userStatus?.enrolledAt && !quest?.userStatus?.completedAt && new Date(quest?.config?.expiresAt) >= new Date());
+    const quest = [...QuestsStore.quests.values()].find(quest => quest.userStatus?.enrolledAt && !quest?.userStatus?.completedAt && new Date(quest?.config?.expiresAt).getTime() > Date.now());
     return quest;
 }
 
@@ -52,34 +52,10 @@ function encodeStreamKey(e): string {
     }
 }
 
-function decodeStreamKey(e: string): any {
-    const t = e.split(":");
-    const n = t[0];
-    switch (n) {
-        case "guild": {
-            const [e, n, i, r] = t;
-            return {
-                streamType: e,
-                guildId: n,
-                channelId: i,
-                ownerId: r
-            };
-        }
-        case "call": {
-            const [e, n, i] = t;
-            return {
-                streamType: e,
-                channelId: n,
-                ownerId: i
-            };
-        }
-        default:
-            throw new Error("Unknown stream type ".concat(n));
-    }
-}
 
 
-let quest, interval, applicationId, applicationName, secondsNeeded, secondsDone, canPlay;
+// let quest, interval, applicationId, applicationName, secondsNeeded, secondsDone, taskName;
+const isApp = navigator.userAgent.includes("Electron/");
 let shouldDisable = true;
 let questRunning = false;
 let ImagesConfig = {};
@@ -102,17 +78,16 @@ export default definePlugin({
         return <IconWithTooltip text="Complete Quest" isDisabled={shouldDisable} icon={<QuestIcon />} onClick={this.openCompleteQuest} />;
     },
     settingsAboutComponent() {
-        const isDesktop = navigator.userAgent.includes("discord/");
 
         return (<>
             {
-                isDesktop ?
+                isApp ?
                     <Text variant="text-lg/bold">
                         The plugin should work properly because you are on the Desktop Client.
                     </Text>
                     :
                     <Text variant="text-lg/bold" style={{ color: "red" }}>
-                        Error: This plugin only works on the Desktop Client.
+                        Error: This plugin only works for non-video quests in the browser.
                     </Text>
             }
 
@@ -125,16 +100,19 @@ export default definePlugin({
     renderQuestButton() {
         const currentStream = ApplicationStreamingStore.getCurrentUserActiveStream();
 
-        if (!currentStream) {
+        const quest = getLeftQuests();
+        if (!quest) {
             return false;
         }
 
-        const userIds = ApplicationStreamingStore.getViewerIds(encodeStreamKey(currentStream));
-        if (!userIds.length) {
+        const taskName = ["WATCH_VIDEO", "PLAY_ON_DESKTOP", "STREAM_ON_DESKTOP", "PLAY_ACTIVITY"].find(x => quest.config.taskConfig.tasks[x] != null);
+
+
+        if (!currentStream && taskName === "STREAM_ON_DESKTOP") {
             return false;
         }
 
-        if (!getLeftQuests()) {
+        if (currentStream && !ApplicationStreamingStore.getViewerIds(encodeStreamKey(currentStream)).length && taskName === "STREAM_ON_DESKTOP") {
             return false;
         }
 
@@ -149,87 +127,249 @@ export default definePlugin({
                 body: "Stopping the current quest completion.",
                 ...ImagesConfig
             });
-            clearInterval(interval);
-            interval = null;
             questRunning = false;
             return;
         }
-        const currentStream = ApplicationStreamingStore.getCurrentUserActiveStream();
-        const encodedStreamKey = encodeStreamKey(currentStream);
-        quest = getLeftQuests();
+        const quest = getLeftQuests();
         ImagesConfig = {
-            icon: `${questAssetsBaseUrl}${quest.id}/reward.jpg`,
-            image: `${questAssetsBaseUrl}${quest.id}/hero.jpg`
+            icon: `${questAssetsBaseUrl}${quest.id}/dark/${quest.config.assets.logotype}`,
+            image: `${questAssetsBaseUrl}${quest.id}/${quest.config.assets.hero}`
         };
-
-        const heartBeat = async () => {
-            const sendHeartbeat = findByCode("QUESTS_HEARTBEAT");
-            sendHeartbeat({ questId: quest.id, streamKey: encodedStreamKey });
-        };
-
-        heartBeat();
-        interval = setInterval(heartBeat, 60000); // send the heartbeat each minute
+        const pid = Math.floor(Math.random() * 30000) + 1000;
+        const applicationId = quest.config.application.id;
+        const applicationName = quest.config.application.name;
+        const taskName = ["WATCH_VIDEO", "PLAY_ON_DESKTOP", "STREAM_ON_DESKTOP", "PLAY_ACTIVITY"].find(x => quest.config.taskConfig.tasks[x] != null);
+        const secondsNeeded = taskName ? quest.config.taskConfig.tasks[taskName].target : 0;
+        const secondsDone = taskName ? quest.userStatus?.progress?.[taskName]?.value ?? 0 : 0;
+        const questsHeartbeat = findByCode("QUESTS_HEARTBEAT");
+        const questsVideoProgress = findByCode("QUESTS_VIDEO_PROGRESS");
+        const RunningGameStore = findStoreLazy("RunningGameStore");
         questRunning = true;
-        if (quest.config.configVersion === 1) {
-            applicationId = quest.config.applicationId;
-            applicationName = quest.config.applicationName;
-            secondsNeeded = quest.config.streamDurationRequirementMinutes * 60;
-            secondsDone = quest.userStatus?.streamProgressSeconds ?? 0;
-            canPlay = quest.config.variants.includes(2);
-        } else if (quest.config.configVersion === 2) {
-            applicationId = quest.config.application.id;
-            applicationName = quest.config.application.name;
-            canPlay = quest.config.taskConfig.tasks.PLAY_ON_DESKTOP;
-            const taskName = canPlay ? "PLAY_ON_DESKTOP" : "STREAM_ON_DESKTOP";
-            secondsNeeded = quest.config.taskConfig.tasks[taskName].target;
-            secondsDone = quest.userStatus?.progress?.[taskName]?.value ?? 0;
-        }
-        return;
-    },
-    flux: {
-        STREAM_STOP: event => {
-            const stream = decodeStreamKey(event.streamKey);
-            // we check if the stream is by the current user id so we do not clear the interval without any reason.
-            if (stream.ownerId === window.currentUserId && interval) {
-                clearInterval(interval);
-                interval = null;
-            }
-        },
-        QUESTS_SEND_HEARTBEAT_FAILURE: () => {
+
+        if (taskName === "WATCH_VIDEO") {
+            const tolerance = 2, speed = 10;
+            const diff = Math.floor((Date.now() - new Date(quest.userStatus.enrolledAt).getTime()) / 1000);
+            const startingPoint = Math.min(Math.max(Math.ceil(secondsDone), diff), secondsNeeded);
+            const fn = async () => {
+                for (let i = startingPoint; i <= secondsNeeded; i += speed) {
+                    if (!questRunning) break;
+                    try {
+                        questsVideoProgress(quest.id, Math.min(secondsNeeded, i + Math.random()));
+                    } catch {
+                        showNotification(
+                            {
+                                title: "Error",
+                                body: "Failed to send increment of " + i,
+                                ...ImagesConfig
+                            }
+                        );
+                    }
+                    await new Promise(resolve => setTimeout(resolve, tolerance * 1000));
+                }
+                if (questRunning && (secondsNeeded - secondsDone) % speed !== 0) {
+                    questsVideoProgress(quest.id, secondsNeeded);
+                }
+                if (questRunning) {
+                    showNotification({
+                        title: `${applicationName} - Quests Completer`,
+                        body: "Quest Completed",
+                        ...ImagesConfig
+                    });
+                }
+            };
+            fn();
             showNotification(
                 {
-                    title: "Couldn't start Completing Quest",
-                    body: "You are probally using web, please check the plugin settings for help.",
+                    title: `${applicationName} - Quests Completer`,
+                    body: `Spoofing video for ${applicationName}. Wait for ${Math.ceil((secondsNeeded - startingPoint) / speed * tolerance)} more seconds.`,
                     ...ImagesConfig
                 }
             );
-            clearInterval(interval);
-            interval = null;
-        },
-        QUESTS_SEND_HEARTBEAT_SUCCESS: event => {
-            let progress = 0;
-            if (canPlay) {
-                progress = quest.config.configVersion === 1 ? event.userStatus.streamProgressSeconds : Math.floor(event.userStatus.progress.PLAY_ON_DESKTOP.value);
-                // WIP - Need to add game emulation
-            } else {
-                progress = quest.config.configVersion === 1 ? event.userStatus.streamProgressSeconds : Math.floor(event.userStatus.progress.STREAM_ON_DESKTOP.value);
-            }
-            if (progress >= secondsNeeded) {
-                showNotification({
-                    title: `${applicationName} - Quests Completer`,
-                    body: "Quest Completed",
-                    ...ImagesConfig
-                });
-                clearInterval(interval);
-                interval = null;
+        } else if (taskName === "PLAY_ON_DESKTOP") {
+            if (!isApp) {
+                showNotification(
+                    {
+                        title: "Error",
+                        body: "This no longer works in browser for non-video quests. Use the desktop app to complete the " + applicationName + " quest!",
+                        ...ImagesConfig
+                    }
+                );
+                questRunning = false;
                 return;
-            } else {
+            }
+            RestAPI.get({ url: `/applications/public?application_ids=${applicationId}` }).then(res => {
+                const appData = res.body[0];
+                const exeName = appData.executables.find(x => x.os === "win32").name.replace(">", "");
+
+                const games = RunningGameStore.getRunningGames();
+                const fakeGame = {
+                    cmdLine: `C:\\Program Files\\${appData.name}\\${exeName}`,
+                    exeName,
+                    exePath: `c:/program files/${appData.name.toLowerCase()}/${exeName}`,
+                    hidden: false,
+                    isLauncher: false,
+                    id: applicationId,
+                    name: appData.name,
+                    pid: pid,
+                    pidPath: [pid],
+                    processName: appData.name,
+                    start: Date.now(),
+                };
+                games.push(fakeGame);
+                FluxDispatcher.dispatch({ type: "RUNNING_GAMES_CHANGE", removed: [], added: [fakeGame], games: games });
+
+                const fn = data => {
+                    if (!questRunning) {
+                        // Remove the fake game and unsubscribe when questRunning is false
+                        const idx = games.indexOf(fakeGame);
+                        if (idx > -1) {
+                            games.splice(idx, 1);
+                            FluxDispatcher.dispatch({ type: "RUNNING_GAMES_CHANGE", removed: [fakeGame], added: [], games: games });
+                        }
+                        FluxDispatcher.unsubscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+                        showNotification({
+                            title: "Quest Completer",
+                            body: "Quest stopped.",
+                            ...ImagesConfig
+                        });
+                        return;
+                    }
+                    const progress = quest.config.configVersion === 1 ? data.userStatus.streamProgressSeconds : Math.floor(data.userStatus.progress.PLAY_ON_DESKTOP.value);
+                    showNotification({
+                        title: `${applicationName} - Quests Completer`,
+                        body: `Current progress: ${Math.floor(progress / secondsNeeded * 100)}% (${Math.ceil((secondsNeeded - progress) / 60)} minutes left.)`,
+                        ...ImagesConfig
+                    });
+
+                    if (progress >= secondsNeeded) {
+                        showNotification({
+                            title: `${applicationName} - Quests Completer`,
+                            body: "Quest Completed",
+                            ...ImagesConfig
+                        });
+
+                        const idx = games.indexOf(fakeGame);
+                        if (idx > -1) {
+                            games.splice(idx, 1);
+                            FluxDispatcher.dispatch({ type: "RUNNING_GAMES_CHANGE", removed: [fakeGame], added: [], games: [] });
+                        }
+                        FluxDispatcher.unsubscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+                        questRunning = false;
+                        return;
+                    }
+                };
+                FluxDispatcher.subscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+
                 showNotification({
                     title: `${applicationName} - Quests Completer`,
-                    body: `Current progress: ${Math.floor(progress / secondsNeeded * 100)}% (${Math.ceil((secondsNeeded - secondsDone) / 60)} minutes left.)`,
+                    body: `Spoofed your game to ${applicationName}. Wait for ${Math.ceil((secondsNeeded - secondsDone) / 60)} more minutes.`,
                     ...ImagesConfig
                 });
+            });
+        } else if (taskName === "STREAM_ON_DESKTOP") {
+            if (!isApp) {
+                showNotification({
+                    title: "Error",
+                    body: "This no longer works in browser for non-video quests. Use the desktop app to complete the " + applicationName + " quest!",
+                    ...ImagesConfig
+                });
+                questRunning = false;
+                return;
             }
+
+            const realFunc = ApplicationStreamingStore.getStreamerActiveStreamMetadata;
+            ApplicationStreamingStore.getStreamerActiveStreamMetadata = () => ({
+                id: applicationId,
+                pid,
+                sourceName: null
+            });
+
+            const fn = data => {
+                if (!questRunning) {
+                    // Stop spoofing the stream and unsubscribe when questRunning is false
+                    ApplicationStreamingStore.getStreamerActiveStreamMetadata = realFunc; // Restore the real function
+                    FluxDispatcher.unsubscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+                    showNotification({
+                        title: "Quest Completer",
+                        body: "Quest stopped.",
+                        ...ImagesConfig
+                    });
+                    return;
+                }
+
+                const progress = quest.config.configVersion === 1
+                    ? data.userStatus.streamProgressSeconds
+                    : Math.floor(data.userStatus.progress.STREAM_ON_DESKTOP.value);
+
+                showNotification({
+                    title: `${applicationName} - Quests Completer`,
+                    body: `Current progress: ${Math.floor(progress / secondsNeeded * 100)}% (${Math.ceil((secondsNeeded - progress) / 60)} minutes left.)`,
+                    ...ImagesConfig
+                });
+
+                if (progress >= secondsNeeded) {
+                    showNotification({
+                        title: `${applicationName} - Quests Completer`,
+                        body: "Quest Completed",
+                        ...ImagesConfig
+                    });
+
+                    ApplicationStreamingStore.getStreamerActiveStreamMetadata = realFunc;
+                    FluxDispatcher.unsubscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+                    questRunning = false;
+                    return;
+                }
+            };
+            FluxDispatcher.subscribe("QUESTS_SEND_HEARTBEAT_SUCCESS", fn);
+
+            showNotification({
+                title: `${applicationName} - Quests Completer`,
+                body: `Spoofed your stream to ${applicationName}. Stream any window in vc for ${Math.ceil((secondsNeeded - secondsDone) / 60)} more minutes.`,
+                ...ImagesConfig
+            });
+        } else if (taskName === "PLAY_ACTIVITY") {
+            const channelId = ChannelStore.getSortedPrivateChannels()[0]?.id ?? (Object.values(GuildChannelStore.getAllGuilds()) as any[])
+                .find(x => x?.VOCAL?.length > 0)?.VOCAL?.[0].channel?.id ?? null;
+            const streamKey = `call:${channelId}:1`;
+
+            const fn = async () => {
+                showNotification({
+                    title: `${applicationName} - Quests Completer`,
+                    body: `Completing quest ${quest.config.messages.questName}`,
+                    ...ImagesConfig
+                });
+
+                while (questRunning) {
+                    const res = await questsHeartbeat({ questId: quest.id, streamKey: streamKey, terminal: false });
+                    const progress = res.body.progress.PLAY_ACTIVITY.value;
+                    showNotification({
+                        title: `${applicationName} - Quests Completer`,
+                        body: `Current progress: ${Math.floor(progress / secondsNeeded * 100)}% (${Math.ceil((secondsNeeded - progress) / 60)} minutes left.)`,
+                        ...ImagesConfig
+                    });
+
+                    if (progress >= secondsNeeded) {
+                        showNotification({
+                            title: `${applicationName} - Quests Completer`,
+                            body: "Quest Completed",
+                            ...ImagesConfig
+                        });
+                        await questsHeartbeat({ questId: quest.id, stream_key: streamKey, terminal: true });
+                        questRunning = false;
+                        break;
+                    }
+                    await new Promise(resolve => setTimeout(resolve, 20 * 1000));
+                }
+                if (!questRunning) {
+                    showNotification({
+                        title: "Quest Completer",
+                        body: "Quest stopped.",
+                        ...ImagesConfig
+                    });
+                    return;
+                }
+            };
+            fn();
         }
     }
 });
