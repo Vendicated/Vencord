806e9330cdfda63821dbbcb63960ddd761011cb7
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-01-17 09:01:28 -0500

    Update CustomSounds

--- src/equicordplugins/customSounds/components/SoundOverrideComponent.tsx
+++ src/equicordplugins/customSounds/components/SoundOverrideComponent.tsx
@@ -5,21 +5,21 @@
  */
 
 import { classNameFactory } from "@api/Styles";
-import { makeRange } from "@components/PluginSettings/components";
 import { Margins } from "@utils/margins";
 import { classes } from "@utils/misc";
 import { useForceUpdater } from "@utils/react";
 import { findByCodeLazy, findLazy } from "@webpack";
-import { Button, Card, Forms, Slider, Switch, useRef } from "@webpack/common";
+import { Button, Card, Forms, Slider, Switch, Tooltip, useRef } from "@webpack/common";
 import { ComponentType, Ref, SyntheticEvent } from "react";
 
 import { SoundOverride, SoundPlayer, SoundType } from "../types";
 
 type FileInput = ComponentType<{
     ref: Ref<HTMLInputElement>;
-    onChange: (e: SyntheticEvent<HTMLInputElement>) => void;
+    onChange: (event: SyntheticEvent<HTMLInputElement>) => void;
     multiple?: boolean;
     filters?: { name?: string; extensions: string[]; }[];
+    disabled?: boolean;
 }>;
 
 const playSound: (id: string) => SoundPlayer = findByCodeLazy(".playWithListener().then");
@@ -43,8 +43,72 @@ export function SoundOverrideComponent({ type, override, onChange }: { type: Sou
                 className={Margins.bottom16}
                 hideBorder={true}
             >
-                {type.name} <span className={cl("id")}>({type.id})</span>
+                <Tooltip text={type.id}>
+                    {tooltipProps => <span {...tooltipProps}>{type.name}</span>}
+                </Tooltip>
             </Switch>
+            <Forms.FormTitle>Sound File</Forms.FormTitle>
+            <div className={classes(cl("file"), Margins.bottom16)} >
+                <Forms.FormText className={cl("file-name")}>{override.url.length === 0 ? "Discord Default" : (override.fileName || "Custom Sound")}</Forms.FormText>
+                <Button
+                    color={Button.Colors.PRIMARY}
+                    size={Button.Sizes.SMALL}
+                    disabled={!override.enabled}
+                    className={cl("file-replace")}
+                >
+                    Replace
+                    <FileInput
+                        ref={fileInputRef}
+                        disabled={!override.enabled}
+                        onChange={event => {
+                            event.stopPropagation();
+                            event.preventDefault();
+
+                            if (!event.currentTarget?.files?.length)
+                                return;
+
+                            const { files } = event.currentTarget;
+                            const file = files[0];
+
+                            // Set override URL to a data URI
+                            const reader = new FileReader;
+                            reader.onload = () => {
+                                override.url = reader.result as string;
+                                override.fileName = file.name;
+                                onChange();
+                                update();
+                            };
+                            reader.readAsDataURL(file);
+                        }}
+                        // Sorry .caf lovers, https://en.wikipedia.org/wiki/HTML5_audio#Supported_audio_coding_formats
+                        filters={[{ extensions: ["mp3", "wav", "ogg", "webm", "flac"] }]}
+                    />
+                </Button>
+                <Button
+                    color={Button.Colors.RED}
+                    size={Button.Sizes.SMALL}
+                    onClick={() => {
+                        override.url = "";
+                        onChange();
+                        update();
+                    }}
+                    disabled={!(override.enabled && override.url.length !== 0)}
+                    className={cl("file-reset")}
+                >
+                    Reset
+                </Button>
+            </div>
+            <Forms.FormTitle>Volume</Forms.FormTitle>
+            <Slider
+                initialValue={override.volume}
+                onValueChange={value => {
+                    override.volume = value;
+                    onChange();
+                    update();
+                }}
+                className={Margins.bottom16}
+                disabled={!override.enabled}
+            />
             <Button
                 color={Button.Colors.PRIMARY}
                 className={Margins.bottom16}
@@ -57,63 +121,6 @@ export function SoundOverrideComponent({ type, override, onChange }: { type: Sou
             >
                 Preview
             </Button>
-            <Forms.FormTitle>Replacement Sound</Forms.FormTitle>
-            <Button
-                color={Button.Colors.PRIMARY}
-                disabled={!override.enabled}
-                className={classes(Margins.right8, Margins.bottom16, cl("upload"))}
-            >
-                Upload
-                <FileInput
-                    ref={fileInputRef}
-                    onChange={event => {
-                        event.stopPropagation();
-                        event.preventDefault();
-
-                        if (!event.currentTarget?.files?.length)
-                            return;
-
-                        const { files } = event.currentTarget;
-                        const file = files[0];
-
-                        // Set override URL to a data URI
-                        const reader = new FileReader;
-                        reader.onload = () => {
-                            override.url = reader.result as string;
-                            onChange();
-                            update();
-                        };
-                        reader.readAsDataURL(file);
-                    }}
-                    // Sorry .caf lovers, https://en.wikipedia.org/wiki/HTML5_audio#Supported_audio_coding_formats
-                    filters={[{ extensions: ["mp3", "wav", "ogg", "webm", "flac"] }]}
-                />
-            </Button>
-            <Button
-                color={Button.Colors.RED}
-                onClick={() => {
-                    override.url = "";
-                    onChange();
-                    update();
-                }}
-                disabled={!(override.enabled && override.url.length !== 0)}
-                style={{ display: "inline" }}
-                className={classes(Margins.right8, Margins.bottom16)}
-            >
-                Clear
-            </Button>
-            <Forms.FormTitle>Volume</Forms.FormTitle>
-            <Slider
-                markers={makeRange(0, 100, 10)}
-                initialValue={override.volume}
-                onValueChange={value => {
-                    override.volume = value;
-                    onChange();
-                    update();
-                }}
-                className={Margins.bottom16}
-                disabled={!override.enabled}
-            />
         </Card>
     );
 }
