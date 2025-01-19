589a03f8222e7fde6d7202fffe3e3bdb344b0855
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-01-19 17:41:35 -0300

    another one

--- src/plugins/pinDms/index.tsx
+++ src/plugins/pinDms/index.tsx
@@ -64,7 +64,7 @@ export const settings = definePluginSettings({
 
 export default definePlugin({
     name: "PinDMs",
-    description: "Allows you to pin private channels to the top of your DM list. To pin/unpin or reorder pins, right click DMs",
+    description: "Allows you to pin private channels to the top of your DM list. To pin/unpin or re-order pins, right click DMs",
     authors: [Devs.Ven, Devs.Aria],
     settings,
     contextMenus,
