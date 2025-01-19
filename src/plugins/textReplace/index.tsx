f394906a4c629c3b24816d698a10054420cb634f
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-01-19 15:46:53 -0300

    Migrate TextReplace

--- src/plugins/textReplace/index.tsx
+++ src/plugins/textReplace/index.tsx
@@ -23,7 +23,6 @@ import { Flex } from "@components/Flex";
 import { DeleteIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
 import { Logger } from "@utils/Logger";
-import { useForceUpdater } from "@utils/react";
 import definePlugin, { OptionType } from "@utils/types";
 import { Button, Forms, React, TextInput, useState } from "@webpack/common";
 
@@ -35,8 +34,6 @@ type Rule = Record<"find" | "replace" | "onlyIfIncludes", string>;
 interface TextReplaceProps {
     title: string;
     rulesArray: Rule[];
-    rulesKey: string;
-    update: () => void;
 }
 
 const makeEmptyRule: () => Rule = () => ({
@@ -46,34 +43,40 @@ const makeEmptyRule: () => Rule = () => ({
 });
 const makeEmptyRuleArray = () => [makeEmptyRule()];
 
-let stringRules = makeEmptyRuleArray();
-let regexRules = makeEmptyRuleArray();
-
 const settings = definePluginSettings({
     replace: {
         type: OptionType.COMPONENT,
         description: "",
         component: () => {
-            const update = useForceUpdater();
+            const { stringRules, regexRules } = settings.use(["stringRules", "regexRules"]);
+
             return (
                 <>
                     <TextReplace
                         title="Using String"
                         rulesArray={stringRules}
-                        rulesKey={STRING_RULES_KEY}
-                        update={update}
                     />
                     <TextReplace
                         title="Using Regex"
                         rulesArray={regexRules}
-                        rulesKey={REGEX_RULES_KEY}
-                        update={update}
                     />
                     <TextReplaceTesting />
                 </>
             );
         }
     },
+    stringRules: {
+        type: OptionType.ARRAY,
+        description: "",
+        hidden: true,
+        default: makeEmptyRuleArray(),
+    },
+    regexRules: {
+        type: OptionType.ARRAY,
+        description: "",
+        hidden: true,
+        default: makeEmptyRuleArray(),
+    }
 });
 
 function stringToRegex(str: string) {
@@ -120,28 +123,27 @@ function Input({ initialValue, onChange, placeholder }: {
     );
 }
 
-function TextReplace({ title, rulesArray, rulesKey, update }: TextReplaceProps) {
+function TextReplace({ title, rulesArray }: TextReplaceProps) {
     const isRegexRules = title === "Using Regex";
 
     async function onClickRemove(index: number) {
         if (index === rulesArray.length - 1) return;
         rulesArray.splice(index, 1);
-
-        await DataStore.set(rulesKey, rulesArray);
-        update();
     }
 
     async function onChange(e: string, index: number, key: string) {
-        if (index === rulesArray.length - 1)
+        if (index === rulesArray.length - 1) {
             rulesArray.push(makeEmptyRule());
+        }
 
-        rulesArray[index][key] = e;
+        rulesArray[index] = {
+            ...rulesArray[index],
+            [key]: e
+        };
 
-        if (rulesArray[index].find === "" && rulesArray[index].replace === "" && rulesArray[index].onlyIfIncludes === "" && index !== rulesArray.length - 1)
+        if (rulesArray[index].find === "" && rulesArray[index].replace === "" && rulesArray[index].onlyIfIncludes === "" && index !== rulesArray.length - 1) {
             rulesArray.splice(index, 1);
-
-        await DataStore.set(rulesKey, rulesArray);
-        update();
+        }
     }
 
     return (
@@ -211,26 +213,22 @@ function applyRules(content: string): string {
     if (content.length === 0)
         return content;
 
-    if (stringRules) {
-        for (const rule of stringRules) {
-            if (!rule.find) continue;
-            if (rule.onlyIfIncludes && !content.includes(rule.onlyIfIncludes)) continue;
+    for (const rule of settings.store.stringRules) {
+        if (!rule.find) continue;
+        if (rule.onlyIfIncludes && !content.includes(rule.onlyIfIncludes)) continue;
 
-            content = ` ${content} `.replaceAll(rule.find, rule.replace.replaceAll("\\n", "\n")).replace(/^\s|\s$/g, "");
-        }
+        content = ` ${content} `.replaceAll(rule.find, rule.replace.replaceAll("\\n", "\n")).replace(/^\s|\s$/g, "");
     }
 
-    if (regexRules) {
-        for (const rule of regexRules) {
-            if (!rule.find) continue;
-            if (rule.onlyIfIncludes && !content.includes(rule.onlyIfIncludes)) continue;
+    for (const rule of settings.store.regexRules) {
+        if (!rule.find) continue;
+        if (rule.onlyIfIncludes && !content.includes(rule.onlyIfIncludes)) continue;
 
-            try {
-                const regex = stringToRegex(rule.find);
-                content = content.replace(regex, rule.replace.replaceAll("\\n", "\n"));
-            } catch (e) {
-                new Logger("TextReplace").error(`Invalid regex: ${rule.find}`);
-            }
+        try {
+            const regex = stringToRegex(rule.find);
+            content = content.replace(regex, rule.replace.replaceAll("\\n", "\n"));
+        } catch (e) {
+            new Logger("TextReplace").error(`Invalid regex: ${rule.find}`);
         }
     }
 
@@ -249,8 +247,18 @@ export default definePlugin({
     settings,
 
     async start() {
-        stringRules = await DataStore.get(STRING_RULES_KEY) ?? makeEmptyRuleArray();
-        regexRules = await DataStore.get(REGEX_RULES_KEY) ?? makeEmptyRuleArray();
+        // TODO: Remove DataStore rules migrations once enough time has passed
+        const oldStringRules = await DataStore.get<Rule[]>(STRING_RULES_KEY);
+        if (oldStringRules != null) {
+            settings.store.stringRules = oldStringRules;
+            await DataStore.del(STRING_RULES_KEY);
+        }
+
+        const oldRegexRules = await DataStore.get<Rule[]>(REGEX_RULES_KEY);
+        if (oldRegexRules != null) {
+            settings.store.regexRules = oldRegexRules;
+            await DataStore.del(REGEX_RULES_KEY);
+        }
 
         this.preSend = addPreSendListener((channelId, msg) => {
             // Channel used for sharing rules, applying rules here would be messy
