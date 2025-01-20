7e30481969fae05e9b1027c68d8c027783c77524
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-01-20 20:52:05 -0300

    I wonder

--- scripts/generateReport.ts
+++ scripts/generateReport.ts
@@ -37,7 +37,8 @@ const CANARY = process.env.USE_CANARY === "true";
 
 const browser = await pup.launch({
     headless: true,
-    executablePath: process.env.CHROMIUM_BIN
+    executablePath: process.env.CHROMIUM_BIN,
+    args: ["--no-sandbox"]
 });
 
 const page = await browser.newPage();
