bd2da8ffefead421fc3ab4fb8e47a3c76cf2ab91
arutonee <arutonee@proton.me>
2025-01-23 16:59:33 -0500

    Added settings for keybind

--- src/plugins/inboxJumpKeybind/index.ts
+++ src/plugins/inboxJumpKeybind/index.ts
@@ -1,28 +1,68 @@
 /*
-* Vencord, a Discord client mod
-* Copyright (c) 2025 Vendicated and contributors
-* SPDX-License-Identifier: GPL-3.0-or-later
-*/
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
+import { definePluginSettings, Settings } from "@api/Settings";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType, PluginSettingDef } from "@utils/types";
 
-function onKeyDown(e: KeyboardEvent) {
-    if (e.key == ';' && e.ctrlKey) {
-        const btn = <HTMLButtonElement | null> document.querySelector('div[class^=jumpButton]');
-        if (btn) btn.click();
-    }
-};
+function modifier_option(name: string, def: boolean = false): PluginSettingDef {
+    return {
+        description: `Require ${name}`,
+        type: OptionType.BOOLEAN,
+        default: def,
+    };
+}
+
+function modifiers_match(
+    e: KeyboardEvent,
+    ctrl: boolean,
+    alt: boolean,
+    shift: boolean,
+): boolean {
+    if (ctrl && !e.ctrlKey) return false;
+    if (alt && !e.altKey) return false;
+    if (shift && !e.shiftKey) return false;
+    return true;
+}
 
 export default definePlugin({
     name: "InboxJumpKeybind",
-    description: "Adds a keybind (Ctrl + semicolon) for that tiny jump button in the inbox",
+    description: "Adds a keybind for that tiny jump button in the inbox",
     authors: [Devs.arutonee],
-
+    settings: definePluginSettings({
+        ctrl: modifier_option("ctrl", true),
+        alt: modifier_option("alt"),
+        shift: modifier_option("shift"),
+        key: {
+            description:
+                "The key to use (See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key for docs)",
+            type: OptionType.STRING,
+            default: ";",
+        },
+    }),
     start() {
-        document.addEventListener("keydown", onKeyDown);
+        document.addEventListener("keydown", this.onKeyDown);
     },
     stop() {
-        document.removeEventListener("keydown", onKeyDown);
+        document.removeEventListener("keydown", this.onKeyDown);
+    },
+    onKeyDown(e: KeyboardEvent) {
+        if (
+            e.key == Settings.plugins.InboxJumpKeybind.key &&
+            modifiers_match(
+                e,
+                Settings.plugins.InboxJumpKeybind.ctrl,
+                Settings.plugins.InboxJumpKeybind.alt,
+                Settings.plugins.InboxJumpKeybind.shift,
+            )
+        ) {
+            const btn = <HTMLButtonElement | null> document.querySelector(
+                "div[class^=jumpButton]",
+            );
+            if (btn) btn.click();
+        }
     },
 });
