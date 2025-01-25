401c8c43c57db0fb8f78c7b3f194aa67891bdb3b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-01-25 15:37:04 -0500

    Remove Extra Const

--- src/equicordplugins/loginWithQR/index.tsx
+++ src/equicordplugins/loginWithQR/index.tsx
@@ -15,7 +15,6 @@ import { preload, unload } from "./images";
 import { cl } from "./ui";
 import openQrModal from "./ui/modals/QrModal";
 
-const qrModalOpen = false;
 export default definePlugin({
     name: "LoginWithQR",
     description: "Allows you to login to another device by scanning a login QR code, just like on mobile!",
@@ -88,7 +87,7 @@ export default definePlugin({
         }
     ],
 
-    qrModalOpen,
+    qrModalOpen: false,
 
     insertScanQrButton: (button: ReactElement) => (
         <div className={cl("settings-btns")}>
