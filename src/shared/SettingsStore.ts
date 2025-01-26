4e355040a026a96a209f8f97695f5f5601bccd45
Suffocate <contact@swezy.dev>
2025-01-26 15:32:34 +0000

    Fix top level settings notifying global listeners (#3166)

--- src/shared/SettingsStore.ts
+++ src/shared/SettingsStore.ts
@@ -167,6 +167,8 @@ export class SettingsStore<T extends object> {
 
             this.globalListeners.forEach(cb => cb(root, settingPathStr));
             this.pathListeners.get(settingPathStr)?.forEach(cb => cb(settingValue));
+        } else {
+            this.globalListeners.forEach(cb => cb(root, pathStr));
         }
 
         this.pathListeners.get(pathStr)?.forEach(cb => cb(value));
