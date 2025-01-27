e9ea0c3956dd7354e81b8e104301e768681ce6e4
programminglaboratorys <programming.laboratorys@gmail.com>
2025-01-27 20:29:52 +0300

    important changes

--- src/api/ExpressionPickerTabs.tsx
+++ src/api/ExpressionPickerTabs.tsx
@@ -6,6 +6,7 @@
 
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Channel } from "discord-types/general";
+import { JSX } from "react";
 
 export interface ExpressionPickerButtonProps {
     id?: string;
@@ -30,32 +31,35 @@ export type ExpressionPickerPanelComponent = (props: ExpressionPickerPanelProps)
 export interface ExpressionPickerTabItem {
     tab: string,
     Component: ExpressionPickerPanelComponent;
-    autoFocus?: boolean;
 }
 
 const ExpressionPickerComponents = new Map<string, ExpressionPickerTabItem>();
 
 
-export const addExpressionPickerTabButton = (id: string, tab: string, PanelComponent: ExpressionPickerPanelComponent, autoFocus?: boolean) => ExpressionPickerComponents.set(id, { tab: tab, Component: PanelComponent, autoFocus: autoFocus });
-export const removeExpressionPickerTabButton = (id: string) => ExpressionPickerComponents.delete(id);
+export const addExpressionPickerTab = (id: string, tab: string, PanelComponent: ExpressionPickerPanelComponent) => ExpressionPickerComponents.set(id, { tab: tab, Component: PanelComponent });
+export const removeExpressionPickerTab = (id: string) => ExpressionPickerComponents.delete(id);
 
-
-export function* RenderTabButtons(ExpressionPickerButtonComponent: ExpressionPickerButtonComponent, selectedTab: string) {
-    for (const [id, { tab }] of ExpressionPickerComponents) {
-        yield (<ErrorBoundary><ExpressionPickerButtonComponent
-            id={id + "-picker-tab"}
-            aria-controls={id + "-picker-tab-panel"}
-            aria-selected={id === selectedTab}
-            viewType={id}
-            isActive={id === selectedTab}
-        >{tab}</ExpressionPickerButtonComponent></ErrorBoundary>);
-    }
+export function RenderTabButtons(ExpressionPickerButtonComponent: ExpressionPickerButtonComponent, selectedTab: string) {
+    return Array.from(ExpressionPickerComponents, ([id, { tab }]) => (
+        <ErrorBoundary key={`vc-expression-picker-button-${id}`}>
+            <ExpressionPickerButtonComponent
+                id={`${id}-picker-tab`}
+                aria-controls={`${id}-picker-tab-panel`}
+                aria-selected={id === selectedTab}
+                viewType={id}
+                isActive={id === selectedTab}
+            >
+                {tab}
+            </ExpressionPickerButtonComponent>
+        </ErrorBoundary>
+    ));
 }
 
-export function* TabPanels(selectedTab: string, channel: Channel) {
-    for (const [id, { Component }] of ExpressionPickerComponents) {
-        if (id !== selectedTab) { continue; }
-        const PanelComponent: ExpressionPickerPanelComponent = Component;
-        yield (<ErrorBoundary><PanelComponent selectedTab={selectedTab} channel={channel} /></ErrorBoundary>);
-    }
+export function TabPanels(selectedTab: string, channel: Channel) {
+    return Array.from(ExpressionPickerComponents, ([id, { Component }]) => (
+        id === selectedTab &&
+        <ErrorBoundary key={`vc-expression-picker-panel-${id}`}>
+            <Component selectedTab={selectedTab} channel={channel} />
+        </ErrorBoundary>
+    ));
 }
