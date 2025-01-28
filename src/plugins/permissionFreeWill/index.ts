4977309393dc76a18b225f88a7172dbb5f79689d
sadan <117494111+sadan4@users.noreply.github.com>
2025-01-28 18:06:35 -0500

    fix permissionFreeWill

--- src/plugins/permissionFreeWill/index.ts
+++ src/plugins/permissionFreeWill/index.ts
@@ -46,8 +46,9 @@ export default definePlugin({
             find: "#{intl::ONBOARDING_CHANNEL_THRESHOLD_WARNING}",
             replacement: [
                 {
-                    match: /{(\i:function\(\){return \i},?){2}}/,
-                    replace: m => m.replaceAll(canonicalizeMatch(/return \i/g), "return ()=>Promise.resolve(true)")
+                    // why in the world are these arrow funcitons
+                    match: /{(\i:\(\)=>\i,?){2}}/,
+                    replace: m => m.replaceAll(canonicalizeMatch(/\(\)=>\i/g), "()=>()=>Promise.resolve(true)")
                 }
             ],
             predicate: () => settings.store.onboarding
