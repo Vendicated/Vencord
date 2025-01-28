851b93c479a2ef04e9f530c1008aec11674a47db
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-01-28 17:10:44 -0300

    fix

--- src/webpack/common/components.ts
+++ src/webpack/common/components.ts
@@ -22,10 +22,10 @@ import { filters, findByPropsLazy, mapMangledModuleLazy, waitFor } from "@webpac
 import { waitForComponent } from "./internal";
 import * as t from "./types/components";
 
-const FormTitle = waitForComponent<t.FormTitle>("FormTitle", filters.byCode(".SELECTABLE),"));
-const FormSection = waitForComponent<t.FormSection>("FormSection", filters.byCode(".titleId)&&"));
-const FormDivider = waitForComponent<t.FormDivider>("FormDivider", filters.byCode(".divider,", ",style:", '"div"', /\.divider,\i\),style:/));
-const FormText = waitForComponent<t.FormText>("FormText", filters.byCode('["defaultMargin".concat'));
+const FormTitle = waitForComponent<t.FormTitle>("FormTitle", filters.componentByCode(".SELECTABLE),"));
+const FormSection = waitForComponent<t.FormSection>("FormSection", filters.componentByCode(".titleId)&&"));
+const FormDivider = waitForComponent<t.FormDivider>("FormDivider", filters.componentByCode(".divider,", ",style:", '"div"', /\.divider,\i\),style:/));
+const FormText = waitForComponent<t.FormText>("FormText", filters.componentByCode('["defaultMargin".concat'));
 
 export const Forms = {
     FormTitle,
@@ -34,18 +34,13 @@ export const Forms = {
     FormText
 };
 
-waitFor(filters.byCode(".SELECTABLE),"), m => Forms.FormText = m);
-waitFor(filters.byCode(".titleId)&&"), m => Forms.FormSection = m);
-waitFor(filters.byCode(".divider,", ",style:", '"div"', /\.divider,\i\),style:/), m => Forms.FormDivider = m);
-waitFor(filters.byCode('["defaultMargin".concat'), m => Forms.FormText = m);
-
-export const Card = waitForComponent<t.Card>("Card", filters.byCode(".editable),"));
-export const Button = waitForComponent<t.Button>("Button", filters.byCode("#{intl::A11Y_LOADING_STARTED}))),!1"));
-export const Switch = waitForComponent<t.Switch>("Switch", filters.byCode('("Switch")'));
+export const Card = waitForComponent<t.Card>("Card", filters.componentByCode(".editable),"));
+export const Button = waitForComponent<t.Button>("Button", filters.componentByCode("#{intl::A11Y_LOADING_STARTED}))),!1"));
+export const Switch = waitForComponent<t.Switch>("Switch", filters.componentByCode('("Switch")'));
 
 const Tooltips = mapMangledModuleLazy(".tooltipTop,bottom:", {
-    Tooltip: filters.byCode("this.renderTooltip()]"),
-    TooltipContainer: filters.byCode('="div",')
+    Tooltip: filters.componentByCode("this.renderTooltip()]"),
+    TooltipContainer: filters.componentByCode('="div",')
 }) as {
     Tooltip: t.Tooltip,
     TooltipContainer: t.TooltipContainer;
@@ -54,23 +49,23 @@ const Tooltips = mapMangledModuleLazy(".tooltipTop,bottom:", {
 export const Tooltip = LazyComponent(() => Tooltips.Tooltip);
 export const TooltipContainer = LazyComponent(() => Tooltips.TooltipContainer);
 
-export const TextInput = waitForComponent<t.TextInput>("TextInput", filters.byCode(".error]:this.hasError()"));
-export const TextArea = waitForComponent<t.TextArea>("TextArea", filters.byCode("this.getPaddingRight()},id:"));
-export const Text = waitForComponent<t.Text>("Text", filters.byCode('case"always-white"'));
-export const Heading = waitForComponent<t.Heading>("Heading", filters.byCode(">6?{", "variant:"));
-export const Select = waitForComponent<t.Select>("Select", filters.byCode('.selectPositionTop]:"top"===', '"Escape"==='));
-export const SearchableSelect = waitForComponent<t.SearchableSelect>("SearchableSelect", filters.byCode('.selectPositionTop]:"top"===', ".multi]:"));
-export const Slider = waitForComponent<t.Slider>("Slider", filters.byCode('"markDash".concat('));
-export const Popout = waitForComponent<t.Popout>("Popout", filters.byCode("ref:this.ref,preload:"));
-export const Dialog = waitForComponent<t.Dialog>("Dialog", filters.byCode('role:"dialog",tabIndex:-1'));
-export const TabBar = waitForComponent("TabBar", filters.byCode("ref:this.tabBarRef,className:"));
-export const Paginator = waitForComponent<t.Paginator>("Paginator", filters.byCode('rel:"prev",children:'));
-export const ScrollerThin = waitForComponent<t.ScrollerThin>("ScrollerThin", filters.byCode('="ltr",orientation:', "onScroll:"));
-export const Clickable = waitForComponent<t.Clickable>("Clickable", filters.byCode("this.context?this.renderNonInteractive():"));
-export const Avatar = waitForComponent<t.Avatar>("Avatar", filters.byCode(".size-1.375*"));
+export const TextInput = waitForComponent<t.TextInput>("TextInput", filters.componentByCode(".error]:this.hasError()"));
+export const TextArea = waitForComponent<t.TextArea>("TextArea", filters.componentByCode("this.getPaddingRight()},id:"));
+export const Text = waitForComponent<t.Text>("Text", filters.componentByCode('case"always-white"'));
+export const Heading = waitForComponent<t.Heading>("Heading", filters.componentByCode(">6?{", "variant:"));
+export const Select = waitForComponent<t.Select>("Select", filters.componentByCode('.selectPositionTop]:"top"===', '"Escape"==='));
+export const SearchableSelect = waitForComponent<t.SearchableSelect>("SearchableSelect", filters.componentByCode('.selectPositionTop]:"top"===', ".multi]:"));
+export const Slider = waitForComponent<t.Slider>("Slider", filters.componentByCode('"markDash".concat('));
+export const Popout = waitForComponent<t.Popout>("Popout", filters.componentByCode("ref:this.ref,preload:"));
+export const Dialog = waitForComponent<t.Dialog>("Dialog", filters.componentByCode('role:"dialog",tabIndex:-1'));
+export const TabBar = waitForComponent("TabBar", filters.componentByCode("ref:this.tabBarRef,className:"));
+export const Paginator = waitForComponent<t.Paginator>("Paginator", filters.componentByCode('rel:"prev",children:'));
+export const ScrollerThin = waitForComponent<t.ScrollerThin>("ScrollerThin", filters.componentByCode('="ltr",orientation:', "onScroll:"));
+export const Clickable = waitForComponent<t.Clickable>("Clickable", filters.componentByCode("this.context?this.renderNonInteractive():"));
+export const Avatar = waitForComponent<t.Avatar>("Avatar", filters.componentByCode(".size-1.375*"));
 
 const { FocusLock_ } = mapMangledModuleLazy("attachTo:null!==", {
-    FocusLock_: filters.byCode(".containerRef")
+    FocusLock_: filters.componentByCode(".containerRef")
 }) as {
     FocusLock_: t.FocusLock;
 };
@@ -83,7 +78,7 @@ export let useToken: t.useToken;
 waitFor(filters.byCode('("useToken")'), m => useToken = m);
 
 export const MaskedLink = waitForComponent<t.MaskedLink>("MaskedLink", filters.componentByCode("MASKED_LINK)"));
-export const Timestamp = waitForComponent<t.Timestamp>("Timestamp", filters.byCode("#{intl::MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL}"));
+export const Timestamp = waitForComponent<t.Timestamp>("Timestamp", filters.componentByCode("#{intl::MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL}"));
 export const Flex = waitForComponent<t.Flex>("Flex", ["Justify", "Align", "Wrap"]);
 
 export const { OAuth2AuthorizeModal } = findByPropsLazy("OAuth2AuthorizeModal");
