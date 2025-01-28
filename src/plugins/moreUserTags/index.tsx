f9ab7d306b69848b48d4f4e3c296349628510900
simidzu2ay <simidzu2ay@protonmail.com>
2025-01-28 08:39:31 +0100

    moreUserTags: rename obj/format

--- src/plugins/moreUserTags/index.tsx
+++ src/plugins/moreUserTags/index.tsx
@@ -6,18 +6,41 @@
 
 import { Devs } from "@utils/constants";
 import { getIntlMessage } from "@utils/discord";
-import definePlugin, { StartAt } from "@utils/types";
+import definePlugin from "@utils/types";
 import { ChannelStore, GuildStore, PermissionsBits } from "@webpack/common";
 import { Channel, Message, User } from "discord-types/general";
 
 import { computePermissions, isWebhook, Tag, tags } from "./consts";
 import { settings } from "./settings";
 
+const genTagTypes = () => {
+    let i = 100;
+    const obj = {};
+
+    for (const { name } of tags) {
+        obj[name] = ++i;
+        obj[i] = name;
+        obj[`${name}-BOT`] = ++i;
+        obj[i] = `${name}-BOT`;
+        obj[`${name}-OP`] = ++i;
+        obj[i] = `${name}-OP`;
+    }
+
+    return obj;
+};
 
 export default definePlugin({
     name: "MoreUserTags",
-    description: "Adds tags for webhooks and moderative roles (owner, admin, etc.)",
-    authors: [Devs.Cyn, Devs.TheSun, Devs.RyanCaoDev, Devs.LordElias, Devs.AutumnVN, Devs.hen],
+    description:
+        "Adds tags for webhooks and moderative roles (owner, admin, etc.)",
+    authors: [
+        Devs.Cyn,
+        Devs.TheSun,
+        Devs.RyanCaoDev,
+        Devs.LordElias,
+        Devs.AutumnVN,
+        Devs.hen,
+    ],
     settings,
     patches: [
         // Render Tags in messages
@@ -26,39 +49,20 @@ export default definePlugin({
             find: ".Types.ORIGINAL_POSTER",
             replacement: {
                 match: /(?<=let (\i).{1,200}children:\i}=\i;.{0,100}.isSystemDM.{0,350}),null==\i\)(?=.{1,30}?null:)/,
-                replace: ',($1=$self.getTag({...arguments[0],location:"chat",origType:$1}))$&'
-            }
+                replace:
+                    ',($1=$self.getTag({...arguments[0],location:"chat",origType:$1}))$&',
+            },
         },
         // Make discord actually use our tags
         {
             find: ".STAFF_ONLY_DM:",
             replacement: {
                 match: /(?<=type:(\i).{10,1000}.REMIX.{10,100})default:(\i)=/,
-                replace: "default:$2=$self.getTagText($self.tagObj[$1]);"
-            }
-        }
+                replace: "default:$2=$self.getTagText($self.localTags[$1]);",
+            },
+        },
     ],
-    tagObj: {},
-    startAt: StartAt.Init,
-    start() {
-        this.genTagTypes();
-    },
-
-    genTagTypes() {
-        let i = 100;
-        const obj = {};
-
-        for (const { name } of tags) {
-            obj[name] = ++i;
-            obj[i] = name;
-            obj[`${name}-BOT`] = ++i;
-            obj[i] = `${name}-BOT`;
-            obj[`${name}-OP`] = ++i;
-            obj[i] = `${name}-OP`;
-        }
-
-        this.tagObj = obj;
-    },
+    localTags: genTagTypes(),
 
     getTagText(passedTagName: string) {
         if (!passedTagName) return getIntlMessage("APP_TAG");
@@ -106,29 +110,47 @@ export default definePlugin({
         const perms = this.getPermissions(user, channel);
 
         for (const tag of tags) {
-            if (location === "chat" && !settings.tagSettings[tag.name].showInChat) continue;
-            if (location === "not-chat" && !settings.tagSettings[tag.name].showInNotChat) continue;
+            if (
+                location === "chat" &&
+                !settings.tagSettings[tag.name].showInChat
+            )
+                continue;
+            if (
+                location === "not-chat" &&
+                !settings.tagSettings[tag.name].showInNotChat
+            )
+                continue;
 
             // If the owner tag is disabled, and the user is the owner of the guild,
             // avoid adding other tags because the owner will always match the condition for them
             if (
-                tag.name !== "OWNER" &&
-                GuildStore.getGuild(channel?.guild_id)?.ownerId === user.id &&
-                (location === "chat" && !settings.tagSettings.OWNER.showInChat) ||
-                (location === "not-chat" && !settings.tagSettings.OWNER.showInNotChat)
-            ) continue;
+                (tag.name !== "OWNER" &&
+                    GuildStore.getGuild(channel?.guild_id)?.ownerId ===
+                        user.id &&
+                    location === "chat" &&
+                    !settings.tagSettings.OWNER.showInChat) ||
+                (location === "not-chat" &&
+                    !settings.tagSettings.OWNER.showInNotChat)
+            )
+                continue;
 
             if (
-                tag.permissions?.some(perm => perms.includes(perm)) ||
-                (tag.condition?.(message!, user, channel))
+                tag.permissions?.some((perm) => perms.includes(perm)) ||
+                tag.condition?.(message!, user, channel)
             ) {
-                console.log("TAGS HELP", tag, this.tagObj);
-                if ((channel.isForumPost() || channel.isMediaPost()) && channel.ownerId === user.id)
-                    type = this.tagObj[`${tag.name}-OP`];
-                else if (user.bot && !isWebhook(message!, user) && !settings.dontShowBotTag)
-                    type = this.tagObj[`${tag.name}-BOT`];
-                else
-                    type = this.tagObj[tag.name];
+                console.log("TAGS HELP", tag, this.localTags);
+                if (
+                    (channel.isForumPost() || channel.isMediaPost()) &&
+                    channel.ownerId === user.id
+                )
+                    type = this.localTags[`${tag.name}-OP`];
+                else if (
+                    user.bot &&
+                    !isWebhook(message!, user) &&
+                    !settings.dontShowBotTag
+                )
+                    type = this.localTags[`${tag.name}-BOT`];
+                else type = this.localTags[tag.name];
                 break;
             }
         }
