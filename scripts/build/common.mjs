5f54a914e395b90373bf89b5e119ac007aeb6104
Vendicated <vendicated@riseup.net>
2025-02-11 05:59:45 +0100

    dont ignore error

--- scripts/build/common.mjs
+++ scripts/build/common.mjs
@@ -64,7 +64,8 @@ export async function buildOrWatchAll(buildConfigs) {
         ));
     } else {
         await Promise.all(buildConfigs.map(cfg => build(cfg)))
-            .catch(() => {
+            .catch(error => {
+                console.error(error.message);
                 // esbuild will already print the error, so just exit and swallow the error (to avoid ugly stacktrace)
                 process.exit(1);
             });
