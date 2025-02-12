fad2f1e5b4016f095029f85bcbe25b2385a6f445
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-02-12 19:26:47 -0300

    Fix PatchHelper and DevCompanion not replacing $self correctly

--- src/plugins/devCompanion.dev/index.tsx
+++ src/plugins/devCompanion.dev/index.tsx
@@ -173,7 +173,7 @@ function initWs(isManual = false) {
 
                     try {
                         const matcher = canonicalizeMatch(parseNode(match));
-                        const replacement = canonicalizeReplace(parseNode(replace), "PlaceHolderPluginName");
+                        const replacement = canonicalizeReplace(parseNode(replace), 'Vencord.Plugins.plugins["PlaceHolderPluginName"]"');
 
                         const newSource = src.replace(matcher, replacement as string);
 
