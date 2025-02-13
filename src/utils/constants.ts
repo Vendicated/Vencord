8963fbb72fd9800ac1d93e316ec24f0533ddc95f
merex <66756785+merex27@users.noreply.github.com>
2025-02-13 01:14:03 +0000

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -589,6 +589,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "samsam",
         id: 836452332387565589n,
     },
+    merex: {
+        name: "nos.gov",
+        id: 275650834065522689n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
