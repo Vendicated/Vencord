e71a378e2cab7aca49e1244837ea25f7aee387ab
sadan <117494111+sadan4@users.noreply.github.com>
2025-02-19 15:25:25 -0500

    fix betterUploadButton

--- src/plugins/betterUploadButton/index.ts
+++ src/plugins/betterUploadButton/index.ts
@@ -27,8 +27,8 @@ export default definePlugin({
         {
             find: '"ChannelAttachButton"',
             replacement: {
-                match: /\.attachButtonInner,"aria-label":.{0,50},onDoubleClick:(.+?:void 0),.{0,30}?\.\.\.(\i),/,
-                replace: "$&onClick:$1,onContextMenu:$2.onClick,",
+                match: /\.attachButtonInner,"aria-label":.{0,50},onDoubleClick:(.+?:void 0),.{0,100}\},(\i)\).{0,100}children:\i/,
+                replace: "$&,onClick:$1,onContextMenu:$2.onClick,",
             },
         },
     ],
