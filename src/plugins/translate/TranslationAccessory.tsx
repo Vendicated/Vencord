34462a54e119d61e75f1a8ac366f966244db1f6b
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2025-02-24 13:29:43 +0800

    add markup class

--- src/plugins/translate/TranslationAccessory.tsx
+++ src/plugins/translate/TranslationAccessory.tsx
@@ -16,11 +16,15 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { classes } from "@utils/misc";
+import { findByPropsLazy } from "@webpack";
 import { Parser, useEffect, useState } from "@webpack/common";
 import { Message } from "discord-types/general";
 
 import { TranslateIcon } from "./TranslateIcon";
 import { cl, TranslationValue } from "./utils";
+
+const MessageClasses = findByPropsLazy("markup");
 
 const TranslationSetters = new Map<string, (v: TranslationValue) => void>();
 
@@ -54,7 +58,7 @@ export function TranslationAccessory({ message }: { message: Message; }) {
     if (!translation) return null;
 
     return (
-        <span className={cl("accessory")}>
+        <span className={classes(cl("accessory"), MessageClasses?.markup)}>
             <TranslateIcon width={16} height={16} className={cl("accessory-icon")} />
             {Parser.parse(translation.text)}
             {" "}
