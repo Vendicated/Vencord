b2dc39e2c02c5558cdeb7c4c967d5a98833833b0
Ashlyn <38675333+AstralFennex@users.noreply.github.com>
2025-03-08 14:58:53 -0500

    attempted fix again

--- src/pawesomeplugins/meow/index.tsx
+++ src/pawesomeplugins/meow/index.tsx
@@ -11,15 +11,16 @@ import definePlugin from "@utils/types";
 const randmeow = ["meow", "mrrp", "mreow", "nya", "miau", ":3", "miaou", "mia", "mew", "mrow", "myah", "myaa", "mrrrr", "myuuu", "myaaow", "mrowr"];
 
 async function handleButtonClick() {
-    if (Math.random() <= 1 / 100) {
-        var selectedmeow = "IM A CAT LMAO";
-    } else {
-        if (Math.random() <= 1 / 1000) {
-            var selectedmeow = "woof.";
-        } else if (Math.random() <= 1 / 15000) {
-            var selectedmeow = "rawr x3 nuzzles >w<";
-        } else { var selectedmeow = randmeow[Math.floor(Math.random() * randmeow.length)]; }
-    }
+    //    if (Math.random() <= 1 / 100) {
+    //        var selectedmeow = "IM A CAT LMAO";
+    //    } else {
+    //        if (Math.random() <= 1 / 1000) {
+    //            var selectedmeow = "woof.";
+    //        } else if (Math.random() <= 1 / 15000) {
+    //            var selectedmeow = "rawr x3 nuzzles >w<";
+    //        } else { var selectedmeow = randmeow[Math.floor(Math.random() * randmeow.length)]; }
+    //    }
+    var selectedmeow = randmeow[Math.floor(Math.random() * randmeow.length)];
     sendMessage(getCurrentChannel().id, { content: selectedmeow });
 }
 
