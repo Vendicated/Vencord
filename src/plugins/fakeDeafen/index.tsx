ee67b032c47ca9eb9ddecbbc5ba107c09035d68f
viciouscal <78273450+viciouscal@users.noreply.github.com>
2025-03-10 12:41:27 +0300

    Add files via upload

---
+++ src/plugins/fakeDeafen/index.tsx
@@ -0,0 +1,89 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+import { addSettingsPanelButton, DeafenIcon, removeSettingsPanelButton } from "../philsPluginLibrary";
+
+export let fakeD = false;
+
+function mute() {
+    (document.querySelector('[aria-label="Mute"]') as HTMLElement).click();
+}
+
+function deafen() {
+    (document.querySelector('[aria-label="Deafen"]') as HTMLElement).click();
+}
+
+const settings = definePluginSettings({
+    muteUponFakeDeafen: {
+        type: OptionType.BOOLEAN,
+        description: "",
+        default: false
+    },
+    mute: {
+        type: OptionType.BOOLEAN,
+        description: "",
+        default: true
+    },
+    deafen: {
+        type: OptionType.BOOLEAN,
+        description: "",
+        default: true
+    },
+    cam: {
+        type: OptionType.BOOLEAN,
+        description: "",
+        default: false
+    }
+});
+
+export default definePlugin({
+    name: "FakeDeafen",
+    description: "You're deafened but you're not",
+    dependencies: ["PhilsPluginLibrary"],
+    authors: [Devs.viciouscal],
+
+    patches: [
+        {
+            find: "}voiceStateUpdate(",
+            replacement: {
+                match: /self_mute:([^,]+),self_deaf:([^,]+),self_video:([^,]+)/,
+                replace: "self_mute:$self.toggle($1, 'mute'),self_deaf:$self.toggle($2, 'deaf'),self_video:$self.toggle($3, 'video')"
+            }
+        }
+    ],
+
+    settings,
+    toggle: (au: any, what: string) => {
+        if (fakeD === false)
+            return au;
+        else
+            switch (what) {
+                case "mute": return settings.store.mute;
+                case "deaf": return settings.store.deafen;
+                case "video": return settings.store.cam;
+            }
+    },
+
+    start() {
+        addSettingsPanelButton({
+            name: "faked", icon: DeafenIcon, tooltipText: "Fake Deafen", onClick: () => {
+                fakeD = !fakeD;
+                deafen();
+                setTimeout(deafen, 250);
+
+                if (settings.store.muteUponFakeDeafen)
+                    setTimeout(mute, 300);
+            }
+        });
+    },
+    stop() {
+        removeSettingsPanelButton("faked");
+    }
+});
