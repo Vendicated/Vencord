7d803816d31c9ad3d9172e52445b0533fb70c6ff
dolfies <jeyalfie47@gmail.com>
2025-04-01 02:40:46 -0400

    Refetch stale affinities on implicit tab

--- src/plugins/implicitRelationships/index.ts
+++ src/plugins/implicitRelationships/index.ts
@@ -20,7 +20,7 @@ import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { findStoreLazy } from "@webpack";
-import { Constants, FluxDispatcher, GuildStore, RelationshipStore, SnowflakeUtils, UserStore } from "@webpack/common";
+import { Constants, FluxDispatcher, GuildStore, RelationshipStore, RestAPI, SnowflakeUtils, UserStore } from "@webpack/common";
 import { Settings } from "Vencord";
 
 const UserAffinitiesStore = findStoreLazy("UserAffinitiesStore");
@@ -121,10 +121,24 @@ export default definePlugin({
             : comparator(row);
     },
 
+    async refreshUserAffinities() {
+        try {
+            await RestAPI.get({ url: "/users/@me/affinities/users", retries: 3 }).then(({ body }) => {
+                FluxDispatcher.dispatch({
+                    type: "LOAD_USER_AFFINITIES_SUCCESS",
+                    affinities: body,
+                });
+            });
+        } catch (e) {
+            // Not a critical error if this fails for some reason
+        }
+    },
+
     async fetchImplicitRelationships() {
         // Implicit relationships are defined as users that you:
         // 1. Have an affinity for
         // 2. Do not have a relationship with
+        await this.refreshUserAffinities();
         const userAffinities: Set<string> = UserAffinitiesStore.getUserAffinitiesUserIds();
         const relationships = RelationshipStore.getRelationships();
         const nonFriendAffinities = Array.from(userAffinities).filter(
