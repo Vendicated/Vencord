8a2abd5f19941e46963efb0c1fedb282f5de7aaf
Vending Machine <vendicated@riseup.net>
2025-04-02 05:03:45 +0200

    Update VencordTab.tsx

--- src/components/VencordSettings/VencordTab.tsx
+++ src/components/VencordSettings/VencordTab.tsx
@@ -115,7 +115,7 @@ function VencordSettings() {
                     <SpecialCard
                         title="Donations"
                         subtitle="Thank you for donating!"
-                        description="If you have gotten a badge from donating, Vencord users are able to see it! You can change it at any time by messaging @vending.machine."
+                        description="You can manage your perks at any time by messaging @vending.machine."
                         cardImage={VENNIE_DONATOR_IMAGE}
                         backgroundImage={DONOR_BACKGROUND_IMAGE}
                         backgroundColor="#ED87A9"
