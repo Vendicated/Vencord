e1e49248feb9c0e63d59d7894a8ddcdbbc17805a
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-04-03 16:44:05 -0400

    All Fixes

--- src/plugins/_api/serverList.ts
+++ src/plugins/_api/serverList.ts
@@ -39,7 +39,7 @@ export default definePlugin({
                     replace: "Vencord.Api.ServerList.renderAll(Vencord.Api.ServerList.ServerListRenderPosition.In).concat($&)"
                 },
                 {
-                    match: /discoveryIcon\}\)\}\)\]/,
+                    match: /children:\i\}\)\]/,
                     replace: "$&.concat(Vencord.Api.ServerList.renderAll(Vencord.Api.ServerList.ServerListRenderPosition.Below))"
                 }
             ]
