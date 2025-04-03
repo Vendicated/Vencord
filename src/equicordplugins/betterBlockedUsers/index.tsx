6c4a9d4cc8481ec05e28d3c1297ccacbc9075161
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-04-03 07:04:42 -0400

    Fix Lint

--- src/equicordplugins/betterBlockedUsers/index.tsx
+++ src/equicordplugins/betterBlockedUsers/index.tsx
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import "./styles.css";
-
 import { definePluginSettings } from "@api/Settings";
 import { Devs, EquicordDevs } from "@utils/constants";
 import { openUserProfile } from "@utils/discord";
