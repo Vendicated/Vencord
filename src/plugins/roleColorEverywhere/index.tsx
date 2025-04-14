b7bae48e33753318e48aa2e84ce7fa7dbe3eaa89
Vendicated <vendicated@riseup.net>
2025-04-15 01:28:01 +0200

    unexplode my brain

--- src/plugins/roleColorEverywhere/index.tsx
+++ src/plugins/roleColorEverywhere/index.tsx
@@ -84,8 +84,8 @@ export default definePlugin({
             find: ".USER_MENTION)",
             replacement: [
                 {
-                    match: /(?<=onContextMenu:\i,color:)\i(?=.{0,200}?user:(\i))/,
-                    replace: "$self.getColorInt($1?.id,$2?.id)",
+                    match: /(?<=onContextMenu:\i,color:)\i(?<=\.getNickname\((\i),\i,(\i).+?)/,
+                    replace: "$self.getColorInt($2?.id,$1)",
                 }
             ],
             predicate: () => settings.store.chatMentions
