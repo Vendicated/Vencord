fa2fb341c8ad4dfd5708eacfec19385078f74e59
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-04-13 21:46:55 -0400

    Fix FriendCodes

--- src/equicordplugins/friendCodes/index.tsx
+++ src/equicordplugins/friendCodes/index.tsx
@@ -17,7 +17,7 @@ export default definePlugin({
         {
             find: "#{intl::ADD_FRIEND})}),(",
             replacement: {
-                match: /\.Fragment[^]*?children:\[[^]*?}\)/,
+                match: /header,children:\[.*?\{\}\)/,
                 replace: "$&,$self.FriendCodesPanel"
             }
         }
