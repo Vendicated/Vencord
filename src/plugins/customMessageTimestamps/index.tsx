cf2355ed05b8d6396dd2989a6cdac77795e40cb3
Suffocate <70031311+lolsuffocate@users.noreply.github.com>
2025-04-14 15:39:47 +0100

    Add support for timestamp markdown, improve html structure and css, add support for relative seconds under a minute
    Co-authored-by: Obsidian <108832807+obsidianninja11@users.noreply.github.com>

--- src/plugins/customMessageTimestamps/index.tsx
+++ src/plugins/customMessageTimestamps/index.tsx
@@ -1,12 +1,15 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
+
+import "./style.css";
 
 import { definePluginSettings, useSettings } from "@api/Settings";
 import { Link } from "@components/Link";
 import { Devs } from "@utils/constants";
+import { Margins } from "@utils/margins";
 import { useForceUpdater } from "@utils/react";
 import definePlugin, { OptionType } from "@utils/types";
 import { Forms, moment, TextInput, useEffect, useState } from "@webpack/common";
@@ -61,8 +64,8 @@ const timeFormats: Record<string, TimeFormat> = {
         default: "ddd DD.MM.YYYY HH:mm:ss",
         offset: -1000 * 60 * 60 * 24 * 7,
     },
-    sameElseFormat: {
-        name: "Same else",
+    olderDateFormat: {
+        name: "Older Date",
         description: "[calendar] format for older dates",
         default: "ddd DD.MM.YYYY HH:mm:ss",
         offset: -1000 * 60 * 60 * 24 * 31,
@@ -72,17 +75,21 @@ const timeFormats: Record<string, TimeFormat> = {
 const format = (date: Date, formatTemplate: string): string => {
     const mmt = moment(date);
 
+    moment.relativeTimeThreshold("s", 60);
+    moment.relativeTimeThreshold("ss", -1);
+    moment.relativeTimeThreshold("m", 60);
+
     const sameDayFormat = settings.store?.formats?.sameDayFormat || timeFormats.sameDayFormat.default;
     const lastDayFormat = settings.store?.formats?.lastDayFormat || timeFormats.lastDayFormat.default;
     const lastWeekFormat = settings.store?.formats?.lastWeekFormat || timeFormats.lastWeekFormat.default;
-    const sameElseFormat = settings.store?.formats?.sameElseFormat || timeFormats.sameElseFormat.default;
+    const olderDateFormat = settings.store?.formats?.olderDateFormat || timeFormats.olderDateFormat.default;
 
     return mmt.format(formatTemplate)
         .replace("calendar", () => mmt.calendar(null, {
             sameDay: sameDayFormat,
             lastDay: lastDayFormat,
             lastWeek: lastWeekFormat,
-            sameElse: sameElseFormat
+            sameElse: olderDateFormat
         }))
         .replace("relative", () => mmt.fromNow());
 };
@@ -105,12 +112,12 @@ const TimeRow = (props: TimeRowProps) => {
     }, [state]);
 
     return (
-        <div style={{ padding: "0 0 20px 0" }}>
-            <Forms.FormTitle tag="h4">{props.format.name}</Forms.FormTitle>
+        <>
+            <Forms.FormTitle tag="h5">{props.format.name}</Forms.FormTitle>
             <Forms.FormText>{props.format.description}</Forms.FormText>
             <TextInput value={state} onChange={handleChange}/>
-            <Forms.FormText style={{ color: "yellow", marginTop: "10px" }}>{preview}</Forms.FormText>
-        </div>
+            <Forms.FormText className={"vc-cmt-preview-text"}>{preview}</Forms.FormText>
+        </>
     );
 };
 
@@ -127,28 +134,25 @@ const settings = definePluginSettings({
                 componentProps.setValue(newSettings);
             };
 
-            return (
-                <Forms.FormSection>
-                    {Object.entries(timeFormats).map(([key, value]) => (
-                        <div key={key}>
-                            {key === "sameDayFormat" && (
-                                <div style={{ padding: "0 0 20px 0" }}>
-                                    <Forms.FormDivider style={{ marginBottom: "10px" }}/>
-                                    <Forms.FormTitle tag="h1">Calendar formats</Forms.FormTitle>
-                                    <Forms.FormText>
-                                        How to format the [calendar] value if used in the above timestamps.
-                                    </Forms.FormText>
-                                </div>
-                            )}
-                            <TimeRow
-                                id={key}
-                                format={value}
-                                onChange={setNewValue}
-                                pluginSettings={settingsState}
-                            />
-                        </div>
-                    ))}
-                </Forms.FormSection>
+            return Object.entries(timeFormats).map(([key, value]) => (
+                    <Forms.FormSection key={key}>
+                        {key === "sameDayFormat" && (
+                            <div className={Margins.bottom20}>
+                                <Forms.FormDivider style={{ marginBottom: "10px" }}/>
+                                <Forms.FormTitle tag="h1">Calendar formats</Forms.FormTitle>
+                                <Forms.FormText>
+                                    How to format the [calendar] value if used in the above timestamps.
+                                </Forms.FormText>
+                            </div>
+                        )}
+                        <TimeRow
+                            id={key}
+                            format={value}
+                            onChange={setNewValue}
+                            pluginSettings={settingsState}
+                        />
+                    </Forms.FormSection>
+                )
             );
         }
     }
@@ -160,7 +164,7 @@ const settings = definePluginSettings({
         sameDayFormat: string;
         lastDayFormat: string;
         lastWeekFormat: string;
-        sameElseFormat: string;
+        olderDateFormat: string;
     };
 }>();
 
@@ -170,45 +174,47 @@ export default definePlugin({
     authors: [
         Devs.Rini,
         Devs.nvhhr,
-        Devs.Suffocate
+        Devs.Suffocate,
+        Devs.Obsidian
     ],
     settings,
     settingsAboutComponent: () => (
-        <div
-            style={{
-                backgroundColor: "var(--info-help-background)",
-                border: "1px solid var(--info-help-foreground)",
-                borderRadius: "5px",
-                padding: "5px",
-                marginTop: "10px",
-                marginBottom: "10px"
-            }}
-        >
+        <div className={"vc-cmt-info-card"}>
             <Forms.FormTitle tag="h2">How to use:</Forms.FormTitle>
             <Forms.FormText>
                 <Link href="https://momentjs.com/docs/#/displaying/format/">Moment.js formatting documentation</Link>
-                <p>
+                <div className={Margins.top8}>
                     Additionally you can use these in your inputs:<br/>
                     <b>[calendar]</b> enables dynamic date formatting such
                     as &quot;Today&quot; or &quot;Yesterday&quot;.<br/>
                     <b>[relative]</b> gives you times such as &quot;4 hours ago&quot;.<br/>
-                </p>
+                </div>
             </Forms.FormText>
         </div>
     ),
-    patches: [{
-        find: "#{intl::MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL}",
-        replacement: [
-            {
-                match: /(\i)\?\(0,\i\.\i\)\((\i),"LT"\):\(0,\i\.\i\)\(\i,!0\)/,
-                replace: "$self.renderTimestamp($2,$1?'compact':'cozy')",
-            },
-            {
-                match: /(?<=text:)\(0,\i.\i\)\((\i),"LLLL"\)(?=,)/,
-                replace: "$self.renderTimestamp($1,'tooltip')",
-            },
-        ]
-    }],
+    patches: [
+        {
+            find: "#{intl::MESSAGE_EDITED_TIMESTAMP_A11Y_LABEL}",
+            replacement: [
+                {
+                    match: /(\i)\?\(0,\i\.\i\)\((\i),"LT"\):\(0,\i\.\i\)\(\i,!0\)/,
+                    replace: "$self.renderTimestamp($2,$1?'compact':'cozy')",
+                },
+                {
+                    match: /(?<=text:)\(0,\i.\i\)\((\i),"LLLL"\)(?=,)/,
+                    replace: "$self.renderTimestamp($1,'tooltip')",
+                },
+            ]
+        },
+        // Tooltip for timestamps (e.g. <t:1234567890>)
+        {
+            find: ".full,tooltipClassName:",
+            replacement: {
+                match: /text:(\i).full,/,
+                replace: "text: $self.renderTimestamp(new Date($1.timestamp*1000),'tooltip'),"
+            }
+        }
+    ],
 
     renderTimestamp: (date: Date, type: "cozy" | "compact" | "tooltip") => {
         const forceUpdater = useForceUpdater();
@@ -227,7 +233,7 @@ export default definePlugin({
 
         useEffect(() => {
             if (formatTemplate.includes("calendar") || formatTemplate.includes("relative")) {
-                const interval = setInterval(forceUpdater, 30000);
+                const interval = setInterval(forceUpdater, 1000);
                 return () => clearInterval(interval);
             }
         }, []);
