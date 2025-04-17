1a8ea79d485a7a9b2d25e8bfc65ce6400345ed33
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-04-17 07:46:59 -0400

    BannersEverywhere Fix

--- src/equicordplugins/bannersEverywhere/index.tsx
+++ src/equicordplugins/bannersEverywhere/index.tsx
@@ -58,7 +58,7 @@ export default definePlugin({
             replacement: [
                 {
                     // We add the banner as a property while we can still access the user id
-                    match: /verified:(\i).isVerifiedBot.*?nameplate:(\i).*?name:null.*?(?=avatar:)/,
+                    match: /user:(\i).{0,150}nameplate:(\i).*?name:null.*?(?=avatar:)/,
                     replace: "$&banner:$self.memberListBannerHook($1, $2),",
                 },
                 {
