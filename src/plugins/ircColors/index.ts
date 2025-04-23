736f868e6c75528a0f493511d966d93901e42d7b
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-22 20:45:05 -0400

    fix ircColors

--- src/plugins/ircColors/index.ts
+++ src/plugins/ircColors/index.ts
@@ -66,8 +66,8 @@ export default definePlugin({
         {
             find: '="SYSTEM_TAG"',
             replacement: {
-                match: /\i.gradientClassName]\),style:/,
-                replace: "$&{color:$self.calculateNameColorForMessageContext(arguments[0])},_style:"
+                match: /(style:)(.{0,500}textDecorationColor)/,
+                replace: "$1{color:$self.calculateNameColorForMessageContext(arguments[0])},_style:$2"
             }
         },
         {
