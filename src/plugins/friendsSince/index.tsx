a6cfe61c63e6bd17f4c95fb53fc9635b90a51cfd
sadan <117494111+sadan4@users.noreply.github.com>
2025-04-28 10:19:34 -0400

    drop friends since patch

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -29,14 +29,6 @@ export default definePlugin({
                 match: /#{intl::USER_PROFILE_MEMBER_SINCE}\),.{0,100}userId:(\i\.id)}\)}\)/,
                 replace: "$&,$self.FriendsSinceComponent({userId:$1,isSidebar:true})"
             }
-        },
-        // User Profile Modal
-        {
-            find: "action:\"PRESS_APP_CONNECTION\"",
-            replacement: {
-                match: /#{intl::USER_PROFILE_MEMBER_SINCE}\),.{0,100}userId:(\i\.id),.{0,100}}\)}\),/,
-                replace: "$&,$self.FriendsSinceComponent({userId:$1,isSidebar:false}),"
-            }
         }
     ],
 
