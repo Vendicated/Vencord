e105119293af74d192520e0b18bc4bc350394350
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-05-13 19:06:15 -0400

    Fix PinIcon (Broken With Attachments)

--- src/equicordplugins/pinIcon/index.tsx
+++ src/equicordplugins/pinIcon/index.tsx
@@ -28,9 +28,9 @@ export default definePlugin({
     authors: [EquicordDevs.iamme],
     patches: [
         {
-            find: "#{intl::MESSAGE_EDITED}),",
+            find: "isUnsupported})",
             replacement: {
-                match: /#{intl::MESSAGE_EDITED}\),(?:[^}]*[}]){3}\)/,
+                match: /WITH_CONTENT\}\)/,
                 replace: "$&,$self.PinnedIcon(arguments[0].message)"
             }
         }
