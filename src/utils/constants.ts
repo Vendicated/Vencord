c0e0bfd00c6446a8c678e9a6121e0d5cbdef3ba7
santiago amerio <136385908+santiago-amerio@users.noreply.github.com>
2025-05-18 16:00:17 -0300

    Plugin add BetterTitleBar

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -589,6 +589,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "samsam",
         id: 836452332387565589n,
     },
+    thiagosch: {
+        name: "thiagosch",
+        id: 180067587042836480n,
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
