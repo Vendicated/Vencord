f7b8c74aeaa42ab0a3cafe5848b56370d743f0a5
cootshk <cootshk@duck.com>
2025-06-03 17:54:23 -0500

    add myself as an author

--- src/plugins/betterNotifications.desktop/index.tsx
+++ src/plugins/betterNotifications.desktop/index.tsx
@@ -260,7 +260,7 @@ function replaceVariables(advancedNotification: AdvancedNotification, basicNotif
 export default definePlugin({
     name: "BetterNotifications",
     description: "Improves discord's desktop notifications.",
-    authors: [Devs.ctih],
+    authors: [Devs.ctih, Devs.Cootshk],
     tags: ["native", "notifications", "better"],
     settings: settings,
 
