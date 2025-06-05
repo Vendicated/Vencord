4f220bde7a82184076e981822b067abbf4ef48d1
ctih1 <nevalaonni@gmail.com>
2025-06-05 18:16:35 +0300

    added vencord header

--- src/plugins/betterNotifications/types/advancedNotification.ts
+++ src/plugins/betterNotifications/types/advancedNotification.ts
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 export interface AdvancedNotification {
     omitViewTracking: boolean;
     tag: string;
--- src/plugins/betterNotifications/types/basicNotification.ts
+++ src/plugins/betterNotifications/types/basicNotification.ts
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 export interface BasicNotification {
     notif_type: string;
     notif_user_id: string;
