5dd3d60cd73c3a642a2d0c7026bb64a9e3e881d9
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-06-04 20:46:38 -0400

    Fix Patch

--- src/equicordplugins/fixFileExtensions/index.tsx
+++ src/equicordplugins/fixFileExtensions/index.tsx
@@ -34,22 +34,19 @@ export default definePlugin({
     patches: [
         // Taken from AnonymiseFileNames
         {
-            find: 'type:"UPLOAD_START"',
-            replacement: {
-                match: /await \i\.uploadFiles\((\i),/,
-                replace: "$1.forEach($self.fixExt),$&"
-            },
+            find: "async uploadFiles(",
+            replacement: [
+                {
+                    match: /async uploadFiles\((\i),\i\){/,
+                    replace: "$&$1.forEach($self.anonymise);"
+                },
+                {
+                    match: /async uploadFilesSimple\((\i)\){/,
+                    replace: "$&$1.forEach($self.anonymise);"
+                }
+            ],
             predicate: () => !Settings.plugins.AnonymiseFileNames.enabled,
         },
-        // Also taken from AnonymiseFileNames
-        {
-            find: "FirstThreadMessage,options",
-            replacement: {
-                match: /\i.uploadFilesSimple\((\i)\);/,
-                replace: "$&$1.forEach($self.fixExt);",
-            },
-            predicate: () => !Settings.plugins.AnonymiseFileNames.enabled,
-        }
     ],
     fixExt(upload: ExtUpload) {
         const file = upload.filename;
