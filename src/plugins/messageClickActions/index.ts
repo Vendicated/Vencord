b687f35b6fbcd4087dfb99c31e45deed73bcb3d9
Mia <mia@github.com>
2025-06-07 19:39:51 -0400

    make delete key detection consistent on lost focus

--- src/plugins/messageClickActions/index.ts
+++ src/plugins/messageClickActions/index.ts
@@ -28,6 +28,7 @@ const EditStore = findByPropsLazy("isEditing", "isEditingAny");
 let isDeletePressed = false;
 const keydown = (e: KeyboardEvent) => e.key === "Backspace" && (isDeletePressed = true);
 const keyup = (e: KeyboardEvent) => e.key === "Backspace" && (isDeletePressed = false);
+const blur = () => isDeletePressed = false;
 
 const settings = definePluginSettings({
     enableDeleteOnClick: {
@@ -62,11 +63,13 @@ export default definePlugin({
     start() {
         document.addEventListener("keydown", keydown);
         document.addEventListener("keyup", keyup);
+        window.addEventListener("blur", blur);
     },
 
     stop() {
         document.removeEventListener("keydown", keydown);
         document.removeEventListener("keyup", keyup);
+        window.removeEventListener("blur", blur);
     },
 
     onMessageClick(msg: any, channel, event) {
