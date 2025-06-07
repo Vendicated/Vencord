6dc523eec179267b0b8640650dc3aef528cb9b18
EnergoStalin <ens.stalin@gmail.com>
2025-06-07 21:40:44 +0300

    fix(roleColorEverywhere): fix imports

--- src/plugins/roleColorEverywhere/index.tsx
+++ src/plugins/roleColorEverywhere/index.tsx
@@ -32,8 +32,10 @@ import { Guild } from "discord-types/general";
 
 import { RoleModal } from "./components/RolesModal";
 import { toggleRole } from "./storeHelper";
-import { ContextMenu } from "./components/ContextMenu";
+
 import { brewUserColor } from "./witchCauldron";
+
+import { ChannelStore, GuildMemberStore, GuildStore, Menu, React } from "@webpack/common";
 
 const cl = classNameFactory("rolecolor");
 const DeveloperMode = getUserSettingLazy("appearance", "developerMode")!;
