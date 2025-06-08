49ba12630b342fe076168c5f03f37e976fb1349e
Cookie <contact@swezy.dev>
2025-06-08 11:47:59 -0400

    WebScreenShareFixes: only apply stereo parameter to video audio (#3474)

--- src/plugins/webScreenShareFixes.web/index.ts
+++ src/plugins/webScreenShareFixes.web/index.ts
@@ -25,8 +25,8 @@ export default definePlugin({
                     replace: ";b=AS:800000;level-asymmetry-allowed=1"
                 },
                 {
-                    match: "useinbandfec=1",
-                    replace: "useinbandfec=1;stereo=1;sprop-stereo=1"
+                    match: /;usedtx=".concat\((\i)\?"0":"1"\)/,
+                    replace: '$&.concat($1?";stereo=1;sprop-stereo=1":"")'
                 }
             ]
         }
