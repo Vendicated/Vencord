12e5eaa432096492c6da9eb2a91f21fdf73585b7
Vendicated <vendicated@riseup.net>
2025-06-12 01:24:23 +0200

    organise

--- src/main/csp/manager.ts
+++ src/main/csp/manager.ts
@@ -6,26 +6,16 @@
 
 import { NativeSettings } from "@main/settings";
 import { IpcEvents } from "@shared/IpcEvents";
-import { dialog, ipcMain } from "electron";
+import { dialog, ipcMain, IpcMainInvokeEvent } from "electron";
 
 import { CspPolicies, ImageAndCssSrc } from ".";
 
 export type CspRequestResult = "invalid" | "cancelled" | "unchecked" | "ok" | "conflict";
 
 export function registerCspIpcHandlers() {
-    ipcMain.handle(IpcEvents.CSP_REMOVE_OVERRIDE, (_, domain: string) => removeCspRule(domain));
-    ipcMain.handle(IpcEvents.CSP_REQUEST_ADD_OVERRIDE, async (_, url: string, directives: string[], callerName: string) =>
-        addCspRule(url, directives, callerName)
-    );
-    ipcMain.handle(IpcEvents.CSP_IS_DOMAIN_ALLOWED, (_, url: string, directives: string[]) => {
-        try {
-            const domain = new URL(url).hostname;
-            const ruleForDomain = CspPolicies[domain] ?? NativeSettings.store.customCspRules[domain];
-            return ruleForDomain && directives.every(d => ruleForDomain.includes(d));
-        } catch (e) {
-            return false;
-        }
-    });
+    ipcMain.handle(IpcEvents.CSP_REMOVE_OVERRIDE, removeCspRule);
+    ipcMain.handle(IpcEvents.CSP_REQUEST_ADD_OVERRIDE, addCspRule);
+    ipcMain.handle(IpcEvents.CSP_IS_DOMAIN_ALLOWED, isDomainAllowed);
 }
 
 // TODO: remove this when URL.canParse is more mature
@@ -81,7 +71,7 @@ function getMessage(url: string, directives: string[], callerName: string) {
     return { message, detail };
 }
 
-async function addCspRule(url: string, directives: string[], callerName: string): Promise<CspRequestResult> {
+async function addCspRule(_: IpcMainInvokeEvent, url: string, directives: string[], callerName: string): Promise<CspRequestResult> {
     if (!validate(url, directives)) {
         return "invalid";
     }
@@ -116,11 +106,21 @@ async function addCspRule(url: string, directives: string[], callerName: string)
     return "ok";
 }
 
-function removeCspRule(domain: string) {
+function removeCspRule(_: IpcMainInvokeEvent, domain: string) {
     if (domain in NativeSettings.store.customCspRules) {
         delete NativeSettings.store.customCspRules[domain];
         return true;
     }
 
     return false;
+}
+
+function isDomainAllowed(_: IpcMainInvokeEvent, url: string, directives: string[]) {
+    try {
+        const domain = new URL(url).hostname;
+        const ruleForDomain = CspPolicies[domain] ?? NativeSettings.store.customCspRules[domain];
+        return ruleForDomain && directives.every(d => ruleForDomain.includes(d));
+    } catch (e) {
+        return false;
+    }
 }
