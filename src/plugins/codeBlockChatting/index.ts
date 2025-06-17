b7de06f28d3cff0b733f41fbe7dfd248e6d79dac
(known as thenoppy12) <108082159+BussyBakks@users.noreply.github.com>
2025-06-17 12:20:30 +0700

    fix more discord markdown startsWith

--- src/plugins/codeBlockChatting/index.ts
+++ src/plugins/codeBlockChatting/index.ts
@@ -22,7 +22,7 @@ const log = new Logger("CodeBlockChatting", "#8caaee");
 // idk how to make shortcut button using svg. sorry for inconvenience
 export default definePlugin({
     name: "CodeBlockChatting",
-    description: "Automatically add codeblock \"```\" at head and end of message. (j4f)",
+    description: "Automatically add codeblock \"```\" at head and end of message. (j4f) (buggy + much unexpected behaviors rn)",
     authors: [Devs.thenoppy12],
     settings: setting,
     enabledByDefault: false,
@@ -32,8 +32,17 @@ export default definePlugin({
             const content = msg.content.trim();
             const lang = setting.store.codeBlockLanguage;
             if (
-                content.startsWith("```") ||
-                content.startsWith("/") ||
+                content.startsWith("```") || // multiline codeblocks
+                content.startsWith("*") || // italics, bold, bold italics
+                content.startsWith("_") || // underline
+                content.startsWith("~") || // strikethrough
+                content.startsWith("#") || // headers
+                content.startsWith("-#") || // subtext
+                content.startsWith("[") || // links
+                content.startsWith("- ") || content.startsWith("* ") || // lists (maybe?)
+                content.startsWith("`") || // singleline codeblocks
+                content.startsWith(">") || // block quotes
+                // can be more? edit this fk as you like
                 content.length === 0
             ) return;
             msg.content = `\`\`\`${lang}\n${content}\n\`\`\``;
