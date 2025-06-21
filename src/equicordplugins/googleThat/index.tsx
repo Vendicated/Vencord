fbc444df8af0395732cdeb07565d7dd5c3ef0969
Indiana Jone (Indi) <150982280+KrstlSkll69@users.noreply.github.com>
2025-06-21 12:10:07 -0400

    Update googleThat (#292)
    * Update googleThat
    
    * remove extra space??
    
    * remove periods
    
    * Make devs one line
    
    * THOROREN 😭😭😭😭😭😭

--- src/equicordplugins/googleThat/index.tsx
+++ src/equicordplugins/googleThat/index.tsx
@@ -6,12 +6,17 @@
 
 import { ApplicationCommandOptionType, findOption } from "@api/Commands";
 import { definePluginSettings } from "@api/Settings";
-import { Devs } from "@utils/constants";
+import { Devs, EquicordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+
 function getMessage(opts) {
     const inputOption = findOption(opts, "input", "");
 
-    const queryURL = "" + searchEngines[settings.store.defaultEngine] + encodeURIComponent(inputOption);
+    let queryURL = "" + searchEngines[settings.store.defaultEngine] + encodeURIComponent(inputOption);
+
+    if (settings.store.customEngineURL) {
+        queryURL = "" + [settings.store.customEngineURL] + encodeURIComponent(inputOption);
+    }
 
     if (settings.store.hyperlink) {
         return `[${inputOption}](${queryURL})`;
@@ -30,6 +35,7 @@ const searchEngines = {
     "Yandex": "https://yandex.com/search/?text=",
     "Ecosia": "https://www.ecosia.org/search?q=",
     "Ask": "https://www.ask.com/web?q=",
+    "AOL": "https://search.aol.com/aol/search?q=",
     "LetMeGoogleThatForYou": "https://letmegooglethat.com/?q="
 };
 
@@ -37,10 +43,9 @@ const settings = definePluginSettings({
     hyperlink: {
         type: OptionType.BOOLEAN,
         description: "If the sent link should hyperlink with the query as the label",
-        default: true
+        default: false
     },
-    defaultEngine:
-    {
+    defaultEngine: {
         type: OptionType.SELECT,
         description: "The search engine to use",
         options: Object.keys(searchEngines).map((key, index) => ({
@@ -48,19 +53,24 @@ const settings = definePluginSettings({
             value: key,
             default: index === 0
         }))
+    },
+    customEngineURL: {
+        description: "The URL of the Engine you wish to use",
+        type: OptionType.STRING,
+        placeholder: "https://search.vmohammad.dev/?q="
     }
 });
 
 export default definePlugin({
     name: "GoogleThat",
-    description: "Adds a command to send a google search link to a query",
-    authors: [Devs.Samwich],
+    description: "Adds a command to send a internet search link",
+    authors: [Devs.Samwich, EquicordDevs.KrystalSkull],
     tags: ["search", "google", "query", "duckduckgo", "command"],
     settings,
     commands: [
         {
             name: "googlethat",
-            description: "send a search engine link to a query",
+            description: "send a search engine link",
             options: [
                 {
                     name: "input",
