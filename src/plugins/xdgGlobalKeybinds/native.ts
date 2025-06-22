1727a41e054579184d715267a96be98706a18e4a
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-06-22 18:01:16 +0300

    feat: add xdg functionality in native.ts

---
+++ src/plugins/xdgGlobalKeybinds/native.ts
@@ -0,0 +1,55 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import fs from "node:fs";
+
+import { sessionBus, Variant } from "dbus-next";
+
+function log(message: string) {
+    fs.writeFile("xdgLog", "LOG: " + message + "\n", () => {});
+}
+
+(async function () {
+    const bus = sessionBus();
+
+    const obj = await bus.getProxyObject(
+        "org.freedesktop.portal.Desktop",
+        "/org/freedesktop/portal/desktop",
+    );
+
+    const globalShortcuts = obj.getInterface(
+        "org.freedesktop.portal.GlobalShortcuts",
+    );
+
+    // HACK: not sure if "createSessionToken" the correct session handle token, but it seems to work
+    const TOKEN = "createSessionToken";
+    const session_request_handle: string = await globalShortcuts.CreateSession({
+        session_handle_token: new Variant("s", TOKEN),
+    });
+    const SENDER = session_request_handle.match(/request\/([^/]+)\//)?.[1];
+
+    const session_handle = `/org/freedesktop/portal/desktop/session/${SENDER}/${TOKEN}`;
+    // TODO: get parent_window using the xdg_foreign protocol
+    // (https://gitlab.freedesktop.org/wayland/wayland-protocols/-/blob/main/unstable/xdg-foreign/xdg-foreign-unstable-v2.xml)
+    const parent_window = "";
+
+    const bindShortcuts_request_handle = await globalShortcuts.BindShortcuts(
+        session_handle,
+        [["toggle_mute", { description: new Variant("s", "Toggle mute") }]],
+        parent_window,
+        { handle_token: new Variant("s", "bindShortcutsToken") },
+    );
+
+    globalShortcuts.on(
+        "Activated",
+        (session_handle: string, shortcut_id: string, timestamp, options) => {
+            log(`Shortcut activated: ${shortcut_id}`);
+            log(`Session handle: ${session_handle}`);
+            log(`Timestamp: ${timestamp}`);
+            log(`Options: ${JSON.stringify(options)}`);
+        },
+    );
+})();
