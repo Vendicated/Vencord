c4cc26bbd2271647085daac5110b98545624e608
nin0 <personal@nin0.dev>
2025-06-30 16:05:15 -0400

    AppleMusicRichPresence: fix token regex

--- src/plugins/appleMusic.desktop/native.ts
+++ src/plugins/appleMusic.desktop/native.ts
@@ -27,7 +27,7 @@ interface RemoteData {
 let cachedRemoteData: { id: string, data: RemoteData; } | { id: string, failures: number; } | null = null;
 
 const APPLE_MUSIC_BUNDLE_REGEX = /<script type="module" crossorigin src="([a-zA-Z0-9.\-/]+)"><\/script>/;
-const APPLE_MUSIC_TOKEN_REGEX = canonicalizeMatch(/Promise.allSettled\(\i\)\}const \i="([A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*)"/);
+const APPLE_MUSIC_TOKEN_REGEX = canonicalizeMatch(/for the "web" platform`\)}const \i="([A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*)"/);
 
 let cachedToken: string | undefined = undefined;
 
