1d1ad365b0cd2635ecc042546ab7c484feeeb660
sadan4 <117494111+sadan4@users.noreply.github.com>
2025-06-30 17:09:30 -0400

    Update packages/discord-types/src/stores/UserStore.d.ts
    Co-authored-by: V <vendicated@riseup.net>

--- packages/discord-types/src/stores/UserStore.d.ts
+++ packages/discord-types/src/stores/UserStore.d.ts
@@ -1,9 +1,9 @@
 import { FluxStore, User } from "..";
 
 export class UserStore extends FluxStore {
-    filter(filter: () => boolean, sort?: boolean): Record<string, User>;
+    filter(filter: (user: User) => boolean, sort?: boolean): Record<string, User>;
     findByTag(username: string, discriminator: string): User;
-    forEach(action: Function): void;
+    forEach(action: (user: User) => void): void;
     getCurrentUser(): User;
     getUser(userId: string): User;
     getUsers(): Record<string, User>;
