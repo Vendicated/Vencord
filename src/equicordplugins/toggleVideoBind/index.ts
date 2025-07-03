e96ac514f0c82148344a6c34e0166a8d4fa3ab64
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-07-03 16:43:42 -0400

    FastDeleteChannels: Custom Binds

--- src/equicordplugins/toggleVideoBind/index.ts
+++ src/equicordplugins/toggleVideoBind/index.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { definePluginSettings } from "@api/Settings";
+import { definePluginSettings, migratePluginSettings } from "@api/Settings";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { findByPropsLazy } from "@webpack";
@@ -56,8 +56,9 @@ function handleKeydown({ code, ctrlKey, shiftKey, altKey }: KeyboardEvent) {
     });
 }
 
+migratePluginSettings("ToggleVideoBind", "toggleVideoBind");
 export default definePlugin({
-    name: "toggleVideoBind",
+    name: "ToggleVideoBind",
     description: "Adds a customizable bind to toggle webcam.",
     authors: [EquicordDevs.mochienya],
     settings,
