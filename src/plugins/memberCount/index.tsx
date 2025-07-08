cc989ff33c184d87933f2c06d1e30719b902017b
Vendicated <vendicated@riseup.net>
2025-07-09 00:43:12 +0200

    Run autofix to sort these imports!

--- src/plugins/memberCount/index.tsx
+++ src/plugins/memberCount/index.tsx
@@ -23,8 +23,8 @@ import { classNameFactory } from "@api/Styles";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { findStoreLazy } from "@webpack";
 import { FluxStore } from "@vencord/discord-types";
+import { findStoreLazy } from "@webpack";
 
 import { MemberCount } from "./MemberCount";
 
