9d33e9f088d69a024e7ad3cd5e741d5df356e9a7
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-07-13 07:34:37 -0400

    MLE:  Temp Remove Button

--- src/plugins/messageLogger/index.tsx
+++ src/plugins/messageLogger/index.tsx
@@ -74,28 +74,34 @@ const patchMessageContextMenu: NavContextMenuPatchCallback = (
         );
     }
 
+    let label;
+
     if (!Vencord.Plugins.isPluginEnabled("MessageLoggerEnhanced")) {
-        children.push(
-            <Menu.MenuItem
-                id={REMOVE_HISTORY_ID}
-                key={REMOVE_HISTORY_ID}
-                label="Remove Message History"
-                color="danger"
-                action={() => {
-                    if (deleted) {
-                        FluxDispatcher.dispatch({
-                            type: "MESSAGE_DELETE",
-                            channelId: channel_id,
-                            id,
-                            mlDeleted: true,
-                        });
-                    } else {
-                        message.editHistory = [];
-                    }
-                }}
-            />,
-        );
+        label = "Remove Message History";
+    } else {
+        label = "Remove Message (Temporary)";
     }
+
+    children.push(
+        <Menu.MenuItem
+            id={REMOVE_HISTORY_ID}
+            key={REMOVE_HISTORY_ID}
+            label={label}
+            color="danger"
+            action={() => {
+                if (deleted) {
+                    FluxDispatcher.dispatch({
+                        type: "MESSAGE_DELETE",
+                        channelId: channel_id,
+                        id,
+                        mlDeleted: true,
+                    });
+                } else {
+                    message.editHistory = [];
+                }
+            }}
+        />,
+    );
 };
 
 const patchChannelContextMenu: NavContextMenuPatchCallback = (
