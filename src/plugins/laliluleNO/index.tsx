43abf138a317e29e3fc32ae1f4b0d5a0705be13b
hideki2k02 <5614680+hideki2k02@users.noreply.github.com>
2025-07-13 11:02:44 -0300

    Leet Text Support

--- src/plugins/laliluleNO/index.tsx
+++ src/plugins/laliluleNO/index.tsx
@@ -16,6 +16,12 @@ const settings = definePluginSettings({
         description: "What words will be replaced (add a comma in between)",
         default: "gregtech, gt, gtnh, greg"
     },
+
+    Leet: {
+        type: OptionType.BOOLEAN,
+        description: "Leet Text Support",
+        default: true
+    }
 });
 
 export default definePlugin({
@@ -29,6 +35,30 @@ export default definePlugin({
             .map((w) => w.trim())
             .filter(Boolean);
 
+        const leet_alphabet = {
+            a: "4",
+            b: "8",
+            e: "3",
+            g: "6",
+            i: "1",
+            o: "0",
+            p: "9",
+            s: "5",
+            t: "7",
+            z: "2"
+        };
+
+        function leet_convert(text: string) {
+            for (let [letter, number] of Object.entries(leet_alphabet)) {
+                // Required as .replace() only replaces the first instance
+                let global_regex = new RegExp(number, 'g');
+
+                text = text.toLowerCase().replace(global_regex, letter);
+            }
+
+            return text;
+        }
+
         FluxDispatcher.addInterceptor(e => {
             // if (e.type === 'MESSAGE_CREATE' || e.type === 'MESSAGE_UPDATE') {
             //     console.log(e.message.content)
@@ -36,13 +66,20 @@ export default definePlugin({
 
             if (e.type === 'LOAD_MESSAGES_SUCCESS') {
                 e.messages.forEach((message, index) => {
+                    let input_message = message.content;
+
+                    // Leet mode support
+                    if (settings.store.Leet) { input_message = leet_convert(input_message); }
+
+                    // Replaces all the instances of the blacklisted word in the message
+                    // With lalilulelo
                     message.content = blacklist.reduce(
                         (message, blacklisted_word) => message.replace(
                             new RegExp(blacklisted_word, "gi"),
                             "lalilulelo"
                         ),
 
-                        message.content
+                        input_message
                     );
                 });
             }
