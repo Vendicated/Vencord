e8f5404527ef92368e93697619d396d787ba298c
Nuckyz <contact@swezy.dev>
2025-07-17 22:05:42 -0300

    Fix broken Decor style classes find

--- src/plugins/decor/ui/index.ts
+++ src/plugins/decor/ui/index.ts
@@ -8,7 +8,7 @@ import { classNameFactory } from "@api/Styles";
 import { extractAndLoadChunksLazy, findByPropsLazy } from "@webpack";
 
 export const cl = classNameFactory("vc-decor-");
-export const DecorationModalStyles = findByPropsLazy("modalFooterShopButton");
+export const DecorationModalStyles = findByPropsLazy("modalPreview", "modalCloseButton", "spinner", "modal");
 
 export const requireAvatarDecorationModal = extractAndLoadChunksLazy([".COLLECTIBLES_SHOP_FULLSCREEN&&"]);
 export const requireCreateStickerModal = extractAndLoadChunksLazy(["stickerInspected]:"]);
