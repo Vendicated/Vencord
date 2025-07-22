15fba555c2a15a83370624e982b25b7c7da2b498
henmalib <simidzu2ay@protonmail.com>
2025-07-22 13:02:28 +0200

    added myself back to devs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -598,6 +598,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Cootshk",
         id: 921605971577548820n
     },
+    hen: {
+        id: 279266228151779329n,
+        name: "Hen"
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
