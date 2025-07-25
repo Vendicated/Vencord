cf02a0dcf9089c0a0605fb2a8b7daac49c3308dd
viciouscal <78273450+viciouscal@users.noreply.github.com>
2025-07-25 18:22:08 +0300

    Update MicrophoneSettingsModal.tsx

--- src/plugins/betterMicrophone.desktop/components/MicrophoneSettingsModal.tsx
+++ src/plugins/betterMicrophone.desktop/components/MicrophoneSettingsModal.tsx
@@ -17,10 +17,10 @@
 */
 
 import { Flex } from "@components/Flex";
-import { Switch } from "@components/Switch";
+import { Switch } from "@components/settings/Switch";
 import { ModalSize } from "@utils/modal";
+import { SelectOption } from "@vencord/discord-types";
 import { Card, Forms, Select, Slider, TextInput, useEffect, useState } from "@webpack/common";
-import { SelectOption } from "@webpack/types";
 
 import {
     ProfilableStore,
