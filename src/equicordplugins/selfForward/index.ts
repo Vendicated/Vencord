9fa2ba9c3c898fc20f37eb471bdb0facf016a6b1
VR <101288516+VillainsRule@users.noreply.github.com>
2025-07-29 06:09:22 -0400

    add selfForward plugin (#307)
    * add inviteDefaults plugin
    
    * Update index.ts
    
    * add selfForward plugin
    
    ---------
    
    Co-authored-by: thororen <78185467+thororen1234@users.noreply.github.com>

---
+++ src/equicordplugins/selfForward/index.ts
@@ -0,0 +1,23 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { EquicordDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "SelfForward",
+    description: "adds the current channel to the forward list popup",
+    authors: [EquicordDevs.VillainsRule],
+    patches: [
+        {
+            find: ".getChannelHistory(),",
+            replacement: [{
+                match: /\i.id\]/,
+                replace: "]"
+            }]
+        }
+    ]
+});
