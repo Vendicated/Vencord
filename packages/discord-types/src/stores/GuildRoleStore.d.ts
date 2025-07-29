3082e3240e4e58df7a48c700284cc10ea1a1a748
sadan <117494111+sadan4@users.noreply.github.com>
2025-07-28 20:06:40 -0400

    add comment to prefer getSortedRoles

--- packages/discord-types/src/stores/GuildRoleStore.d.ts
+++ packages/discord-types/src/stores/GuildRoleStore.d.ts
@@ -3,6 +3,11 @@ import { FluxStore, Role } from "..";
 // TODO: add the rest of the methods for GuildRoleStore
 export class GuildRoleStore extends FluxStore {
     getRole(guildId: string, roleId: string): Role;
+    /**
+     * Prefer {@link getSortedRoles} to this
+     *
+     * @see {@link getSortedRoles}
+     */
     getUnsafeMutableRoles(guildId: string): Record<string, Role>;
     getSortedRoles(guildId: string): Role[];
 }
