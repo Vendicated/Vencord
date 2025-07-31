ae41fe229324c47ead3939fd259d824375afa1fc
EtorixDev <92535668+EtorixDev@users.noreply.github.com>
2025-07-31 04:44:53 -0700

    Semantics.

--- src/plugins/silentTyping/index.tsx
+++ src/plugins/silentTyping/index.tsx
@@ -117,7 +117,7 @@ function toggleLocation(locationId: string, effectiveList: string[], defaultHidd
     }
 }
 
-const silentTypingChatToggle: ChatBarButtonFactory = ({ channel, type }) => {
+const SilentTypingChatToggle: ChatBarButtonFactory = ({ channel, type }) => {
     const {
         enabled,
         chatIcon,
@@ -233,7 +233,7 @@ function checkEnabled(channel: string | Channel): boolean {
     }
 }
 
-const chatBarContextCheckbox: NavContextMenuPatchCallback = children => {
+const ChatBarContextCheckbox: NavContextMenuPatchCallback = children => {
     const {
         chatIcon,
         chatContextMenu,
@@ -295,7 +295,7 @@ export default definePlugin({
     shouldHideMembersListTypingIndicators,
 
     contextMenus: {
-        "textarea-context": chatBarContextCheckbox
+        "textarea-context": ChatBarContextCheckbox
     },
 
     patches: [
@@ -418,5 +418,5 @@ export default definePlugin({
         FluxDispatcher.dispatch({ type: "TYPING_START_LOCAL", channelId });
     },
 
-    renderChatBarButton: silentTypingChatToggle,
+    renderChatBarButton: SilentTypingChatToggle,
 });
