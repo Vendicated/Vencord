3097a0505db4c9f5a597d2f5c39df8ace5e4aa18
Vendicated <vendicated@riseup.net>
2025-08-06 16:40:10 +0200

    better find

--- src/plugins/vencordToolbox/index.tsx
+++ src/plugins/vencordToolbox/index.tsx
@@ -140,7 +140,7 @@ export default definePlugin({
 
     patches: [
         {
-            find: "=!0,toolbar:",
+            find: ".controlButtonWrapper,",
             replacement: {
                 match: /(?<=function (\i).{0,100}\()\i.Fragment,(?=.+?toolbar:\1\(\))/,
                 replace: "$self.ToolboxFragmentWrapper,"
