15ee16bf7baa3b3cde90246e6e9a09e7ec1bfccb
sadan4 <contact@swezy.dev>
2025-08-07 15:04:59 -0400

    SpotifyShareCommands: correctly handle local tracks (#3592)

--- src/plugins/spotifyShareCommands/index.ts
+++ src/plugins/spotifyShareCommands/index.ts
@@ -46,7 +46,7 @@ interface Artist {
 }
 
 interface Track {
-    id: string;
+    id: string | null;
     album: Album;
     artists: Artist[];
     duration: number;
@@ -68,6 +68,13 @@ function makeCommand(name: string, formatUrl: (track: Track) => string): Command
             if (!track) {
                 return sendBotMessage(channel.id, {
                     content: "You're not listening to any music."
+                });
+            }
+
+            // local tracks have an id of null
+            if (track.id == null) {
+                return sendBotMessage(channel.id, {
+                    content: "Failed to find the track on spotify."
                 });
             }
 
