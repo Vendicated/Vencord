220c32f6e98cd0aff3a7894db89b0238e4a1c6b6
Suffocate <70031311+lolsuffocate@users.noreply.github.com>
2025-08-08 00:52:42 +0100

    Use webm for readme preview and fix after update

--- src/plugins/reorderAttachments/README.md
+++ src/plugins/reorderAttachments/README.md
@@ -2,4 +2,4 @@
 
 Allows you to reorder attachments before sending them.
 
-![Preview](https://github.com/user-attachments/assets/ef5eacb3-f0ba-4982-bdd7-ff87f74a6421)
+[preview](https://github.com/user-attachments/assets/64ff388f-1d8a-4f70-9229-52074047343f)
--- src/plugins/reorderAttachments/index.tsx
+++ src/plugins/reorderAttachments/index.tsx
@@ -63,12 +63,20 @@ const DraggableItem = ({ uploadItem, index, moveItem, children }) => {
     );
 };
 
-const DraggableList = ({ attachments, channelId, draftType, keyboardModeEnabled }) => {
+const DraggableList = ({ channelId, draftType, keyboardModeEnabled, size, attachments, ignoredFilename }) => {
     const forceUpdate = useForceUpdater();
 
+    // discord filters the attachments before rendering them, but we need the original unfiltered attachments
+    // to edit the upload order so we remove the ignored files here while retaining the array reference
+    for (let i = attachments.length - 1; i >= 0; i--) {
+        if (attachments[i].filename === ignoredFilename) {
+            attachments.splice(i, 1);
+        }
+    }
+
     const moveItem = (from, to) => {
-        const [movedItem] = attachments.splice(from, 1);
-        attachments.splice(to, 0, movedItem);
+        if (from === to || from < 0 || to < 0 || from >= attachments.length || to >= attachments.length) return;
+        attachments.splice(to, 0, ...attachments.splice(from, 1));
         forceUpdate();
     };
 
@@ -85,6 +93,7 @@ const DraggableList = ({ attachments, channelId, draftType, keyboardModeEnabled 
                 draftType={draftType}
                 keyboardModeEnabled={keyboardModeEnabled}
                 clip={uploadItem.clip}
+                size={size}
             />
         </DraggableItem>
     ));
@@ -99,13 +108,13 @@ export default definePlugin({
             find: ')("attachments",',
             replacement: [
                 {
-                    match: /:(\i).map\(\i=>.*?(channelId:\i),(draftType:.*?),(keyboardModeEnabled:\i),.*?\.id\)\)/,
-                    replace: ":$self.DraggableList({attachments:$1,$2,$3,$4})"
+                    match: /:(\i).map\(\i=>.*?(channelId:\i,.*?\i\.\i\.MEDIUM)},\i\.id\)\)(?<=\1=(\i).filter\(\i=>\i.filename!==(\i)\).{0,500})/,
+                    replace: ":$self.DraggableList({$2,attachments:$3,ignoredFilename:$4})"
                 }
             ]
         },
         { // make the img in AttachmentItem not draggable so it doesn't try to add it as a new attachment
-            find: '["image/jpeg",',
+            find: '"video/quicktime","video/mp4"];',
             replacement: [
                 {
                     match: /"img",{src:\i,/,
