462c8e49535f7bc29d21f76d39c34c11fdaff30f
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2025-08-15 17:34:38 -0300

    Revert bundler change

--- src/plugins/superReactionTweaks/index.ts
+++ src/plugins/superReactionTweaks/index.ts
@@ -42,8 +42,8 @@ export default definePlugin({
         {
             find: ",BURST_REACTION_EFFECT_PLAY",
             replacement: {
-                match: /(?<=(\i)=\i=>{.+?)(\i\(\i,\i\))>=\i(?=\).+BURST_REACTION_EFFECT_PLAY:\1)/,
-                replace: "!$self.shouldPlayBurstReaction($2)"
+                match: /(?<=BURST_REACTION_EFFECT_PLAY:\i=>{.+?)(\i\(\i,\i\))>=\i(?=\))/,
+                replace: "!$self.shouldPlayBurstReaction($1)"
             }
         },
         {
