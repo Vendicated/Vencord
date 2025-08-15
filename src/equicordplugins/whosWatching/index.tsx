563d6f3f07dde14f98776eb1976112a2e3430799
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-08-15 14:02:29 -0400

    WhosWatching: Fix Patch

--- src/equicordplugins/whosWatching/index.tsx
+++ src/equicordplugins/whosWatching/index.tsx
@@ -84,7 +84,7 @@ export default definePlugin({
         {
             find: "this.renderEmbeddedActivity()",
             replacement: {
-                match: /"div"(?=.{0,100}this.renderActions)/,
+                match: /"div"(?=.{0,50}this.renderActions)/,
                 replace: "$self.WrapperComponent"
             }
         }
