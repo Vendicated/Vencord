a4a82c7fb1f994438fb7a0cbc03e3f085d17c83c
sumfall <24795287+sumfall@users.noreply.github.com>
2025-08-28 03:32:34 -0400

    FirstMessage v1

--- src/plugins/firstMessage/index.ts
+++ src/plugins/firstMessage/index.ts
@@ -4,11 +4,23 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { ApplicationCommandInputType } from "@api/Commands";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { NavigationRouter } from "@webpack/common";
 
 export default definePlugin({
     name: "FirstMessage",
-    description: "Quickly find the first message in a channel with /firstmessage",
+    description: "Quickly find the first message in a channel with /firstmessage!",
     authors: [Devs.sumfall],
+    commands: [
+        {
+            name: "firstmessage",
+            description: "Jumps to the first message in this channel",
+            inputType: ApplicationCommandInputType.BUILT_IN,
+            execute: (options, { channel }) => {
+                NavigationRouter.transitionTo(`/channels/${channel.guild_id ?? "@me"}/${channel.id}/${channel.id}`);
+            },
+        },
+    ],
 });
