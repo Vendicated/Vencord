31125cccfa3f4dbf69fc8bf0d5cba9fac51e7119
Aqualunem <furrycality@gmail.com>
2025-08-31 22:11:16 -0500

    Added my name and Discord ID

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -598,6 +598,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Cootshk",
         id: 921605971577548820n
     },
+    Aqualunem: {
+        name: "Aqualunem",
+        id: 440145069262241813n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
