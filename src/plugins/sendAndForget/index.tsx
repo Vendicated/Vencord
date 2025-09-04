a7d6e69cfa2bd16c1589991495dab85cd4bce567
sadan <117494111+sadan4@users.noreply.github.com>
2025-09-03 22:01:12 -0400

    add shift as override

--- src/plugins/sendAndForget/index.tsx
+++ src/plugins/sendAndForget/index.tsx
@@ -6,19 +6,44 @@
 
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { KeyboardEvent } from "react";
+
+let ignore = false;
 
 export default definePlugin({
     name: "SendAndForget",
-    description: "After forwarding a single message, don't jump to it",
+    description: "After forwarding a single message, don't jump to it. Hold shift to ignore this behavior",
     authors: [Devs.Sqaaakoi, Devs.sadan],
 
     patches: [
         {
             find: ".ToastType.FORWARD",
+            replacement: [
+                {
+                    match: /(?<=transitionToDestination:)(1===\i\.length)(?=,|\})/,
+                    replace: "$self.shouldTransition($1)"
+                }
+            ]
+        },
+        {
+            // the event for clicking the final forward button is dispatched here
+            find: "#{intl::MESSAGE_FORWARD_MESSAGE_PLACEHOLDER}",
             replacement: {
-                match: /(?<=transitionToDestination:)1===\i\.length/,
-                replace: "false"
+                match: /(useCallback\(\()(\)=>\{)(\i\.\i\.clearDraft)/,
+                replace: (_, beforeParen, beforeBody, body) => `${beforeParen}vencordArg1${beforeBody}$self.setShift(vencordArg1);${body}`
             }
         }
-    ]
+    ],
+
+    shouldTransition(origCond: boolean): boolean {
+        return ignore ? origCond : false;
+    },
+
+    setShift(event: KeyboardEvent | undefined) {
+        if (event?.shiftKey) {
+            ignore = true;
+        } else {
+            ignore = false;
+        }
+    }
 });
