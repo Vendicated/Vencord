5fc0b6d436338fa85ce03632afa672fad1d5f32b
Vendicated <vendicated@riseup.net>
2025-09-09 01:40:56 +0200

    this is wrong

--- src/api/MessageEvents.ts
+++ src/api/MessageEvents.ts
@@ -45,7 +45,6 @@ export interface MessageOptions {
     content: string;
     channel: Channel;
     type?: any;
-    attachmentsToUpload: Upload[];
     openWarningPopout: (props: any) => any;
 }
 
