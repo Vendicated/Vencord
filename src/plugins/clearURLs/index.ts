e550f6491c62e2ec1439cccee8079abd7f76bc1f
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-09-07 22:07:48 -0400

    Change URL

--- src/plugins/clearURLs/index.ts
+++ src/plugins/clearURLs/index.ts
@@ -50,7 +50,7 @@ export default definePlugin({
     },
 
     async createRules() {
-        const url = "https://raw.githubusercontent.com/ClearURLs/Rules/refs/heads/master/data.min.json";
+        const url = "https://raw.githubusercontent.com/ClearURLs/Rules/master/data.min.json";
 
         const response = await fetch(url);
         const data = await response.json();
