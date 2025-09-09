e8e1974df8c662634d3fb92e32221c3e77bc24e0
Gabriel <profissional.gabrielm@gmail.com>
2025-09-09 18:32:26 -0300

    feat: show mic muted icon

--- src/plugins/userVoiceShow/components.tsx
+++ src/plugins/userVoiceShow/components.tsx
@@ -73,6 +73,27 @@ function LockedSpeakerIcon(props: IconProps) {
     );
 }
 
+function MuteIcon(props: IconProps) {
+    props.size ??= 16;
+
+    return (
+        <div
+            {...props}
+            className={classes(cl("speaker"), props.className)}
+        >
+            <svg
+                className={props.iconClassName}
+                width={props.size}
+                height={props.size}
+                viewBox="0 0 24 24"
+                fill="currentColor"
+            >
+                <path d="m2.7 22.7 20-20a1 1 0 0 0-1.4-1.4l-20 20a1 1 0 1 0 1.4 1.4ZM10.8 17.32c-.21.21-.1.58.2.62V20H9a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-2v-2.06A8 8 0 0 0 20 10a1 1 0 0 0-2 0c0 1.45-.52 2.79-1.38 3.83l-.02.02A5.99 5.99 0 0 1 12.32 16a.52.52 0 0 0-.34.15l-1.18 1.18ZM15.36 4.52c.15-.15.19-.38.08-.56A4 4 0 0 0 8 6v4c0 .3.03.58.1.86.07.34.49.43.74.18l6.52-6.52ZM5.06 13.98c.16.28.53.31.75.09l.75-.75c.16-.16.19-.4.08-.61A5.97 5.97 0 0 1 6 10a1 1 0 0 0-2 0c0 1.45.39 2.81 1.06 3.98Z" />
+            </svg>
+        </div>
+    );
+}
+
 function DeafIcon(props: IconProps) {
     props.size ??= 16;
 
@@ -160,6 +181,14 @@ const clickTimers = {} as Record<string, any>;
 export const VoiceChannelIndicator = ErrorBoundary.wrap(({ userId, isProfile, isActionButton, shouldHighlight }: VoiceChannelIndicatorProps) => {
     const channelId = useStateFromStores([VoiceStateStore], () => VoiceStateStore.getVoiceStateForUser(userId)?.channelId as string | undefined);
 
+    const { isMute, isDeaf } = useStateFromStores([VoiceStateStore], () => {
+        const voiceState = VoiceStateStore.getVoiceStateForUser(userId);
+        return {
+            isMute: voiceState?.mute || voiceState?.selfMute || false,
+            isDeaf: voiceState?.deaf || voiceState?.selfDeaf || false
+        };
+    });
+
     const channel = channelId == null ? undefined : ChannelStore.getChannel(channelId);
     if (channel == null) return null;
 
@@ -167,11 +196,6 @@ export const VoiceChannelIndicator = ErrorBoundary.wrap(({ userId, isProfile, is
     if (!isDM && !PermissionStore.can(PermissionsBits.VIEW_CHANNEL, channel) && !Vencord.Plugins.isPluginEnabled("ShowHiddenChannels")) return null;
 
     const isLocked = !isDM && (!PermissionStore.can(PermissionsBits.VIEW_CHANNEL, channel) || !PermissionStore.can(PermissionsBits.CONNECT, channel));
-
-    const isDeaf = useStateFromStores([VoiceStateStore], () => {
-        const voiceState = VoiceStateStore.getVoiceStateForUser(userId);
-        return voiceState?.deaf || voiceState?.selfDeaf || false;
-    });
 
     function onClick(e: React.MouseEvent) {
         e.preventDefault();
@@ -215,9 +239,11 @@ export const VoiceChannelIndicator = ErrorBoundary.wrap(({ userId, isProfile, is
 
                 return isDeaf
                     ? <DeafIcon {...iconProps} />
-                    : isLocked
-                        ? <LockedSpeakerIcon {...iconProps} />
-                        : <SpeakerIcon {...iconProps} />;
+                    : isMute
+                        ? <MuteIcon {...iconProps} />
+                        : isLocked
+                            ? <LockedSpeakerIcon {...iconProps} />
+                            : <SpeakerIcon {...iconProps} />;
             }}
         </Tooltip>
     );
