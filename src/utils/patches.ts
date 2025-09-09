b1caeb3a330368cd2cf27b738fc7133ef5901a80
sadan <117494111+sadan4@users.noreply.github.com>
2025-09-09 10:23:17 -0400

    fix
    Co-authored-by: V <vendicated@riseup.net>

--- src/utils/patches.ts
+++ src/utils/patches.ts
@@ -40,7 +40,11 @@ export function canonicalizeMatch<T extends RegExp | string>(match: T): T {
         return partialCanon as T;
     }
 
-    const canonSource = partialCanon.replaceAll(/(\\*)\\i/g, (_, leadingEscapes) => leadingEscapes.length % 2 === 0 ? `${leadingEscapes}${String.raw`(?:[A-Za-z_$][\w$]*)`}` : _);
+    const canonSource = partialCanon.replaceAll(/(\\*)\\i/g, (match, leadingEscapes) =>
+        leadingEscapes.length % 2 === 0
+            ? `${leadingEscapes}${String.raw`(?:[A-Za-z_$][\w$]*)`}`
+            : match.slice(1)
+    );
     const canonRegex = new RegExp(canonSource, match.flags);
     canonRegex.toString = match.toString.bind(match);
 
