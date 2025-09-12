21230c42cce5750f5e6941127b191e3cfc56ea3d
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-09-11 22:11:52 -0400

    BetterCommands: Refresh CMD

--- src/equicordplugins/betterCommands/index.ts
+++ src/equicordplugins/betterCommands/index.ts
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { ApplicationCommandInputType, ApplicationCommandOptionType, findOption, sendBotMessage } from "@api/Commands";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+import { FluxDispatcher, UserStore } from "@webpack/common";
 
 const settings = definePluginSettings({
     autoFillArguments: {
@@ -15,6 +17,13 @@ const settings = definePluginSettings({
         default: true,
     }
 });
+
+function fetchIndex(target: object) {
+    FluxDispatcher.dispatch({
+        type: "APPLICATION_COMMAND_INDEX_FETCH_REQUEST",
+        target
+    });
+}
 
 export default definePlugin({
     name: "BetterCommands",
@@ -32,5 +41,52 @@ export default definePlugin({
                 },
             ]
         }
+    ],
+    commands: [
+        {
+            name: "refresh",
+            description: "Refreshes Discord application commands locally",
+            options: [
+                {
+                    name: "user",
+                    description: "specific user to try and refresh",
+                    type: ApplicationCommandOptionType.USER,
+                }
+            ],
+            inputType: ApplicationCommandInputType.BOT,
+            execute: async (opts, ctx) => {
+                try {
+                    const channelId = ctx.channel.id;
+                    const guildId = ctx.guild?.id;
+                    const user = findOption(opts, "user") as string;
+
+                    sendBotMessage(ctx.channel.id, {
+                        content: "Refreshing application commands...",
+                    });
+
+                    fetchIndex({ type: "channel", channelId });
+
+                    if (guildId) fetchIndex({ type: "guild", guildId });
+
+                    if (user) {
+                        const target = await UserStore.getUser(user);
+                        fetchIndex(
+                            target.bot
+                                ? { type: "application", applicationId: target.id }
+                                : { type: "user" }
+                        );
+                    }
+
+                    sendBotMessage(ctx.channel.id, {
+                        content: "Commands refreshed successfully!",
+                    });
+                } catch (err) {
+                    console.error("[Refresh Command] Error refreshing commands:", err);
+                    sendBotMessage(ctx.channel.id, {
+                        content: "Failed to refresh commands. Check the console for details.",
+                    });
+                }
+            },
+        },
     ]
 });
