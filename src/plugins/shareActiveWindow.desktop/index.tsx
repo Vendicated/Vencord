82c5193b0d4bc726c00148af07ae405b889e4d03
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2025-09-25 12:27:38 +0900

    Rewrite "Share Active Window" checkbox using plugin's "contextMenus" property

--- src/plugins/shareActiveWindow.desktop/index.tsx
+++ src/plugins/shareActiveWindow.desktop/index.tsx
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType, PluginNative } from "@utils/types";
@@ -67,6 +68,10 @@ function initActiveWindowLoop(): void {
         clearInterval(activeWindowInterval);
     }
 
+    if (!isSharingWindow) {
+        return;
+    }
+
     activeWindowInterval = setInterval(async () => {
         if (!isSharingWindow) {
             return;
@@ -100,6 +105,28 @@ function initActiveWindowLoop(): void {
         });
     }, settings.store.checkInterval);
 }
+
+const manageStreamsContextMenuPatch: NavContextMenuPatchCallback = (children): void => {
+    const { isEnabled } = settings.use(["isEnabled"]);
+
+    if (!isSharingWindow) {
+        return;
+    }
+
+    const mainGroup = findGroupChildrenByChildId("stream-settings-audio-enable", children);
+    if (!mainGroup) {
+        return;
+    }
+
+    mainGroup.push(
+        <Menu.MenuCheckboxItem
+            id="vc-saw-share-active-window"
+            label="Share Active Window"
+            checked={isEnabled}
+            action={() => settings.store.isEnabled = !isEnabled}
+        />
+    );
+};
 
 const settings = definePluginSettings({
     isEnabled: {
@@ -154,27 +181,10 @@ export default definePlugin({
                 replace: "let f=function(e){$1;};window.vencord_plugins_shareActiveWindow_setGoLiveSource=f;return f(e);",
             },
         },
-        {
-            find: /id:\s*"stop-streaming",\s*label:/,
-            replacement: {
-                match: /(?<=id:\s*"stop-streaming"[\s\S]*?return)[\s\S]*(?=}})/,
-                replace: (match: string, ..._groups: string[]): string => {
-                    return match.replaceAll(/(?<=children:\s*)(\[[^\[\]]*\])/g, "($&).concat([$self.IsEnabledButton(arguments[1])])");
-                },
-            },
-        },
     ],
 
-    IsEnabledButton(_node: any) {
-        if (isSharingWindow) {
-            const { isEnabled } = settings.use(["isEnabled"]);
-            return <Menu.MenuCheckboxItem
-                id="vc-saw-share-active-window"
-                label="Share Active Window"
-                checked={isEnabled}
-                action={() => settings.store.isEnabled = !isEnabled}
-            />;
-        }
+    contextMenus: {
+        "manage-streams": manageStreamsContextMenuPatch,
     },
 
     MEDIA_ENGINE_SET_GO_LIVE_SOURCE(event: { settings: SourceSettings; }): void {
@@ -185,6 +195,11 @@ export default definePlugin({
 
     STREAM_START(event: StreamStartEvent): void {
         isSharingWindow = event.sourceId.startsWith("window:");
+
+        if (!settings.store.isEnabled) {
+            return;
+        }
+
         if (isSharingWindow) {
             initActiveWindowLoop();
         }
@@ -305,6 +320,98 @@ export default definePlugin({
         // renderScreenshare
 
         // getStreamerActiveStreamMetadata
+
+        // section: h.jXE.CONTEXT_MENU,
+        // children: (0,
+        // r.jsxs)(o.v2r, {
+        //     onSelect: E,
+        //     contextMenuAPIArguments: typeof arguments !== 'undefined' ? arguments : [],
+        //     navId: "manage-streams",
+        //     onClose: p,
+        //     onInteraction: O,
+        //     "aria-label": null != v ? m.intl.string(m.t.S5anIS) : m.intl.string(m.t.fjBNo6),
+        //     children: [(0,
+        //     r.jsx)(o.kSQ, {
+        //         children: I.map(e => {
+        //             let {stream: t, username: n} = e;
+        //             return (0,
+        //             r.jsx)(o.sNh, {
+        //                 id: t.ownerId,
+        //                 label: m.intl.formatToPlainString(m.t["7rkg+/"], {
+        //                     username: n
+        //                 }),
+        //                 icon: o.g5r,
+        //                 action: () => (0,
+        //                 d.Z)(t)
+        //             }, "manage-stream-menu".concat(t.ownerId))
+        //         }
+        //         )
+        //     }), l ? null : S, l ? null : (0,
+
+        // {
+        //     "type": "TRACK",
+        //     "event": "call_button_clicked",
+        //     "properties": {
+        //         "button_name": "Stream",
+        //         "toggled_active": false,
+        //         "guild_id": "1399442154123821097",
+        //         "channel_id": "1399442157093523479",
+        //         "channel_type": 2,
+        //         "location": "voice control tray",
+        //         "client_performance_cpu": 3.739011322950423,
+        //         "client_performance_memory": 2468160,
+        //         "cpu_core_count": 24,
+        //         "accessibility_features": 524416,
+        //         "rendered_locale": "en-US",
+        //         "uptime_app": 741,
+        //         "uptime_process_renderer": 742,
+        //         "launch_signature": "691ee267-b575-444f-9766-521dfc6db56e",
+        //         "client_rtc_state": "RTC_CONNECTED",
+        //         "client_app_state": "focused",
+        //         "client_viewport_width": 2176,
+        //         "client_viewport_height": 1288
+        //     },
+        //     "flush": false
+        // }
+
+        // {
+        //     "type": "TRACK",
+        //     "event": "app_crashed",
+        //     "properties": {
+        //         "path": "/channels/1399442154123821097/1399442157093523479",
+        //         "extra": {
+        //             "componentStack": "\n    at E (file:///WebpackModule544384:2:795)\n    at a (https://discord.com/assets/web.549dbf99b720d7e8.js:12:11631366)\n    at div (<anonymous>)\n    at p (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3194045)\n    at M (https://discord.com/assets/web.549dbf99b720d7e8.js:29:106235)\n    at div (<anonymous>)\n    at _ (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3332236)\n    at div (<anonymous>)\n    at D (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3079475)\n    at E (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14112268)\n    at s (https://discord.com/assets/web.549dbf99b720d7e8.js:12:13213882)\n    at N (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3185803)\n    at y (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3192095)\n    at div (<anonymous>)\n    at s (https://discord.com/assets/web.549dbf99b720d7e8.js:12:11630892)\n    at eE (https://discord.com/assets/8e6db1cde8aff846.js:1:556968)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at f (https://discord.com/assets/web.549dbf99b720d7e8.js:12:2755992)\n    at u (https://discord.com/assets/web.549dbf99b720d7e8.js:12:4203296)\n    at eT (https://discord.com/assets/8e6db1cde8aff846.js:1:562694)\n    at div (<anonymous>)\n    at b (https://discord.com/assets/web.549dbf99b720d7e8.js:12:5560026)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at M (https://discord.com/assets/web.549dbf99b720d7e8.js:29:106235)\n    at f (https://discord.com/assets/8e6db1cde8aff846.js:1:596076)\n    at div (<anonymous>)\n    at m (https://discord.com/assets/8e6db1cde8aff846.js:1:596928)\n    at u (https://discord.com/assets/web.549dbf99b720d7e8.js:12:11629787)\n    at div (<anonymous>)\n    at o (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3332923)\n    at d (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3334452)\n    at div (<anonymous>)\n    at ez (https://discord.com/assets/8e6db1cde8aff846.js:1:51883)\n    at p (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14111680)\n    at VoiceChannelEffectsCallLayerProvider (<anonymous>)\n    at p (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14111680)\n    at ChannelCallChatLayerProvider (<anonymous>)\n    at f (https://discord.com/assets/web.549dbf99b720d7e8.js:12:2755992)\n    at u (https://discord.com/assets/web.549dbf99b720d7e8.js:12:4203296)\n    at eW (https://discord.com/assets/8e6db1cde8aff846.js:1:61725)\n    at div (<anonymous>)\n    at tl (https://discord.com/assets/9983a7dce1b6b4cb.js:1:37125)\n    at div (<anonymous>)\n    at https://discord.com/assets/web.549dbf99b720d7e8.js:12:2945479\n    at https://discord.com/assets/9983a7dce1b6b4cb.js:1:44330\n    at eH (https://discord.com/assets/2270486918d7b170.js:1:13445)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1287880)\n    at d (https://discord.com/assets/web.549dbf99b720d7e8.js:12:4209641)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1288550)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at e7 (https://discord.com/assets/2270486918d7b170.js:1:21423)\n    at div (<anonymous>)\n    at n (https://discord.com/assets/web.549dbf99b720d7e8.js:1:536386)\n    at D (https://discord.com/assets/2270486918d7b170.js:1:213660)\n    at div (<anonymous>)\n    at _ (https://discord.com/assets/web.549dbf99b720d7e8.js:12:15032600)\n    at k (https://discord.com/assets/2270486918d7b170.js:1:213889)\n    at M (https://discord.com/assets/2270486918d7b170.js:1:214829)\n    at div (<anonymous>)\n    at h (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3273529)\n    at https://discord.com/assets/2270486918d7b170.js:1:197933\n    at Authenticated(<Unknown>) (<anonymous>)\n    at p (https://discord.com/assets/2270486918d7b170.js:1:146127)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1287880)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1288550)\n    at tp (https://discord.com/assets/2270486918d7b170.js:1:50677)\n    at Suspense (<anonymous>)\n    at f (<anonymous>)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1287880)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1288550)\n    at div (<anonymous>)\n    at o (https://discord.com/assets/web.549dbf99b720d7e8.js:12:5375629)\n    at p (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14111680)\n    at VerificationLayerProvider (<anonymous>)\n    at m (https://discord.com/assets/web.549dbf99b720d7e8.js:12:5378164)\n    at d (https://discord.com/assets/web.549dbf99b720d7e8.js:12:5376157)\n    at div (<anonymous>)\n    at div (<anonymous>)\n    at p (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14111680)\n    at AppLayerProvider (<anonymous>)\n    at b (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3899110)\n    at M (https://discord.com/assets/web.549dbf99b720d7e8.js:29:106235)\n    at N (file:///WebpackModule628123:2:2292)\n    at d (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3571588)\n    at h (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3918767)\n    at b (https://discord.com/assets/web.549dbf99b720d7e8.js:12:10499465)\n    at s (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3333837)\n    at c (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14113485)\n    at R (https://discord.com/assets/web.549dbf99b720d7e8.js:12:10495256)\n    at J (https://discord.com/assets/web.549dbf99b720d7e8.js:12:5549414)\n    at ei (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3519063)\n    at eo (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3523833)\n    at f (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14212393)\n    at https://discord.com/assets/web.549dbf99b720d7e8.js:12:1081513\n    at m (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1075959)\n    at f (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3021930)\n    at App (<anonymous>)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1285217)\n    at t (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1264002)\n    at c (https://discord.com/assets/web.549dbf99b720d7e8.js:12:9985820)\n    at b (https://discord.com/assets/web.549dbf99b720d7e8.js:12:10499465)\n    at s (https://discord.com/assets/web.549dbf99b720d7e8.js:12:3333837)\n    at c (https://discord.com/assets/web.549dbf99b720d7e8.js:12:14113485)\n    at R (https://discord.com/assets/web.549dbf99b720d7e8.js:12:10495256)"
+        //         },
+        //         "error_message": "Minified React error #300; visit https://react.dev/errors/300 for the full message or use the non-minified dev environment for full errors and additional helpful warnings.",
+        //         "error_stack": "Error: Minified React error #300; visit https://react.dev/errors/300 for the full message or use the non-minified dev environment for full errors and additional helpful warnings.\n    at iZ (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1131399)\n    at iB (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1131288)\n    at os (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1152183)\n    at oT (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1162898)\n    at lZ (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1204116)\n    at lU (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1203201)\n    at lk (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1203043)\n    at lS (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1200128)\n    at ca (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1209817)\n    at ct (https://discord.com/assets/web.549dbf99b720d7e8.js:12:1208603)",
+        //         "uses_client_mods": false,
+        //         "error_level": "fatal",
+        //         "client_performance_cpu": 3.739011322950423,
+        //         "client_performance_memory": 2468160,
+        //         "cpu_core_count": 24,
+        //         "accessibility_features": 524416,
+        //         "rendered_locale": "en-US",
+        //         "uptime_app": 741,
+        //         "uptime_process_renderer": 742,
+        //         "launch_signature": "691ee267-b575-444f-9766-521dfc6db56e",
+        //         "client_rtc_state": "RTC_CONNECTED",
+        //         "client_app_state": "focused",
+        //         "client_viewport_width": 2176,
+        //         "client_viewport_height": 1288
+        //     },
+        //     "flush": false
+        // }
+
+
+        // const useMessageMenu = findByCodeLazy(".MESSAGE,commandTargetId:");
+
+        // interface CopyIdMenuItemProps {
+        //     id: string;
+        //     label: string;
+        // }
+
+        // let CopyIdMenuItem: (props: CopyIdMenuItemProps) => React.ReactElement | null = NoopComponent;
+        // waitFor(filters.componentByCode('"devmode-copy-id-".concat'), m => CopyIdMenuItem = m);
     },
 
     stop() {
