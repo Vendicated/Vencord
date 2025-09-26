85a1f04c6ad146555a8546c1a87884875a1cfe0f
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2025-09-27 01:07:29 +0900

    Move interfaces to a dedicated "types.ts" file

---
+++ src/plugins/shareActiveWindow.desktop/types.ts
@@ -0,0 +1,76 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export interface CandidateGame {
+    readonly cmdLine: string;
+    readonly elevated: boolean;
+    readonly exeName: string;
+    readonly exePath: string;
+    readonly fullscreenType: number;
+    readonly hidden: boolean;
+    readonly isLauncher: boolean;
+    readonly name: string;
+    readonly pid: number;
+    readonly pidPath: number[];
+    readonly sandboxed: boolean;
+    readonly windowHandle: string;
+}
+
+export interface StreamSettings {
+    preset?: number;
+    fps?: number;
+    resolution?: number;
+    soundshareEnabled?: boolean;
+    previewDisabled?: boolean;
+    audioSourceId?: string;
+    goLiveModalDurationMs?: number;
+    analyticsLocations?: string[];
+    sourceId?: string;
+}
+
+export interface StreamUpdateSettingsEvent {
+    readonly frameRate: number;
+    readonly preset: number;
+    readonly resolution: number;
+    readonly soundshareEnabled: boolean;
+}
+
+export interface StreamStartEvent {
+    readonly analyticsLocations: string[];
+    readonly appContext: string;
+    readonly audioSourceId: string;
+    readonly channelId: string;
+    readonly goLiveModalDurationMs: number;
+    readonly guildId: string;
+    readonly previewDisabled: boolean;
+    readonly sound: boolean;
+    readonly sourceIcon: string;
+    readonly sourceId: string;
+    readonly sourceName: string;
+    readonly streamType: string;
+}
+
+export interface MediaEngineSetGoLiveSourceEvent {
+    readonly settings: {
+        readonly context: string;
+        readonly desktopSettings: {
+            readonly sourceId: string;
+            readonly sound: boolean;
+        };
+        readonly qualityOptions: {
+            readonly frameRate: number;
+            readonly preset: number;
+            readonly resolution: number;
+        };
+    };
+}
+
+export interface WindowDescriptor {
+    readonly id: string;
+    readonly url?: string;
+    readonly icon: string;
+    readonly name: string;
+}
