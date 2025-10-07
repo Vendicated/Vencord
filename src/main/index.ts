99dda626d1619a398223448aec199e68e48fd70d
vMohammad <62218284+vMohammad24@users.noreply.github.com>
2025-10-07 22:21:46 +0300

    bypass cors for everything (#445)

--- src/main/index.ts
+++ src/main/index.ts
@@ -96,6 +96,19 @@ if (!IS_VANILLA && !IS_EXTENSION) {
             }
         });
 
+        protocol.handle("https", async req => {
+            const res = await net.fetch(req, { bypassCustomProtocolHandlers: true });
+            const headers = new Headers(res.headers);
+            headers.set("Access-Control-Allow-Origin", "*");
+            headers.set("Access-Control-Allow-Methods", "*");
+            headers.set("Access-Control-Allow-Headers", "*");
+
+            return new Response(res.body, {
+                ...res,
+                headers,
+            });
+        });
+
         try {
             if (RendererSettings.store.enableReactDevtools)
                 installExt("fmkadmapgofadopljbjfkapdkoienihi")
@@ -106,6 +119,7 @@ if (!IS_VANILLA && !IS_EXTENSION) {
 
         initCsp();
     });
+
 }
 
 if (IS_DISCORD_DESKTOP) {
