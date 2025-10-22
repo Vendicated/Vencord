943bc524066b2065e621908d36c6906bb0f28a83
Fafa <87046111+Faf4a@users.noreply.github.com>
2025-10-22 03:13:11 +0200

    Add user plugin filter (#3734)

--- src/components/settings/tabs/plugins/index.tsx
+++ src/components/settings/tabs/plugins/index.tsx
@@ -35,7 +35,7 @@ import { findByPropsLazy } from "@webpack";
 import { Alerts, Button, Card, lodash, Parser, React, Select, TextInput, Tooltip, useMemo, useState } from "@webpack/common";
 import { JSX } from "react";
 
-import Plugins, { ExcludedPlugins } from "~plugins";
+import Plugins, { ExcludedPlugins, PluginMeta } from "~plugins";
 
 import { PluginCard } from "./PluginCard";
 
@@ -72,7 +72,8 @@ const enum SearchStatus {
     ALL,
     ENABLED,
     DISABLED,
-    NEW
+    NEW,
+    USER_PLUGINS
 }
 
 function ExcludedPluginsList({ search }: { search: string; }) {
@@ -171,6 +172,9 @@ function PluginSettings() {
             case SearchStatus.NEW:
                 if (!newPlugins?.includes(plugin.name)) return false;
                 break;
+            case SearchStatus.USER_PLUGINS:
+                if (!PluginMeta[plugin.name]?.userPlugin) return false;
+                break;
         }
 
         if (!search.length) return true;
@@ -201,6 +205,7 @@ function PluginSettings() {
 
     const plugins = [] as JSX.Element[];
     const requiredPlugins = [] as JSX.Element[];
+    const userPlugins = !IS_STANDALONE && Object.values(PluginMeta).some(p => p.userPlugin);
 
     const showApi = searchValue.value.includes("API");
     for (const p of sortedPlugins) {
@@ -260,6 +265,7 @@ function PluginSettings() {
                         <Select
                             options={[
                                 { label: "Show All", value: SearchStatus.ALL, default: true },
+                                ...(userPlugins ? [{ label: "Show UserPlugins", value: SearchStatus.USER_PLUGINS }] : []),
                                 { label: "Show Enabled", value: SearchStatus.ENABLED },
                                 { label: "Show Disabled", value: SearchStatus.DISABLED },
                                 { label: "Show New", value: SearchStatus.NEW }
