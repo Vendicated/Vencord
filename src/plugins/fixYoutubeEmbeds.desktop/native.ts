140a27f21c8479255937b61730c1713a89f37353
Emily Mabrey <emilymabrey93@gmail.com>
2025-10-24 16:00:06 -0400

    Fix Youtube plugins
    Fix FixYoutubeEmbeds plugin to prevent "Error 15" issue
    Add support for FixYoutubeEmbeds to workaround bot-detection blocks
    Add youtube-nocookie.com domain to main code's CSP generation
    Modify YoutubeAdblock plugin to inject to new nocookie domain
    
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- src/plugins/fixYoutubeEmbeds.desktop/native.ts
+++ src/plugins/fixYoutubeEmbeds.desktop/native.ts
@@ -5,23 +5,61 @@
  */
 
 import { RendererSettings } from "@main/settings";
-import { app } from "electron";
+import { Logger } from "@utils/Logger";
+import { app, BrowserWindow, WebFrameMain } from "electron";
 
-app.on("browser-window-created", (_, win) => {
-    win.webContents.on("frame-created", (_, { frame }) => {
-        frame?.once("dom-ready", () => {
-            if (frame.url.startsWith("https://www.youtube.com/")) {
-                const settings = RendererSettings.store.plugins?.FixYoutubeEmbeds;
-                if (!settings?.enabled) return;
+const fixYoutubeEmbedsLogger = new Logger("FixYoutubeEmbeds");
 
-                frame.executeJavaScript(`
-                new MutationObserver(() => {
-                    if(
-                        document.querySelector('div.ytp-error-content-wrap-subreason a[href*="www.youtube.com/watch?v="]')
-                    ) location.reload()
-                }).observe(document.body, { childList: true, subtree:true });
-                `);
+app.on("browser-window-created", (_, window) => {
+    window.webContents.on("frame-created", (_, { frame: frame }) => {
+        const pluginSettings = RendererSettings.store.plugins;
+        const pluginDisabled = (pluginSettings === undefined || pluginSettings.FixYoutubeEmbeds === undefined || !pluginSettings.FixYoutubeEmbeds.enabled);
+        const nullValues = (frame == null);
+
+        if (pluginDisabled || nullValues) {
+            if (nullValues) {
+                fixYoutubeEmbedsLogger.error("Unable to acquire non-null window and/or frame from respective Electron creation callbacks");
             }
+            return;
+        }
+
+        overrideIframeSource(window);
+        frame.once("dom-ready", () => {
+            reloadFrameLocation(frame);
         });
     });
 });
+
+function overrideIframeSource(
+    window: BrowserWindow,
+    videoIDPattern: RegExp = /(?:youtube(?:-nocookie)?\.com\/(?:[^/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+) {
+    window.webContents.executeJavaScript(`
+        new MutationObserver(() => {
+            document.querySelectorAll("iframe").forEach(iframe => {
+                if (iframe.src.startsWith("https://www.youtube.com/embed/")) {
+                    const video_id = iframe.src.match(${videoIDPattern})[1];
+                    const params = new URL(iframe.src).search + "&app=mobile";
+                    iframe.src = "https://www.youtube-nocookie.com/embed/" + video_id + params;
+                }
+            });
+        }).observe(document.body, { childList: true, subtree:true });
+    `);
+}
+
+function reloadFrameLocation(frame: WebFrameMain) {
+    if (frame.url.startsWith("https://www.youtube.com/") || frame.url.startsWith("https://www.youtube-nocookie.com/")) {
+        frame.executeJavaScript(`;
+            new MutationObserver(() => {
+                if (document.querySelector('div.ytp-error-content-wrap-subreason a[href*="www.youtube.com/watch?v="]')) {
+                    // Reload if we see the UMG style block
+                    location.reload();
+                }
+                if (document.querySelector('div.ytp-error-content-wrap-reason span')) {
+                    // Attempt to reload if we see the bot "please sign in" error
+                    location.reload();
+                }
+            }).observe(document.body, { childList: true, subtree: true });
+        `);
+    }
+}
