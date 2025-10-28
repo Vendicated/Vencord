4d4eb1196c6f4c6a046102362cd3b41e6ddb80bd
Swan Sarli <swan.sarli@epitech.eu>
2025-10-28 16:21:33 +0100

    fix: modal + settings for groups leave

--- src/plugins/closeDms/index.ts
+++ src/plugins/closeDms/index.ts
@@ -1,27 +1,24 @@
-import definePlugin from "@utils/types";
-import { ChannelStore, SelectedChannelStore } from "@webpack/common";
-import { findByPropsLazy } from "@webpack";
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { Alerts, ChannelStore, SelectedChannelStore, UserStore } from "@webpack/common";
+import { findByPropsLazy } from "@webpack";
 
 const PrivateChannelActions = findByPropsLazy("closePrivateChannel");
-const PrivateChannelUtils = findByPropsLazy("openPrivateChannelConfirmModal");
 
-function closePrivateChannel(channelId: string) {
-    if (PrivateChannelActions?.closePrivateChannel) {
-        PrivateChannelActions.closePrivateChannel(channelId);
+const settings = definePluginSettings({
+    confirmGroupDMLeave: {
+        type: OptionType.BOOLEAN,
+        description: "Show confirmation dialog before leaving group DMs",
+        default: true
     }
-}
-
-function leaveGroupDM(channelId: string) {
-    if (PrivateChannelUtils?.openPrivateChannelConfirmModal) {
-        PrivateChannelUtils.openPrivateChannelConfirmModal(channelId);
-    }
-}
+});
 
 export default definePlugin({
     name: "CloseDMsWithCtrlW",
     description: "Closes the current DM when pressing Ctrl + W",
     authors: [Devs.IamSwan],
+    settings,
 
     start() {
         this.handleKey = (e: KeyboardEvent) => {
@@ -31,10 +28,25 @@ export default definePlugin({
                 const channelId = SelectedChannelStore.getChannelId();
                 const channel = ChannelStore.getChannel(channelId);
 
-                if (channel?.isDM()) {
-                    closePrivateChannel(channelId);
-                } else if (channel?.isGroupDM()) {
-                    leaveGroupDM(channelId);
+                if (channel?.isDM())
+                    PrivateChannelActions.closePrivateChannel(channelId);
+
+                if (channel?.isGroupDM()) {
+                    if (settings.store.confirmGroupDMLeave) {
+                        const groupName = channel.name || "this group";
+
+                        Alerts.show({
+                            title: "Leave Group",
+                            body: `Are you sure you want to leave ${groupName}? You won't be able to rejoin unless you are re-invited.`,
+                            confirmText: "Leave Group",
+                            cancelText: "Cancel",
+                            onConfirm: () => {
+                                PrivateChannelActions.closePrivateChannel(channelId);
+                            }
+                        });
+                    } else {
+                        PrivateChannelActions.closePrivateChannel(channelId);
+                    }
                 }
             }
         };
