9b968df7a87f309812451dbd5b3ac3ad8a6b04ee
Vendicated <vendicated@riseup.net>
2025-11-15 15:15:21 +0100

    nvm

--- browser/Vencord.ts
+++ browser/Vencord.ts
@@ -16,6 +16,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-export * from "../src/Vencord";
-
 import "./VencordNativeStub";
+
+export * from "../src/Vencord";
