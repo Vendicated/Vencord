08396840a6611225c1963637f32bfd9633d32509
Vendicated <vendicated@riseup.net>
2025-11-15 12:20:41 +0100

    fix broken patch

--- src/plugins/_api/badges/index.tsx
+++ src/plugins/_api/badges/index.tsx
@@ -98,8 +98,8 @@ export default definePlugin({
             find: "#{intl::PROFILE_USER_BADGES}",
             replacement: [
                 {
-                    match: /(alt:" ","aria-hidden":!0,src:)(.+?)(?=,)(?<=href:(\i)\.link.+?)/,
-                    replace: (_, rest, originalSrc, badge) => `...${badge}.props,${rest}${badge}.image??(${originalSrc})`
+                    match: /alt:" ","aria-hidden":!0,src:.{0,50}(\i).iconSrc/,
+                    replace: "...$1.props,$&"
                 },
                 {
                     match: /(?<="aria-label":(\i)\.description,.{0,200})children:/,
