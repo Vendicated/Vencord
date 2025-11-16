7aa5ee3f80992863768545c3d93fc2cea0293abf
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-16 14:48:29 -0500

    PetPet: Fix Naming Scheme

--- src/plugins/petpet/index.ts
+++ src/plugins/petpet/index.ts
@@ -17,6 +17,7 @@
 */
 
 import { ApplicationCommandInputType, ApplicationCommandOptionType, findOption, sendBotMessage } from "@api/Commands";
+import { migratePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import { makeLazy } from "@utils/lazy";
 import definePlugin from "@utils/types";
@@ -85,8 +86,9 @@ async function resolveImage(options: CommandArgument[], ctx: CommandContext, noS
     return null;
 }
 
+migratePluginSettings("PetPet", "petpet");
 export default definePlugin({
-    name: "petpet",
+    name: "PetPet",
     description: "Adds a /petpet slash command to create headpet gifs from any image",
     authors: [Devs.Ven],
     commands: [
