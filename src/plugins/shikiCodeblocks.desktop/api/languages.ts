032a178153a7f62a1881035df0044ea13cabfebe
V <vendicated@riseup.net>
2025-12-06 01:42:37 +0100

    Update JSON_REPO to new repository location

--- src/plugins/shikiCodeblocks.desktop/api/languages.ts
+++ src/plugins/shikiCodeblocks.desktop/api/languages.ts
@@ -20,7 +20,7 @@ import { ILanguageRegistration } from "@vap/shiki";
 
 import { SHIKI_REPO, SHIKI_REPO_COMMIT } from "./themes";
 
-export const JSON_REPO = "thororen1234/shikijsons"; // temp i hope
+export const JSON_REPO = "Vencord/ShikiPluginAssets";
 export const JSON_REPO_COMMIT = "f418661f27c3b6b23e5b92156fb77b3e221ef823";
 export const JSON_URL = `https://cdn.jsdelivr.net/gh/${JSON_REPO}@${JSON_REPO_COMMIT}/grammars.json`;
 export const shikiRepoGrammar = (name: string) => `https://cdn.jsdelivr.net/gh/${SHIKI_REPO}@${SHIKI_REPO_COMMIT}/packages/tm-grammars/grammars/${name}.json`;
