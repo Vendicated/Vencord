a9017e51368168d9b3b002c2d07c40e9ee9eb54a
nin0 <personal@nin0.dev>
2025-12-05 21:18:49 -0500

    Fix suggestion from Claude Code

--- packages/discord-types/src/common/messages/Message.d.ts
+++ packages/discord-types/src/common/messages/Message.d.ts
@@ -2,7 +2,7 @@ import { CommandOption } from './Commands';
 import { User, UserJSON } from '../User';
 import { Embed, EmbedJSON } from './Embed';
 import { DiscordRecord } from "../Record";
-import { MessageFlags, MessageType, StickerFormatType } from "../../../enums";
+import { ApplicationIntegrationType, MessageFlags, MessageType, StickerFormatType } from "../../../enums";
 
 /*
  * TODO: looks like discord has moved over to Date instead of Moment;
@@ -78,9 +78,7 @@ export class Message extends DiscordRecord {
         command_type?: number;
         ephemerality_reason?: number;
         user: User;
-        authorizing_integration_owners: {
-            [key: ApplicationIntegrationType]: string;
-        };
+        authorizing_integration_owners: Record<ApplicationIntegrationType, string>;
         original_response_message_id?: string;
         interacted_message_id?: string;
         target_user?: User;
