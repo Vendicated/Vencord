243fd624fda8d9b1e4f799ef7e923732c69135cd
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-12-14 14:21:35 -0500

    UserArea: Update to canary sizing for fix

--- src/api/UserArea.tsx
+++ src/api/UserArea.tsx
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import "./UserArea.css";
+
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Logger } from "@utils/Logger";
 import { findComponentByCodeLazy } from "@webpack";
