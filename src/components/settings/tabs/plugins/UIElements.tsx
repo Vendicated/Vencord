ae3dc0096139b835d1d1cf7c30cc73aed41a9792
wearrrrr <contact@wearr.dev>
2025-12-14 16:34:46 -0600

    Settings: Allow reordering chat and popover buttons registered by plugins

--- src/components/settings/tabs/plugins/UIElements.tsx
+++ src/components/settings/tabs/plugins/UIElements.tsx
@@ -19,10 +19,22 @@ import { Margins } from "@utils/margins";
 import { classes } from "@utils/misc";
 import { ModalContent, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
 import { IconComponent } from "@utils/types";
-import { Clickable } from "@webpack/common";
+import { Clickable, useEffect, useState } from "@webpack/common";
 
 
 const cl = classNameFactory("vc-plugin-ui-elements-");
+
+export function getOrderedNames(buttonMap: Map<string, any>, settings: SettingsPluginUiElements) {
+    const known = new Set(buttonMap.keys());
+    const ordered = Object.keys(settings).filter(k => known.has(k));
+    for (const name of known) {
+        if (!ordered.includes(name)) {
+            ordered.push(name);
+        }
+    }
+
+    return ordered;
+}
 
 export function UIElementsButton() {
     return (
@@ -56,16 +68,63 @@ function Section(props: {
 }) {
     const { buttonMap, description, title, settings } = props;
 
+    const [order, setOrder] = useState(() =>
+        getOrderedNames(buttonMap, settings)
+    );
+
+    useEffect(() => {
+        setOrder(getOrderedNames(buttonMap, settings));
+    }, [buttonMap, settings]);
+
+    const onDragStart = (e: React.DragEvent, index: number) => {
+        e.dataTransfer.setData("text/plain", index.toString());
+        e.dataTransfer.effectAllowed = "move";
+    };
+
+    const onDrop = (e: React.DragEvent, dropIndex: number) => {
+        e.preventDefault();
+        const dragIndex = Number(e.dataTransfer.getData("text/plain"));
+
+        if (dragIndex === dropIndex) return;
+
+        setOrder(prev => {
+            const next = [...prev];
+            const [moved] = next.splice(dragIndex, 1);
+            next.splice(dropIndex, 0, moved);
+
+            // Persist order into settings
+            const reordered: SettingsPluginUiElements = {};
+            for (const name of next) {
+                reordered[name] = settings[name] ?? {};
+            }
+
+            Object.keys(settings).forEach(k => delete settings[k]);
+            Object.assign(settings, reordered);
+
+            return next;
+        });
+    };
+
     return (
         <section>
             <BaseText tag="h3" size="xl" weight="bold">{title}</BaseText>
             <Paragraph size="sm" className={classes(Margins.top8, Margins.bottom20)}>{description}</Paragraph>
 
             <div className={cl("switches")}>
-                {Array.from(buttonMap, ([name, { icon }]) => {
-                    const Icon = icon ?? PlaceholderIcon;
+                {order.map((name, index) => {
+                    const Icon = buttonMap.get(name)?.icon ?? PlaceholderIcon;
+
                     return (
-                        <Paragraph size="md" weight="semibold" key={name} className={cl("switches-row")}>
+                        <Paragraph
+                            key={name}
+                            size="md"
+                            weight="semibold"
+                            className={cl("switches-row")}
+                            draggable
+                            onDragStart={e => onDragStart(e, index)}
+                            onDragOver={e => e.preventDefault()}
+                            onDrop={e => onDrop(e, index)}
+                        >
                             <Icon height={20} width={20} />
                             {name}
                             <Switch
