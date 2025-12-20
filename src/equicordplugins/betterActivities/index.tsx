64e4d7763e074f7bc01ea41e0e89f52fc60c9e15
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-12-19 20:42:04 -0500

    BetterActivities: Fix Patch

--- src/equicordplugins/betterActivities/index.tsx
+++ src/equicordplugins/betterActivities/index.tsx
@@ -50,7 +50,7 @@ export default definePlugin({
             // Show all activities in the user popout/sidebar
             find: '"UserProfilePopoutBody"',
             replacement: {
-                match: /((\i)=.{0,10}(\i)\.id\).*?,)\i\?.{0,250}onClose:\i\}\)/,
+                match: /((\i)=.{0,10}(\i)\.id\).*?,onInteraction:\i\}\),).{0,250}onClose:\i\}\)/,
                 replace: "$1$self.showAllActivitiesComponent({ activity: $2, user: $3 })"
             },
             predicate: () => settings.store.userPopout
