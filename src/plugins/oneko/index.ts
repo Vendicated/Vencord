3b2d73de3da5d8b12629f0cc369e009aa67253bb
adjoly <contact@adjoly.fr>
2025-12-20 20:57:39 +0100

    「✨」 feat(plugins/oneko): added the hability to change the oneko script and/or gif

--- src/plugins/oneko/index.ts
+++ src/plugins/oneko/index.ts
@@ -16,8 +16,24 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
+
+export const settings = definePluginSettings({
+    onekoLink: {
+        type: OptionType.STRING,
+        restartNeeded: true,
+        description: "The url of script for the oneko",
+        default: "https://raw.githubusercontent.com/adryd325/oneko.js/c4ee66353b11a44e4a5b7e914a81f8d33111555e/oneko.js",
+    },
+    onekoGif: {
+        type: OptionType.STRING,
+        restartNeeded: true,
+        description: "The url of the gif used by the oneko",
+        default: "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif",
+    }
+})
 
 export default definePlugin({
     name: "oneko",
@@ -25,10 +41,12 @@ export default definePlugin({
     // Listing adryd here because this literally just evals her script
     authors: [Devs.Ven, Devs.adryd],
 
+    settings,
+
     start() {
-        fetch("https://raw.githubusercontent.com/adryd325/oneko.js/c4ee66353b11a44e4a5b7e914a81f8d33111555e/oneko.js")
+        fetch(settings.store.onekoLink)
             .then(x => x.text())
-            .then(s => s.replace("./oneko.gif", "https://raw.githubusercontent.com/adryd325/oneko.js/14bab15a755d0e35cd4ae19c931d96d306f99f42/oneko.gif")
+            .then(s => s.replace("./oneko.gif", settings.store.onekoGif)
                 .replace("(isReducedMotion)", "(false)"))
             .then(eval);
     },
