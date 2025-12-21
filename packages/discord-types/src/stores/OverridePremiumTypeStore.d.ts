531252bf866fd2d4b6baa3f160e2e5cf1115734d
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-12-21 15:37:36 -0500

    OverridePremiumTypeStore: Add GetState

--- packages/discord-types/src/stores/OverridePremiumTypeStore.d.ts
+++ packages/discord-types/src/stores/OverridePremiumTypeStore.d.ts
@@ -1,6 +1,13 @@
 import { FluxStore } from "..";
 
+type State = {
+    createdAtOverride: Date;
+    premiumTypeActual: number;
+    premiumTypeOverride: number;
+};
+
 export class OverridePremiumTypeStore extends FluxStore {
+    getState(): State;
     getCreatedAtOverride(): Date;
     getPremiumTypeActual(): number;
     getPremiumTypeOverride(): number;
