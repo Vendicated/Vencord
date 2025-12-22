b9a08c77cdd5518cd0a3ad2cc2da29308f238b3d
prism <snawalt420@proton.me>
2025-12-22 16:37:03 +0100

    discord-types: FIXME GuildStore

--- packages/discord-types/src/stores/GuildStore.d.ts
+++ packages/discord-types/src/stores/GuildStore.d.ts
@@ -1,8 +1,10 @@
 import { Guild, FluxStore } from "..";
 
 export class GuildStore extends FluxStore {
+    /** @correctType Guild | returns undefined for invalid guildId */
     getGuild(guildId: string): Guild;
     getGuildCount(): number;
     getGuilds(): Record<string, Guild>;
+    getGuildsArray(): Guild[];
     getGuildIds(): string[];
 }
