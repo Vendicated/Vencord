5520b2c93b873055b66f0440a043ef89b7d0ff49
programminglaboratorys <programming.laboratorys@gmail.com>
2025-12-24 04:50:07 +0300

    refactor: refactored and optimized patches

--- src/plugins/betterInvites/index.tsx
+++ src/plugins/betterInvites/index.tsx
@@ -6,13 +6,14 @@
 
 import "./style.css";
 
+import { InfoIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
 import { openUserProfile } from "@utils/discord";
 import { classes } from "@utils/misc";
 import definePlugin, { StartAt } from "@utils/types";
+import { Guild } from "@vencord/discord-types";
 import { findByPropsLazy } from "@webpack";
-import { Parser } from "@webpack/common";
-
+import { Parser, Tooltip, UserStore } from "@webpack/common";
 
 const AvatarStyles = findByPropsLazy("avatar", "zalgo");
 const GuildManager = findByPropsLazy("joinGuild");
@@ -30,46 +31,81 @@ function lurk(id: string) {
         .catch(() => { throw new Error("Guild is not lurkable"); });
 }
 
-
 export default definePlugin({
     name: "BetterInvites",
     description: "See invites expiration date, view inviter profile and preview discoverable servers before joining by clicking their name",
-    authors: [Devs.iamme],
+    authors: [Devs.iamme, Devs.thororen],
     patches: [
         {
-            find: ".Messages.HUB_INVITE_ANOTHER_SCHOOL_LINK",
+            find: ".hideDetailsButtonContainer,",
             replacement: [
                 {
-                    match: /,(\i)&&(\(.{0,15}\i\.TooltipContainer.+)(\i\.\i\.Messages.GUEST_MEMBERSHIP_EXPLANATION)/,
-                    replace: ",($1||((!$1)&&arguments[0].invite.expires_at)) && $2$self.RenderTip($1, $3, arguments[0].invite.expires_at)"
-                },
-                {
-                    match: /(\.jsx\)\(\i.\i.Info,{.+onClick):(\i\?\i:null),/,
-                    replace: "$1:$2 || $self.Lurkable(arguments[0].invite.guild.id, arguments[0].invite.guild.features),"
-                },
-                {
-                    match: /(\.jsx\)\(\i\.\i\.Header,\{)text:(\i)/,
-                    replace: "$1text: $self.Header(arguments[0].currentUserId, arguments[0].invite.inviter, $2)"
+                    match: /banner\}\),.{0,25}profile:\i\}\),.{0,15}profile:\i/,
+                    replace: "$&,invite:arguments[0].invite"
                 }
             ]
-        }
+        },
+        {
+            find: ".guildNameContainer,onClick:",
+            replacement: [
+                {
+                    // make the button clickable
+                    match: /children:(\i)\.name\}\).{0,100}\.guildNameContainer/,
+                    replace: "onClick:$self.Lurkable($1),$&"
+                },
+                {
+                    // tip gets inserted in the name container, header gets inserted beside it
+                    match: /(:(\i),disableGuildNameClick:.{0,20}\}\),\i)(\]\}\))/,
+                    replace: "$1,$self.RenderTip(arguments[0].invite?.expires_at)$3,$self.Header(arguments[0].invite?.inviter,$2.name)"
+                }
+            ]
+        },
     ],
-    RenderTip(isGuest: boolean, message: string, expires_at: string) {
-        return <>This invite will expire {Parser.parse(`<t:${Math.round(new Date(expires_at).getTime() / 1000)}:R>`)}{isGuest ? ". " + message : ""}</>;
+    RenderTip(expires_at: string) {
+        if (!expires_at) return null;
+        const timestamp = <>{Parser.parse(`<t:${Math.round(new Date(expires_at).getTime() / 1000)}:R>`)}</>;
+        const tooltipText = (
+            <>
+                This invite will {expires_at ? <>expire {timestamp}</> : <>not expire</>}
+            </>
+        );
+
+        return (
+            <Tooltip text={tooltipText}>
+                {({ onMouseEnter, onMouseLeave }) => (
+                    <div
+                        className="vc-bi-tooltip"
+                        onMouseEnter={onMouseEnter}
+                        onMouseLeave={onMouseLeave}
+                    >
+                        <InfoIcon className="vc-bi-tooltip-icon" />
+                    </div>
+                )}
+            </Tooltip>
+        );
     },
-    Header(currentUserId: string, inviter: User | undefined, defaultMessage: string) {
-        return <div className="vc-bi-header-inner">
-            {(inviter && (currentUserId !== inviter.id)) ? <>
+    Header(inviter: User | undefined, guildName: string) {
+        const userId = UserStore.getCurrentUser().id;
+        if (!inviter || userId === inviter.id) return null;
+        return (
+            <div className="vc-bi-header-inner">
                 <img
+                    alt=""
                     className={classes(AvatarStyles.avatar, AvatarStyles.clickable) + " vc-bi-inviter-avatar"}
                     onClick={() => openUserProfile(inviter.id)}
-                    src={inviter.avatar ? `https://cdn.discordapp.com/avatars/${inviter.id}/${inviter.avatar}.webp?size=80` : "/assets/1f0bfc0865d324c2587920a7d80c609b.png?size=128"}
-                /> {inviter.global_name ? inviter.global_name.toUpperCase() : inviter.username.toUpperCase()} HAS INVITED YOU TO JOIN
-            </> : defaultMessage}</div>;
+                    src={inviter.avatar
+                        ? `https://cdn.discordapp.com/avatars/${inviter.id}/${inviter.avatar}.webp?size=80`
+                        : "/assets/1f0bfc0865d324c2587920a7d80c609b.png?size=128"}
+                />
+                <div className="vc-bi-header-text">
+                    {inviter.global_name || inviter.username} invited you to {guildName}
+                </div>
+            </div>
+        );
     },
-    Lurkable: (id: string, features: Iterable<string> | undefined) => {
-        return new Set(features).has("DISCOVERABLE") ? () => lurk(id) : null;
+    Lurkable: (guild?: Guild) => {
+        if (!guild) return null;
+        return new Set(guild.features).has("DISCOVERABLE") ? () => lurk(guild.id) : null;
     },
     startAt: StartAt.WebpackReady
 });
-
