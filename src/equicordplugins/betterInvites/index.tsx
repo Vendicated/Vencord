92f847bb373cdd3109f431b3f1fee0bb101f4f17
iamme <107296738+programminglaboratorys@users.noreply.github.com>
2025-12-24 05:22:08 +0300

    fix: fix icon being a single pixel off (#592)
    * fix: fix icon being a single pixel off
    
    * refactor: refactored and optimized patches
    feat: display inviter global name

--- src/equicordplugins/betterInvites/index.tsx
+++ src/equicordplugins/betterInvites/index.tsx
@@ -11,6 +11,7 @@ import { Devs, EquicordDevs } from "@utils/constants";
 import { openUserProfile } from "@utils/discord";
 import { classes } from "@utils/misc";
 import definePlugin, { StartAt } from "@utils/types";
+import { Guild } from "@vencord/discord-types";
 import { findByPropsLazy } from "@webpack";
 import { Parser, Tooltip, UserStore } from "@webpack/common";
 
@@ -48,20 +49,14 @@ export default definePlugin({
             find: ".guildNameContainer,onClick:",
             replacement: [
                 {
-                    match: /children:\i\.name\}\).{0,100}\.guildNameContainer/,
-                    replace: "onClick:$self.Lurkable(arguments[0].invite?.guild?.id,arguments[0].invite?.guild?.features),$&"
+                    // make the button clickable
+                    match: /children:(\i)\.name\}\).{0,100}\.guildNameContainer/,
+                    replace: "onClick:$self.Lurkable($1),$&"
                 },
                 {
-                    match: /\.nameContainer.{0,100}disableGuildNameClick:\i/,
-                    replace: "$&,invite:arguments[0].invite"
-                },
-                {
-                    match: /disableGuildNameClick:\i.{0,50}\}\),\i/,
-                    replace: "$&,$self.RenderTip(arguments[0].invite?.expires_at)"
-                },
-                {
-                    match: /\.nameContainer.{0,200}\]\}\)/,
-                    replace: "$&,$self.Header(arguments[0].invite?.inviter,arguments[0].profile.name)"
+                    // tip gets inserted in the name container, header gets inserted beside it
+                    match: /(:(\i),disableGuildNameClick:.{0,20}\}\),\i)(\]\}\))/,
+                    replace: "$1,$self.RenderTip(arguments[0].invite?.expires_at)$3,$self.Header(arguments[0].invite?.inviter,$2.name)"
                 }
             ]
         },
@@ -103,14 +98,14 @@ export default definePlugin({
                         : "/assets/1f0bfc0865d324c2587920a7d80c609b.png?size=128"}
                 />
                 <div className="vc-bi-header-text">
-                    {inviter.username} invited you to {guildName}
+                    {inviter.global_name || inviter.username} invited you to {guildName}
                 </div>
             </div>
         );
     },
-    Lurkable: (id: string, features: Iterable<string> | undefined) => {
-        if (!id || !features) return null;
-        return new Set(features).has("DISCOVERABLE") ? () => lurk(id) : null;
+    Lurkable: (guild?: Guild) => {
+        if (!guild) return null;
+        return new Set(guild.features).has("DISCOVERABLE") ? () => lurk(guild.id) : null;
     },
     startAt: StartAt.WebpackReady
 });
