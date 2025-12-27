2d836e432104c2a9ea38c014a28d16dbddb66784
prism <snawalt420@proton.me>
2025-12-27 03:15:48 +0100

    EmojiStore: full type and docs

--- packages/discord-types/enums/misc.ts
+++ packages/discord-types/enums/misc.ts
@@ -48,3 +48,29 @@ export const enum VideoSourceType {
     VIDEO = 0,
     CAMERA_PREVIEW = 1,
 }
+
+export const enum EmojiIntention {
+    REACTION = 0,
+    STATUS = 1,
+    COMMUNITY_CONTENT = 2,
+    CHAT = 3,
+    GUILD_STICKER_RELATED_EMOJI = 4,
+    GUILD_ROLE_BENEFIT_EMOJI = 5,
+    SOUNDBOARD = 6,
+    VOICE_CHANNEL_TOPIC = 7,
+    GIFT = 8,
+    AUTO_SUGGESTION = 9,
+    POLLS = 10,
+    PROFILE = 11,
+    MESSAGE_CONFETTI = 12,
+    GUILD_PROFILE = 13,
+    CHANNEL_NAME = 14,
+    DEFAULT_REACT_EMOJI = 15,
+}
+
+export const enum LoadState {
+    NOT_LOADED = 0,
+    LOADING = 1,
+    LOADED = 2,
+    ERROR = 3,
+}
