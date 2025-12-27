746f31e412677b39e31222e15c092d2e5250c48e
thororen <78185467+thororen1234@users.noreply.github.com>
2025-12-27 17:22:20 -0500

    Type a bunch of stores (#3861)
    Co-authored-by: prism <snawalt420@proton.me>

--- packages/discord-types/src/stores/VoiceStateStore.d.ts
+++ packages/discord-types/src/stores/VoiceStateStore.d.ts
@@ -24,19 +24,28 @@ export interface VoiceState extends DiscordRecord {
 
 export class VoiceStateStore extends FluxStore {
     getAllVoiceStates(): VoiceStates;
+    getVoiceStateVersion(): number;
 
     getVoiceStates(guildId?: string | null): UserVoiceStateRecords;
     getVoiceStatesForChannel(channelId: string): UserVoiceStateRecords;
     getVideoVoiceStatesForChannel(channelId: string): UserVoiceStateRecords;
 
     getVoiceState(guildId: string | null, userId: string): VoiceState | undefined;
-    getUserVoiceChannelId(guildId: string | null, userId: string): string | undefined;
+    getDiscoverableVoiceState(guildId: string | null, userId: string): VoiceState | null;
     getVoiceStateForChannel(channelId: string, userId?: string): VoiceState | undefined;
     getVoiceStateForUser(userId: string): VoiceState | undefined;
+    getDiscoverableVoiceStateForUser(userId: string): VoiceState | undefined;
+    getVoiceStateForSession(userId: string, sessionId?: string | null): VoiceState | null | undefined;
 
+    getUserVoiceChannelId(guildId: string | null, userId: string): string | undefined;
     getCurrentClientVoiceChannelId(guildId: string | null): string | undefined;
-    isCurrentClientInVoiceChannel(): boolean;
 
+    getUsersWithVideo(channelId: string): Set<string>;
+    getVoicePlatformForChannel(channelId: string, guildId: string): string | undefined;
+
+    isCurrentClientInVoiceChannel(): boolean;
     isInChannel(channelId: string, userId?: string): boolean;
     hasVideo(channelId: string): boolean;
+
+    get userHasBeenMovedVersion(): number;
 }
