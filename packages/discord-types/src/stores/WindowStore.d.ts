effbeb8cecd18da4de6e1d80f30b822608b536c2
prism <snawalt420@proton.me>
2025-12-27 19:18:34 +0100

    minor changes and docs

--- packages/discord-types/src/stores/WindowStore.d.ts
+++ packages/discord-types/src/stores/WindowStore.d.ts
@@ -6,11 +6,17 @@ export interface WindowSize {
 }
 
 export class WindowStore extends FluxStore {
+    /** returns focused window ID, or null if no window is focused */
     getFocusedWindowId(): string | null;
     getLastFocusedWindowId(): string;
+    /** true if any window is focused (getFocusedWindowId() !== null) */
     isAppFocused(): boolean;
-    isElementFullScreen(): boolean;
-    isFocused(): boolean;
-    isVisible(): boolean;
-    windowSize(): WindowSize;
+    /** @param windowId defaults to current window */
+    isElementFullScreen(windowId?: string): boolean;
+    /** @param windowId defaults to current window */
+    isFocused(windowId?: string): boolean;
+    /** @param windowId defaults to current window */
+    isVisible(windowId?: string): boolean;
+    /** @param windowId defaults to current window, returns {width: 0, height: 0} for invalid ID */
+    windowSize(windowId?: string): WindowSize;
 }
