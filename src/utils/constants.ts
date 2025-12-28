6d8f2f2d71535c5fee630ba3927943c00fef872b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-12-28 15:56:09 -0500

    add prism to devs for work on types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -625,6 +625,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "u32",
         id: 1063237286818488351n,
     },
+    prism: {
+        name: "dear.prism",
+        id: 1454940717465206987n,
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
