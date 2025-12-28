6a678415f6a6a9344aeb72ea0b2485275cdcedfb
rz30 <xryanxt@gmail.com>
2025-12-28 14:25:59 +0300

    Add files via upload

---
+++ src/plugins/amITyping/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "10 AmITyping",
+    description: "Shows you if other people can see you typing.",
+    authors: [{
+        name: "10",
+        id: 786315593963536415n
+    }],
+
+    patches: [
+        {
+            find: "\"handleDismissInviteEducation\"",
+            replacement: {
+                match: /\i\.default\.getCurrentUser\(\)/,
+                replace: "\"\""
+            }
+        }
+    ]
+});
