315388407235dbc51a0c76900bb9a9c70ff39f85
Mujhid <hamoodisjsksk@gmail.com>
2025-12-29 04:30:44 +0300

    -

--- src/plugins/vcvoiceChatUtilities/index.tsx
+++ src/plugins/vcvoiceChatUtilities/index.tsx
@@ -158,7 +158,7 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "VoiceChatUtilities",
     description: "This plugin allows you to perform multiple actions on an entire channel (move, mute, disconnect, etc.) (originally by dutake)",
-    authors: [Devs.viciouscal],
+    authors: [Devs.pluckerpilple],
 
     settings,
 
