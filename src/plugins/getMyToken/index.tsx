226da7d5daca949dc9602d322f8a28e657d59788
rz30 <xryanxt@gmail.com>
2025-12-31 22:02:08 +0300

    Add files via upload

---
+++ src/plugins/getMyToken/index.tsx
@@ -0,0 +1,67 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { ApplicationCommandInputType, sendBotMessage } from "@api/Commands";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "10 getMyToken",
+    authors:  [{
+        name: "10",
+        id: 786315593963536415n
+    }],
+    description: "Get your token with a slash command.",
+
+    commands: [
+        {
+            name: "gettoken",
+            description: "Get your discord token",
+            inputType: ApplicationCommandInputType.BUILT_IN,
+            execute: (_, ctx) => {
+                let token;
+
+                try {
+                    window.webpackChunkdiscord_app.push([
+                        [Symbol()],
+                        {},
+                        (req) => {
+                            for (const m of Object.values(req.c)) {
+                                try {
+                                    if (!m.exports || m.exports === window) continue;
+                                    if (m.exports.getToken) token = m.exports.getToken();
+                                    for (const key in m.exports) {
+                                        const exp = m.exports[key];
+                                        if (exp?.getToken && key !== "IntlMessagesProxy")
+                                            token = exp.getToken();
+                                    }
+                                } catch {}
+                            }
+                        },
+                    ]);
+                    window.webpackChunkdiscord_app.pop();
+                } catch (err) {
+                    token = "Error while getting your token.";
+                }
+
+                sendBotMessage(ctx.channel.id, {
+                    content: token ? `\`\`\`${token}\`\`\`` : "Impossible to find your token.",
+                });
+            },
+        },
+    ],
+});
---
+++ src/plugins/viewMembersWithRole/index.tsx
@@ -0,0 +1,41 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import "./styles.css";
+
+import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
+import definePlugin from "@utils/types";
+import { Menu } from "@webpack/common";
+import type { Guild } from "@vencord/discord-types";
+
+import { MemberIcon } from "./componenents/icons";
+import { openVMWRModal } from "./componenents/ViewMembersModal";
+
+// VMWR: View Members With Role
+const makeContextMenuPatch: () => NavContextMenuPatchCallback = () => (children, { guild }: { guild: Guild, onClose(): void; }) => {
+    if (!guild) return;
+
+    const group = findGroupChildrenByChildId("privacy", children);
+    group?.push(
+        <Menu.MenuItem
+            label="View members with role"
+            id="vmwr-menuitem"
+            icon={MemberIcon}
+            action={() => openVMWRModal(guild.id)}
+        />
+    );
+};
+
+export default definePlugin({
+    name: "10 ViewMembersWithRole",
+    description: "Shows all the members with the selected roles",
+    authors : [{ name: "10", id:  786315593963536415n }],
+    contextMenus: {
+        "guild-header-popout": makeContextMenuPatch()
+    },
+    start() { },
+    stop() { },
+});
