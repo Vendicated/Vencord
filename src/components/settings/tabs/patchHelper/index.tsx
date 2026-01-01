0896dd54cd7c1473877e39a7dcbcd39592f5e448
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2026-01-01 14:24:43 +0100

    fix(api): removed deprecated components
    Signed-off-by: prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>

--- src/components/settings/tabs/patchHelper/index.tsx
+++ src/components/settings/tabs/patchHelper/index.tsx
@@ -8,6 +8,7 @@
 
 import { CodeBlock } from "@components/CodeBlock";
 import { Divider } from "@components/Divider";
+import { Button } from "@components/Button";
 import { Flex } from "@components/Flex";
 import { HeadingTertiary } from "@components/Heading";
 import { SettingsTab, wrapTab } from "@components/settings/tabs/BaseTab";
@@ -17,7 +18,7 @@ import { Margins } from "@utils/margins";
 import { stripIndent } from "@utils/text";
 import { ReplaceFn } from "@utils/types";
 import { search } from "@webpack";
-import { Button, React, TextInput, useMemo, useState } from "@webpack/common";
+import { React, TextInput, useMemo, useState } from "@webpack/common";
 
 import { FullPatchInput } from "./FullPatchInput";
 import { PatchPreview } from "./PatchPreview";
