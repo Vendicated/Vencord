05fcbeea307793eda3b331f3900a9c1707df6cfd
abb3v <166838215+abb3v@users.noreply.github.com>
2026-01-03 22:15:55 +1100

    init commit
    allow command short hands to be saved in gui
    todo:
    allow better formatting in the form
    quick action on existing messages...?

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -625,6 +625,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "u32",
         id: 1063237286818488351n,
     },
+    abb3v: {
+        name: "abb3v",
+        id: 1229019850404266007n,
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
