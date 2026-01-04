ae2cb5814bd31d4d788521033afecbf827892384
prism <snawalt420@proton.me>
2026-01-04 20:32:43 +0100

    revert, fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,8 +18,8 @@ const btnCls = classNameFactory("vc-btn-");
 const textBtnCls = classNameFactory("vc-text-btn-");
 
 export type ButtonVariant =
-    "primary" | "secondary" | "dangerPrimary" | "dangerSecondary" | "overlayPrimary" | "positive" | "link" | "none" | "iconOnly";
-export type ButtonSize = "min" | "xs" | "small" | "medium";
+    "primary" | "secondary" | "dangerPrimary" | "dangerSecondary" | "overlayPrimary" | "positive" | "link" | "none";
+export type ButtonSize = "min" | "xs" | "small" | "medium" | "iconOnly";
 
 export type ButtonProps = ComponentPropsWithRef<"button"> & {
     variant?: ButtonVariant;
