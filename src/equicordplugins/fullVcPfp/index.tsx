d3c8366112692ed8d45152997bf85f6b1a628d9f
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-01-08 20:38:28 -0500

    FullVCPFP: Support Speaking For Gifs

--- src/equicordplugins/fullVcPfp/index.tsx
+++ src/equicordplugins/fullVcPfp/index.tsx
@@ -7,7 +7,7 @@
 import { disableStyle, enableStyle } from "@api/Styles";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { IconUtils, UserStore } from "@webpack/common";
+import { ChannelRTCStore, IconUtils, UserStore, VoiceStateStore } from "@webpack/common";
 
 import style from "./style.css?managed";
 
@@ -29,7 +29,9 @@ export default definePlugin({
         if (!className.includes("tile") || !participantUserId) return;
 
         const user = UserStore.getUser(participantUserId);
-        const avatarUrl = IconUtils.getUserAvatarURL(user, false, 1024);
+        const channelId = VoiceStateStore.getVoiceStateForUser(participantUserId)?.channelId!;
+        const isSpeaking = ChannelRTCStore.getSpeakingParticipants(channelId).some(p => p.user.id === participantUserId && p.speaking);
+        const avatarUrl = IconUtils.getUserAvatarURL(user, isSpeaking, 1024);
 
         return {
             "--full-res-avatar": `url(${avatarUrl})`
