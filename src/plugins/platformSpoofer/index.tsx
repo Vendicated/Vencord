b7c3589861070475b1999ab0a82234fa195780d0
rz30 <xryanxt@gmail.com>
2026-01-11 01:42:45 +0300

    Import styles.css in platformSpoofer plugin

--- src/plugins/platformSpoofer/index.tsx
+++ src/plugins/platformSpoofer/index.tsx
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import "plugins/_misc/styles.css";
 
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
