d6236ef3df75bdb9c23bb11f4183328453ada686
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-01-14 16:59:25 -0500

    Fix Patches

--- src/plugins/favGifSearch/index.tsx
+++ src/plugins/favGifSearch/index.tsx
@@ -92,7 +92,7 @@ export default definePlugin({
                 {
                     // https://regex101.com/r/07gpzP/1
                     // ($1 renderHeaderContent=function { ... switch (x) ... case FAVORITES:return) ($2) ($3 case default:return r.jsx(($<searchComp>), {...props}))
-                    match: /(renderHeaderContent\(\).{1,150}FAVORITES:return)(.{1,150});(case.{1,200}default:return\(0,\i\.jsx\)\((?<searchComp>\i\..{1,10}),)/,
+                    match: /(renderHeaderContent\(\).{1,150}FAVORITES:return)(.{1,150});(case.{1,200}default:.{0,50}return\(0,\i\.jsx\)\((?<searchComp>\i\..{1,10}),)/,
                     replace: "$1 this.state.resultType === 'Favorites' ? $self.renderSearchBar(this, $<searchComp>) : $2;$3"
                 },
                 {
