7548777d2fdfb3819869ec68ec81da0aaf307168
Artavazd <90024173+Artavazdpet@users.noreply.github.com>
2026-01-15 17:33:27 +0400

    Update index.tsx

--- src/plugins/fakeNitro/index.tsx
+++ src/plugins/fakeNitro/index.tsx
@@ -922,7 +922,7 @@ export default definePlugin({
                 hasBypass = true;
 
                 const emojiSize = s.emojiSize ?? 48;
-                
+
                 const url = new URL(IconUtils.getEmojiURL({ id: emoji.id, animated: emoji.animated, size: emojiSize }));
                 url.searchParams.set("size", emojiSize.toString());
                 url.searchParams.set("name", emoji.name);
