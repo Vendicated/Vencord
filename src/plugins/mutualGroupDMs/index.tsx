674a627aceb7812ec1e1c0b2d9b539fa38320922
Vendicated <vendicated@riseup.net>
2026-01-24 00:16:27 +0100

    improve ExpandableList find

--- src/plugins/mutualGroupDMs/index.tsx
+++ src/plugins/mutualGroupDMs/index.tsx
@@ -35,8 +35,7 @@ const UserUtils = findByPropsLazy("getGlobalName");
 const ProfileListClasses = findCssClassesLazy("empty", "textContainer", "connectionIcon");
 const TabBarClasses = findCssClassesLazy("tabPanelScroller", "tabBarPanel");
 const MutualsListClasses = findCssClassesLazy("row", "icon", "name", "details");
-// FIXME
-const ExpandableList = findComponentByCodeLazy('"PRESS_SECTION"');
+const ExpandableList = findComponentByCodeLazy('action:"PRESS_SECTION"', "section");
 
 function getGroupDMName(channel: Channel) {
     return channel.name ||
