b4a27ea1b035de5b60bf2bc404f2c2c93748e86e
sadan <117494111+sadan4@users.noreply.github.com>
2026-01-23 13:57:15 -0500

    MemberCount: Fix patch

--- src/plugins/memberCount/index.tsx
+++ src/plugins/memberCount/index.tsx
@@ -70,14 +70,14 @@ export default definePlugin({
             find: "{isSidebarVisible:",
             replacement: [
                 {
-                    match: /(?<=var\{className:(\i),.+?children):\[(\i\.useMemo[^}]+"aria-multiselectable")/,
+                    match: /(?<=\{className:(\i),[^}]+?=\i.+?children):\[(\i\.useMemo[^}]+"aria-multiselectable")/,
                     replace: ":[$1?.includes('members')?$self.render():null,$2",
                 },
             ],
             predicate: () => settings.store.memberList
         },
         {
-            find: ".invitesDisabledTooltip",
+            find: "GuildTooltip - ",
             replacement: {
                 match: /#{intl::VIEW_AS_ROLES_MENTIONS_WARNING}.{0,100}(?=])/,
                 replace: "$&,$self.renderTooltip(arguments[0].guild)"
