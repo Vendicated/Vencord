e7c5c90bba0cdd37664577c1f79c3213493afb70
prism <snawalt420@proton.me>
2026-01-23 21:04:59 +0100

    VoiceDownload: fix patch

--- src/plugins/voiceDownload/index.tsx
+++ src/plugins/voiceDownload/index.tsx
@@ -15,10 +15,10 @@ export default definePlugin({
     authors: [Devs.puv],
     patches: [
         {
-            find: "rippleContainer,children",
+            find: "VOICE_MESSAGE_PLAYBACK_STARTED,{src:",
             replacement: {
-                match: /\(0,\i\.jsx\).{0,150},children:.{0,50}\("source",{src:(\i)}\)}\)/,
-                replace: "[$&, $self.renderDownload($1)]"
+                match: /(?<=onVolumeHide:\i\}\))/,
+                replace: ",$self.renderDownload(arguments[0].src)"
             }
         }
     ],
