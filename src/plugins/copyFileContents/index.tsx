8f6bb1de6be6dd33ce422e02f18d95c0810d29e2
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-01-23 15:08:56 -0500

    fix copyfilecontents

--- src/plugins/copyFileContents/index.tsx
+++ src/plugins/copyFileContents/index.tsx
@@ -27,7 +27,7 @@ export default definePlugin({
         {
             find: "#{intl::PREVIEW_BYTES_LEFT}",
             replacement: {
-                match: /\.footerGap.+?url:\i,fileName:\i,fileSize:\i}\),(?<=fileContents:(\i),bytesLeft:(\i).+?)/g,
+                match: /(?<=fileContents:(\i),bytesLeft:(\i).+?)url:\i,fileName:\i,fileSize:\i}\),(?=.{0,25}setLanguage:)/g,
                 replace: "$&$self.addCopyButton({fileContents:$1,bytesLeft:$2}),"
             }
         }
