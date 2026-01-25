40040315feddef74b225d752cc9b678fbc54970a
rz30 <xryanxt@gmail.com>
2026-01-25 17:10:11 +0300

    Update index.tsx

--- src/plugins/philsPluginLibrary/icons/index.tsx
+++ src/plugins/philsPluginLibrary/icons/index.tsx
@@ -140,27 +140,3 @@ export const MicrophoneSettingsIcon =
                 vectorEffect="non-scaling-stroke"
             ></path>
         </svg>;
-
-export const MuteIcon =
-    (props: React.ComponentProps<"svg">) =>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            {...props}
-        >
-            <path
-                fill="currentColor"
-                d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66 11 5V11C11 11.34 11.2 12 12 12C12.8 12 13 11.34 13 11V5C13 4.66 12.8 4 12 4Z"
-            />
-            <rect
-                fill="currentColor"
-                x="7"
-                y="2.5"
-                width="2"
-                height="19"
-                rx="1"
-                transform="rotate(45 7 2.5)"
-            />
-        </svg>;
