a51217551e549d529b446aa4d38443c344280f88
rz30 <xryanxt@gmail.com>
2026-01-25 17:11:45 +0300

    Update userPanel.tsx

--- src/plugins/philsPluginLibrary/patches/userPanel.tsx
+++ src/plugins/philsPluginLibrary/patches/userPanel.tsx
@@ -16,91 +16,267 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { SettingsPanel } from "@plugins/philsPluginLibrary/components";
-import { IconComponent, SettingsPanelButton } from "@plugins/philsPluginLibrary/components/settingsPanel/SettingsPanelButton";
-import { SettingsPanelRow } from "@plugins/philsPluginLibrary/components/settingsPanel/SettingsPanelRow";
-import { SettingsPanelTooltipButton } from "@plugins/philsPluginLibrary/components/settingsPanel/SettingsPanelTooltipButton";
-import { React } from "@webpack/common";
-import { JSX } from "react";
+import { Logger } from "@utils/Logger";
+import { lodash } from "@webpack/common";
 
-export interface PanelButton {
-    name: string,
-    tooltipText?: string,
-    icon?: IconComponent;
-    onClick?: () => void;
+import { ScreenshareProfile, ScreenshareStore } from "../../betterScreenshare.desktop/stores";
+import { ProfilableStore, replaceObjectValuesIfExist, types, utils } from "../../philsPluginLibrary";
+
+
+export function getDefaultVideoTransportationOptions(connection: types.Connection) {
+    return {
+        ...connection.videoQualityManager.applyQualityConstraints({}).constraints,
+        videoEncoder: {
+            ...connection.getCodecOptions("", "H264", "stream").videoEncoder
+        },
+        streamParameters: connection.videoStreamParameters[0],
+        keyframeInterval: 0,
+    };
 }
 
-const settingsPanelButtonsSubscriptions = new Set<React.DispatchWithoutAction>();
-export const settingsPanelButtons: PanelButton[] = new Proxy<PanelButton[]>([], {
-    set: (target, p, newValue) => {
-        target[p] = newValue;
-        settingsPanelButtonsSubscriptions.forEach(fn => fn());
-        return true;
-    },
-});
+export function getDefaultVideoDesktopSourceOptions(connection: types.Connection) {
+    const [type, sourceId] = connection.goLiveSourceIdentifier?.split(":") ?? ["screen", 0];
 
-export const useButtons = () => {
-    const [, forceUpdate] = React.useReducer(() => ({}), {});
+    return {
+        hdrCaptureMode: "never",
+        allowScreenCaptureKit: true,
+        useQuartzCapturer: true,
+        useGraphicsCapture: true,
+        useVideoHook: true,
+        sourceId: sourceId,
+        type: type
+    };
+}
 
-    React.useEffect(() => {
-        settingsPanelButtonsSubscriptions.add(forceUpdate);
-        return () => void settingsPanelButtonsSubscriptions.delete(() => forceUpdate);
-    }, []);
+export function getStreamParameters(connection: types.Connection, get: ProfilableStore<ScreenshareStore, ScreenshareProfile>["get"]) {
+    const { currentProfile } = get();
+    const {
+        framerate,
+        framerateEnabled,
+        height,
+        resolutionEnabled,
+        videoBitrate,
+        videoBitrateEnabled,
+        width,
+    } = currentProfile;
 
-    return settingsPanelButtons;
-};
+    const { bitrateMax, capture } = connection.applyQualityConstraints({}).quality;
 
-export const ButtonsSettingsPanel = () => {
-    const rawPanelButtons = useButtons();
+    return {
+        ...connection.videoStreamParameters[0],
+        quality: 100,
+        ...(videoBitrateEnabled && videoBitrate
+            ? {
+                maxBitrate: videoBitrate * 1000,
+            }
+            : {
+                maxBitrate: bitrateMax
+            }
+        ),
+        ...((resolutionEnabled && width && height)
+            ? {
+                maxResolution: {
+                    height: height,
+                    width: width,
+                    type: "fixed"
+                },
+                maxPixelCount: width * height
+            }
+            : {
+                maxResolution: !capture.height || !capture.width ? {
+                    height: capture.height,
+                    width: capture.width,
+                    type: "source"
+                } : {
+                    height: capture.height,
+                    width: capture.width,
+                    type: "fixed"
+                }
+            }
+        ),
+        ...(framerateEnabled && framerate
+            ? {
+                maxFrameRate: framerate,
+            }
+            : {
+                maxFrameRate: capture.framerate
+            }
+        ),
+        active: true,
+    };
+}
 
-    const convertRawPanelButtons = (buttons: PanelButton[]) => {
-        const settingsPanelButtonsClone = [...buttons].sort();
-        const groupedButtons: JSX.Element[][] = [];
+export function getReplaceableVideoTransportationOptions(connection: types.Connection, get: ProfilableStore<ScreenshareStore, ScreenshareProfile>["get"]) {
+    const { currentProfile, audioSource, audioSourceEnabled } = get();
+    const {
+        framerate,
+        framerateEnabled,
+        height,
+        keyframeInterval,
+        keyframeIntervalEnabled,
+        resolutionEnabled,
+        videoBitrate,
+        videoBitrateEnabled,
+        videoCodec,
+        videoCodecEnabled,
+        width,
+    } = currentProfile;
 
-        while (settingsPanelButtonsClone.length) {
-            const splicedButtons =
-                settingsPanelButtonsClone
-                    .splice(0, 3)
-                    .map(({ icon, tooltipText, onClick }, index) =>
-                        tooltipText
-                            ? <SettingsPanelTooltipButton
-                                key={`tooltip-button-${index}`} // Add a unique key here
-                                tooltipProps={{ text: tooltipText }}
-                                icon={icon}
-                                onClick={onClick}
-                            />
-                            : <SettingsPanelButton
-                                key={`button-${index}`} // Add a unique key here
-                                icon={icon}
-                                onClick={onClick}
-                            />
-                    );
+    return {
+        ...(videoBitrateEnabled && videoBitrate
+            ? {
+                encodingVideoBitRate: Math.round(videoBitrate * 1000),
+                encodingVideoMinBitRate: Math.round(videoBitrate * 1000),
+                encodingVideoMaxBitRate: Math.round(videoBitrate * 1000),
+                callBitRate: Math.round(videoBitrate * 1000),
+                callMinBitRate: Math.round(videoBitrate * 1000),
+                callMaxBitRate: Math.round(videoBitrate * 1000)
+            }
+            : {}
+        ),
+        ...((resolutionEnabled && width && height)
+            ? {
+                encodingVideoHeight: height,
+                encodingVideoWidth: width,
+                remoteSinkWantsPixelCount: height * width
+            }
+            : {}
+        ),
+        ...(framerateEnabled && framerate
+            ? {
+                encodingVideoFrameRate: framerate,
+                remoteSinkWantsMaxFramerate: framerate,
+                captureVideoFrameRate: framerate
+            }
+            : {}
+        ),
+        ...(keyframeIntervalEnabled && keyframeInterval
+            ? {
+                keyframeInterval: keyframeInterval
+            }
+            : {}
+        ),
+        ...(videoCodecEnabled && videoCodec
+            ? {
+                videoEncoder: connection.getCodecOptions("", videoCodec, "stream").videoEncoder
+            }
+            : {}
+        ),
+        ...(audioSourceEnabled && audioSource && utils.getPidFromDesktopSource(audioSource)
+            ? {
+                soundsharePid: utils.getPidFromDesktopSource(audioSource),
+                soundshareEventDriven: true,
+                soundshareLoopback: true
+            }
+            : {}
+        ),
+        streamParameters: getStreamParameters(connection, get)
+    };
+}
 
-            groupedButtons.push(splicedButtons);
-        }
+export function getReplaceableVideoDesktopSourceOptions(get: ProfilableStore<ScreenshareStore, ScreenshareProfile>["get"]) {
+    const { currentProfile } = get();
+    const {
+        hdrEnabled,
+    } = currentProfile;
 
-        return groupedButtons;
+    return {
+        ...(hdrEnabled
+            ? {
+                hdrCaptureMode: "always"
+            }
+            : {}
+        ),
+    };
+}
+
+export function patchConnectionVideoSetDesktopSourceWithOptions(
+    connection: types.Connection,
+    get: ProfilableStore<ScreenshareStore, ScreenshareProfile>["get"],
+    logger?: Logger
+) {
+    const oldSetDesktopSourceWithOptions = connection.conn.setDesktopSourceWithOptions;
+
+    connection.conn.setDesktopSourceWithOptions = function (this: any, options: Record<string, any>) {
+        const replaceableDesktopSourceOptions = getReplaceableVideoDesktopSourceOptions(get);
+        replaceObjectValuesIfExist(options, replaceableDesktopSourceOptions);
+
+        logger?.info("Overridden Desktop Source Options", options);
+
+        return Reflect.apply(oldSetDesktopSourceWithOptions, this, [options]);
     };
 
-    return rawPanelButtons.length > 0
-        ? <SettingsPanel>
-            {convertRawPanelButtons(rawPanelButtons).map((value, index) => (
-                <SettingsPanelRow key={`panel-row-${index}`}>
-                    {value}
-                </SettingsPanelRow>
-            ))}
-        </SettingsPanel>
-        : null;
-};
+    const forceUpdateDesktopSourceOptions = () => {
+        const desktopSourceOptions = lodash.merge({ ...getDefaultVideoDesktopSourceOptions(connection) }, getReplaceableVideoDesktopSourceOptions(get));
 
-export function replacedUserPanelComponent(oldComponent) {
-    return <ButtonsSettingsPanel />;
+        logger?.info("Force Updated Desktop Source Options", desktopSourceOptions);
+
+        oldSetDesktopSourceWithOptions(desktopSourceOptions);
+    };
+
+    return {
+        oldSetDesktopSourceWithOptions,
+        forceUpdateDesktopSourceOptions
+    };
 }
 
-export function addSettingsPanelButton(settings: PanelButton) {
-    settingsPanelButtons.push(settings);
-}
+export function patchConnectionVideoTransportOptions(
+    connection: types.Connection,
+    get: ProfilableStore<ScreenshareStore, ScreenshareProfile>["get"],
+    logger?: Logger
+) {
+    const oldSetTransportOptions = connection.conn.setTransportOptions;
+    const oldGetQuality = connection.videoQualityManager.getQuality;
 
-export function removeSettingsPanelButton(name: string) {
-    settingsPanelButtons.splice(0, settingsPanelButtons.length, ...settingsPanelButtons.filter(value => value.name !== name));
+    connection.videoQualityManager.getQuality = function (src) {
+        const { currentProfile } = get();
+        const { videoBitrateEnabled, videoBitrate, framerateEnabled, framerate, resolutionEnabled, width, height } = currentProfile;
+
+        const quality = oldGetQuality.call(this, src);
+
+        if (videoBitrateEnabled) {
+            quality.bitrateMax = Math.round(videoBitrate! * 1000);
+            quality.bitrateMin = Math.round(videoBitrate! * 1000);
+            quality.bitrateTarget = Math.round(videoBitrate! * 1000);
+        }
+
+        quality.localWant = 100;
+        quality.capture.framerate = framerateEnabled ? framerate : quality.capture.framerate;
+
+        quality.capture.width = resolutionEnabled ? width : quality.capture.width;
+        quality.capture.height = resolutionEnabled ? height : quality.capture.height;
+        quality.capture.pixelCount = quality.capture.width * quality.capture.height;
+
+        quality.encode = quality.capture;
+
+        logger?.info("Overridden getQuality", quality);
+
+        return quality;
+    };
+
+    connection.conn.setTransportOptions = function (this: any, options: Record<string, any>) {
+        const replaceableTransportOptions = getReplaceableVideoTransportationOptions(connection, get);
+
+        if (options.streamParameters)
+            connection.videoStreamParameters = Array.isArray(options.streamParameters) ? options.streamParameters : [options.streamParameters];
+
+        replaceObjectValuesIfExist(options, replaceableTransportOptions);
+
+        logger?.info("Overridden Transport Options", options);
+
+        return Reflect.apply(oldSetTransportOptions, this, [options]);
+    };
+
+    const forceUpdateTransportationOptions = () => {
+        const transportOptions = lodash.merge({ ...getDefaultVideoTransportationOptions(connection) }, getReplaceableVideoTransportationOptions(connection, get));
+
+        logger?.info("Force Updated Transport Options", transportOptions);
+
+        oldSetTransportOptions(transportOptions);
+    };
+
+    return {
+        oldSetTransportOptions,
+        forceUpdateTransportationOptions,
+    };
 }
