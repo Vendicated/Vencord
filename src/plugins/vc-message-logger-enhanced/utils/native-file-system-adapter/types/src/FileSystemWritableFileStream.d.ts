29a26c6c716dd275b589740eb2a81a2d2a9ef3cc
rz30 <xryanxt@gmail.com>
2026-01-27 18:04:21 +0300

    Delete src/plugins/vc-message-logger-enhanced directory

--- src/plugins/vc-message-logger-enhanced/utils/native-file-system-adapter/types/src/FileSystemWritableFileStream.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export default FileSystemWritableFileStream;
-export class FileSystemWritableFileStream extends WritableStream<any> {
-    constructor(...args: any[]);
-    private _closed;
-    seek(position: number): Promise<void>;
-    truncate(size: number): Promise<void>;
-    write(data: any): Promise<void>;
-}
--- src/plugins/vc-message-logger-enhanced/utils/native-file-system-adapter/types/src/adapters/downloader.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export class FileHandle {
-    constructor(name?: string);
-    name: string;
-    kind: string;
-    getFile(): Promise<void>;
-    isSameEntry(other: any): Promise<boolean>;
-    createWritable(options?: object): Promise<WritableStreamDefaultWriter<any>>;
-}
