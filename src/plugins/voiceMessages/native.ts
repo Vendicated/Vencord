c09de9f23f12aa40cf8d9b6e522c35ed7c987790
rz30 <xryanxt@gmail.com>
2026-01-27 19:19:41 +0300

    Add files via upload

---
+++ src/plugins/voiceMessages/native.ts
@@ -0,0 +1,24 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2023 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { app } from "electron";
+import { readFile, rm } from "fs/promises";
+import { basename, normalize } from "path";
+
+export async function readRecording(_, filePath: string) {
+    filePath = normalize(filePath);
+    const filename = basename(filePath);
+    const discordBaseDirWithTrailingSlash = normalize(app.getPath("userData") + "/");
+    if (!/^\d*recording\.ogg$/.test(filename) || !filePath.startsWith(discordBaseDirWithTrailingSlash)) return null;
+
+    try {
+        const buf = await readFile(filePath);
+        rm(filePath).catch(() => { });
+        return new Uint8Array(buf.buffer);
+    } catch {
+        return null;
+    }
+}
