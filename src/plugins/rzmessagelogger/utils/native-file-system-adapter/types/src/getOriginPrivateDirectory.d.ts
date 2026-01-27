a972009882ef8ffa43695f43c16f85da597332f7
rz30 <xryanxt@gmail.com>
2026-01-27 18:46:40 +0300

    Delete src/plugins/rzmessagelogger directory

--- src/plugins/rzmessagelogger/utils/native-file-system-adapter/types/src/getOriginPrivateDirectory.d.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-export default getOriginPrivateDirectory;
-export type FileSystemDirectoryHandle = import('./FileSystemDirectoryHandle.js').default;
-declare function getOriginPrivateDirectory(driver?: object | undefined, options?: {}): Promise<import("./FileSystemDirectoryHandle.js").default>;
--- src/plugins/rzmessagelogger/utils/native-file-system-adapter/types/src/showDirectoryPicker.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default showDirectoryPicker;
-export type FileSystemDirectoryHandle = import('./FileSystemDirectoryHandle.js').default;
-export function showDirectoryPicker(options?: {
-    _preferPolyfill?: boolean;
-}): Promise<FileSystemDirectoryHandle>;
