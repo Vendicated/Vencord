ddde0631c43aca713dd620e01bcb20d44056ae8b
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2026-02-01 01:22:14 +0100

    fix: showDevBanner

--- src/plugins/showDevBanner/index.ts
+++ src/plugins/showDevBanner/index.ts
@@ -9,37 +9,33 @@
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
-import { settings } from "./settings";
-
 export default definePlugin({
     name: "ShowDevBanner",
     description: "Displays a customizable developer banner with EagleCord and Discord build information.",
     authors: [Devs.Eagle],
-    settings,
     required: true,
     isEagleCord: true,
 
     patches: [
         {
-            find: ".devBanner,",
+            find: '"isHideDevBanner"',
             replacement: [
-                { match: '"staging"===window.GLOBAL_ENV.RELEASE_CHANNEL', replace: "true" },
                 {
-                    predicate: () => settings.store.removeCloseButton,
-                    match: /(\i=\(\)=>)\(.*?\}\);/,
-                    replace: "$1null;",
+                    match: '"staging"===window.GLOBAL_ENV.RELEASE_CHANNEL',
+                    replace: "true"
                 },
                 {
-                    match: /\i\.\i\.format\(.{0,30},{buildNumber:(.{0,20})}\)/,
-                    replace: "$self.transform($1)",
+                    match: /children:\[.*?\{\}\)\]/g,
+                    replace: "children:$self.makeDevBanner()"
                 },
-            ],
-        },
+            ]
+        }
     ],
-
-    transform() {
-        const releaseChannel: string = window.GLOBAL_ENV.RELEASE_CHANNEL;
-
-        return `eagleCord v${VERSION} / ${releaseChannel}`;
-    },
+    makeDevBanner,
 });
+
+function makeDevBanner(): string {
+    const releaseChannel: string = window.GLOBAL_ENV.RELEASE_CHANNEL;
+
+    return `eagleCord v${VERSION} / ${releaseChannel}`;
+}
