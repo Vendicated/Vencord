58c453079a5da9ee10f9ab70b5e1a18bfc39d77c
- <120268595+pluckerpilple@users.noreply.github.com>
2026-02-03 00:28:33 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -633,6 +633,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    sexwithppl: {
+        name: "sexwithppl",
+        id: 298055455614173184n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
