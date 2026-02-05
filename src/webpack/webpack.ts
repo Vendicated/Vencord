a0495e5037dd786e92809cf901dabc785ada50a3
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-05 18:32:24 -0500

    fix various finds

--- src/webpack/webpack.ts
+++ src/webpack/webpack.ts
@@ -657,7 +657,7 @@ export const mapMangledModule = traceFunction("mapMangledModule", function mapMa
         const member = mod[key];
         for (const newName in mappers) {
             // if the current mapper matches this module
-            if (mappers[newName](member)) {
+            if (!(newName in exports) && mappers[newName](member)) {
                 exports[newName] = member;
                 continue outer;
             }
