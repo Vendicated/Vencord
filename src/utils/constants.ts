4aff70ab654a0d7a43bf9ca6e5dfae99f46942f8
RumBugen <vladislavwanner@gmail.com>
2026-02-06 12:28:44 +0100

    feat(translate): auto-translate received messages and recent channel history
    also added an cache for translations

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    RumBugen: {
+        name: "RumBugen",
+        id: 296659878985072642n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
