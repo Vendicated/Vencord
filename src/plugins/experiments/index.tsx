02d3718c21f524a12f6d4d33a84605d5fa15a1e1
1xstore <ryankharj@gmail.com>
2026-02-06 15:49:52 +0300

    Update index.tsx

--- src/plugins/experiments/index.tsx
+++ src/plugins/experiments/index.tsx
@@ -16,155 +16,29 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { definePluginSettings } from "@api/Settings";
-import { disableStyle, enableStyle } from "@api/Styles";
-import ErrorBoundary from "@components/ErrorBoundary";
-import { ErrorCard } from "@components/ErrorCard";
-import { Paragraph } from "@components/Paragraph";
-import { Devs, IS_MAC } from "@utils/constants";
-import { Margins } from "@utils/margins";
-import definePlugin, { OptionType } from "@utils/types";
-import { findByPropsLazy, findLazy } from "@webpack";
-import { Forms, React } from "@webpack/common";
-
-import hideBugReport from "./hideBugReport.css?managed";
-
-const KbdStyles = findByPropsLazy("key", "combo");
-const BugReporterExperiment = findLazy(m => m?.definition?.id === "2024-09_bug_reporter");
-
-const modKey = IS_MAC ? "cmd" : "ctrl";
-const altKey = IS_MAC ? "opt" : "alt";
-
-const settings = definePluginSettings({
-    toolbarDevMenu: {
-        type: OptionType.BOOLEAN,
-        description: "Change the Help (?) toolbar button (top right in chat) to Discord's developer menu",
-        default: false,
-        restartNeeded: true
-    }
-});
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
 
 export default definePlugin({
-    name: "Experiments",
-    description: "Enable Access to Experiments & other dev-only features in Discord!",
-    authors: [
-        Devs.Megu,
-        Devs.Ven,
-        Devs.Nickyux,
-        Devs.BanTheNons,
-        Devs.Nuckyz,
-    ],
-
-    settings,
-
+    name: "DisableCallIdle",
+    description: "Disables automatically getting kicked from a DM voice call after 3 minutes and being moved to an AFK voice channel.",
+    authors: [Devs.Nuckyz],
     patches: [
         {
-            find: "Object.defineProperties(this,{isDeveloper",
+            find: "this.idleTimeout.start(",
             replacement: {
-                match: /(?<={isDeveloper:\{[^}]+?,get:\(\)=>)\i/,
-                replace: "true"
+                match: /this\.idleTimeout\.(start|stop)/g,
+                replace: "$self.noop"
             }
         },
         {
-            find: 'type:"user",revision',
+            find: "handleIdleUpdate(){",
             replacement: {
-                match: /!(\i)(?=&&"CONNECTION_OPEN")/,
-                replace: "!($1=true)"
-            }
-        },
-        {
-            find: 'placeholder:"Search experiments"',
-            replacement: {
-                match: /(?<=children:\[)(?=\(0,\i\.jsx?\)\(\i\.\i,{placeholder:"Search experiments")/,
-                replace: "$self.WarningCard(),"
-            }
-        },
-        // Change top right toolbar button from the help one to the dev one
-        {
-            find: '?"BACK_FORWARD_NAVIGATION":',
-            replacement: {
-                match: /hasBugReporterAccess:(\i)/,
-                replace: "_hasBugReporterAccess:$1=true"
-            },
-            predicate: () => settings.store.toolbarDevMenu
-        },
-        // Disable opening the bug report menu when clicking the top right toolbar dev button
-        {
-            find: 'navId:"staff-help-popout"',
-            replacement: {
-                match: /(isShown.+?)onClick:\i/,
-                replace: (_, rest) => `${rest}onClick:()=>{}`
-            }
-        },
-        // Make the Favourites Server experiment allow favouriting DMs and threads
-        {
-            find: "useCanFavoriteChannel",
-            replacement: {
-                match: /\i\.isDM\(\)\|\|\i\.isThread\(\)/,
-                replace: "false",
-            }
-        },
-        // Enable experiment embed on sent experiment links
-        {
-            find: "Clear Treatment ",
-            replacement: [
-                {
-                    match: /\i\??\.isStaff\(\)/,
-                    replace: "true"
-                },
-                // Fix some tricky experiments name causing a client crash
-                {
-                    match: /\.isStaffPersonal\(\).+?if\(null==(\i)\|\|null==\i(?=\)return null;)/,
-                    replace: "$&||({})[$1]!=null"
-                }
-            ]
-        },
-        // Fix another function which cases crashes with tricky experiment names and the experiment embed
-        {
-            find: "}getServerAssignment(",
-            replacement: {
-                match: /}getServerAssignment\((\i),\i,\i\){/,
-                replace: "$&if($1==null)return;"
+                match: "handleIdleUpdate(){",
+                replace: "handleIdleUpdate(){return;"
             }
         }
     ],
 
-    start: () => !BugReporterExperiment.getCurrentConfig().hasBugReporterAccess && enableStyle(hideBugReport),
-    stop: () => disableStyle(hideBugReport),
-
-    settingsAboutComponent: () => {
-        return (
-            <React.Fragment>
-                <Forms.FormTitle tag="h3">More Information</Forms.FormTitle>
-                <Paragraph size="md">
-                    You can open Discord's DevTools via {" "}
-                    <div className={KbdStyles.combo} style={{ display: "inline-flex" }}>
-                        <kbd className={KbdStyles.key}>{modKey}</kbd> +{" "}
-                        <kbd className={KbdStyles.key}>{altKey}</kbd> +{" "}
-                        <kbd className={KbdStyles.key}>O</kbd>{" "}
-                    </div>
-                </Paragraph>
-            </React.Fragment>
-        );
-    },
-
-    WarningCard: ErrorBoundary.wrap(() => (
-        <ErrorCard id="vc-experiments-warning-card" className={Margins.bottom16}>
-            <Forms.FormTitle tag="h2">Hold on!!</Forms.FormTitle>
-
-            <Forms.FormText>
-                Experiments are unreleased Discord features. They might not work, or even break your client or get your account disabled.
-            </Forms.FormText>
-
-            <Forms.FormText className={Margins.top8}>
-                Only use experiments if you know what you're doing. Vencord is not responsible for any damage caused by enabling experiments.
-
-                If you don't know what an experiment does, ignore it. Do not ask us what experiments do either, we probably don't know.
-            </Forms.FormText>
-
-            <Forms.FormText className={Margins.top8}>
-                No, you cannot use server-side features like checking the "Send to Client" box.
-            </Forms.FormText>
-        </ErrorCard>
-    ), { noop: true })
+    noop() { }
 });
