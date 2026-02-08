253d5ddec0ac9effa1d31222132f5cacb7e126ee
Omniologist <sjtciu@gmail.com>
2026-02-08 17:02:40 +1030

    fix: add Omniologist to Devs object

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    omniologist: {
+        name: "omniologist",
+        id: 0n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
