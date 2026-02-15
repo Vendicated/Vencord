7622dbb8c3d4311124d397051eb8da730789451e
leandrocoding <leandro@zazzi.dev>
2026-02-15 23:33:31 +0100

    add preferCurrentServerEmoji setting to FakeNitro plugin and sort emojis accordingly

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    laz: {
+        name: "laz",
+        id: 137670410677256192n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
