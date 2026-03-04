00d797b77c4d43847858cbdf202e8c8e4e06e6d3
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-03-04 10:02:19 -0500

    AlwaysAnimate: Update Nameplate Patch

--- src/plugins/alwaysAnimate/index.ts
+++ src/plugins/alwaysAnimate/index.ts
@@ -92,7 +92,7 @@ export default definePlugin({
             find: ".MINI_PREVIEW,[",
             predicate: () => settings.store.nameplates,
             replacement: {
-                match: /animate:\i,loop:(?:!0===\i(?:&&)?){2}/,
+                match: /animate:\i,loop:!0===\i&&!0===\i/,
                 replace: "animate:true,loop:true"
             },
         },
