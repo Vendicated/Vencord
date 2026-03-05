6ef55c1df0cfe5845486cbabe3a03a0474d99107
thororen1234 <contact@swezy.dev>
2026-03-04 21:06:54 -0500

    fix BetterFolders, SpotifyControls, GameActivityToggle

--- src/plugins/betterFolders/index.tsx
+++ src/plugins/betterFolders/index.tsx
@@ -149,7 +149,7 @@ export default definePlugin({
                 // Create the isBetterFolders and betterFoldersExpandedIds variables in the GuildsBar component
                 // Needed because we access this from a non-arrow closure so we can't use arguments[0]
                 {
-                    match: /let{disableAppDownload:\i=\i\.isPlatformEmbedded,isOverlay:.+?(?=}=\i,)/,
+                    match: /let{disableAppDownload:\i=\i\.isPlatformEmbedded,isOverlay:.+?(?=}=\i)/,
                     replace: "$&,isBetterFolders,betterFoldersExpandedIds"
                 },
                 // Export the isBetterFolders and betterFoldersExpandedIds variable to the Guild List component
--- src/plugins/gameActivityToggle/index.tsx
+++ src/plugins/gameActivityToggle/index.tsx
@@ -112,7 +112,7 @@ export default definePlugin({
 
     patches: [
         {
-            find: ".WIDGETS_RTC_UPSELL_COACHMARK),",
+            find: ".DISPLAY_NAME_STYLES_COACHMARK),",
             replacement: {
                 match: /children:\[(?=.{0,25}?accountContainerRef)/,
                 replace: "children:[$self.GameActivityToggleButton(arguments[0]),"
