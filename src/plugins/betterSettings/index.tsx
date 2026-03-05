876e8b9d197f232d3b098cc86c39c84ad4f0743b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-03-05 15:40:33 -0500

    BetterSettings: Actually use organizemenu setting

--- src/plugins/betterSettings/index.tsx
+++ src/plugins/betterSettings/index.tsx
@@ -168,6 +168,7 @@ export default definePlugin({
         },
         { // Settings cog context menu
             find: "#{intl::USER_SETTINGS_ACTIONS_MENU_LABEL}",
+            predicate: () => settings.store.organizeMenu,
             replacement: [
                 {
                     match: /(\i)=(.{0,20}openUserSettings\)\(\i\)\));/,
