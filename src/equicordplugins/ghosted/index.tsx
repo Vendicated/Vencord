7dd37b53bc37d3ac0a5e2fad670934846162cc94
creations <creations@creations.works>
2026-03-07 00:39:25 -0500

    goodbye

--- src/equicordplugins/ghosted/index.tsx
+++ src/equicordplugins/ghosted/index.tsx
@@ -7,179 +7,197 @@
 import "./styles.css";
 
 import { findGroupChildrenByChildId } from "@api/ContextMenu";
-import { addServerListElement, removeServerListElement, ServerListRenderPosition } from "@api/ServerList";
+import {
+	addServerListElement,
+	removeServerListElement,
+	ServerListRenderPosition,
+} from "@api/ServerList";
 import { definePluginSettings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs, EquicordDevs } from "@utils/constants";
 import { classNameFactory } from "@utils/css";
 import { closeModal, openModal } from "@utils/modal";
 import definePlugin, { OptionType } from "@utils/types";
-import { Channel } from "@vencord/discord-types";
+import type { Channel } from "@vencord/discord-types";
 import { Menu, Tooltip, useEffect, useState } from "@webpack/common";
 
-import { Boo, clearChannelFromGhost, getBooCount, getGhostedChannels, onBooCountChange } from "./Boo";
-import { getChannelDisplayName, GhostedUsersModal } from "./GhostedUsersModal";
+import {
+	Boo,
+	clearChannelFromGhost,
+	getBooCount,
+	getGhostedChannels,
+	onBooCountChange,
+} from "./Boo";
+import { getChannelDisplayName,GhostedUsersModal } from "./GhostedUsersModal";
 import { IconGhost } from "./IconGhost";
 
 export const cl = classNameFactory("vc-boo-");
 
 export const settings = definePluginSettings({
-    showIndicator: {
-        type: OptionType.BOOLEAN,
-        description: "Show the ghost counter at the top of the server list",
-        default: true,
-        restartNeeded: false
-    },
-    showDmIcons: {
-        type: OptionType.BOOLEAN,
-        description: "Show ghost icons next to individual DMs",
-        default: true,
-        restartNeeded: false
-    },
-    ignoreGroupDms: {
-        type: OptionType.BOOLEAN,
-        description: "Exclude all group dms from ghosting",
-        default: false
-    },
-    exemptedChannels: {
-        type: OptionType.STRING,
-        description: "Comma-separated list of channel IDs to exempt from ghosting (right-click a DM channel to copy its ID)",
-        default: "",
-        restartNeeded: false
-    },
-    ignoreBots: {
-        type: OptionType.BOOLEAN,
-        description: "Ignore DMs from bots",
-        default: true,
-        restartNeeded: false
-    }
+	showIndicator: {
+		type: OptionType.BOOLEAN,
+		description: "Show the ghost counter at the top of the server list",
+		default: true,
+		restartNeeded: false,
+	},
+	showDmIcons: {
+		type: OptionType.BOOLEAN,
+		description: "Show ghost icons next to individual DMs",
+		default: true,
+		restartNeeded: false,
+	},
+	ignoreGroupDms: {
+		type: OptionType.BOOLEAN,
+		description: "Exclude all group dms from ghosting",
+		default: false,
+	},
+	exemptedChannels: {
+		type: OptionType.STRING,
+		description:
+			"Comma-separated list of channel IDs to exempt from ghosting (right-click a DM channel to copy its ID)",
+		default: "",
+		restartNeeded: false,
+	},
+	ignoreBots: {
+		type: OptionType.BOOLEAN,
+		description: "Ignore DMs from bots",
+		default: true,
+		restartNeeded: false,
+	},
 });
 
 function BooIndicator() {
-    const [count, setCount] = useState(getBooCount());
-    const [showJumpscare, setShowJumpscare] = useState(false);
+	const [count, setCount] = useState(getBooCount());
 
-    useEffect(() => {
-        const unsubscribe = onBooCountChange(newCount => {
-            setCount(newCount);
-        });
+	useEffect(() => {
+		const unsubscribe = onBooCountChange(newCount => {
+			setCount(newCount);
+		});
 
-        return () => {
-            unsubscribe();
-        };
-    }, []);
+		return () => {
+			unsubscribe();
+		};
+	}, []);
 
-    if (!settings.store.showIndicator && !showJumpscare) return null;
+	if (!settings.store.showIndicator) return null;
 
-    const handleClick = () => {
-        const ghostedChannels = getGhostedChannels();
-        const modalKey = openModal(modalProps => (
-            <ErrorBoundary>
-                <GhostedUsersModal
-                    modalProps={modalProps}
-                    ghostedChannels={ghostedChannels}
-                    onClose={() => closeModal(modalKey)}
-                    onClearGhost={clearChannelFromGhost}
-                />
-            </ErrorBoundary>
-        ));
-    };
+	const handleClick = () => {
+		const ghostedChannels = getGhostedChannels();
+		const modalKey = openModal(modalProps => (
+			<ErrorBoundary>
+				<GhostedUsersModal
+					modalProps={modalProps}
+					ghostedChannels={ghostedChannels}
+					onClose={() => closeModal(modalKey)}
+					onClearGhost={clearChannelFromGhost}
+				/>
+			</ErrorBoundary>
+		));
+	};
 
-    const getTooltipText = () => {
-        const ghostedChannels = getGhostedChannels();
-        if (ghostedChannels.length === 0) {
-            return "No Ghosted Users";
-        }
-        if (ghostedChannels.length <= 5) {
-            return ghostedChannels
-                .map(id => getChannelDisplayName(id))
-                .join(", ");
-        }
-        return `${ghostedChannels.length} Ghosted Users`;
-    };
+	const getTooltipText = () => {
+		const ghostedChannels = getGhostedChannels();
+		if (ghostedChannels.length === 0) {
+			return "No Ghosted Users";
+		}
+		if (ghostedChannels.length <= 5) {
+			return ghostedChannels.map(id => getChannelDisplayName(id)).join(", ");
+		}
+		return `${ghostedChannels.length} Ghosted Users`;
+	};
 
-    return (
-        <>
-            {settings.store.showIndicator && getGhostedChannels().length > 0 && (
-                <div id={cl("container")}>
-                    <Tooltip text={getTooltipText()} position="right">
-                        {({ onMouseEnter, onMouseLeave }) => (
-                            <div
-                                id={cl("container")}
-                                className={cl("clickable")}
-                                onMouseEnter={onMouseEnter}
-                                onMouseLeave={onMouseLeave}
-                                onClick={handleClick}
-                            >
-                                {count} <IconGhost fill="currentColor" />
-                            </div>
-                        )}
-                    </Tooltip>
-                </div>
-            )}
-        </>
-    );
+	return (
+		<>
+			{settings.store.showIndicator && getGhostedChannels().length > 0 && (
+				<div id={cl("container")}>
+					<Tooltip text={getTooltipText()} position="right">
+						{({ onMouseEnter, onMouseLeave }) => (
+							<div
+								id={cl("container")}
+								className={cl("clickable")}
+								onMouseEnter={onMouseEnter}
+								onMouseLeave={onMouseLeave}
+								onClick={handleClick}
+							>
+								{count} <IconGhost fill="currentColor" />
+							</div>
+						)}
+					</Tooltip>
+				</div>
+			)}
+		</>
+	);
 }
 
 function makeContextItem(props) {
-    return <Menu.MenuItem
-        id="ec-ghosted-clear"
-        key="ec-ghosted-clear"
-        label="unghost"
-        action={() => {
-            clearChannelFromGhost(props.channel.id);
-        }}
-    />;
+	return (
+		<Menu.MenuItem
+			id="ec-ghosted-clear"
+			key="ec-ghosted-clear"
+			label="unghost"
+			action={() => {
+				clearChannelFromGhost(props.channel.id);
+			}}
+		/>
+	);
 }
 
 export default definePlugin({
-    name: "Ghosted",
-    description: "A cute ghost will appear if you don't answer their DMs",
-    authors: [EquicordDevs.vei, Devs.sadan, EquicordDevs.justjxke, EquicordDevs.iamme],
-    settings,
-    dependencies: ["AudioPlayerAPI", "ServerListAPI"],
-    contextMenus: {
-        "gdm-context": (menuItems, props) => {
-            const group = findGroupChildrenByChildId("leave", menuItems, true);
-            group?.unshift(makeContextItem(props));
-        },
-        "user-context": (menuItems, props) => {
-            const group = findGroupChildrenByChildId("close-dm", menuItems);
-            group?.push(makeContextItem(props));
-        }
-    },
+	name: "Ghosted",
+	description: "A cute ghost will appear if you don't answer their DMs",
+	authors: [
+		EquicordDevs.vei,
+		Devs.sadan,
+		EquicordDevs.justjxke,
+		EquicordDevs.iamme,
+	],
+	settings,
+	dependencies: ["AudioPlayerAPI", "ServerListAPI"],
+	contextMenus: {
+		"gdm-context": (menuItems, props) => {
+			const group = findGroupChildrenByChildId("leave", menuItems, true);
+			group?.unshift(makeContextItem(props));
+		},
+		"user-context": (menuItems, props) => {
+			const group = findGroupChildrenByChildId("close-dm", menuItems);
+			group?.push(makeContextItem(props));
+		},
+	},
 
-    patches: [
-        {
-            find: "PrivateChannel.renderAvatar",
-            replacement: {
-                match: /\]:\i\|\|\i.{0,50}children:\[/,
-                replace: "$&$self.renderBoo(arguments[0]),"
-            }
-        },
-    ],
+	patches: [
+		{
+			find: "PrivateChannel.renderAvatar",
+			replacement: {
+				match: /\]:i\|\|i.{0,50}children:\[/,
+				replace: "$&$self.renderBoo(arguments[0]),",
+			},
+		},
+	],
 
-    renderBoo(props: { channel: Channel; }) {
-        return (
-            <ErrorBoundary noop>
-                <Boo {...props} />
-            </ErrorBoundary>
-        );
-    },
+	renderBoo(props: { channel: Channel }) {
+		return (
+			<ErrorBoundary noop>
+				<Boo {...props} />
+			</ErrorBoundary>
+		);
+	},
 
-    renderIndicator() {
-        return (
-            <ErrorBoundary noop>
-                <BooIndicator />
-            </ErrorBoundary>
-        );
-    },
+	renderIndicator() {
+		return (
+			<ErrorBoundary noop>
+				<BooIndicator />
+			</ErrorBoundary>
+		);
+	},
 
-    start() {
-        addServerListElement(ServerListRenderPosition.Above, this.renderIndicator);
-    },
+	start() {
+		addServerListElement(ServerListRenderPosition.Above, this.renderIndicator);
+	},
 
-    stop() {
-        removeServerListElement(ServerListRenderPosition.Above, this.renderIndicator);
-    },
+	stop() {
+		removeServerListElement(
+			ServerListRenderPosition.Above,
+			this.renderIndicator,
+		);
+	},
 });
