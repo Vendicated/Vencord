971ed355459b9981d8db99d777c610a4df282344
benzokones <>
2026-03-09 14:30:08 +0000

    Added benzokones to Devs constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    benzokones: {
+        name: "benzokones",
+        id: 1319511318109093949n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
