48549d3790eac3a8061f16c21f0ecd9f03d1c5c5
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-03-09 23:17:48 -0400

    WhosWatching: Update Patches & Formatting

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -21,16 +21,18 @@ import type { CSSProperties, HTMLAttributes } from "react";
 export interface FlexProps extends HTMLAttributes<HTMLDivElement> {
     flexDirection?: CSSProperties["flexDirection"];
     gap?: CSSProperties["gap"];
+    alignContent?: CSSProperties["alignContent"];
     justifyContent?: CSSProperties["justifyContent"];
     alignItems?: CSSProperties["alignItems"];
     flexWrap?: CSSProperties["flexWrap"];
 }
 
-export function Flex({ flexDirection, gap = "1em", justifyContent, alignItems, flexWrap, children, style, ...restProps }: FlexProps) {
+export function Flex({ flexDirection, gap = "1em", alignContent, justifyContent, alignItems, flexWrap, children, style, ...restProps }: FlexProps) {
     style = {
         display: "flex",
         flexDirection,
         gap,
+        alignContent,
         justifyContent,
         alignItems,
         flexWrap,
