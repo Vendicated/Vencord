81224166c76a3d94b2e95382d75c31f1781ea4b9
YashRaj <91825864+YashRajCodes@users.noreply.github.com>
2026-03-12 21:41:34 +0530

    feat: add HideMessages plugin

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    yash: {
+        name: "yash",
+        id: 889150838658977874n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
