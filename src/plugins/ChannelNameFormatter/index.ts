bce31d87b0cd5dc4f7263ca64c86482593b992de
Gh0sTyNZ <51747691+Gh0sTyNZ@users.noreply.github.com>
2026-04-14 01:06:54 +1200

    Update index.ts

--- src/plugins/ChannelNameFormatter/index.ts
+++ src/plugins/ChannelNameFormatter/index.ts
@@ -2,32 +2,27 @@ import definePlugin from "@utils/types";
 import { ChannelStore } from "@webpack/common";
 import { Devs } from "@utils/constants";
 
-/**
- * Formats Discord channel names:
- * ingame-chat → Ingame Chat
- * Version 2.0.0 
- */
+let originalGetChannel: typeof ChannelStore.getChannel | null = null;
 
 function format(name: string) {
     if (!name) return name;
 
     return name
-        .replace(/-/g, " ")
-        .replace(/_/g, " ")
-        .replace(/\band\b/gi, "&")
+        .replace(/[-_]/g, " ")
         .replace(/\b\w/g, c => c.toUpperCase());
 }
 
 export default definePlugin({
-    name: "Clean Channel Names",
-    description: "Formats Channel names into readable text (removes dashes/underscores + adds uppercase lettering).",
+    name: "ChannelNameFormatter",
+    description: "Formats channel names into readable text by removing dashes and underscores and applying capitalization.",
     authors: [Devs.Gh0sTyNZ],
 
     start() {
-        const orig = ChannelStore.getChannel;
+        if (originalGetChannel) return;
+        originalGetChannel = ChannelStore.getChannel;
 
         ChannelStore.getChannel = function (channelId: string) {
-            const channel = orig.call(this, channelId);
+            const channel = originalGetChannel!.call(this, channelId);
 
             if (channel?.name) {
                 channel.name = format(channel.name);
@@ -37,5 +32,10 @@ export default definePlugin({
         };
     },
 
-    stop() {}
+    stop() {
+        if (originalGetChannel) {
+            ChannelStore.getChannel = originalGetChannel;
+            originalGetChannel = null;
+        }
+    }
 });
