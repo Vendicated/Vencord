26aabd3e953238a092ecb41aba774f8e229a8664
kikkudayo <gabisaac2000@gmail.com>
2026-04-19 00:08:07 +0800

    added author to devs object

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    kikkudayo: {
+        name: "kikkudayo",
+        id: 743729410124808284n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
