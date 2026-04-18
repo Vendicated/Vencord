f4ba01fcf92691d8f76311a8def131b47d513ae9
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-04-18 15:39:04 -0400

    feat(CharacterCounter): Add Selected Text Option

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    creations: {
+        name: "creations",
+        id: 209830981060788225n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
