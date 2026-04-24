1e7fbf54552e7ea2aa36a39ea33f78d622171cd3
sadan <117494111+sadan4@users.noreply.github.com>
2026-04-23 20:44:54 -0400

    FriendsSince: Fix Patch

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -27,7 +27,7 @@ export default definePlugin({
     patches: [
         // DM User Sidebar
         {
-            find: ".SIDEBAR}),nicknameIcons",
+            find: /\?.popularApplicationCommandIds!=null&&.+?#{intl::USER_PROFILE_MEMBER_SINCE}\),headingColor/,
             replacement: {
                 match: /#{intl::USER_PROFILE_MEMBER_SINCE}\),.{0,100}userId:(\i\.id)}\)}\)/,
                 replace: "$&,$self.FriendsSinceComponent({userId:$1,isSidebar:true})"
