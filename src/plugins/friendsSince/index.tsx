01e0c03afa036764af7a29d0bc32efcf414caaa0
sadan <117494111+sadan4@users.noreply.github.com>
2026-04-23 22:52:09 -0400

    avoid regex find

--- src/plugins/friendsSince/index.tsx
+++ src/plugins/friendsSince/index.tsx
@@ -27,7 +27,7 @@ export default definePlugin({
     patches: [
         // DM User Sidebar
         {
-            find: /\?.popularApplicationCommandIds!=null&&.+?#{intl::USER_PROFILE_MEMBER_SINCE}\),headingColor/,
+            find: "#{intl::PREMIUM_GIFTING_BUTTON}),action:",
             replacement: {
                 match: /#{intl::USER_PROFILE_MEMBER_SINCE}\),.{0,100}userId:(\i\.id)}\)}\)/,
                 replace: "$&,$self.FriendsSinceComponent({userId:$1,isSidebar:true})"
