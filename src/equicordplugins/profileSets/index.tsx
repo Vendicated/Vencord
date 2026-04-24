b5d0d8136371f8e074f8dceac09985c366b4e919
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-04-24 13:11:40 -0400

    optimize profilesets

--- src/equicordplugins/profileSets/index.tsx
+++ src/equicordplugins/profileSets/index.tsx
@@ -36,7 +36,7 @@ export default definePlugin({
         {
             find: "DefaultCustomizationSections: user cannot be undefined",
             replacement: {
-                match: /return.{0,50}children:\[(?<=\.getLegacyUsername\(\).*?)/,
+                match: /return.{0,50}children:\[(?=.{0,50},\{placeholder:)/,
                 replace: "$&$self.renderPresetSection(\"main\"),"
             }
         },
