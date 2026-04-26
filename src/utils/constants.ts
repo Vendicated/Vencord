4da584066ff0eb5c082a87a71bc3d380fe88f620
user undefined <user1.undefined@protonmail.com>
2026-04-26 01:34:58 +0100

    added myself to Devs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,11 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    uu: {
+        name: "user undefined",
+        id: 777777777777777777777n,
+        badge: false,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
