087de5fbc1d7e4726a61190b34a8307cf1a1538d
kiwi <kiwi@froot.com>
2026-04-28 06:47:58 -0700

    Merge

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -47,6 +47,10 @@ export interface Dev {
  * If you are fine with attribution but don't want the badge, add badge: false
  */
 export const Devs = /* #__PURE__*/ Object.freeze({
+    Kiwi: {
+        name: "Kiwi",
+        id: 802123497831661578n
+    },
     Ven: {
         name: "V",
         id: 343383572805058560n
