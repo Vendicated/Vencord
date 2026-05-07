300610e2f6ade8a76fe6da89f89e8bb5c3912ff5
toju! <amajuatigann13@gmail.com>
2026-05-07 02:14:35 -0230

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -638,6 +638,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    tojuszn: {
+        name: "tojuszn",
+        id: 735250023713079379n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
