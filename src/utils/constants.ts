bd76064aa565ff1ab023e5955aa51303ddaaf3e8
paige <paigely@tuta.io>
2026-05-09 09:35:14 +0200

    feat: hide blocked/ignored WhoReacted users

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -638,6 +638,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    paige: {
+        name: "paige",
+        id: 1375697625864601650n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
