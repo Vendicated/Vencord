9b07cf2653f9b7b55a5f55cdba392e912d47926a
silencestack <zvanny1337@gmail.com>
2026-05-17 14:54:33 +0200

    ShowHiddenChannels: Fix uncategorized channels not showing category and name (#4215)

--- src/plugins/showHiddenChannels/index.tsx
+++ src/plugins/showHiddenChannels/index.tsx
@@ -65,7 +65,7 @@ export const settings = definePluginSettings({
 });
 
 function isUncategorized(objChannel: { channel: Channel; comparator: number; }) {
-    return objChannel.channel.id === "null" && objChannel.channel.name === "Uncategorized" && objChannel.comparator === -1;
+    return (objChannel.channel.id === "null" || objChannel.channel.id === null) && objChannel.channel.name === "Uncategorized" && objChannel.comparator === -1;
 }
 
 export default definePlugin({
