596bfff94e3c8737f43590cf3c2379e9e607c810
thororen <78185467+thororen1234@users.noreply.github.com>
2026-05-22 14:48:40 -0400

    Fix FakeProfileThemes & FriendsSince (#4235)

--- src/plugins/fakeProfileThemes/index.tsx
+++ src/plugins/fakeProfileThemes/index.tsx
@@ -207,7 +207,7 @@ export default definePlugin({
             },
         },
         {
-            find: "#{intl::USER_SETTINGS_RESET_PROFILE_THEME}",
+            find: "#{intl::USER_SETTINGS_RESET_PROFILE_THEME}),onClick:",
             replacement: {
                 match: /#{intl::USER_SETTINGS_RESET_PROFILE_THEME}\).+?}\)(?=\])(?<=color:(\i),.{0,500}?color:(\i),.{0,500}?)/,
                 replace: "$&,$self.addCopy3y3Button({primary:$1,accent:$2})"
