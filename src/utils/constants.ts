bbb2151c3a1b4773111cb246aa61d0b2c98edfe8
littlecommandcat <cyingda742@gmail.com>
2026-07-05 14:52:17 +0800

    Add messageGuard plugin.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -646,6 +646,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "NightmareSan",
         id: 304239816466235392n
     },
+    commandcat: {
+        name: "command_cat",
+        id: 1058383460043083787n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
