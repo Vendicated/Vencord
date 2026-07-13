b83e29ad8992039803560ad2d2bd4fd8e56f2fe1
Dave Bosley <dave.l.bosley@gmail.com>
2026-07-12 20:40:37 -0600

    WebScreenShareFixes: Fix stream preview leaking hidden video elements

--- src/plugins/webScreenShareFixes.web/index.ts
+++ src/plugins/webScreenShareFixes.web/index.ts
@@ -10,7 +10,7 @@ import definePlugin from "@utils/types";
 export default definePlugin({
     name: "WebScreenShareFixes",
     authors: [Devs.Kaitlyn],
-    description: "Removes 2500kbps bitrate cap on chromium and vesktop clients.",
+    description: "Removes 2500kbps bitrate cap on chromium and vesktop clients and fixes CPU usage growing endlessly while screensharing.",
     tags: ["Voice"],
     enabledByDefault: true,
 
@@ -27,6 +27,13 @@ export default definePlugin({
                     replace: '$&${$1?";stereo=1;sprop-stereo=1":""}'
                 },
             ]
+        },
+        {
+            find: "ApplicationStreamPreviewUploadManager",
+            replacement: {
+                match: /(\i)\.removeAttribute\("srcObject"\)/,
+                replace: "$1.pause(),$1.srcObject=null"
+            }
         }
     ]
 });
