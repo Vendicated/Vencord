bf42bf0d15cae18332538e298aa411da4b94166a
Vendicated <vendicated@riseup.net>
2026-07-15 16:55:20 +0200

    add back fallback

--- src/plugins/musicRichPresence/listenbrainz.ts
+++ src/plugins/musicRichPresence/listenbrainz.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { VENCORD_USER_AGENT } from "@shared/vencordUserAgent";
 import { Logger } from "@utils/Logger";
 
 import { ScrobblerBackend, TrackData } from ".";
@@ -16,6 +17,54 @@ async function fetchCoverArt(releaseGroupMBID: string) {
     const res = await fetch(`https://coverartarchive.org/release-group/${releaseGroupMBID}`);
     if (!res.ok) return null;
     return res.json().then(json => json.images[0].thumbnails.large);
+}
+
+async function getUrls(additionalInfo: Record<string, string> | undefined, trackName: string, artistName: string): Promise<Partial<TrackData>> {
+    // Well tagged music will have MBIDs which we can use directly. These are optional but highly recommended in ListenBrainz scrobbles.
+    // If your music doesn't have these, it's highly recommended to use https://picard.musicbrainz.org/ to automatically add them
+    if (additionalInfo?.recording_mbid) {
+        const { release_group_mbid, release_mbid, recording_mbid, artist_mbids } = additionalInfo;
+
+        return {
+            imageURL: release_group_mbid ? await fetchCoverArt(release_group_mbid) : undefined,
+            trackURL: recording_mbid ? url(`/track/${recording_mbid}`) : undefined,
+            albumURL: release_group_mbid
+                ? url(`/release-group/${release_group_mbid}`)
+                : release_mbid
+                    ? url(`/release/${release_mbid}`)
+                    : undefined,
+            artistURL: artist_mbids?.length ? url(`/artist/${artist_mbids[0]}`) : undefined,
+        };
+    }
+
+    // If no MBIDs are present, try to search for the track on MusicBrainz
+
+    // this needs to be encoded separately—URLSearchParams encodes spaces as "+"
+    const query = encodeURIComponent(`artist:"${artistName}" AND recording:"${trackName}"`);
+    const params = new URLSearchParams({
+        fmt: "json",
+        limit: "1"
+    });
+
+    const metadata = await fetch("https://musicbrainz.org/ws/2/recording/?" + params + "&query=" + query, {
+        headers: { "User-Agent": VENCORD_USER_AGENT }
+    })
+        .then(res => res.ok ? res.json() : Promise.reject(new Error(`${res.status} ${res.statusText}`)))
+        .then(json => json.recordings?.[0]);
+
+    if (!metadata) {
+        return {};
+    }
+
+    const artist = metadata["artist-credit"]?.[0]?.artist;
+    const release = metadata.releases?.[0];
+
+    return {
+        imageURL: release?.["release-group"] ? await fetchCoverArt(release["release-group"].id) : undefined,
+        trackURL: url(`/track/${metadata.id}/`),
+        albumURL: release?.id ? url(`/release/${release.id}/`) : release?.["release-group"]?.id ? url(`/release-group/${release["release-group"].id}/`) : undefined,
+        artistURL: artist?.id ? url(`/artist/${artist.id}/`) : undefined,
+    };
 }
 
 export const ListenBrainzScrobbler: ScrobblerBackend = {
@@ -37,23 +86,9 @@ export const ListenBrainzScrobbler: ScrobblerBackend = {
                 name: track_name || "Unknown",
                 artist: artist_name,
                 album: release_name || "Unknown",
+                serviceName: additional_info?.music_service_name || additional_info?.submission_client,
+                ...await getUrls(additional_info, track_name, artist_name)
             } as TrackData;
-
-            if (additional_info) {
-                const { music_service_name, submission_client, release_group_mbid, release_mbid, recording_mbid, artist_mbids } = additional_info;
-
-                Object.assign(trackData, {
-                    imageURL: release_group_mbid ? await fetchCoverArt(release_group_mbid) : undefined,
-                    serviceName: music_service_name || submission_client,
-                    albumURL: release_group_mbid
-                        ? url(`/release-group/${release_group_mbid}`)
-                        : release_mbid
-                            ? url(`/release/${release_mbid}`)
-                            : undefined,
-                    trackURL: recording_mbid ? url(`/track/${recording_mbid}`) : undefined,
-                    artistURL: artist_mbids?.length ? url(`/artist/${artist_mbids[0]}`) : undefined,
-                });
-            }
 
             return trackData;
         } catch (e) {
