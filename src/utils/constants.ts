637d9f7e475ed6d45d5abd599d17c5668df56fca
jax <taznetlify10@outlook.com>
2026-07-18 16:17:18 +0000

    Fix PinDMs virtualization breaking with large DM lists

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -649,6 +649,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Lunascape: {
         name: "Lunascape",
         id: 383365021415243776n
+    },
+    jax: {
+        name: "jax",
+        id: 1493703027801194598n
     }
 } satisfies Record<string, Dev>);
 
