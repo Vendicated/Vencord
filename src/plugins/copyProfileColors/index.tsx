fb08cb9f5d0eee5ad69ebb7ffeeca0119c5fa49a
AlexAgian <flishflashf@gmail.com>
2026-07-19 16:32:10 +0300

    Add CopyProfileColors plugin

---
+++ src/plugins/copyProfileColors/index.tsx
@@ -0,0 +1,50 @@
+
+import { NavContextMenuPatchCallback } from "@api/ContextMenu";
+import { Devs } from "@utils/constants";
+import { copyWithToast, fetchUserProfile } from "@utils/discord";
+import definePlugin from "@utils/types";
+import { User } from "@vencord/discord-types";
+import { Menu, showToast, Toasts } from "@webpack/common";
+
+function toHex(color: number) {
+    return `#${color.toString(16).padStart(6, "0")}`;
+}
+
+async function copyProfileColors(user: User) {
+    const profile = await fetchUserProfile(user.id);
+    const colors = profile?.themeColors;
+
+    if (!colors) {
+        showToast("This user doesn't have any profile theme colors", Toasts.Type.FAILURE);
+        return;
+    }
+
+    const [primary, accent] = colors;
+    copyWithToast(`Primary: ${toHex(primary)}, Accent: ${toHex(accent)}`, "Profile colors copied");
+}
+
+const userContextPatch: NavContextMenuPatchCallback = (children, { user }: { user?: User; }) => {
+    if (!user) return;
+
+    children.push(
+        <Menu.MenuItem
+            id="copy-profile-colors"
+            key="copy-profile-colors"
+            label="Copy Profile Colors"
+            action={() => copyProfileColors(user)}
+        />
+    );
+};
+
+export default definePlugin({
+    name: "CopyProfileColors",
+    description: "Adds a right click button to the user's profile to copy their profile colors ",
+    authors: [Devs.alexagian],
+    tags: ["Utility", "Appearance"],
+
+    contextMenus: {
+        "user-context": userContextPatch,
+        "user-profile-actions": userContextPatch,
+        "user-profile-overflow-menu": userContextPatch
+    }
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -649,6 +649,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
     Lunascape: {
         name: "Lunascape",
         id: 383365021415243776n
+    },
+    alexagian: {
+        name: "alexagian",
+        id: 1043876811664273549n
     }
 } satisfies Record<string, Dev>);
 
