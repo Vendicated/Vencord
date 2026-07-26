2dcf0d0b17fb14e06b05bfa6732c1234fc6a2516
Vendicated <vendicated@riseup.net>
2026-07-26 16:37:11 +0200

    fix

--- src/main/utils/extensions.ts
+++ src/main/utils/extensions.ts
@@ -50,7 +50,7 @@ async function extract(data: Buffer, outDir: string) {
                 }
 
                 const pathElements = f.split("/");
-                const directories = pathElements.join("/");
+                const directories = pathElements.slice(-1).join("/");
 
                 const dir = ensureSafePath(outDir, directories);
                 if (!dir) throw new Error(`Path traversal detected: "${f}"`);
