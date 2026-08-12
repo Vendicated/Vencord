57092b42ad3c9e372372ace1231511f4fda70b67
honeyxm8 <jasperlmansfield@gmail.com>
2026-08-13 09:28:05 +1000

    feat(MessageLogger): Swapped whitelist & blacklist out for filter that can toggle between the two

--- src/plugins/messageLogger/index.tsx
+++ src/plugins/messageLogger/index.tsx
@@ -53,6 +53,11 @@ interface MLAttachment extends MessageAttachment {
 
 const MessageClasses = findCssClassesLazy("edited", "communicationDisabled", "isSystemMessage");
 
+const enum FilterMode {
+    Whitelist,
+    Blacklist
+}
+
 const settings = definePluginSettings({
     deleteStyle: {
         type: OptionType.SELECT,
@@ -95,49 +100,36 @@ const settings = definePluginSettings({
         description: "Whether to ignore messages by yourself",
         default: false
     },
-    whitelistEnabled: {
-        type: OptionType.BOOLEAN,
-        description: "Whether to enforce the whitelist, logging only whitelisted channels or users",
-        default: false,
+    filterMode: {
+        type: OptionType.SELECT,
+        description: "Change the mode of the filter. If filters are empty it will do nothing",
+        options: [
+            {
+                label: "Whitelist",
+                value: FilterMode.Whitelist,
+                default: true
+            },
+            {
+                label: "Blacklist",
+                value: FilterMode.Blacklist,
+            }
+        ]
     },
-    whitelistedUsers: {
+    filteredUsers: {
         type: OptionType.STRING,
-        description: "Comma-separated list of user IDs to whitelist",
+        description: "Comma-separated list of user IDs to filter",
         default: "",
         multiline: true
     },
-    whitelistedChannels: {
+    filteredChannels: {
         type: OptionType.STRING,
-        description: "Comma-separated list of channel IDs to whitelist",
+        description: "Comma-separated list of channel IDs to filter",
         default: "",
         multiline: true
     },
-    whitelistedGuilds: {
+    filteredGuilds: {
         type: OptionType.STRING,
-        description: "Comma-separated list of guild IDs to whitelist",
-        default: "",
-        multiline: true
-    },
-    blacklistEnabled: {
-        type: OptionType.BOOLEAN,
-        description: "Whether to enforce the blacklist, ignoring blacklisted channels and users",
-        default: true,
-    },
-    ignoreUsers: {
-        type: OptionType.STRING,
-        description: "Comma-separated list of user IDs to blacklist",
-        default: "",
-        multiline: true
-    },
-    ignoreChannels: {
-        type: OptionType.STRING,
-        description: "Comma-separated list of channel IDs to blacklist",
-        default: "",
-        multiline: true
-    },
-    ignoreGuilds: {
-        type: OptionType.STRING,
-        description: "Comma-separated list of guild IDs to blacklist",
+        description: "Comma-separated list of guild IDs to filter",
         default: "",
         multiline: true
     }
@@ -358,28 +350,25 @@ export default definePlugin({
 
     shouldIgnore(message: any, isEdit = false) {
         try {
-            const { ignoreBots, ignoreSelf, blacklistEnabled, ignoreUsers, ignoreChannels, ignoreGuilds, whitelistEnabled, whitelistedUsers, whitelistedChannels, whitelistedGuilds, logEdits, logDeletes } = settings.store;
+            const { ignoreBots, ignoreSelf, filterMode, filteredChannels, filteredGuilds, filteredUsers, logEdits, logDeletes } = settings.store;
             const myId = UserStore.getCurrentUser().id;
+
+            const filterIsEmpty = (
+                filteredUsers.trim() == "" &&
+                filteredChannels.trim() == "" &&
+                filteredGuilds.trim() == ""
+            );
+            const messageInFilter = (
+                filteredUsers.includes(message.author?.id) ||
+                filteredChannels.includes(message.channel_id) ||
+                filteredChannels.includes(ChannelStore.getChannel(message.channel_id)?.guild_id) ||
+                filteredGuilds.includes(ChannelStore.getChannel(message.channel_id)?.guild_id)
+            );
 
             return ignoreBots && message.author?.bot ||
                 ignoreSelf && message.author?.id === myId ||
                 (isEdit ? !logEdits : !logDeletes) ||
-
-                // Is the message whitelisted?
-                (whitelistEnabled && !(
-                    whitelistedUsers.includes(message.author?.id) ||
-                    whitelistedChannels.includes(message.channel_id) ||
-                    whitelistedChannels.includes(ChannelStore.getChannel(message.channel_id)?.guild_id) ||
-                    whitelistedGuilds.includes(ChannelStore.getChannel(message.channel_id)?.guild_id)
-                )) ||
-
-                // Is the message blacklisted?
-                (blacklistEnabled && (
-                    ignoreUsers.includes(message.author?.id) ||
-                    ignoreChannels.includes(message.channel_id) ||
-                    ignoreChannels.includes(ChannelStore.getChannel(message.channel_id)?.parent_id) ||
-                    ignoreGuilds.includes(ChannelStore.getChannel(message.channel_id)?.guild_id)
-                )) ||
+                !filterIsEmpty && !((filterMode == FilterMode.Whitelist) == messageInFilter) ||
 
                 // Ignore Venbot in the support channels
                 (message.author?.id === VENBOT_USER_ID && ChannelStore.getChannel(message.channel_id)?.parent_id === SUPPORT_CATEGORY_ID);
