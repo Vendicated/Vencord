18412d7992a7f9be8eceed5f66657bcd4335f992
TiggerTakkyTTT <mir12345001@gmail.com>
2026-08-12 09:21:11 +0700

    Update src/plugins/noBlockedMessages/index.ts
    Co-authored-by: Nuckyz <61953774+Nuckyz@users.noreply.github.com>

--- src/plugins/noBlockedMessages/index.ts
+++ src/plugins/noBlockedMessages/index.ts
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
 
 import { definePluginSettings, migratePluginSetting } from "@api/Settings";
 import { Devs } from "@utils/constants";
