2d490abe2f95e51fc0c179476c7d70bfff47e73c
sadan <117494111+sadan4@users.noreply.github.com>
2026-08-27 18:42:10 -0400

    update find

--- src/plugins/fakeNitro/index.tsx
+++ src/plugins/fakeNitro/index.tsx
@@ -320,7 +320,7 @@ export default definePlugin({
         },
         // Allow users to use custom client themes
         {
-            find: ':"reset_button"===',
+            find: ".CLIENT_THEMES_EDITOR?",
             replacement: {
                 match: /(?<=\i=)\(0,\i\.\i\)\(\i\.\i\.TIER_2\)(?=,|;)/g,
                 replace: "true"
