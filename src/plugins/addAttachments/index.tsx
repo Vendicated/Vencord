0779e832d81adba832d2881c9362dbcb4c6c1471
Vendicated <vendicated@riseup.net>
2026-09-06 01:54:52 +0200

    Add button to edit bar & support paste

--- src/plugins/addAttachments/index.tsx
+++ src/plugins/addAttachments/index.tsx
@@ -4,17 +4,19 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { ChatBarButton } from "@api/ChatButtons";
 import { UploadIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
-import { getCurrentChannel } from "@utils/discord";
+import { pluralise } from "@utils/misc";
 import definePlugin from "@utils/types";
+import { Message } from "@vencord/discord-types";
 import { MessageFlags, MessageType } from "@vencord/discord-types/enums";
 import { findByPropsLazy } from "@webpack";
-import { ChannelStore, MessageStore, PermissionsBits, PermissionStore, RestAPI, showToast, Toasts, UserStore } from "@webpack/common";
+import { AuthenticationStore, ChannelStore, EditMessageStore, MessageStore, PermissionsBits, PermissionStore, RestAPI, SelectedChannelStore, showToast, Toasts } from "@webpack/common";
 
 const { uniqueId } = findByPropsLazy("uniqueId");
 
-function addAttachments(channelId: string, messageId: string, existingAttachmentCount: number) {
+function chooseAttachments(channelId: string, messageId: string, existingAttachmentCount: number) {
     const input = document.createElement("input");
     input.type = "file";
     input.accept = "*/*";
@@ -22,57 +24,94 @@ function addAttachments(channelId: string, messageId: string, existingAttachment
     input.click();
 
     input.addEventListener("change", async () => {
-        const { files } = input;
-        if (!files) return input.remove();
+        try {
+            if (!input.files) return;
+            await addAttachments(channelId, messageId, existingAttachmentCount, input.files);
+        } finally {
+            input.remove();
+        }
+    });
+}
 
-        if (files.length + existingAttachmentCount > 10) {
-            showToast(`You can only add ${10 - existingAttachmentCount} more attachments to this message.`, Toasts.Type.FAILURE);
-            return input.remove();
+async function addAttachments(channelId: string, messageId: string, existingAttachmentCount: number, files: FileList) {
+    if (!files) return;
+
+    if (files.length + existingAttachmentCount > 10) {
+        const remaining = 10 - existingAttachmentCount;
+        if (remaining <= 0) {
+            showToast("You cannot add more attachments to this message.", Toasts.Type.FAILURE);
+            return;
         }
 
-        showToast("Uploading, this can take a while...", Toasts.Type.CLOCK);
+        showToast(`You can only add ${pluralise(remaining, "more attachment")} to this message.`, Toasts.Type.FAILURE);
+        return;
+    }
 
-        const { body: { attachments } } = await RestAPI.post({
-            url: `/channels/${channelId}/attachments`,
-            body: {
-                files: Array.from(files, file => ({
-                    filename: file.name,
-                    file_size: file.size,
-                    id: uniqueId(),
-                    is_clip: false
-                }))
-            }
-        }) as { body: { attachments: { id: string, upload_url: string, upload_filename: string; }[]; }; };
+    showToast("Uploading, this can take a while...", Toasts.Type.CLOCK);
 
-        const newAttachments = await Promise.all(
-            attachments.map((file, i) =>
-                fetch(file.upload_url, {
-                    method: "PUT",
-                    body: files[i]
-                }).then(() => ({
-                    id: file.id,
-                    uploaded_filename: file.upload_filename,
-                    filename: files[i].name
-                }))
-            )
-        );
+    const { body: { attachments } } = await RestAPI.post({
+        url: `/channels/${channelId}/attachments`,
+        body: {
+            files: Array.from(files, file => ({
+                filename: file.name,
+                file_size: file.size,
+                id: uniqueId(),
+                is_clip: false
+            }))
+        }
+    }) as { body: { attachments: { id: string, upload_url: string, upload_filename: string; }[]; }; };
+
+    const newAttachments = await Promise.all(
+        attachments.map((file, i) =>
+            fetch(file.upload_url, {
+                method: "PUT",
+                body: files[i]
+            }).then(() => ({
+                id: file.id,
+                uploaded_filename: file.upload_filename,
+                filename: files[i].name
+            }))
+        )
+    );
 
 
-        const msg = MessageStore.getMessage(channelId, messageId);
+    const msg = MessageStore.getMessage(channelId, messageId);
 
-        await RestAPI.patch({
-            url: `/channels/${channelId}/messages/${messageId}`,
-            body: {
-                attachments: [
-                    ...msg.attachments,
-                    ...newAttachments
-                ]
-            }
-        });
-
-        showToast("Attachments added successfully!", Toasts.Type.SUCCESS);
-        input.remove();
+    await RestAPI.patch({
+        url: `/channels/${channelId}/messages/${messageId}`,
+        body: {
+            attachments: [
+                ...msg.attachments,
+                ...newAttachments
+            ]
+        }
     });
+
+    showToast(`${files.length === 1 ? "Attachment" : "Attachments"} added successfully!`, Toasts.Type.SUCCESS);
+}
+
+function canAddAttachments(msg: Message) {
+    return (
+        [MessageType.DEFAULT, MessageType.REPLY].includes(msg.type) &&
+        !msg.hasFlag(MessageFlags.IS_VOICE_MESSAGE) &&
+        AuthenticationStore.getId() === msg.author.id &&
+        msg.attachments.length < 10
+    );
+}
+
+function handlePaste(e: ClipboardEvent) {
+    if (!e.clipboardData) return;
+
+    const msg = EditMessageStore.getEditingMessage(SelectedChannelStore.getChannelId());
+    if (!msg) return;
+
+    const images = Array.from(e.clipboardData.files).filter(file => file.type.startsWith("image/"));
+    if (images.length === 0) return;
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    addAttachments(msg.channel_id, msg.id, msg.attachments.length, e.clipboardData.files);
 }
 
 export default definePlugin({
@@ -80,22 +119,47 @@ export default definePlugin({
     description: "Allows you to add attachments to a pre-existing message of yours",
     authors: [Devs.Lumap],
 
+    start() {
+        document.addEventListener("paste", handlePaste, { capture: true });
+    },
+    stop() {
+        document.removeEventListener("paste", handlePaste, { capture: true });
+    },
+
     messagePopoverButton: {
         icon: UploadIcon,
         render(msg) {
-            if (![MessageType.DEFAULT, MessageType.REPLY].includes(msg.type) || msg.hasFlag(MessageFlags.IS_VOICE_MESSAGE)) return null;
-            if (UserStore.getCurrentUser().id !== msg.author.id || msg.attachments.length === 10) return null;
+            if (!canAddAttachments(msg)) return null;
 
-            const currentChannel = getCurrentChannel()!;
-            if (!currentChannel.isPrivate() && !PermissionStore.can(PermissionsBits.SEND_MESSAGES, currentChannel)) return null;
+            const channel = ChannelStore.getChannel(msg.channel_id);
+            if (!channel.isPrivate() && !PermissionStore.can(PermissionsBits.SEND_MESSAGES, channel)) return null;
 
             return {
                 label: "Add Attachments",
                 icon: UploadIcon,
                 message: msg,
                 channel: ChannelStore.getChannel(msg.channel_id),
-                onClick: () => addAttachments(msg.channel_id, msg.id, msg.attachments.length)
+                onClick: () => chooseAttachments(msg.channel_id, msg.id, msg.attachments.length)
             };
         },
     },
+
+    chatBarButton: {
+        icon: UploadIcon,
+        render({ channel, type }) {
+            if (type.analyticsName !== "edit") return null;
+
+            const msg = EditMessageStore.getEditingMessage(channel.id);
+            if (!msg || !canAddAttachments(msg)) return null;
+
+            return (
+                <ChatBarButton
+                    tooltip="Add Attachments"
+                    onClick={() => chooseAttachments(msg.channel_id, msg.id, msg.attachments.length)}
+                >
+                    <UploadIcon height={18} width={18} />
+                </ChatBarButton>
+            );
+        }
+    }
 });
