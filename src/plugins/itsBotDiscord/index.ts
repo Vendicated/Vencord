9afe909c1cdc8cf3e0a350437a4f0c26430372aa
John <roblox888i12345@gmail.com>
2026-09-20 14:01:03 -0400

    Implement itsBotDiscord plugin to revert bot changes
    Added a plugin to revert Discord's bot update, modifying bot text display.

--- src/plugins/itsBotDiscord/index.ts
+++ src/plugins/itsBotDiscord/index.ts
@@ -1,3 +1,40 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2026 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "itsBotDiscord",
+    description: "Revert Discord's stupidt update of changing Bots to Apps",
+    tags: ["Utility"],
+    authors: [Devs.John],
+    patches: [
+        {
+            find: "hasFlag:{writable",
+            replacement: {
+                match: /if\((\i)<=(?:0x40000000|(?:1<<30|1073741824))\)return/,
+                replace: "if($1===(1<<20))return false;$&",
+            },
+        },
+    ],
+});
+
 const botTag = new MutationObserver(() => {
   document.querySelectorAll(".botText__82f07").forEach(el => {
     el.textContent = "BOT";
